364396ce45625e8a253e25ff6915a2eeecf31a9d
YiYing He <yiying@secondstate.io>
2019-07-11 18:15:04 +0800

    [Test] Add unit tests of segments.
    The following unit tests of segments added:
    1. ElementSegment
    2. CodeSegment
    3. DataSegment

---
+++ test/loader/segmentTest.cpp
@@ -0,0 +1,133 @@
+//===-- ssvm/test/loader/segmentTest.cpp - AST segment unit tests ---------===//
+//
+// Part of the SSVM Project.
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file contents unit tests of AST segment nodes, which are element
+/// segment, code segment, and data segment.
+///
+//===----------------------------------------------------------------------===//
+
+#include "loader/segment.h"
+#include "filemgrTest.h"
+#include "gtest/gtest.h"
+
+namespace {
+
+FileMgrTest Mgr;
+
+TEST(SegmentTest, LoadElementSegment) {
+  /// 1. Test load element segment.
+  ///
+  ///   1.  Load invalid empty element segment.
+  ///   2.  Load element segment with expression of only End operation and empty
+  ///       function indices list.
+  ///   3.  Load element segment with expression and function indices list.
+  Mgr.clearBuffer();
+  AST::ElementSegment Seg1;
+  EXPECT_FALSE(Seg1.loadBinary(Mgr));
+
+  Mgr.clearBuffer();
+  std::vector<unsigned char> Vec2 = {
+      0xFFU, 0xFFU, 0xFFU, 0xFFU, 0x0FU, /// Table index
+      0x0BU,                             /// Expression
+      0x00U                              /// Function indices list
+  };
+  Mgr.setVector(Vec2);
+  AST::ElementSegment Seg2;
+  EXPECT_TRUE(Seg2.loadBinary(Mgr));
+
+  Mgr.clearBuffer();
+  std::vector<unsigned char> Vec3 = {
+      0xFFU, 0xFFU, 0xFFU, 0xFFU, 0x0FU, /// Table index
+      0x45U, 0x46U, 0x47U, 0x0BU,        /// Expression
+      0x03U,                             /// Vector length = 3
+      0xFFU, 0xFFU, 0xFFU, 0xFFU, 0x0FU, /// vec[0]
+      0x00U,                             /// vec[1]
+      0xB9U, 0x60U                       /// vec[2]
+  };
+  Mgr.setVector(Vec3);
+  AST::ElementSegment Seg3;
+  EXPECT_TRUE(Seg3.loadBinary(Mgr));
+}
+
+TEST(SegmentTest, LoadCodeSegment) {
+  /// 2. Test load code segment.
+  ///
+  ///   1.  Load invalid empty code segment.
+  ///   2.  Load invalid code segment of zero content size.
+  ///   3.  Load code segment of empty locals and expression with only End
+  ///       operation.
+  ///   4.  Load code segment with expression and local lists.
+  Mgr.clearBuffer();
+  AST::CodeSegment Seg1;
+  EXPECT_FALSE(Seg1.loadBinary(Mgr));
+
+  Mgr.clearBuffer();
+  std::vector<unsigned char> Vec2 = {
+      0x80U, 0x80U, 0x80U, 0x80U, 0x00U /// Code segment size
+  };
+  Mgr.setVector(Vec2);
+  AST::CodeSegment Seg2;
+  EXPECT_FALSE(Seg2.loadBinary(Mgr));
+
+  Mgr.clearBuffer();
+  std::vector<unsigned char> Vec3 = {
+      0x82U, 0x80U, 0x80U, 0x80U, 0x00U, /// Code segment size
+      0x00U,                             /// Vector length = 0
+      0x0BU                              /// Expression
+  };
+  Mgr.setVector(Vec3);
+  AST::CodeSegment Seg3;
+  EXPECT_TRUE(Seg3.loadBinary(Mgr));
+
+  Mgr.clearBuffer();
+  std::vector<unsigned char> Vec4 = {
+      0x95U, 0x80U, 0x80U, 0x80U, 0x00U,        /// Code segment size
+      0x04U,                                    /// Vector length = 4
+      0x01U, 0x7CU,                             /// vec[0]
+      0x03U, 0x7DU,                             /// vec[1]
+      0xFFU, 0xFFU, 0xFFU, 0xFFU, 0x0FU, 0x7EU, /// vec[2]
+      0xF3U, 0xFFU, 0xFFU, 0xFFU, 0x0FU, 0x7FU, /// vec[3]
+      0x45U, 0x46U, 0x47U, 0x0BU                /// Expression
+  };
+  Mgr.setVector(Vec4);
+  AST::CodeSegment Seg4;
+  EXPECT_TRUE(Seg4.loadBinary(Mgr));
+}
+
+TEST(SegmentTest, LoadDataSegment) {
+  /// 3. Test load data segment.
+  ///
+  ///   1.  Load invalid empty data segment.
+  ///   2.  Load data segment of expression with only End operation and empty
+  ///       initialization data.
+  ///   3.  Load data segment with expression and initialization data.
+  Mgr.clearBuffer();
+  AST::DataSegment Seg1;
+  EXPECT_FALSE(Seg1.loadBinary(Mgr));
+
+  Mgr.clearBuffer();
+  std::vector<unsigned char> Vec2 = {
+      0xFFU, 0xFFU, 0xFFU, 0xFFU, 0x0FU, /// Memory index
+      0x0BU,                             /// Expression
+      0x00U                              /// Vector length = 0
+  };
+  Mgr.setVector(Vec2);
+  AST::DataSegment Seg2;
+  EXPECT_TRUE(Seg2.loadBinary(Mgr));
+
+  Mgr.clearBuffer();
+  std::vector<unsigned char> Vec3 = {
+      0xFFU, 0xFFU, 0xFFU, 0xFFU, 0x0FU, /// Memory index
+      0x45U, 0x46U, 0x47U, 0x0BU,        /// Expression
+      0x04U, 0x74U, 0x65U, 0x73U, 0x74U  /// Vector length = 4, "test"
+  };
+  Mgr.setVector(Vec3);
+  AST::DataSegment Seg3;
+  EXPECT_TRUE(Seg3.loadBinary(Mgr));
+}
+
+} // namespace

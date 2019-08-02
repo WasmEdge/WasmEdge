f9c89ae14d4de69f07f90c97d1d722b7d04b6c19
YiYing He <yiying@secondstate.io>
2019-08-02 18:49:21 +0800

    [Executor] Add the stack manager definition.

--- include/executor/stackmgr.h
+++ include/executor/stackmgr.h
@@ -15,10 +15,43 @@
 #include "labelentry.h"
 #include "valueentry.h"
 #include <memory>
-#include <queue>
 #include <vector>
 
 class StackMgr {
 public:
+  /// Variant of entry classes.
+  using EntryType =
+      std::variant<std::unique_ptr<FrameEntry>, std::unique_ptr<LabelEntry>,
+                   std::unique_ptr<ValueEntry>>;
+
+  /// Getters of top entry of stack.
+  Executor::ErrCode getTop(FrameEntry *&Frame);
+  Executor::ErrCode getTop(LabelEntry *&Label);
+  Executor::ErrCode getTop(ValueEntry *&Value);
+
+  /// Push a new entry to stack.
+  Executor::ErrCode push(EntryType NewEntry);
+
+  /// Pop and return the top entry.
+  Executor::ErrCode pop(std::unique_ptr<FrameEntry> &Frame);
+  Executor::ErrCode pop(std::unique_ptr<LabelEntry> &Label);
+  Executor::ErrCode pop(std::unique_ptr<ValueEntry> &Value);
+
+  /// Drop the top entry.
+  Executor::ErrCode drop();
+
+  /// Frame related operation.
+  Executor::ErrCode getCurrentFrame(FrameEntry *&Frame);
+
+  /// Label related operation.
+  Executor::ErrCode getLabelWithCount(LabelEntry *&Label);
+
+  /// Checking the top entry's attribute
+  bool isTopFrame();
+  bool isTopLabel();
+  bool isTopValue();
+
 private:
+  std::vector<EntryType> Stack;
+  std::vector<unsigned int> LabelIdx;
 };

25c3905877b471e9c0679be1e2a88febacdd292b
YiYing He <yiying@secondstate.io>
2019-08-14 17:28:05 +0800

    [Executor] Add namespace of entries.

--- lib/executor/stackmgr.cpp
+++ lib/executor/stackmgr.cpp
@@ -5,43 +5,41 @@ namespace SSVM {
 namespace Executor {
 
 /// Getter of top entry. See "include/executor/stackmgr.h".
-template <typename T> Executor::ErrCode StackManager::getTop(T *&Entry) {
+template <typename T> ErrCode StackManager::getTop(T *&Entry) {
   /// Check the size of stack.
   if (Stack.size() == 0)
-    return Executor::ErrCode::StackEmpty;
+    return ErrCode::StackEmpty;
   /// Check is the top entry type matched and get pointer.
   try {
     Entry = std::get<T>(Stack.back()).get();
   } catch (std::bad_variant_access E) {
-    return Executor::ErrCode::StackWrongEntry;
+    return ErrCode::StackWrongEntry;
   }
-  return Executor::ErrCode::Success;
+  return ErrCode::Success;
 }
 
 /// Push a new frame entry to stack. See "include/executor/stackmgr.h".
-template <typename T>
-Executor::ErrCode StackManager::push(std::unique_ptr<T> &NewEntry) {
+template <typename T> ErrCode StackManager::push(std::unique_ptr<T> &NewEntry) {
   Stack.push_back(std::move(NewEntry));
   /// If is frame or label, record the index in stack.
   if (std::is_same<T, FrameEntry>::value)
     FrameIdx.push_back(Stack.size() - 1);
   if (std::is_same<T, LabelEntry>::value)
     LabelIdx.push_back(Stack.size() - 1);
-  return Executor::ErrCode::Success;
+  return ErrCode::Success;
 }
 
 /// Pop and return the frame entry. See "include/executor/stackmgr.h".
-template <typename T>
-Executor::ErrCode StackManager::pop(std::unique_ptr<T> &Entry) {
+template <typename T> ErrCode StackManager::pop(std::unique_ptr<T> &Entry) {
   /// Check the size of stack.
   if (Stack.size() == 0)
-    return Executor::ErrCode::StackEmpty;
+    return ErrCode::StackEmpty;
 
   /// Check is the top entry type matched and move pointer.
   try {
     Entry = std::move(std::get<T>(Stack.back()));
   } catch (std::bad_variant_access E) {
-    return Executor::ErrCode::StackWrongEntry;
+    return ErrCode::StackWrongEntry;
   }
 
   /// Delete the index of this popped entry.
@@ -51,14 +49,14 @@ Executor::ErrCode StackManager::pop(std::unique_ptr<T> &Entry) {
     LabelIdx.pop_back();
   /// Drop the top entry.
   Stack.pop_back();
-  return Executor::ErrCode::Success;
+  return ErrCode::Success;
 }
 
 /// Drop the top entry of stack. See "include/executor/stackmgr.h".
-Executor::ErrCode StackManager::pop() {
+ErrCode StackManager::pop() {
   /// Check the size of stack.
   if (Stack.size() == 0)
-    return Executor::ErrCode::StackEmpty;
+    return ErrCode::StackEmpty;
   /// Check is the top entry's type.
   switch (Stack.back().index()) {
   case 0: /// Frame entry
@@ -72,29 +70,29 @@ Executor::ErrCode StackManager::pop() {
   }
   /// Drop the top entry.
   Stack.pop_back();
-  return Executor::ErrCode::Success;
+  return ErrCode::Success;
 }
 
 /// Frame related operation. See "include/executor/stackmgr.h".
-Executor::ErrCode StackManager::getCurrentFrame(FrameEntry *&Frame) {
+ErrCode StackManager::getCurrentFrame(Entry::FrameEntry *&Frame) {
   /// Check is there current frame.
   if (FrameIdx.size() == 0)
-    return Executor::ErrCode::WrongInstanceAddress;
+    return ErrCode::WrongInstanceAddress;
   /// Get the current frame pointer.
   Frame = std::get<0>(Stack[FrameIdx.back()]).get();
-  return Executor::ErrCode::Success;
+  return ErrCode::Success;
 }
 
 /// Label related operation. See "include/executor/stackmgr.h".
-Executor::ErrCode StackManager::getLabelWithCount(LabelEntry *&Label,
-                                                  unsigned int Count = 0) {
+ErrCode StackManager::getLabelWithCount(Entry::LabelEntry *&Label,
+                                        unsigned int Count = 0) {
   /// Check is there at least count + 1 labels.
   if (LabelIdx.size() < Count + 1)
-    return Executor::ErrCode::WrongInstanceAddress;
+    return ErrCode::WrongInstanceAddress;
   /// Get the (count + 1)-th top of label.
   unsigned int Idx = LabelIdx.size() - Count - 1;
   Label = std::get<1>(Stack[LabelIdx[Idx]]).get();
-  return Executor::ErrCode::Success;
+  return ErrCode::Success;
 }
 
 } // namespace Executor

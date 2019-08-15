3b8596c7b69bd681b63af40727096a8ca8ffaef5
hydai <hydai@secondstate.io>
2019-08-15 18:08:30 +0800

    [Executor] Fix typo and use helper function

--- lib/executor/worker.cpp
+++ lib/executor/worker.cpp
@@ -4,7 +4,7 @@
 namespace SSVM {
 namespace Executor {
 
-namespace {
+namespace detail {
 using OpCode = AST::Instruction::OpCode;
 
 /// helper functions for execution
@@ -19,7 +19,7 @@ inline bool isControlOp(OpCode Opcode) const {
   return isInRange(OpCode::Unreachable, Opcode, OpCode::Call_indirect);
 }
 
-inline bool isPrarametricOp(OpCode Opcode) const {
+inline bool isParametricOp(OpCode Opcode) const {
   return isInRange(OpCode::Drop, Opcode, OpCode::Select);
 }
 
@@ -39,6 +39,8 @@ inline bool isNumericOp(OpCode Opcode) const {
   return isInRange(OpCode::I32__eqz, Opcode, OpCode::F64__reinterpret_i64);
 }
 
+} // namespace detail
+
 ErrCode Worker::setArguments(Bytes &Input) {
   Args.assign(Input.begin(), Input.end());
   return ErrCode::Success;
@@ -53,7 +55,20 @@ ErrCode Worker::setCode(std::vector<std::unique_ptr<AST::Instruction>> &Instrs) 
 
 ErrCode Worker::run() {
   for (auto &Inst : Instrs) {
-
+    OpCode opcode = Inst.getOpCode();
+    if (isConstNumericOp(opcode)) {
+      // TODO: const numeric instructions
+    } else if (isControlOp(opcode)) {
+      // TODO: control instructions
+    } else if (isNumericOp(opcode)) {
+      // TODO: numeric instructions
+    } else if (isMemoryOp(opcode)) {
+      // TODO: memory instructions
+    } else if (isParametricOp(opcode)) {
+      // TODO: parametric instructions
+    } else if (isVariableOp(opcode)) {
+      // TODO: variable instructions
+    }
   }
   return ErrCode::Success;
 }

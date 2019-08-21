2a39ba521564f64c539f21c5bab132844dc8a0e8
hydai <hydai@secondstate.io>
2019-08-21 09:04:59 +0000

    [Executor] Refine comments of ValueEntry

--- include/executor/entry/value.h
+++ include/executor/entry/value.h
@@ -20,11 +20,13 @@ namespace Executor {
 
 class ValueEntry {
 public:
-  /// Constructors for assigning values. For instructions using.
+  /// Default constructors for temp ValueEntry.
   ValueEntry() : Type(AST::ValType::I32), Value(0) {}
+  /// Copy constructor for duplication.
   explicit ValueEntry(const ValueEntry &VE) : Type(VE.Type), Value(VE.Value) {}
   explicit ValueEntry(AST::ValType &VT, AST::ValVariant &Val)
       : Type(VT), Value(Val) {}
+  /// Constructors for the different value type
   explicit ValueEntry(int32_t Val) : Type(AST::ValType::I32), Value(Val) {}
   explicit ValueEntry(int64_t Val) : Type(AST::ValType::I64), Value(Val) {}
   explicit ValueEntry(float Val) : Type(AST::ValType::F32), Value(Val) {}

cb5256dba3a5a9ba16ac17d790d524fcd5679977
hydai <hydai@secondstate.io>
2019-08-23 16:31:54 +0800

    [Support] Fix convertion bug of intToBytes, and bytesToInt

--- include/support/casting.h
+++ include/support/casting.h
@@ -13,19 +13,17 @@ inline bool isa(const Y ptr) {
 template<typename T>
 inline T bytesToInt(const std::vector<unsigned char> &Bytes) {
   if (std::is_same<T, int32_t>::value) {
-    return (Bytes[3] << 3
-            | Bytes[2] << 2
-            | Bytes[1] << 1
-            | Bytes[0]);
+    T Int = 0;
+    for (unsigned int I = 3; I >= 0; I--) {
+      Int |= Bytes[I] << (I*8);
+    }
+    return Int;
   } else if (std::is_same<T, int64_t>::value) {
-    return (Bytes[7] << 7
-            | Bytes[6] << 6
-            | Bytes[5] << 5
-            | Bytes[4] << 4
-            | Bytes[3] << 3
-            | Bytes[2] << 2
-            | Bytes[1] << 1
-            | Bytes[0]);
+    T Int = 0;
+    for (unsigned int I = 7; I >= 0; I--) {
+      Int |= Bytes[I] << (I*8);
+    }
+    return Int;
   } else {
     /// TODO: We do not handle the `t.loadN_sx` instructions.
     return 0;
@@ -36,19 +34,13 @@ template<typename T>
 inline std::vector<unsigned char> intToBytes(T Int) {
   std::vector<unsigned char> Bytes;
   if (std::is_same<T, int32_t>::value) {
-    Bytes.push_back(Int & 0xFF);
-    Bytes.push_back(Int & 0xFF00);
-    Bytes.push_back(Int & 0xFF0000);
-    Bytes.push_back(Int & 0xFF000000);
+    for (unsigned int I = 0; I <= 3; I++) {
+      Bytes.push_back((Int >> (I * 8)) & 0xFF);
+    }
   } else if (std::is_same<T, int64_t>::value) {
-    Bytes.push_back(Int & 0xFF);
-    Bytes.push_back(Int & 0xFF00);
-    Bytes.push_back(Int & 0xFF0000);
-    Bytes.push_back(Int & 0xFF000000);
-    Bytes.push_back(Int & 0xFF00000000);
-    Bytes.push_back(Int & 0xFF0000000000);
-    Bytes.push_back(Int & 0xFF000000000000);
-    Bytes.push_back(Int & 0xFF00000000000000);
+    for (unsigned int I = 0; I <= 7; I++) {
+      Bytes.push_back((Int >> (I * 8)) & 0xFF);
+    }
   }
 
   return Bytes;

1025a824001cdc2a77f1b2a9bd46c19a39a43f04
YiYing He <yiying@secondstate.io>
2019-09-17 22:40:29 +0800

    [Executor] Fix memory instantiation and add blocks in if statement.

--- lib/executor/executor.cpp
+++ lib/executor/executor.cpp
@@ -14,36 +14,42 @@ ErrCode Executor::setModule(std::unique_ptr<AST::Module> &Module) {
 
 /// Instantiate module instance. See "include/executor/executor.h".
 ErrCode Executor::instantiate(AST::Module *Mod) {
-  if (Mod == nullptr)
+  if (Mod == nullptr) {
     return ErrCode::Success;
+  }
   ErrCode Status = ErrCode::Success;
   auto ModInst = std::make_unique<Instance::ModuleInstance>();
 
   /// Insert the module instance to store manager.
   if ((Status = StoreMgr.insertModuleInst(ModInst, ModInstId)) !=
-      ErrCode::Success)
+      ErrCode::Success) {
     return Status;
+  }
 
   /// Instantiate ImportSection and do import matching. (ImportSec)
   AST::ImportSection *ImportSec = Mod->getImportSection();
-  if ((Status = instantiate(ImportSec)) != ErrCode::Success)
+  if ((Status = instantiate(ImportSec)) != ErrCode::Success) {
     return Status;
+  }
 
   /// Instantiate Function Types in Module Instance. (TypeSec)
   AST::TypeSection *TypeSec = Mod->getTypeSection();
-  if ((Status = instantiate(TypeSec)) != ErrCode::Success)
+  if ((Status = instantiate(TypeSec)) != ErrCode::Success) {
     return Status;
+  }
 
   /// Instantiate Functions in module. (FuncionSec, CodeSec)
   AST::FunctionSection *FuncSec = Mod->getFunctionSection();
   AST::CodeSection *CodeSec = Mod->getCodeSection();
-  if ((Status = instantiate(FuncSec, CodeSec)) != ErrCode::Success)
+  if ((Status = instantiate(FuncSec, CodeSec)) != ErrCode::Success) {
     return Status;
+  }
 
   /// Instantiate GlobalSection (GlobalSec)
   AST::GlobalSection *GlobSec = Mod->getGlobalSection();
-  if ((Status = instantiate(GlobSec)) != ErrCode::Success)
+  if ((Status = instantiate(GlobSec)) != ErrCode::Success) {
     return Status;
+  }
 
   /// Initializa the tables and memories
   /// Make a new frame {ModInst, locals:none} and push
@@ -53,22 +59,25 @@ ErrCode Executor::instantiate(AST::Module *Mod) {
   /// Instantiate TableSection (TableSec, ElemSec)
   AST::TableSection *TabSec = Mod->getTableSection();
   AST::ElementSection *ElemSec = Mod->getElementSection();
-  if ((Status = instantiate(TabSec, ElemSec)) != ErrCode::Success)
+  if ((Status = instantiate(TabSec, ElemSec)) != ErrCode::Success) {
     return Status;
+  }
 
   /// Instantiate MemorySection (MemorySec, DataSec)
   AST::MemorySection *MemSec = Mod->getMemorySection();
   AST::DataSection *DataSec = Mod->getDataSection();
-  if ((Status = instantiate(MemSec, DataSec)) != ErrCode::Success)
+  if ((Status = instantiate(MemSec, DataSec)) != ErrCode::Success) {
     return Status;
+  }
 
   /// Pop Frame.
   StackMgr.pop();
 
   /// Instantiate ExportSection (ExportSec)
   AST::ExportSection *ExportSec = Mod->getExportSection();
-  if ((Status = instantiate(ExportSec)) != ErrCode::Success)
+  if ((Status = instantiate(ExportSec)) != ErrCode::Success) {
     return Status;
+  }
 
   /// Instantiate StartSection (StartSec)
   /// In e-wasm, the start section will always be "main" function.
@@ -79,14 +88,16 @@ ErrCode Executor::instantiate(AST::Module *Mod) {
 
 /// Instantiate imports. See "include/executor/executor.h".
 ErrCode Executor::instantiate(AST::ImportSection *ImportSec) {
-  if (ImportSec == nullptr)
+  if (ImportSec == nullptr) {
     return ErrCode::Success;
+  }
   ErrCode Status = ErrCode::Success;
 
   /// Get the module instance from ID.
   Instance::ModuleInstance *ModInst = nullptr;
-  if ((Status = StoreMgr.getModule(ModInstId, ModInst)) != ErrCode::Success)
+  if ((Status = StoreMgr.getModule(ModInstId, ModInst)) != ErrCode::Success) {
     return Status;
+  }
 
   /// Iterate and instantiate import descriptions.
   auto &ImpDescs = ImportSec->getContent();
@@ -103,18 +114,22 @@ ErrCode Executor::instantiate(AST::ImportSection *ImportSec) {
       /// Find the function instance in Store.
       Instance::FunctionInstance *FuncInst = nullptr;
       if ((Status = StoreMgr.findFunction(ModName, ExtName, FuncInst)) !=
-          ErrCode::Success)
+          ErrCode::Success) {
         return Status;
+      }
       /// Set the function type index.
       unsigned int *TypeIdx = nullptr;
       if ((Status = (*ImpDesc)->getExternalContent(TypeIdx)) !=
-          ErrCode::Success)
+          ErrCode::Success) {
         return Status;
-      if ((Status = FuncInst->setTypeIdx(*TypeIdx)) != ErrCode::Success)
+      }
+      if ((Status = FuncInst->setTypeIdx(*TypeIdx)) != ErrCode::Success) {
         return Status;
+      }
       /// Set the function address to module instance.
-      if ((Status = ModInst->addFuncAddr(FuncInst->Addr)) != ErrCode::Success)
+      if ((Status = ModInst->addFuncAddr(FuncInst->Addr)) != ErrCode::Success) {
         return Status;
+      }
       break;
     }
     case AST::Desc::ExternalType::Table:  /// Table type TODO
@@ -129,14 +144,16 @@ ErrCode Executor::instantiate(AST::ImportSection *ImportSec) {
 
 /// Instantiate types in module instance. See "include/executor/executor.h".
 ErrCode Executor::instantiate(AST::TypeSection *TypeSec) {
-  if (TypeSec == nullptr)
+  if (TypeSec == nullptr) {
     return ErrCode::Success;
+  }
   ErrCode Status = ErrCode::Success;
 
   /// Get the module instance from ID.
   Instance::ModuleInstance *ModInst = nullptr;
-  if ((Status = StoreMgr.getModule(ModInstId, ModInst)) != ErrCode::Success)
+  if ((Status = StoreMgr.getModule(ModInstId, ModInst)) != ErrCode::Success) {
     return Status;
+  }
 
   /// Iterate and instantiate types.
   auto &FuncTypes = TypeSec->getContent();
@@ -145,8 +162,9 @@ ErrCode Executor::instantiate(AST::TypeSection *TypeSec) {
     /// Copy param and return lists to module instance.
     auto &Param = (*FuncType)->getParamTypes();
     auto &Return = (*FuncType)->getParamTypes();
-    if ((Status = ModInst->addFuncType(Param, Return)) != ErrCode::Success)
+    if ((Status = ModInst->addFuncType(Param, Return)) != ErrCode::Success) {
       return Status;
+    }
   }
   return Status;
 }
@@ -154,14 +172,16 @@ ErrCode Executor::instantiate(AST::TypeSection *TypeSec) {
 /// Instantiate function instance. See "include/executor/executor.h".
 ErrCode Executor::instantiate(AST::FunctionSection *FuncSec,
                               AST::CodeSection *CodeSec) {
-  if (FuncSec == nullptr || CodeSec == nullptr)
+  if (FuncSec == nullptr || CodeSec == nullptr) {
     return ErrCode::Success;
+  }
   ErrCode Status = ErrCode::Success;
 
   /// Get the module instance from ID.
   Instance::ModuleInstance *ModInst = nullptr;
-  if ((Status = StoreMgr.getModule(ModInstId, ModInst)) != ErrCode::Success)
+  if ((Status = StoreMgr.getModule(ModInstId, ModInst)) != ErrCode::Success) {
     return Status;
+  }
 
   /// Get the function type indices.
   auto &TypeIdxs = FuncSec->getContent();
@@ -179,23 +199,29 @@ ErrCode Executor::instantiate(AST::FunctionSection *FuncSec,
 
     /// Set function instance data.
     if ((Status = NewFuncInst->setModuleAddr(ModInst->Addr)) !=
-        ErrCode::Success)
+        ErrCode::Success) {
       return Status;
-    if ((Status = NewFuncInst->setTypeIdx(*TypeIdx)) != ErrCode::Success)
+    }
+    if ((Status = NewFuncInst->setTypeIdx(*TypeIdx)) != ErrCode::Success) {
       return Status;
-    if ((Status = NewFuncInst->setLocals(Locals)) != ErrCode::Success)
+    }
+    if ((Status = NewFuncInst->setLocals(Locals)) != ErrCode::Success) {
       return Status;
-    if ((Status = NewFuncInst->setInstrs(Instrs)) != ErrCode::Success)
+    }
+    if ((Status = NewFuncInst->setInstrs(Instrs)) != ErrCode::Success) {
       return Status;
+    }
 
     /// Insert function instance to store manager.
     if ((Status = StoreMgr.insertFunctionInst(NewFuncInst, NewFuncInstId)) !=
-        ErrCode::Success)
+        ErrCode::Success) {
       return Status;
+    }
 
     /// Set external value (function address) to module instance.
-    if ((Status = ModInst->addFuncAddr(NewFuncInstId)) != ErrCode::Success)
+    if ((Status = ModInst->addFuncAddr(NewFuncInstId)) != ErrCode::Success) {
       return Status;
+    }
 
     CodeSeg++;
     TypeIdx++;
@@ -205,14 +231,16 @@ ErrCode Executor::instantiate(AST::FunctionSection *FuncSec,
 
 /// Instantiate global instance. See "include/executor/executor.h".
 ErrCode Executor::instantiate(AST::GlobalSection *GlobSec) {
-  if (GlobSec == nullptr)
+  if (GlobSec == nullptr) {
     return ErrCode::Success;
+  }
   ErrCode Status = ErrCode::Success;
 
   /// Get the module instance from ID.
   Instance::ModuleInstance *ModInst = nullptr;
-  if ((Status = StoreMgr.getModule(ModInstId, ModInst)) != ErrCode::Success)
+  if ((Status = StoreMgr.getModule(ModInstId, ModInst)) != ErrCode::Success) {
     return Status;
+  }
 
   /// Add a temp module to Store for initialization
   auto TmpMod = std::make_unique<Instance::ModuleInstance>();
@@ -228,33 +256,39 @@ ErrCode Executor::instantiate(AST::GlobalSection *GlobSec) {
     auto &GlobType = (*GlobSeg)->getGlobalType();
     auto Type = GlobType->getValueType();
     auto Mut = GlobType->getValueMutation();
-    if ((Status = NewGlobInst->setGlobalType(Type, Mut)) != ErrCode::Success)
+    if ((Status = NewGlobInst->setGlobalType(Type, Mut)) != ErrCode::Success) {
       return Status;
+    }
 
     /// Insert global instance to store manager.
     if ((Status = StoreMgr.insertGlobalInst(NewGlobInst, NewGlobInstId)) !=
-        ErrCode::Success)
+        ErrCode::Success) {
       return Status;
+    }
 
     /// Set external value (global address) to module instance.
-    if ((Status = ModInst->addGlobalAddr(NewGlobInstId)) != ErrCode::Success)
+    if ((Status = ModInst->addGlobalAddr(NewGlobInstId)) != ErrCode::Success) {
       return Status;
+    }
 
     /// Set external value (global address) to temp module instance.
-    if ((Status = TmpMod->addGlobalAddr(NewGlobInstId)) != ErrCode::Success)
+    if ((Status = TmpMod->addGlobalAddr(NewGlobInstId)) != ErrCode::Success) {
       return Status;
+    }
   }
 
   /// Initialize the globals
   /// Insert the temp. module instance to Store
   unsigned int TmpModInstId = 0;
   if ((Status = StoreMgr.insertModuleInst(TmpMod, TmpModInstId)) !=
-      ErrCode::Success)
+      ErrCode::Success) {
     return Status;
+  }
   Instance::ModuleInstance *TmpModInst = nullptr;
   if ((Status = StoreMgr.getModule(TmpModInstId, TmpModInst)) !=
-      ErrCode::Success)
+      ErrCode::Success) {
     return Status;
+  }
 
   /// Make a new frame {NewModInst:{globaddrs}, locals:none} and push
   auto Frame = std::make_unique<FrameEntry>(TmpModInstId, 0);
@@ -265,8 +299,9 @@ ErrCode Executor::instantiate(AST::GlobalSection *GlobSec) {
   for (unsigned int I = 0; I < TmpModInst->getGlobalNum(); I++, GlobSeg++) {
     /// Set init instrs to engine and run.
     if ((Status = Engine.runExpression((*GlobSeg)->getInstrs())) !=
-        ErrCode::Success)
+        ErrCode::Success) {
       return Status;
+    }
 
     /// Pop result from stack.
     std::unique_ptr<ValueEntry> PopVal;
@@ -294,14 +329,16 @@ ErrCode Executor::instantiate(AST::GlobalSection *GlobSec) {
 /// Instantiate table instance. See "include/executor/executor.h".
 ErrCode Executor::instantiate(AST::TableSection *TabSec,
                               AST::ElementSection *ElemSec) {
-  if (TabSec == nullptr)
+  if (TabSec == nullptr) {
     return ErrCode::Success;
+  }
   ErrCode Status = ErrCode::Success;
 
   /// Get the module instance from ID.
   Instance::ModuleInstance *ModInst = nullptr;
-  if ((Status = StoreMgr.getModule(ModInstId, ModInst)) != ErrCode::Success)
+  if ((Status = StoreMgr.getModule(ModInstId, ModInst)) != ErrCode::Success) {
     return Status;
+  }
 
   /// Iterate and istantiate table types.
   auto &TabTypes = TabSec->getContent();
@@ -313,20 +350,24 @@ ErrCode Executor::instantiate(AST::TableSection *TabSec,
     /// Set table instance data.
     auto ElemType = (*TabType)->getElementType();
     auto &Limit = (*TabType)->getLimit();
-    if ((Status = NewTabInst->setElemType(ElemType)) != ErrCode::Success)
+    if ((Status = NewTabInst->setElemType(ElemType)) != ErrCode::Success) {
       return Status;
-    if ((Status = NewTabInst->setLimit(Limit->hasMax(), Limit->getMax())) !=
-        ErrCode::Success)
+    }
+    if ((Status = NewTabInst->setLimit(Limit->getMin(), Limit->hasMax(),
+                                       Limit->getMax())) != ErrCode::Success) {
       return Status;
+    }
 
     /// Insert table instance to store manager.
     if ((Status = StoreMgr.insertTableInst(NewTabInst, NewTabInstId)) !=
-        ErrCode::Success)
+        ErrCode::Success) {
       return Status;
+    }
 
     /// Set external value (table address) to module instance.
-    if ((Status = ModInst->addTableAddr(NewTabInstId)) != ErrCode::Success)
+    if ((Status = ModInst->addTableAddr(NewTabInstId)) != ErrCode::Success) {
       return Status;
+    }
   }
 
   /// Iterate and evaluate element segments.
@@ -334,26 +375,30 @@ ErrCode Executor::instantiate(AST::TableSection *TabSec,
   for (auto ElemSeg = ElemSegs.begin(); ElemSeg != ElemSegs.end(); ElemSeg++) {
     /// Evaluate instrs in element segment for offset.
     if ((Status = Engine.runExpression((*ElemSeg)->getInstrs())) !=
-        ErrCode::Success)
+        ErrCode::Success) {
       return Status;
+    }
 
     /// Pop the result for offset.
     std::unique_ptr<ValueEntry> PopVal;
     StackMgr.pop(PopVal);
     int32_t Offset = 0;
-    if ((Status = PopVal->getValue(Offset)) != ErrCode::Success)
+    if ((Status = PopVal->getValue(Offset)) != ErrCode::Success) {
       return Status;
+    }
 
     /// Get table instance
     Instance::TableInstance *TabInst = nullptr;
     unsigned int TabAddr = 0;
     if ((Status = ModInst->getMemAddr((*ElemSeg)->getIdx(), TabAddr)) !=
-        ErrCode::Success)
+        ErrCode::Success) {
       return Status;
-    if ((Status = StoreMgr.getTable(TabAddr, TabInst)) != ErrCode::Success)
+    }
+    if ((Status = StoreMgr.getTable(TabAddr, TabInst)) != ErrCode::Success) {
       return Status;
+    }
 
-    /// Copy data to memory instance
+    /// Copy data to table instance
     TabInst->setInitList(Offset, (*ElemSeg)->getFuncIdxes());
   }
 
@@ -363,14 +408,16 @@ ErrCode Executor::instantiate(AST::TableSection *TabSec,
 /// Instantiate memory instance. See "include/executor/executor.h".
 ErrCode Executor::instantiate(AST::MemorySection *MemSec,
                               AST::DataSection *DataSec) {
-  if (MemSec == nullptr)
+  if (MemSec == nullptr) {
     return ErrCode::Success;
+  }
   ErrCode Status = ErrCode::Success;
 
   /// Get the module instance from ID.
   Instance::ModuleInstance *ModInst = nullptr;
-  if ((Status = StoreMgr.getModule(ModInstId, ModInst)) != ErrCode::Success)
+  if ((Status = StoreMgr.getModule(ModInstId, ModInst)) != ErrCode::Success) {
     return Status;
+  }
 
   /// Iterate and istantiate memory types.
   auto &MemTypes = MemSec->getContent();
@@ -381,18 +428,21 @@ ErrCode Executor::instantiate(AST::MemorySection *MemSec,
 
     /// Set memory instance data.
     auto &Limit = (*MemType)->getLimit();
-    if ((Status = NewMemInst->setLimit(Limit->hasMax(), Limit->getMax())) !=
-        ErrCode::Success)
+    if ((Status = NewMemInst->setLimit(Limit->getMin(), Limit->hasMax(),
+                                       Limit->getMax())) != ErrCode::Success) {
       return Status;
+    }
 
     /// Insert memory instance to store manager.
     if ((Status = StoreMgr.insertMemoryInst(NewMemInst, NewMemInstId)) !=
-        ErrCode::Success)
+        ErrCode::Success) {
       return Status;
+    }
 
     /// Set external value (memory address) to module instance.
-    if ((Status = ModInst->addMemAddr(NewMemInstId)) != ErrCode::Success)
+    if ((Status = ModInst->addMemAddr(NewMemInstId)) != ErrCode::Success) {
       return Status;
+    }
   }
 
   /// Iterate and evaluate data segments.
@@ -400,43 +450,50 @@ ErrCode Executor::instantiate(AST::MemorySection *MemSec,
   for (auto DataSeg = DataSegs.begin(); DataSeg != DataSegs.end(); DataSeg++) {
     /// Evaluate instrs in data segment for offset.
     if ((Status = Engine.runExpression((*DataSeg)->getInstrs())) !=
-        ErrCode::Success)
+        ErrCode::Success) {
       return Status;
+    }
 
     /// Pop the result for offset.
     std::unique_ptr<ValueEntry> PopVal;
     StackMgr.pop(PopVal);
     int32_t Offset = 0;
-    if ((Status = PopVal->getValue(Offset)) != ErrCode::Success)
+    if ((Status = PopVal->getValue(Offset)) != ErrCode::Success) {
       return Status;
+    }
 
     /// Get memory instance
     Instance::MemoryInstance *MemInst = nullptr;
     unsigned int MemAddr = 0;
     if ((Status = ModInst->getMemAddr((*DataSeg)->getIdx(), MemAddr)) !=
-        ErrCode::Success)
+        ErrCode::Success) {
       return Status;
-    if ((Status = StoreMgr.getMemory(MemAddr, MemInst)) != ErrCode::Success)
+    }
+    if ((Status = StoreMgr.getMemory(MemAddr, MemInst)) != ErrCode::Success) {
       return Status;
+    }
 
     /// Copy data to memory instance
-    MemInst->setBytes((*DataSeg)->getData(), Offset,
-                      (*DataSeg)->getData().size());
+    if ((Status = MemInst->setBytes((*DataSeg)->getData(), Offset)) !=
+        ErrCode::Success) {
+      return Status;
+    }
   }
-
   return Status;
 }
 
 /// Instantiate export instance. See "include/executor/executor.h".
 ErrCode Executor::instantiate(AST::ExportSection *ExportSec) {
-  if (ExportSec == nullptr)
+  if (ExportSec == nullptr) {
     return ErrCode::Success;
+  }
   ErrCode Status = ErrCode::Success;
 
   /// Get the module instance from ID.
   Instance::ModuleInstance *ModInst = nullptr;
-  if ((Status = StoreMgr.getModule(ModInstId, ModInst)) != ErrCode::Success)
+  if ((Status = StoreMgr.getModule(ModInstId, ModInst)) != ErrCode::Success) {
     return Status;
+  }
 
   /// Iterate and istantiate export descriptions.
   auto &ExpDescs = ExportSec->getContent();
@@ -453,19 +510,24 @@ ErrCode Executor::instantiate(AST::ExportSection *ExportSec) {
       unsigned int FuncAddr = 0;
       Instance::FunctionInstance *FuncInst = nullptr;
       /// Find function address.
-      if ((Status = ModInst->getFuncAddr(ExtIdx, FuncAddr)) != ErrCode::Success)
+      if ((Status = ModInst->getFuncAddr(ExtIdx, FuncAddr)) !=
+          ErrCode::Success) {
         return Status;
+      }
       /// Get function instance.
       if ((Status = StoreMgr.getFunction(FuncAddr, FuncInst)) !=
-          ErrCode::Success)
+          ErrCode::Success) {
         return Status;
+      }
       /// Set function name. TODO: module name
-      if ((Status = FuncInst->setNames("", ExtName)) != ErrCode::Success)
+      if ((Status = FuncInst->setNames("", ExtName)) != ErrCode::Success) {
         return Status;
+      }
       /// Set start function index.
       if (ExtName == "main") {
-        if ((Status = ModInst->setStartIdx(ExtIdx)) != ErrCode::Success)
+        if ((Status = ModInst->setStartIdx(ExtIdx)) != ErrCode::Success) {
           return Status;
+        }
       }
     }
   }

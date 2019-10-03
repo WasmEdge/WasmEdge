a0764fb59295b4295c5210694dc2e04d943d1d87
YiYing He <yiying@secondstate.io>
2019-10-04 04:37:57 +0800

    [VM] Add implementation for EEI function. (GetCallDataSize)

---
+++ include/vm/hostfunc/ethereum/getcalldatasize.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "eei.h"
+
+namespace SSVM {
+namespace Executor {
+
+class EEIGetCallDataSize : public EEI {
+public:
+  EEIGetCallDataSize(VM::Environment &Env) : EEI(Env) {}
+  EEIGetCallDataSize() = delete;
+  virtual ~EEIGetCallDataSize() = default;
+
+  virtual ErrCode run(std::vector<std::unique_ptr<ValueEntry>> &Args,
+                      std::vector<std::unique_ptr<ValueEntry>> &Res,
+                      StoreManager &Store, Instance::ModuleInstance *ModInst);
+};
+
+} // namespace Executor
+} // namespace SSVM
---
+++ lib/vm/hostfunc/ethereum/getcalldatasize.cpp
@@ -0,0 +1,24 @@
+#include "vm/hostfunc/ethereum/getcalldatasize.h"
+#include "executor/common.h"
+#include "stdint.h"
+
+namespace SSVM {
+namespace Executor {
+
+ErrCode EEIGetCallDataSize::run(std::vector<std::unique_ptr<ValueEntry>> &Args,
+                                std::vector<std::unique_ptr<ValueEntry>> &Res,
+                                StoreManager &Store,
+                                Instance::ModuleInstance *ModInst) {
+  /// Arg: void
+  if (Args.size() != 0) {
+    return ErrCode::CallFunctionError;
+  }
+
+  /// Return: Length(u32)
+  Res.push_back(std::make_unique<ValueEntry>(
+      static_cast<uint32_t>(Env.getCallData().size())));
+  return ErrCode::Success;
+}
+
+} // namespace Executor
+} // namespace SSVM

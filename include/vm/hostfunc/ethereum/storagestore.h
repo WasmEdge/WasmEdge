7c5f95a5b843f81a1de046adb1553bd7628e9a67
YiYing He <yiying@secondstate.io>
2019-10-04 14:03:01 +0800

    [VM] Add implementation for EEI function. (StorageStore)

---
+++ include/vm/hostfunc/ethereum/storagestore.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "eei.h"
+
+namespace SSVM {
+namespace Executor {
+
+class EEIStorageStore : public EEI {
+public:
+  EEIStorageStore(VM::Environment &Env) : EEI(Env) {}
+  EEIStorageStore() = delete;
+  virtual ~EEIStorageStore() = default;
+
+  virtual ErrCode run(std::vector<std::unique_ptr<ValueEntry>> &Args,
+                      std::vector<std::unique_ptr<ValueEntry>> &Res,
+                      StoreManager &Store, Instance::ModuleInstance *ModInst);
+};
+
+} // namespace Executor
+} // namespace SSVM
---
+++ lib/vm/hostfunc/ethereum/storagestore.cpp
@@ -0,0 +1,53 @@
+#include "vm/hostfunc/ethereum/storagestore.h"
+#include "executor/common.h"
+#include "executor/worker/util.h"
+#include "support/hexstr.h"
+
+namespace SSVM {
+namespace Executor {
+
+ErrCode EEIStorageStore::run(std::vector<std::unique_ptr<ValueEntry>> &Args,
+                             std::vector<std::unique_ptr<ValueEntry>> &Res,
+                             StoreManager &Store,
+                             Instance::ModuleInstance *ModInst) {
+  /// Arg: pathOffset(u32), valueOffset(u32)
+  if (Args.size() != 1) {
+    return ErrCode::CallFunctionError;
+  }
+  ErrCode Status = ErrCode::Success;
+  unsigned int PathOffset = retrieveValue<uint32_t>(*Args[1].get());
+  unsigned int ValueOffset = retrieveValue<uint32_t>(*Args[0].get());
+
+  /// Get Path data by path offset.
+  std::vector<unsigned char> Data;
+  unsigned int MemoryAddr = 0;
+  std::string Path("");
+  std::string Value("");
+  Instance::MemoryInstance *MemInst = nullptr;
+  if ((Status = ModInst->getMemAddr(0, MemoryAddr)) != ErrCode::Success) {
+    return Status;
+  }
+  if ((Status = Store.getMemory(MemoryAddr, MemInst)) != ErrCode::Success) {
+    return Status;
+  }
+  if ((Status = MemInst->getBytes(Data, PathOffset, 32)) != ErrCode::Success) {
+    return Status;
+  }
+  Support::convertHexToString(Data, Path, 64);
+
+  /// Get Value data by value offset.
+  Data.clear();
+  if ((Status = MemInst->getBytes(Data, ValueOffset, 32)) != ErrCode::Success) {
+    return Status;
+  }
+  Support::convertHexToString(Data, Value, 64);
+
+  /// Set Value data to storage.
+  Env.getStorage()[Path] = Value;
+
+  /// Return: void
+  return Status;
+}
+
+} // namespace Executor
+} // namespace SSVM

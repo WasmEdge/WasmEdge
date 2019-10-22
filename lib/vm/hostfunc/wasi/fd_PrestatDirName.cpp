79abf562902af02334f046569ded4dbaf099415c
YiYing He <yiying@secondstate.io>
2019-10-23 06:54:21 +0800

    [VM] Add WasiFdPrestatDirName() implementation.

--- lib/vm/hostfunc/wasi/fd_PrestatDirName.cpp
+++ lib/vm/hostfunc/wasi/fd_PrestatDirName.cpp
@@ -2,6 +2,11 @@
 #include "executor/common.h"
 #include "executor/worker/util.h"
 
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
 namespace SSVM {
 namespace Executor {
 
@@ -18,14 +23,67 @@ WasiFdPrestatDirName::run(std::vector<std::unique_ptr<ValueEntry>> &Args,
                           std::vector<std::unique_ptr<ValueEntry>> &Res,
                           StoreManager &Store,
                           Instance::ModuleInstance *ModInst) {
-  /// Arg: fd(u32), pathBuf(u32), pathLen(u32)
+  /// Arg: Fd(u32), PathBufPtr(u32), PathLenPtr(u32)
   if (Args.size() != 3) {
     return ErrCode::CallFunctionError;
   }
   ErrCode Status = ErrCode::Success;
+  unsigned int Fd = retrieveValue<uint32_t>(*Args[2].get());
+  unsigned int PathBufPtr = retrieveValue<uint32_t>(*Args[1].get());
+  unsigned int PathLenPtr = retrieveValue<uint32_t>(*Args[0].get());
+  int ErrNo = 0;
+
+  /// Store current working dir and change to Fd working dir.
+  char *CurrPath = getcwd(NULL, 0);
+  if (ErrNo == 0) {
+    ErrNo = fchdir(Fd);
+  }
+  /// Get Fd working dir and change back to current working dir.
+  char *FdPath = getcwd(NULL, 0);
+  if (ErrNo == 0) {
+    ErrNo = chdir(CurrPath);
+  }
+
+  /// Store Path and PathLen.
+  if (FdPath != nullptr) {
+    unsigned int MemoryAddr = 0;
+    Instance::MemoryInstance *MemInst = nullptr;
+    if ((Status = ModInst->getMemAddr(0, MemoryAddr)) != ErrCode::Success) {
+      return Status;
+    }
+    if ((Status = Store.getMemory(MemoryAddr, MemInst)) != ErrCode::Success) {
+      return Status;
+    }
+    std::vector<unsigned char> PathBuf;
+    PathBuf.insert(PathBuf.end(), FdPath, FdPath + strlen(FdPath));
+    PathBuf.push_back('\0');
+    if ((Status = MemInst->setBytes(PathBuf, PathBufPtr, 0, PathBuf.size())) !=
+        ErrCode::Success) {
+      return Status;
+    }
+    if ((Status = MemInst->storeValue(
+             PathLenPtr, 4, (uint32_t)PathBuf.size())) != ErrCode::Success) {
+      return Status;
+    }
+  } else {
+    ErrNo = -1;
+  }
+
+  /// Free char*.
+  if (CurrPath != nullptr) {
+    free(FdPath);
+  }
+  if (FdPath != nullptr) {
+    free(CurrPath);
+  }
 
   /// Return: errno(u32)
-  Res.push_back(std::make_unique<ValueEntry>(0U));
+  if (ErrNo == 0) {
+    Res.push_back(std::make_unique<ValueEntry>(0U));
+  } else {
+    /// TODO: errno
+    Res.push_back(std::make_unique<ValueEntry>(1U));
+  }
   return Status;
 }
 

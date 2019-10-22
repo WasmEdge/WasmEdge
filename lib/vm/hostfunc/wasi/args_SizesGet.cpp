f530e8d3a8c83d87a4e080b771aef384e921bf40
YiYing He <yiying@secondstate.io>
2019-10-22 23:00:40 +0800

    [VM] Add WasiArgsSizesGet() implementation.

--- lib/vm/hostfunc/wasi/args_SizesGet.cpp
+++ lib/vm/hostfunc/wasi/args_SizesGet.cpp
@@ -15,11 +15,40 @@ ErrCode WasiArgsSizesGet::run(std::vector<std::unique_ptr<ValueEntry>> &Args,
                               std::vector<std::unique_ptr<ValueEntry>> &Res,
                               StoreManager &Store,
                               Instance::ModuleInstance *ModInst) {
-  /// Arg: argcPtr(u32), argv_bufPtr(u32)
+  /// Arg: ArgcPtr(u32), ArgvBufSizePtr(u32)
   if (Args.size() != 2) {
     return ErrCode::CallFunctionError;
   }
   ErrCode Status = ErrCode::Success;
+  unsigned int ArgcPtr = retrieveValue<uint32_t>(*Args[1].get());
+  unsigned int ArgvBufSizePtr = retrieveValue<uint32_t>(*Args[0].get());
+
+  /// Get memory instance.
+  unsigned int MemoryAddr = 0;
+  Instance::MemoryInstance *MemInst = nullptr;
+  if ((Status = ModInst->getMemAddr(0, MemoryAddr)) != ErrCode::Success) {
+    return Status;
+  }
+  if ((Status = Store.getMemory(MemoryAddr, MemInst)) != ErrCode::Success) {
+    return Status;
+  }
+
+  /// Store Argc.
+  std::vector<std::string> &CmdArgs = Env.getCmdArgs();
+  if ((Status = MemInst->storeValue(ArgcPtr, 4, (uint32_t)CmdArgs.size())) !=
+      ErrCode::Success) {
+    return Status;
+  }
+
+  /// Store ArgvBufSize.
+  uint32_t CmdArgsSize = 0;
+  for (auto It = CmdArgs.cbegin(); It != CmdArgs.cend(); It++) {
+    CmdArgsSize += It->length() + 1;
+  }
+  if ((Status = MemInst->storeValue(ArgvBufSizePtr, 4, CmdArgsSize)) !=
+      ErrCode::Success) {
+    return Status;
+  }
 
   /// Return: errno(u32)
   Res.push_back(std::make_unique<ValueEntry>(0U));

cb3d8cec68d46d01cee17391ee7fd919c39672f5
YiYing He <yiying@secondstate.io>
2019-10-24 14:59:09 +0800

    [VM] Add WasiPathOpen() implementation.

--- lib/vm/hostfunc/wasi/path_Open.cpp
+++ lib/vm/hostfunc/wasi/path_Open.cpp
@@ -2,6 +2,8 @@
 #include "executor/common.h"
 #include "executor/worker/util.h"
 
+#include <fcntl.h>
+
 namespace SSVM {
 namespace Executor {
 
@@ -22,15 +24,59 @@ ErrCode WasiPathOpen::run(std::vector<std::unique_ptr<ValueEntry>> &Args,
                           std::vector<std::unique_ptr<ValueEntry>> &Res,
                           StoreManager &Store,
                           Instance::ModuleInstance *ModInst) {
-  /// Arg: dirfd(u32), dirflags(u32), pathPtr(u32), path_len(u32), oflags(u32),
-  /// fs_rights_base(u64), fs_rights_inheriting(u64), fs_flags(u32), fdPtr(u32)
+  /// Arg: DirFd(u32), DirFlags(u32), PathPtr(u32), PathLen(u32), OFlags(u32),
+  /// FsRightsBase(u64), FsRightsInheriting(u64), FsFlags(u32), FdPtr(u32)
   if (Args.size() != 9) {
     return ErrCode::CallFunctionError;
   }
   ErrCode Status = ErrCode::Success;
+  /// unsigned int DirFd = retrieveValue<uint32_t>(*Args[8].get());
+  /// unsigned int DirFlags = retrieveValue<uint32_t>(*Args[7].get());
+  unsigned int PathPtr = retrieveValue<uint32_t>(*Args[6].get());
+  unsigned int PathLen = retrieveValue<uint32_t>(*Args[5].get());
+  unsigned int OFlags = retrieveValue<uint32_t>(*Args[4].get());
+  /// unsigned int FsRightsBase = retrieveValue<uint64_t>(*Args[3].get());
+  /// unsigned int FsRightsInheriting = retrieveValue<uint64_t>(*Args[2].get());
+  /// unsigned int FsFlags = retrieveValue<uint32_t>(*Args[1].get());
+  unsigned int FdPtr = retrieveValue<uint32_t>(*Args[0].get());
+  int ErrNo = 0;
+
+  /// Get memory instance.
+  unsigned int MemoryAddr = 0;
+  Instance::MemoryInstance *MemInst = nullptr;
+  if ((Status = ModInst->getMemAddr(0, MemoryAddr)) != ErrCode::Success) {
+    return Status;
+  }
+  if ((Status = Store.getMemory(MemoryAddr, MemInst)) != ErrCode::Success) {
+    return Status;
+  }
+
+  /// Get file path.
+  std::vector<unsigned char> Data;
+  if ((Status = MemInst->getBytes(Data, PathPtr, PathLen)) !=
+      ErrCode::Success) {
+    return Status;
+  }
+  std::string Path(Data.begin(), Data.end());
+
+  /// Open file and store Fd.
+  int Fd = open(Path.c_str(), OFlags);
+  if (Fd == -1) {
+    ErrNo = 1;
+  } else {
+    if ((Status = MemInst->storeValue(FdPtr, 4, (uint32_t)Fd)) !=
+        ErrCode::Success) {
+      return Status;
+    }
+  }
 
   /// Return: errno(u32)
-  Res.push_back(std::make_unique<ValueEntry>(0U));
+  if (ErrNo == 0) {
+    Res.push_back(std::make_unique<ValueEntry>(0U));
+  } else {
+    /// TODO: errno
+    Res.push_back(std::make_unique<ValueEntry>(1U));
+  }
   return Status;
 }
 

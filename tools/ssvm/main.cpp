672daa43518d22a4c28a46ddd204c0a4a45485dc
YiYing He <yiying@secondstate.io>
2019-11-13 17:18:06 +0800

    [Tools] Update ssvm for QITC models.

--- tools/ssvm/main.cpp
+++ tools/ssvm/main.cpp
@@ -2,10 +2,28 @@
 #include "vm/result.h"
 #include "vm/vm.h"
 
+#include <dirent.h>
+#include <iostream>
+#include <sys/types.h>
+#include <unistd.h>
+
 int main(int Argc, char *Argv[]) {
-  std::string InputPath("ethereum/erc20.wasm");
-  SSVM::VM::Configure Conf(SSVM::VM::Configure::VMType::Ewasm);
+
+  char *cwdstr = getcwd(NULL, 0);
+  opendir(cwdstr);
+
+  std::string InputPath(Argv[1]);
+  SSVM::VM::Configure Conf(SSVM::VM::Configure::VMType::Wasi);
   SSVM::VM::VM VM(Conf);
+  SSVM::VM::WasiEnvironment *Env =
+      dynamic_cast<SSVM::VM::WasiEnvironment *>(VM.getEnvironment());
+  std::vector<std::string> &CmdArgsVec = Env->getCmdArgs();
+  for (int I = 1; I < Argc; I++) {
+    CmdArgsVec.push_back(std::string(Argv[I]));
+  }
+  for (auto It = CmdArgsVec.begin(); It != CmdArgsVec.end(); It++) {
+    std::cout << " Args : " << *It << std::endl;
+  }
   SSVM::Result Result;
   VM.setPath(InputPath);
   VM.execute();

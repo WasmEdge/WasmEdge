ba6dffe60538cb53dbf7ad5a87fb452bcc14aed2
Peter Chang <peter@secondstate.io>
2019-11-26 12:54:49 +0000

    [Tools] Create the SSVM proxy tool forked from ssvm-evm.

---
+++ tools/ssvm-proxy/main.cpp
@@ -0,0 +1,21 @@
+#include "proxy/cmdparser.h"
+#include "proxy/proxy.h"
+#include "vm/configure.h"
+#include "vm/result.h"
+#include "vm/vm.h"
+
+#include <cstdio>
+#include <cstring>
+
+int main(int Argc, char *Argv[]) {
+  SSVM::Proxy::CmdParser Cmd;
+  Cmd.parseCommandLine(Argc, Argv);
+
+  SSVM::Proxy::Proxy VMProxy;
+  VMProxy.setInputJSONPath(Cmd.getInputJSONPath());
+  VMProxy.setOutputJSONPath(Cmd.getOutputJSONPath());
+  VMProxy.setWasmPath(Cmd.getWasmPath());
+  VMProxy.runRequest();
+
+  return 0;
+}

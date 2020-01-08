294842103dc1ebaf11dffcada37af07ac15194ba
dm4 <dm4@secondstate.io>
2020-01-08 15:55:23 +0800

    [EVMC] add evmc-vm

---
+++ tools/ssvm-evmc/main.cpp
@@ -0,0 +1,140 @@
+// SPDX-License-Identifier: Apache-2.0
+#include "support/hexstr.h"
+#include "vm/configure.h"
+#include "vm/vm.h"
+#include "evmc/evmc.h"
+#include "evmc/utils.h"
+
+#include <cstdlib>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+
+namespace {
+
+static evmc_capabilities_flagset get_capabilities(struct evmc_instance* vm)
+{
+  return EVMC_CAPABILITY_EWASM;
+}
+
+static void destroy(struct evmc_instance* vm)
+{
+  return;
+}
+
+static void release(const struct evmc_result* result)
+{
+  if (result->output_data != nullptr) {
+    delete[] result->output_data;
+  }
+  return;
+}
+
+static struct evmc_result execute(struct evmc_instance* vm,
+    struct evmc_context* context,
+    enum evmc_revision rev,
+    const struct evmc_message* msg,
+    uint8_t const* code,
+    size_t code_size)
+{
+  evmc_address destination = msg->destination;
+  evmc_address sender = msg->sender;
+  const uint8_t* input_data = msg->input_data;
+  size_t input_size = msg->input_size;
+  evmc_uint256be value = msg->value;
+  evmc_bytes32 create2_salt = msg->create2_salt;
+
+  SSVM::VM::Configure Conf;
+  Conf.addVMType(SSVM::VM::Configure::VMType::Ewasm);
+  SSVM::VM::VM EVM(Conf);
+  SSVM::VM::EVMEnvironment *Env =
+    EVM.getEnvironment<SSVM::VM::EVMEnvironment>(SSVM::VM::Configure::VMType::Ewasm);
+  Env->clear();
+  Env->setContext(context);
+
+  // Set caller & callvalue
+  SSVM::Support::convertValVecToHexStr(std::vector<unsigned char>(sender.bytes, sender.bytes + 20), Env->getCaller());
+  SSVM::Support::convertValVecToHexStr(std::vector<unsigned char>(value.bytes, value.bytes + 16), Env->getCallValue());
+
+  // Debug log
+  std::cout << "code_size: " << code_size << std::endl;
+  std::cout << "msg->gas: " << msg->gas << std::endl;
+  std::cout << "msg->depth: " << msg->depth << std::endl;
+  std::cout << "msg->input_size: " << msg->input_size << std::endl;
+  std::cout << "Caller: " << Env->getCaller() << std::endl;
+  std::cout << "CallValue: " << Env->getCallValue() << std::endl;
+
+  // Set calldata
+  if (msg->input_size > 0) {
+    Env->getCallData() = std::vector<uint8_t>(msg->input_data, msg->input_data + msg->input_size);
+  }
+
+  // Set code & gas
+  std::vector<uint8_t> Code(code, code + code_size);
+  EVM.setCode(Code);
+  EVM.setCostLimit(msg->gas);
+
+  EVM.execute("main");
+
+  // Get execution result
+  uint64_t usedGas = EVM.getUsedCost();
+  SSVM::VM::Result::State returnState = EVM.getResult().getState();
+  std::map<std::string, std::string> &storage = Env->getStorage();
+  std::vector<unsigned char> &returnData = Env->getReturnData();
+
+  // Debug log
+  std::cout << "usedGas: " << usedGas << std::endl;
+  std::cout << "    --- result storage: " << std::endl;
+  for (auto it = storage.begin(); it != storage.end(); ++it) {
+    std::cout << "         " << it->first << " " << it->second << std::endl;
+  }
+  std::cout << "    --- return_size: " << returnData.size() << std::endl;
+  std::cout << "    --- return data: " << std::endl << "         ";
+  for (auto it = returnData.begin(); it != returnData.end(); ++it) {
+    printf("%02x", *it);
+  }
+  std::cout << std::endl;
+
+  // Prepare EVMC result
+  struct evmc_result result;
+  result.gas_left = msg->gas - usedGas;
+  result.output_size = 0;
+  result.output_data = nullptr;
+  result.release = ::release;
+  result.create_address = {};
+
+  if (returnState == SSVM::VM::Result::State::Commit) {
+    result.status_code = EVMC_SUCCESS;
+  } else if (returnState == SSVM::VM::Result::State::Revert) {
+    result.status_code = EVMC_REVERT;
+  } else {
+    result.status_code = EVMC_FAILURE;
+    result.gas_left = 0;
+  }
+
+  if (returnData.size() > 0) {
+    uint8_t *outputData = new uint8_t[returnData.size()];
+    copy(returnData.begin(), returnData.end(), outputData);
+    result.output_size = returnData.size();
+    result.output_data = outputData;
+  }
+
+  return result;
+}
+
+} // namespace
+
+extern "C" EVMC_EXPORT struct evmc_instance* evmc_create() EVMC_NOEXCEPT
+{
+  static evmc_instance vm = {
+    EVMC_ABI_VERSION,
+    "ssvm",
+    "0.3.1",
+    ::destroy, // destroy
+    ::execute, // execute
+    ::get_capabilities,
+    nullptr,
+  };
+
+  return &vm;
+}

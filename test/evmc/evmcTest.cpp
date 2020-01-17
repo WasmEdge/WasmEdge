b48d4cd422cbe77c89334d0fbac5988d6b17a498
dm4 <dm4@secondstate.io>
2020-01-17 15:46:13 +0800

    [TEST] fix evmc erc20 test header

--- test/evmc/evmcTest.cpp
+++ test/evmc/evmcTest.cpp
@@ -24,11 +24,13 @@
 namespace {
 
 evmc_context *context = example_host_create_context(evmc_tx_context{});
+const std::string evmc_library =
+    std::string("../../tools/ssvm-evmc/libssvm-evmc.") +
+    std::string(EVMC_SHARED_LIBRARY_SUFFIX);
 
 TEST(EVMCTest, Run__1_deploy) {
   enum evmc_loader_error_code err;
-  struct evmc_instance *vm =
-      evmc_load_and_create("../../tools/ssvm-evmc/libssvm-evmc.so", &err);
+  struct evmc_instance *vm = evmc_load_and_create(evmc_library.c_str(), &err);
   EXPECT_EQ(err, EVMC_LOADER_SUCCESS);
 
   evmc_address sender = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
@@ -99,8 +101,7 @@ evmc_result evmc_vm_execute(evmc_instance *vm, std::string SenderStr,
 
 TEST(EVMCTest, Run__2_check_balance_of_0x7FFFFFFF) {
   enum evmc_loader_error_code err;
-  struct evmc_instance *vm =
-      evmc_load_and_create("../../tools/ssvm-evmc/libssvm-evmc.so", &err);
+  struct evmc_instance *vm = evmc_load_and_create(evmc_library.c_str(), &err);
   EXPECT_EQ(err, EVMC_LOADER_SUCCESS);
   std::string SenderStr = "000000000000000000000000000000007fffffff";
   std::string CallDataStr =
@@ -123,8 +124,7 @@ TEST(EVMCTest, Run__2_check_balance_of_0x7FFFFFFF) {
 
 TEST(EVMCTest, Run__3_check_total_supply) {
   enum evmc_loader_error_code err;
-  struct evmc_instance *vm =
-      evmc_load_and_create("../../tools/ssvm-evmc/libssvm-evmc.so", &err);
+  struct evmc_instance *vm = evmc_load_and_create(evmc_library.c_str(), &err);
   EXPECT_EQ(err, EVMC_LOADER_SUCCESS);
   std::string SenderStr = "000000000000000000000000000000007fffffff";
   std::string CallDataStr = "18160ddd";
@@ -145,8 +145,7 @@ TEST(EVMCTest, Run__3_check_total_supply) {
 
 TEST(EVMCTest, Run__4_transfer_20_from_0x7FFFFFFF_to_0x01) {
   enum evmc_loader_error_code err;
-  struct evmc_instance *vm =
-      evmc_load_and_create("../../tools/ssvm-evmc/libssvm-evmc.so", &err);
+  struct evmc_instance *vm = evmc_load_and_create(evmc_library.c_str(), &err);
   EXPECT_EQ(err, EVMC_LOADER_SUCCESS);
   std::string SenderStr = "000000000000000000000000000000007fffffff";
   std::string CallDataStr =
@@ -170,8 +169,7 @@ TEST(EVMCTest, Run__4_transfer_20_from_0x7FFFFFFF_to_0x01) {
 
 TEST(EVMCTest, Run__5_check_balance_of_0x01) {
   enum evmc_loader_error_code err;
-  struct evmc_instance *vm =
-      evmc_load_and_create("../../tools/ssvm-evmc/libssvm-evmc.so", &err);
+  struct evmc_instance *vm = evmc_load_and_create(evmc_library.c_str(), &err);
   EXPECT_EQ(err, EVMC_LOADER_SUCCESS);
   std::string SenderStr = "000000000000000000000000000000007fffffff";
   std::string CallDataStr =
@@ -194,8 +192,7 @@ TEST(EVMCTest, Run__5_check_balance_of_0x01) {
 
 TEST(EVMCTest, Run__6_approve_10_from_0x7FFFFFFF_for_0x01_to_spend) {
   enum evmc_loader_error_code err;
-  struct evmc_instance *vm =
-      evmc_load_and_create("../../tools/ssvm-evmc/libssvm-evmc.so", &err);
+  struct evmc_instance *vm = evmc_load_and_create(evmc_library.c_str(), &err);
   EXPECT_EQ(err, EVMC_LOADER_SUCCESS);
   std::string SenderStr = "000000000000000000000000000000007fffffff";
   std::string CallDataStr =
@@ -219,8 +216,7 @@ TEST(EVMCTest, Run__6_approve_10_from_0x7FFFFFFF_for_0x01_to_spend) {
 
 TEST(EVMCTest, Run__7_check_allowance_from_0x7FFFFFFF_by_0x01) {
   enum evmc_loader_error_code err;
-  struct evmc_instance *vm =
-      evmc_load_and_create("../../tools/ssvm-evmc/libssvm-evmc.so", &err);
+  struct evmc_instance *vm = evmc_load_and_create(evmc_library.c_str(), &err);
   EXPECT_EQ(err, EVMC_LOADER_SUCCESS);
   std::string SenderStr = "000000000000000000000000000000007fffffff";
   std::string CallDataStr =
@@ -244,8 +240,7 @@ TEST(EVMCTest, Run__7_check_allowance_from_0x7FFFFFFF_by_0x01) {
 
 TEST(EVMCTest, Run__8_transfer_3_from_0x7FFFFFFF_by_0x01_to_0x02) {
   enum evmc_loader_error_code err;
-  struct evmc_instance *vm =
-      evmc_load_and_create("../../tools/ssvm-evmc/libssvm-evmc.so", &err);
+  struct evmc_instance *vm = evmc_load_and_create(evmc_library.c_str(), &err);
   EXPECT_EQ(err, EVMC_LOADER_SUCCESS);
   std::string SenderStr = "0000000000000000000000000000000000000001";
   std::string CallDataStr =
@@ -270,8 +265,7 @@ TEST(EVMCTest, Run__8_transfer_3_from_0x7FFFFFFF_by_0x01_to_0x02) {
 
 TEST(EVMCTest, Run__9_check_balance_of_0x7FFFFFFF) {
   enum evmc_loader_error_code err;
-  struct evmc_instance *vm =
-      evmc_load_and_create("../../tools/ssvm-evmc/libssvm-evmc.so", &err);
+  struct evmc_instance *vm = evmc_load_and_create(evmc_library.c_str(), &err);
   EXPECT_EQ(err, EVMC_LOADER_SUCCESS);
   std::string SenderStr = "0000000000000000000000000000000000000001";
   std::string CallDataStr =

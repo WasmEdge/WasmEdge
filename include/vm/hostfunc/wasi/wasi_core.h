d604ffb7ec90cbafb8e23edb8e8730fcc9aa4e50
hydai <hydai@secondstate.io>
2020-03-11 12:54:08 +0000

    [VM] Create super header to eliminate messy include section

---
+++ include/vm/hostfunc/wasi/wasi_core.h
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: Apache-2.0
+#pragma once
+
+#include "args_Get.h"
+#include "args_SizesGet.h"
+#include "environ_Get.h"
+#include "environ_SizesGet.h"
+#include "fd_Close.h"
+#include "fd_FdstatGet.h"
+#include "fd_FdstatSetFlags.h"
+#include "fd_PrestatDirName.h"
+#include "fd_PrestatGet.h"
+#include "fd_Read.h"
+#include "fd_Seek.h"
+#include "fd_Write.h"
+#include "path_Open.h"
+#include "proc_Exit.h"

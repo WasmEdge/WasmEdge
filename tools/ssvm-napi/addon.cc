6d77b372b9e3b5938b6be195dadbeac77e5e47d6
YiYing He <yiying@secondstate.io>
2020-04-01 09:54:51 +0800

    [Tools] Delete unused js-addon tool.

--- tools/ssvm-napi/addon.cc
+++
@@ -1,8 +0,0 @@
-#include "ssvmaddon.h"
-#include <napi.h>
-
-Napi::Object InitAll(Napi::Env env, Napi::Object exports) {
-  return SSVMAddon::Init(env, exports);
-}
-
-NODE_API_MODULE(addon, InitAll)

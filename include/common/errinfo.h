71e4addfa0f7c6ff3c203341e16278edc7cbfbfa
YiYing He <yiying@secondstate.io>
2020-06-11 17:50:35 +0800

    [Common] Add error information structures.

---
+++ include/common/errinfo.h
@@ -0,0 +1,243 @@
+// SPDX-License-Identifier: Apache-2.0
+//===-- ssvm/common/errinfo.h - Error information definition --------------===//
+//
+// Part of the SSVM Project.
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file contains the enumerations of SSVM error infomation.
+///
+//===----------------------------------------------------------------------===//
+#pragma once
+
+#include "ast.h"
+#include "types.h"
+#include "value.h"
+
+#include <iomanip>
+#include <ostream>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+
+namespace SSVM {
+namespace ErrInfo {
+
+/// Error info type enumeration class.
+enum class InfoType : uint8_t {
+  File,        /// Information about file name which loading from
+  Loading,     /// Information about bytecode offset
+  AST,         /// Information about tracing AST nodes
+  Registering, /// Information about instantiating modules
+  Linking,     /// Information about linking instances
+  Executing,   /// Information about running functions
+  Mismatch,    /// Information about comparison of instances when error
+  Instruction, /// Information about aborted instructions and parameters
+  Boundary     /// Information about forbidden offset accessing
+};
+
+/// Instance addressing type enumeration class.
+enum class PtrType : uint8_t {
+  Index,  /// Index of instances
+  Address /// Absolute address
+};
+
+static inline std::unordered_map<PtrType, std::string> PtrTypeStr = {
+    {PtrType::Index, "index"}, {PtrType::Address, "address"}};
+
+/// Wasm instance category.
+enum class InstCategory : uint8_t {
+  ExternalType, /// External typing
+  FunctionType, /// Function type
+  Table,        /// Table instance
+  Memory,       /// Memory instance
+  Global,       /// Global instance
+  Version       /// Versions
+};
+
+static inline std::unordered_map<InstCategory, std::string> InstCategoryStr = {
+    {InstCategory::ExternalType, "external type"},
+    {InstCategory::FunctionType, "function type"},
+    {InstCategory::Table, "table"},
+    {InstCategory::Memory, "memory"},
+    {InstCategory::Global, "global"},
+    {InstCategory::Version, "version"}};
+
+/// Information structures.
+struct InfoFile {
+  InfoFile() = default;
+  InfoFile(std::string_view FName) noexcept : FileName(FName) {}
+
+  friend std::ostream &operator<<(std::ostream &OS, const struct InfoFile &Rhs);
+
+  std::string FileName;
+};
+
+struct InfoLoading {
+  InfoLoading() = default;
+  InfoLoading(const uint32_t Off) noexcept : Offset(Off) {}
+
+  friend std::ostream &operator<<(std::ostream &OS,
+                                  const struct InfoLoading &Rhs);
+
+  uint32_t Offset;
+};
+
+struct InfoAST {
+  InfoAST() = default;
+  InfoAST(const ASTNodeAttr Attr) noexcept : NodeAttr(Attr) {}
+
+  friend std::ostream &operator<<(std::ostream &OS, const struct InfoAST &Rhs);
+
+  ASTNodeAttr NodeAttr;
+};
+
+struct InfoRegistering {
+  InfoRegistering() = default;
+  InfoRegistering(std::string_view Mod) noexcept : ModName(Mod) {}
+
+  friend std::ostream &operator<<(std::ostream &OS,
+                                  const struct InfoRegistering &Rhs);
+
+  std::string ModName;
+};
+
+struct InfoLinking {
+  InfoLinking() = default;
+  InfoLinking(std::string_view Mod, std::string_view Ext,
+              const ExternalType ExtT = ExternalType::Function) noexcept
+      : ModName(Mod), ExtName(Ext), ExtType(ExtT) {}
+
+  friend std::ostream &operator<<(std::ostream &OS,
+                                  const struct InfoLinking &Rhs);
+
+  std::string ModName;
+  std::string ExtName;
+  ExternalType ExtType;
+};
+
+struct InfoExecuting {
+  InfoExecuting() = default;
+  InfoExecuting(std::string_view Mod, std::string_view Func) noexcept
+      : ModName(Mod), FuncName(Func) {}
+  InfoExecuting(std::string_view Func) noexcept : ModName(""), FuncName(Func) {}
+
+  friend std::ostream &operator<<(std::ostream &OS,
+                                  const struct InfoExecuting &Rhs);
+
+  std::string ModName;
+  std::string FuncName;
+};
+
+struct InfoMismatch {
+  InfoMismatch() = default;
+  /// Case 1: unexpected external types
+  InfoMismatch(const ExternalType ExpExt, const ExternalType GotExt) noexcept
+      : Category(InstCategory::ExternalType), ExpExtType(ExpExt),
+        GotExtType(GotExt) {}
+
+  /// Case 2: unexpected function types
+  InfoMismatch(const std::vector<ValType> &ExpP,
+               const std::vector<ValType> &ExpR,
+               const std::vector<ValType> &GotP,
+               const std::vector<ValType> &GotR) noexcept
+      : Category(InstCategory::FunctionType), ExpParams(ExpP), ExpReturns(ExpR),
+        GotParams(GotP), GotReturns(GotR) {}
+
+  /// Case 3: unexpected table types
+  InfoMismatch(const ElemType ExpEType, /// Element type
+               const bool ExpHasMax, const uint32_t ExpMin,
+               const uint32_t ExpMax,   /// Expect Limit
+               const ElemType GotEType, /// Got element type
+               const bool GotHasMax, const uint32_t GotMin,
+               const uint32_t GotMax /// Got limit
+               ) noexcept
+      : Category(InstCategory::Table), ExpElemType(ExpEType),
+        ExpLimHasMax(ExpHasMax), ExpLimMin(ExpMin), ExpLimMax(ExpMax),
+        GotElemType(GotEType), GotLimHasMax(GotHasMax), GotLimMin(GotMin),
+        GotLimMax(GotMax) {}
+
+  /// Case 4: unexpected memory limits
+  InfoMismatch(const bool ExpHasMax, const uint32_t ExpMin,
+               const uint32_t ExpMax, /// Expect Limit
+               const bool GotHasMax, const uint32_t GotMin,
+               const uint32_t GotMax /// Got limit
+               ) noexcept
+      : Category(InstCategory::Memory), ExpLimHasMax(ExpHasMax),
+        ExpLimMin(ExpMin), ExpLimMax(ExpMax), GotLimHasMax(GotHasMax),
+        GotLimMin(GotMin), GotLimMax(GotMax) {}
+
+  /// Case 5: unexpected global types
+  InfoMismatch(const ValType ExpVType, const ValMut ExpVMut,
+               const ValType GotVType, const ValMut GotVMut) noexcept
+      : Category(InstCategory::Global), ExpValType(ExpVType),
+        ExpValMut(ExpVMut), GotValType(GotVType), GotValMut(GotVMut) {}
+
+  /// Case 6: unexpected version
+  InfoMismatch(const uint32_t ExpV, const uint32_t GotV) noexcept
+      : Category(InstCategory::Version), ExpVersion(ExpV), GotVersion(GotV) {}
+
+  friend std::ostream &operator<<(std::ostream &OS,
+                                  const struct InfoMismatch &Rhs);
+
+  /// Mismatched category
+  InstCategory Category;
+
+  /// Case 1: unexpected external type
+  ExternalType ExpExtType, GotExtType;
+
+  /// Case 2: unexpected function type
+  std::vector<ValType> ExpParams, GotParams;
+  std::vector<ValType> ExpReturns, GotReturns;
+
+  /// Case 3 & 4: unexpected table or memory limit
+  ElemType ExpElemType, GotElemType;
+  bool ExpLimHasMax, GotLimHasMax;
+  uint32_t ExpLimMin, GotLimMin;
+  uint32_t ExpLimMax, GotLimMax;
+
+  /// Case 5: unexpected global type
+  ValType ExpValType, GotValType;
+  ValMut ExpValMut, GotValMut;
+
+  /// Case 6: unexpected version
+  uint32_t ExpVersion, GotVersion;
+};
+
+struct InfoInstruction {
+  InfoInstruction() = default;
+  InfoInstruction(const OpCode Op, const uint32_t Off,
+                  const std::vector<ValVariant> &ArgsVec = {},
+                  const std::vector<ValType> &ArgsTypesVec = {},
+                  const bool Signed = false) noexcept
+      : Code(Op), Offset(Off), Args(ArgsVec), ArgsTypes(ArgsTypesVec),
+        IsSigned(Signed) {}
+
+  friend std::ostream &operator<<(std::ostream &OS,
+                                  const struct InfoInstruction &Rhs);
+
+  OpCode Code;
+  uint32_t Offset;
+  std::vector<ValVariant> Args;
+  std::vector<ValType> ArgsTypes;
+  bool IsSigned;
+};
+
+struct InfoBoundary {
+  InfoBoundary() = default;
+  InfoBoundary(
+      const uint64_t Off, const uint32_t Len = 0,
+      const uint32_t Lim = std::numeric_limits<uint32_t>::max()) noexcept
+      : Offset(Off), Size(Len), Limit(Lim) {}
+
+  friend std::ostream &operator<<(std::ostream &OS,
+                                  const struct InfoBoundary &Rhs);
+
+  uint64_t Offset;
+  uint32_t Size;
+  uint32_t Limit;
+};
+
+} // namespace ErrInfo
+} // namespace SSVM
--- lib/support/log.cpp
+++ lib/support/log.cpp
@@ -1,4 +1,8 @@
+#include "common/errinfo.h"
+
+#include "support/hexstr.h"
 #include "support/log.h"
+
 #include "easyloggingpp/easylogging++.h"
 
 INITIALIZE_EASYLOGGINGPP
@@ -16,4 +20,157 @@ void setErrorLoggingLevel() {
 }
 
 } // namespace Log
+
+namespace ErrInfo {
+std::ostream &operator<<(std::ostream &OS, const struct InfoFile &Rhs) {
+  if (Rhs.FileName != "") {
+    OS << "    File name: " << Rhs.FileName;
+  }
+  return OS;
+}
+
+std::ostream &operator<<(std::ostream &OS, const struct InfoLoading &Rhs) {
+  OS << "    Bytecode offset: " << Support::convertUIntToHexStr(Rhs.Offset);
+  return OS;
+}
+
+std::ostream &operator<<(std::ostream &OS, const struct InfoAST &Rhs) {
+  OS << "    At AST node: " << ASTNodeAttrStr[Rhs.NodeAttr];
+  return OS;
+}
+
+std::ostream &operator<<(std::ostream &OS, const struct InfoRegistering &Rhs) {
+  OS << "    Module name: \"" << Rhs.ModName << "\"";
+  return OS;
+}
+
+std::ostream &operator<<(std::ostream &OS, const struct InfoLinking &Rhs) {
+  OS << "    When linking module: \"" << Rhs.ModName << "\" , "
+     << ExternalTypeStr[Rhs.ExtType] << " name: \"" << Rhs.ExtName << "\"";
+  return OS;
+}
+
+std::ostream &operator<<(std::ostream &OS, const struct InfoExecuting &Rhs) {
+  OS << "    When executing ";
+  if (Rhs.ModName != "") {
+    OS << "module name: \"" << Rhs.ModName << "\" , ";
+  }
+  OS << "function name: \"" << Rhs.FuncName << "\"";
+  return OS;
+}
+
+std::ostream &operator<<(std::ostream &OS, const struct InfoMismatch &Rhs) {
+  OS << "    Mismatched " << InstCategoryStr[Rhs.Category] << ". ";
+  switch (Rhs.Category) {
+  case InstCategory::ExternalType:
+    OS << "Expected: " << ExternalTypeStr[Rhs.ExpExtType]
+       << " , Got: " << ExternalTypeStr[Rhs.GotExtType];
+    break;
+  case InstCategory::FunctionType:
+    OS << "Expected: params{";
+    for (uint32_t I = 0; I < Rhs.ExpParams.size(); ++I) {
+      OS << ValTypeStr[Rhs.ExpParams[I]];
+      if (I < Rhs.ExpParams.size() - 1) {
+        OS << " , ";
+      }
+    }
+    OS << "} returns{";
+    for (uint32_t I = 0; I < Rhs.ExpReturns.size(); ++I) {
+      OS << ValTypeStr[Rhs.ExpReturns[I]];
+      if (I < Rhs.ExpReturns.size() - 1) {
+        OS << " , ";
+      }
+    }
+    OS << "} , Got: params{";
+    for (uint32_t I = 0; I < Rhs.GotParams.size(); ++I) {
+      OS << ValTypeStr[Rhs.GotParams[I]];
+      if (I < Rhs.GotParams.size() - 1) {
+        OS << " , ";
+      }
+    }
+    OS << "} returns{";
+    for (uint32_t I = 0; I < Rhs.GotReturns.size(); ++I) {
+      OS << ValTypeStr[Rhs.GotReturns[I]];
+      if (I < Rhs.GotReturns.size() - 1) {
+        OS << " , ";
+      }
+    }
+    OS << "}";
+    break;
+  case InstCategory::Table:
+  case InstCategory::Memory:
+    OS << "Expected: limit {" << Rhs.ExpLimMin;
+    if (Rhs.ExpLimHasMax) {
+      OS << " , " << Rhs.ExpLimMax;
+    }
+    OS << "} , Got: limit {" << Rhs.GotLimMin;
+    if (Rhs.GotLimHasMax) {
+      OS << " , " << Rhs.GotLimMax;
+    }
+    OS << "}";
+    break;
+  case InstCategory::Global:
+    OS << "Expected: global type {" << ValMutStr[Rhs.ExpValMut] << " "
+       << ValTypeStr[Rhs.ExpValType] << "} , Got: global type {"
+       << ValMutStr[Rhs.GotValMut] << " " << ValTypeStr[Rhs.GotValType] << "}";
+    break;
+  case InstCategory::Version:
+    OS << "Expected: " << Rhs.ExpVersion << " , Got: " << Rhs.GotVersion;
+    break;
+  default:
+    break;
+  }
+  return OS;
+}
+
+std::ostream &operator<<(std::ostream &OS, const struct InfoInstruction &Rhs) {
+  OS << "    In instruction: " << OpCodeStr[Rhs.Code] << " ("
+     << Support::convertUIntToHexStr(uint32_t(Rhs.Code), 2)
+     << ") , Bytecode offset: " << Support::convertUIntToHexStr(Rhs.Offset);
+  if (Rhs.Args.size() > 0) {
+    OS << " , Args: [";
+    for (uint32_t I = 0; I < Rhs.Args.size(); ++I) {
+      switch (Rhs.ArgsTypes[I]) {
+      case ValType::I32:
+        if (Rhs.IsSigned) {
+          OS << retrieveValue<int32_t>(Rhs.Args[I]);
+        } else {
+          OS << retrieveValue<uint32_t>(Rhs.Args[I]);
+        }
+        break;
+      case ValType::I64:
+        if (Rhs.IsSigned) {
+          OS << retrieveValue<int64_t>(Rhs.Args[I]);
+        } else {
+          OS << retrieveValue<uint64_t>(Rhs.Args[I]);
+        }
+        break;
+      case ValType::F32:
+        OS << retrieveValue<float>(Rhs.Args[I]);
+        break;
+      case ValType::F64:
+        OS << retrieveValue<double>(Rhs.Args[I]);
+        break;
+      default:
+        break;
+      }
+      if (I < Rhs.Args.size() - 1) {
+        OS << " , ";
+      }
+    }
+    OS << "]";
+  }
+  return OS;
+}
+
+std::ostream &operator<<(std::ostream &OS, const struct InfoBoundary &Rhs) {
+  OS << "    Accessing offset from: "
+     << Support::convertUIntToHexStr(Rhs.Offset) << " to: "
+     << Support::convertUIntToHexStr(Rhs.Offset + Rhs.Size -
+                                     (Rhs.Size > 0 ? 1 : 0))
+     << " , Out of boundary: " << Support::convertUIntToHexStr(Rhs.Limit);
+  return OS;
+}
+
+} // namespace ErrInfo
 } // namespace SSVM

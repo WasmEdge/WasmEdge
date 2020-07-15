2af491d199844a83a229f13b4e56b7a712462156
YiYing He <yiying@secondstate.io>
2020-07-15 16:25:25 +0800

    [Common] Add error codes and error info of validation.

--- include/common/errinfo.h
+++ include/common/errinfo.h
@@ -26,15 +26,19 @@ namespace ErrInfo {
 
 /// Error info type enumeration class.
 enum class InfoType : uint8_t {
-  File,        /// Information about file name which loading from
-  Loading,     /// Information about bytecode offset
-  AST,         /// Information about tracing AST nodes
-  Registering, /// Information about instantiating modules
-  Linking,     /// Information about linking instances
-  Executing,   /// Information about running functions
-  Mismatch,    /// Information about comparison of instances when error
-  Instruction, /// Information about aborted instructions and parameters
-  Boundary     /// Information about forbidden offset accessing
+  File,          /// Information about file name which loading from
+  Loading,       /// Information about bytecode offset
+  AST,           /// Information about tracing AST nodes
+  InstanceBound, /// Information about over boundary of limited #instances
+  ForbidIndex,   /// Information about forbidden accessing of indices
+  Exporting,     /// Information about exporting instances
+  Limit,         /// Information about Limit value
+  Registering,   /// Information about instantiating modules
+  Linking,       /// Information about linking instances
+  Executing,     /// Information about running functions
+  Mismatch,      /// Information about comparison error
+  Instruction,   /// Information about aborted instructions and parameters
+  Boundary       /// Information about forbidden offset accessing
 };
 
 /// Instance addressing type enumeration class.
@@ -46,8 +50,12 @@ enum class PtrType : uint8_t {
 static inline std::unordered_map<PtrType, std::string> PtrTypeStr = {
     {PtrType::Index, "index"}, {PtrType::Address, "address"}};
 
-/// Wasm instance category.
-enum class InstCategory : uint8_t {
+/// Mismatch category.
+enum class MismatchCategory : uint8_t {
+  Alignment,    /// Alignment in memory instructions
+  ValueType,    /// Value type
+  ValueTypes,   /// Value type list
+  Mutation,     /// Const or Var
   ExternalType, /// External typing
   FunctionType, /// Function type
   Table,        /// Table instance
@@ -56,13 +64,37 @@ enum class InstCategory : uint8_t {
   Version       /// Versions
 };
 
-static inline std::unordered_map<InstCategory, std::string> InstCategoryStr = {
-    {InstCategory::ExternalType, "external type"},
-    {InstCategory::FunctionType, "function type"},
-    {InstCategory::Table, "table"},
-    {InstCategory::Memory, "memory"},
-    {InstCategory::Global, "global"},
-    {InstCategory::Version, "version"}};
+static inline std::unordered_map<MismatchCategory, std::string>
+    MismatchCategoryStr = {{MismatchCategory::Alignment, "memory alignment"},
+                           {MismatchCategory::ValueType, "value type"},
+                           {MismatchCategory::ValueTypes, "value types"},
+                           {MismatchCategory::Mutation, "mutation"},
+                           {MismatchCategory::ExternalType, "external type"},
+                           {MismatchCategory::FunctionType, "function type"},
+                           {MismatchCategory::Table, "table"},
+                           {MismatchCategory::Memory, "memory"},
+                           {MismatchCategory::Global, "global"},
+                           {MismatchCategory::Version, "version"}};
+
+/// Wasm index category.
+enum class IndexCategory : uint8_t {
+  Label,
+  Local,
+  FunctionType,
+  Function,
+  Table,
+  Memory,
+  Global
+};
+
+static inline std::unordered_map<IndexCategory, std::string> IndexCategoryStr =
+    {{IndexCategory::Label, "label"},
+     {IndexCategory::Local, "local"},
+     {IndexCategory::FunctionType, "function type"},
+     {IndexCategory::Function, "function"},
+     {IndexCategory::Table, "table"},
+     {IndexCategory::Memory, "memory"},
+     {IndexCategory::Global, "global"}};
 
 /// Information structures.
 struct InfoFile {
@@ -91,6 +123,55 @@ struct InfoAST {
   friend std::ostream &operator<<(std::ostream &OS, const struct InfoAST &Rhs);
 
   ASTNodeAttr NodeAttr;
+};
+
+struct InfoInstanceBound {
+  InfoInstanceBound() = default;
+  InfoInstanceBound(const ExternalType Inst, const uint32_t Num,
+                    const uint32_t Lim) noexcept
+      : Instance(Inst), Number(Num), Limited(Lim) {}
+
+  friend std::ostream &operator<<(std::ostream &OS,
+                                  const struct InfoInstanceBound &Rhs);
+
+  ExternalType Instance;
+  uint32_t Number, Limited;
+};
+
+struct InfoForbidIndex {
+  InfoForbidIndex() = default;
+  InfoForbidIndex(const IndexCategory Cate, const uint32_t Idx,
+                  const uint32_t Bound) noexcept
+      : Category(Cate), Index(Idx), Boundary(Bound) {}
+
+  friend std::ostream &operator<<(std::ostream &OS,
+                                  const struct InfoForbidIndex &Rhs);
+
+  IndexCategory Category;
+  uint32_t Index, Boundary;
+};
+
+struct InfoExporting {
+  InfoExporting() = default;
+  InfoExporting(std::string_view Ext) noexcept : ExtName(Ext) {}
+
+  friend std::ostream &operator<<(std::ostream &OS,
+                                  const struct InfoExporting &Rhs);
+
+  std::string ExtName;
+};
+
+struct InfoLimit {
+  InfoLimit() = default;
+  InfoLimit(const bool HasMax, const uint32_t Min,
+            const uint32_t Max = 0) noexcept
+      : LimHasMax(HasMax), LimMin(Min), LimMax(Max) {}
+
+  friend std::ostream &operator<<(std::ostream &OS,
+                                  const struct InfoLimit &Rhs);
+
+  bool LimHasMax;
+  uint32_t LimMin, LimMax;
 };
 
 struct InfoRegistering {
@@ -132,20 +213,42 @@ struct InfoExecuting {
 
 struct InfoMismatch {
   InfoMismatch() = default;
-  /// Case 1: unexpected external types
+
+  /// Case 1: unexpected alignment
+  InfoMismatch(const uint8_t ExpAlign, const uint32_t GotAlign) noexcept
+      : Category(MismatchCategory::Alignment), ExpAlignment(ExpAlign),
+        GotAlignment(GotAlign) {}
+
+  /// Case 2: unexpected value type
+  InfoMismatch(const ValType ExpVT, const ValType GotVT) noexcept
+      : Category(MismatchCategory::ValueType), ExpValType(ExpVT),
+        GotValType(GotVT) {}
+
+  /// Case 3: unexpected value type list
+  InfoMismatch(const std::vector<ValType> &ExpV,
+               const std::vector<ValType> &GotV) noexcept
+      : Category(MismatchCategory::ValueTypes), ExpParams(ExpV),
+        GotParams(GotV) {}
+
+  /// Case 4: unexpected mutation settings
+  InfoMismatch(const ValMut ExpM, const ValMut GotM) noexcept
+      : Category(MismatchCategory::Mutation), ExpValMut(ExpM), GotValMut(GotM) {
+  }
+
+  /// Case 5: unexpected external types
   InfoMismatch(const ExternalType ExpExt, const ExternalType GotExt) noexcept
-      : Category(InstCategory::ExternalType), ExpExtType(ExpExt),
+      : Category(MismatchCategory::ExternalType), ExpExtType(ExpExt),
         GotExtType(GotExt) {}
 
-  /// Case 2: unexpected function types
+  /// Case 6: unexpected function types
   InfoMismatch(const std::vector<ValType> &ExpP,
                const std::vector<ValType> &ExpR,
                const std::vector<ValType> &GotP,
                const std::vector<ValType> &GotR) noexcept
-      : Category(InstCategory::FunctionType), ExpParams(ExpP), GotParams(GotP),
-        ExpReturns(ExpR), GotReturns(GotR) {}
+      : Category(MismatchCategory::FunctionType), ExpParams(ExpP),
+        GotParams(GotP), ExpReturns(ExpR), GotReturns(GotR) {}
 
-  /// Case 3: unexpected table types
+  /// Case 7: unexpected table types
   InfoMismatch(const ElemType ExpEType, /// Element type
                const bool ExpHasMax, const uint32_t ExpMin,
                const uint32_t ExpMax,   /// Expect Limit
@@ -153,55 +256,64 @@ struct InfoMismatch {
                const bool GotHasMax, const uint32_t GotMin,
                const uint32_t GotMax /// Got limit
                ) noexcept
-      : Category(InstCategory::Table), ExpElemType(ExpEType),
+      : Category(MismatchCategory::Table), ExpElemType(ExpEType),
         GotElemType(GotEType), ExpLimHasMax(ExpHasMax), GotLimHasMax(GotHasMax),
         ExpLimMin(ExpMin), GotLimMin(GotMin), ExpLimMax(ExpMax),
         GotLimMax(GotMax) {}
 
-  /// Case 4: unexpected memory limits
+  /// Case 8: unexpected memory limits
   InfoMismatch(const bool ExpHasMax, const uint32_t ExpMin,
                const uint32_t ExpMax, /// Expect Limit
                const bool GotHasMax, const uint32_t GotMin,
                const uint32_t GotMax /// Got limit
                ) noexcept
-      : Category(InstCategory::Memory), ExpLimHasMax(ExpHasMax),
+      : Category(MismatchCategory::Memory), ExpLimHasMax(ExpHasMax),
         GotLimHasMax(GotHasMax), ExpLimMin(ExpMin), GotLimMin(GotMin),
         ExpLimMax(ExpMax), GotLimMax(GotMax) {}
 
-  /// Case 5: unexpected global types
+  /// Case 9: unexpected global types
   InfoMismatch(const ValType ExpVType, const ValMut ExpVMut,
                const ValType GotVType, const ValMut GotVMut) noexcept
-      : Category(InstCategory::Global), ExpValType(ExpVType),
+      : Category(MismatchCategory::Global), ExpValType(ExpVType),
         GotValType(GotVType), ExpValMut(ExpVMut), GotValMut(GotVMut) {}
 
-  /// Case 6: unexpected version
+  /// Case 10: unexpected version
   InfoMismatch(const uint32_t ExpV, const uint32_t GotV) noexcept
-      : Category(InstCategory::Version), ExpVersion(ExpV), GotVersion(GotV) {}
+      : Category(MismatchCategory::Version), ExpVersion(ExpV),
+        GotVersion(GotV) {}
 
   friend std::ostream &operator<<(std::ostream &OS,
                                   const struct InfoMismatch &Rhs);
 
   /// Mismatched category
-  InstCategory Category;
+  MismatchCategory Category;
 
-  /// Case 1: unexpected external type
+  /// Case 1: unexpected alignment
+  uint8_t ExpAlignment;
+  uint32_t GotAlignment;
+
+  /// Case 5: unexpected external type
   ExternalType ExpExtType, GotExtType;
 
-  /// Case 2: unexpected function type
+  /// Case 6: unexpected function type
+  /// Case 3: unexpected value type list
   std::vector<ValType> ExpParams, GotParams;
   std::vector<ValType> ExpReturns, GotReturns;
 
-  /// Case 3 & 4: unexpected table or memory limit
+  /// Case 7 & 8: unexpected table or memory limit
   ElemType ExpElemType, GotElemType;
   bool ExpLimHasMax, GotLimHasMax;
   uint32_t ExpLimMin, GotLimMin;
   uint32_t ExpLimMax, GotLimMax;
 
-  /// Case 5: unexpected global type
+  /// Case 2: unexpected value type
+  /// Case 9: unexpected global type: value type
   ValType ExpValType, GotValType;
+  /// Case 4: unexpected mutation settings
+  /// Case 9: unexpected global type: value mutation
   ValMut ExpValMut, GotValMut;
 
-  /// Case 6: unexpected version
+  /// Case 10: unexpected version
   uint32_t ExpVersion, GotVersion;
 };
 

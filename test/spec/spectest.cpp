255812a03431532be622cdc877642f9157b1957a
YiYing He <yiying@secondstate.io>
2020-09-30 17:00:13 +0800

    [Test] Split core test to helper class

---
+++ test/spec/spectest.cpp
@@ -0,0 +1,323 @@
+// SPDX-License-Identifier: Apache-2.0
+//===-- ssvm/test/spec/spectest.cpp - Wasm test suites --------------------===//
+//
+// Part of the SSVM Project.
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file parse and run tests of Wasm test suites extracted by wast2json.
+/// Test Suits: https://github.com/WebAssembly/spec/tree/master/test/core
+/// wast2json: https://webassembly.github.io/wabt/doc/wast2json.1.html
+///
+//===----------------------------------------------------------------------===//
+
+#define RAPIDJSON_HAS_STDSTRING 1
+#if defined(__SSE4_2__)
+#define RAPIDJSON_SSE42 1
+#elif defined(__SSE2__)
+#define RAPIDJSON_SSE2 1
+#elif defined(__ARM_NEON__) || defined(__ARM_NEON) || defined(__ARM_NEON_FP)
+#define RAPIDJSON_NEON 1
+#endif
+
+#include "spectest.h"
+#include "rapidjson/document.h"
+#include "rapidjson/istreamwrapper.h"
+#include "gtest/gtest.h"
+
+namespace {
+
+using namespace std::literals;
+using namespace SSVM;
+
+/// Preprocessing for set up aliasing.
+void resolveRegister(std::map<std::string, std::string> &Alias,
+                     rapidjson::Value &CmdArray,
+                     rapidjson::Document::AllocatorType &Allocator) {
+  rapidjson::Value::ValueIterator ItMod;
+  for (rapidjson::Value::ValueIterator It = CmdArray.Begin();
+       It != CmdArray.End(); ++It) {
+    const auto CmdType = It->GetObject()["type"].Get<std::string>();
+    if (CmdType == "module"sv) {
+      /// Record last module in order.
+      ItMod = It;
+    } else if (CmdType == "register"sv) {
+      const auto NewName = It->GetObject()["as"].Get<std::string>();
+      if (It->GetObject().HasMember("name")) {
+        /// Set aliasing.
+        Alias.emplace(It->GetObject()["name"].Get<std::string>(), NewName);
+      }
+      if (auto Name = ItMod->FindMember("name"); Name != ItMod->MemberEnd()) {
+        /// Module has origin name. Replace to aliased one.
+        Name->value.SetString(NewName, Allocator);
+      } else {
+        /// Module has no origin name. Add the aliased one.
+        rapidjson::Value Text;
+        Text.SetString(NewName, Allocator);
+        ItMod->AddMember("name", Text, Allocator);
+      }
+    }
+  }
+}
+
+SpecTest::CommandID resolveCommand(std::string_view Name) {
+  static const std::unordered_map<std::string_view, SpecTest::CommandID>
+      CommandMapping = {
+          {"module"sv, SpecTest::CommandID::Module},
+          {"action"sv, SpecTest::CommandID::Action},
+          {"register"sv, SpecTest::CommandID::Register},
+          {"assert_return"sv, SpecTest::CommandID::AssertReturn},
+          {"assert_trap"sv, SpecTest::CommandID::AssertTrap},
+          {"assert_exhaustion"sv, SpecTest::CommandID::AssertExhaustion},
+          {"assert_malformed"sv, SpecTest::CommandID::AssertMalformed},
+          {"assert_invalid"sv, SpecTest::CommandID::AssertInvalid},
+          {"assert_unlinkable"sv, SpecTest::CommandID::AssertUnlinkable},
+          {"assert_uninstantiable"sv,
+           SpecTest::CommandID::AssertUninstantiable},
+      };
+  if (auto Iter = CommandMapping.find(Name); Iter != CommandMapping.end()) {
+    return Iter->second;
+  }
+  return SpecTest::CommandID::Unknown;
+}
+
+/// Helper function to parse parameters from json to vector of value.
+std::vector<SSVM::ValVariant> parseValueList(const rapidjson::Value &Args) {
+  std::vector<SSVM::ValVariant> Result;
+  Result.reserve(Args.Size());
+  for (const auto &Element : Args.GetArray()) {
+    const auto Type = Element["type"].Get<std::string>();
+    const auto Value = Element["value"].Get<std::string>();
+    if (Type == "i32"sv || Type == "f32"sv) {
+      Result.emplace_back(uint32_t(std::stoul(Value)));
+    } else if (Type == "i64"sv || Type == "f64"sv) {
+      Result.emplace_back(uint64_t(std::stoull(Value)));
+    } else {
+      assert(false);
+    }
+  }
+  return Result;
+}
+
+/// Helper function to parse parameters from json to vector of string pair.
+std::vector<std::pair<std::string, std::string>>
+parseExpectedList(const rapidjson::Value &Args) {
+  std::vector<std::pair<std::string, std::string>> Result;
+  Result.reserve(Args.Size());
+  for (const auto &Element : Args.GetArray()) {
+    Result.emplace_back(Element["type"].Get<std::string>(),
+                        Element["value"].Get<std::string>());
+  }
+  return Result;
+}
+
+} // namespace
+
+namespace SSVM {
+
+std::vector<std::string> SpecTest::enumerate() {
+  std::vector<std::string> Cases;
+  for (const auto &Subdir :
+       std::filesystem::directory_iterator(TestsuiteRoot)) {
+    const auto SubdirPath = Subdir.path();
+    const auto UnitName = SubdirPath.filename().u8string();
+    const auto UnitJson = UnitName + ".json"s;
+    if (std::filesystem::is_regular_file(SubdirPath / UnitJson)) {
+      Cases.push_back(std::move(UnitName));
+    }
+  }
+  std::sort(Cases.begin(), Cases.end());
+
+  return Cases;
+}
+
+void SpecTest::run(std::string UnitName) {
+  LOG(INFO) << UnitName;
+  std::ifstream JSONIS(TestsuiteRoot / UnitName / (UnitName + ".json"s));
+  rapidjson::IStreamWrapper JSONISWrapper(JSONIS);
+  rapidjson::Document Doc;
+  auto &Allocator = Doc.GetAllocator();
+  Doc.ParseStream(JSONISWrapper);
+
+  std::map<std::string, std::string> Alias;
+  std::string LastModName;
+
+  /// Helper function to get module name.
+  auto GetModuleName = [&](const rapidjson::Value &Action) -> std::string {
+    if (const auto &Module = Action.FindMember("module"s);
+        Module != Action.MemberEnd()) {
+      /// Get the module name.
+      auto ModName = Module->value.Get<std::string>();
+      if (auto It = Alias.find(ModName); It != Alias.end()) {
+        /// If module name is aliased, use the aliased name.
+        return It->second;
+      }
+      return ModName;
+    }
+    return LastModName;
+  };
+
+  auto Invoke = [&](const rapidjson::Value &Action,
+                    const rapidjson::Value &Expected) {
+    const auto ModName = GetModuleName(Action);
+    const auto Field = Action["field"s].Get<std::string>();
+    const auto Params = parseValueList(Action["args"s]);
+    const auto Returns = parseExpectedList(Expected);
+
+    /// Invoke function of named module. Named modules are registered in Store
+    /// Manager. Anonymous modules are instantiated in VM.
+    if (auto Res = onInvoke(ModName, Field, Params)) {
+      /// Check value.
+      EXPECT_TRUE(onCompare(Returns, *Res));
+    } else {
+      EXPECT_TRUE(false);
+    }
+  };
+  /// Helper function to get values.
+  auto Get = [&](const rapidjson::Value &Action,
+                 const rapidjson::Value &Expected) {
+    const auto ModName = GetModuleName(Action);
+    const auto Field = Action["field"s].Get<std::string>();
+    const auto Returns = parseExpectedList(Expected);
+
+    if (auto Res = onGet(ModName, Field)) {
+      /// Check value.
+      EXPECT_TRUE(onCompare(Returns, *Res));
+    } else {
+      EXPECT_TRUE(false);
+    }
+  };
+  auto TrapInvoke = [&](const rapidjson::Value &Action,
+                        const std::string &Text) {
+    const auto ModName = GetModuleName(Action);
+    const auto Field = Action["field"s].Get<std::string>();
+    const auto Params = parseValueList(Action["args"s]);
+
+    if (auto Res = onInvoke(ModName, Field, Params)) {
+      EXPECT_TRUE(false);
+    } else {
+      /// Check value.
+      EXPECT_TRUE(onStringContains(Text, SSVM::ErrCodeStr[Res.error()]));
+    }
+  };
+  auto TrapValidate = [&](const std::string &Filename,
+                          const std::string &Text) {
+    if (auto Res = onValidate(Filename); Res) {
+      EXPECT_TRUE(false);
+    } else {
+      EXPECT_TRUE(onStringContains(Text, SSVM::ErrCodeStr[Res.error()]));
+    }
+  };
+  auto TrapInstantiate = [&](const std::string &Filename,
+                             const std::string &Text) {
+    if (auto Res = onInstantiate(Filename); Res) {
+      EXPECT_TRUE(false);
+    } else {
+      EXPECT_TRUE(onStringContains(Text, SSVM::ErrCodeStr[Res.error()]));
+    }
+  };
+
+  /// Command processing. Return true for expected result.
+  auto RunCommand = [&](const rapidjson::Value &Cmd) {
+    /// Line number in wast: It->GetObject()["line"].GetInt()
+    if (const auto Type = Cmd.FindMember("type"s); Type != Cmd.MemberEnd()) {
+      switch (resolveCommand(Type->value.Get<std::string>())) {
+      case SpecTest::CommandID::Module: {
+        const auto FileName =
+            (TestsuiteRoot / UnitName / Cmd["filename"].Get<std::string>())
+                .u8string();
+        if (const auto Name = Cmd.FindMember("name"); Name != Cmd.MemberEnd()) {
+          /// Module has name. Register module with module name.
+          LastModName = Name->value.Get<std::string>();
+        } else {
+          /// Instantiate the anonymous module.
+          LastModName.clear();
+        }
+        if (onModule(LastModName, FileName)) {
+          EXPECT_TRUE(true);
+        } else {
+          EXPECT_TRUE(false);
+        }
+        return;
+      }
+      case CommandID::Action: {
+        const auto &Action = Cmd["action"s];
+        const auto &Expected = Cmd["expected"s];
+        Invoke(Action, Expected);
+        return;
+      }
+      case CommandID::Register: {
+        /// Preprocessed. Ignore this.
+        return;
+      }
+      case CommandID::AssertReturn: {
+        const auto &Action = Cmd["action"s];
+        const auto &Expected = Cmd["expected"s];
+        const auto ActType = Action["type"].Get<std::string>();
+        if (ActType == "invoke"sv) {
+          Invoke(Action, Expected);
+          return;
+        } else if (ActType == "get"sv) {
+          Get(Action, Expected);
+          return;
+        }
+        EXPECT_TRUE(false);
+        return;
+      }
+      case CommandID::AssertTrap: {
+        const auto &Action = Cmd["action"s];
+        const auto &Text = Cmd["text"s].Get<std::string>();
+        TrapInvoke(Action, Text);
+        return;
+      }
+      case CommandID::AssertExhaustion: {
+        /// TODO: Add stack overflow mechanism.
+        return;
+      }
+      case CommandID::AssertMalformed: {
+        /// TODO: Wat is not supported in SSVM yet.
+        /// TODO: Add processing binary cases.
+        return;
+      }
+      case CommandID::AssertInvalid: {
+        const auto Filename =
+            (TestsuiteRoot / UnitName / Cmd["filename"s].Get<std::string>())
+                .u8string();
+        const auto &Text = Cmd["text"s].Get<std::string>();
+        TrapValidate(Filename, Text);
+        return;
+      }
+      case CommandID::AssertUnlinkable:
+      case CommandID::AssertUninstantiable: {
+        const auto Filename =
+            (TestsuiteRoot / UnitName / Cmd["filename"s].Get<std::string>())
+                .u8string();
+        const auto &Text = Cmd["text"s].Get<std::string>();
+        TrapInstantiate(Filename, Text);
+        return;
+      }
+      default:;
+      }
+    }
+    /// Unknown command.
+    EXPECT_TRUE(false);
+  };
+
+  /// Get command list.
+  if (auto Commands = Doc.FindMember("commands"s);
+      Commands != Doc.MemberEnd()) {
+    rapidjson::Value CmdArray;
+    CmdArray.CopyFrom(Commands->value, Allocator);
+
+    /// Preprocessing register command.
+    resolveRegister(Alias, CmdArray, Allocator);
+
+    /// Iterate commands.
+    for (const auto &Cmd : CmdArray.GetArray()) {
+      RunCommand(Cmd);
+    }
+  }
+}
+
+} // namespace SSVM
--- test/spec/spectest.h
+++ test/spec/spectest.h
@@ -1,13 +1,29 @@
 // SPDX-License-Identifier: Apache-2.0
+//===-- ssvm/test/spec/spectest.h - Wasm test suites ----------------------===//
+//
+// Part of the SSVM Project.
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file parse and run tests of Wasm test suites extracted by wast2json.
+/// Test Suits: https://github.com/WebAssembly/spec/tree/master/test/core
+/// wast2json: https://webassembly.github.io/wabt/doc/wast2json.1.html
+///
+//===----------------------------------------------------------------------===//
+
 #pragma once
 
 #include "common/errcode.h"
 #include "runtime/hostfunc.h"
 #include "runtime/importobj.h"
 #include "runtime/instance/memory.h"
+#include "support/filesystem.h"
+#include <functional>
+#include <string_view>
+#include <vector>
 
 namespace SSVM {
-namespace Host {
 
 class SpecTestPrint : public Runtime::HostFunction<SpecTestPrint> {
 public:
@@ -85,5 +101,60 @@ public:
   virtual ~SpecTestModule() = default;
 };
 
-} // namespace Host
+class SpecTest {
+public:
+  enum class CommandID {
+    Unknown,
+    Module,
+    Action,
+    Register,
+    AssertReturn,
+    AssertTrap,
+    AssertExhaustion,
+    AssertMalformed,
+    AssertInvalid,
+    AssertUnlinkable,
+    AssertUninstantiable,
+  };
+
+  explicit SpecTest(std::filesystem::path Root)
+      : TestsuiteRoot(std::move(Root)) {}
+
+  std::vector<std::string> enumerate();
+
+  void run(std::string UnitName);
+
+  using ModuleCallback = Expect<void>(const std::string &Modname,
+                                      const std::string &Filename);
+  std::function<ModuleCallback> onModule;
+
+  using ValidateCallback = Expect<void>(const std::string &Filename);
+  std::function<ValidateCallback> onValidate;
+
+  using InstantiateCallback = Expect<void>(const std::string &Filename);
+  std::function<InstantiateCallback> onInstantiate;
+
+  using InvokeCallback = Expect<std::vector<ValVariant>>(
+      const std::string &ModName, const std::string &Field,
+      const std::vector<ValVariant> &Params);
+  std::function<InvokeCallback> onInvoke;
+
+  using GetCallback = Expect<std::vector<ValVariant>>(
+      const std::string &ModName, const std::string &Field);
+  std::function<GetCallback> onGet;
+
+/// Helper function to compare return values with expected values.
+  using CompareCallback =
+      bool(const std::vector<std::pair<std::string, std::string>> &Expected,
+           const std::vector<ValVariant> &Got);
+  std::function<CompareCallback> onCompare;
+
+  using StringContainsCallback = bool(const std::string &Expected,
+                                      const std::string &Got);
+  std::function<StringContainsCallback> onStringContains;
+
+private:
+  std::filesystem::path TestsuiteRoot;
+};
+
 } // namespace SSVM

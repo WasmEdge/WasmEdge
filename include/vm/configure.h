61427c1e75b5ee5bb358e924cc94887d90dd1e50
Shen-Ta Hsieh <beststeve@secondstate.io>
2020-10-20 09:17:50 +0000

    [VM] Use std::bitset for configure

--- include/vm/configure.h
+++ include/vm/configure.h
@@ -11,9 +11,9 @@
 //===----------------------------------------------------------------------===//
 #pragma once
 
+#include <bitset>
 #include <memory>
 #include <string>
-#include <unordered_set>
 
 namespace SSVM {
 namespace VM {
@@ -21,21 +21,19 @@ namespace VM {
 class Configure {
 public:
   /// VM type enum class.
-  enum class VMType : uint8_t { Wasm = 0, Wasi, SSVM_Process };
+  enum class VMType : uint8_t { Wasm = 0, Wasi, SSVM_Process, Max };
 
-  Configure() { Types.insert(VMType::Wasm); }
+  Configure() { addVMType(VMType::Wasm); }
   ~Configure() = default;
 
-  void addVMType(const VMType Type) { Types.insert(Type); }
+  void addVMType(const VMType Type) { Types.set(uint8_t(Type)); }
 
-  void removeVMType(const VMType Type) { Types.erase(Type); }
+  void removeVMType(const VMType Type) { Types.reset(uint8_t(Type)); }
 
-  bool hasVMType(const VMType Type) const {
-    return ((Types.find(Type) != Types.end()) ? true : false);
-  }
+  bool hasVMType(const VMType Type) const { return Types.test(uint8_t(Type)); }
 
 private:
-  std::unordered_set<VMType> Types;
+  std::bitset<uint8_t(VMType::Max)> Types;
 };
 
 } // namespace VM

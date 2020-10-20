95ad2ecc76e8d9ca9833ffe76285310645b0bef5
Shen-Ta Hsieh <beststeve@secondstate.io>
2020-10-20 09:34:18 +0000

    [VM] Use array for multiple CostTable storage

--- include/vm/costtable.h
+++ include/vm/costtable.h
@@ -12,7 +12,7 @@
 
 #include "configure.h"
 
-#include <unordered_map>
+#include <array>
 #include <vector>
 
 namespace SSVM {
@@ -25,38 +25,35 @@ public:
 
   /// Add default cost table by types.
   bool setCostTable(const Configure::VMType &Type) {
-    switch (Type) {
-    case Configure::VMType::Wasm:
-    case Configure::VMType::Wasi:
-    case Configure::VMType::SSVM_Process:
-      Costs[Type] = std::vector<uint64_t>(UINT16_MAX, 1);
-      return true;
-    default:
-      break;
+    if (Type >= Configure::VMType::Max) {
+      return false;
     }
-    return false;
+    Costs[uint8_t(Type)].assign(UINT16_MAX + 1, 1);
+    return true;
   }
 
   /// Set customized cost table.
-  bool setCostTable(const Configure::VMType &Type,
-                    const std::vector<uint64_t> &Table) {
-    if (Table.size() < UINT16_MAX) {
+  bool setCostTable(Configure::VMType Type, std::vector<uint64_t> Table) {
+    if (Table.size() <= UINT16_MAX) {
       return false;
     }
-    Costs[Type] = Table;
+    if (Type >= Configure::VMType::Max) {
+      Type = Configure::VMType::Wasm;
+    }
+    Costs[uint8_t(Type)] = std::move(Table);
     return true;
   }
 
   /// Get cost table reference by types.
-  const std::vector<uint64_t> &getCostTable(const Configure::VMType &Type) {
-    if (Costs.find(Type) != Costs.end()) {
-      return Costs.at(Type);
+  const std::vector<uint64_t> &getCostTable(Configure::VMType Type) {
+    if (Type >= Configure::VMType::Max) {
+      Type = Configure::VMType::Wasm;
     }
-    return Costs.at(Configure::VMType::Wasm);
+    return Costs[uint8_t(Type)];
   }
 
 private:
-  std::unordered_map<Configure::VMType, std::vector<uint64_t>> Costs;
+  std::array<std::vector<uint64_t>, uint8_t(Configure::VMType::Max)> Costs;
 };
 
 } // namespace VM

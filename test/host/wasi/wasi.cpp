b3001e87f3aa042bdc4445c0e631af355537495d
Shen-Ta Hsieh <beststeve@secondstate.io>
2020-11-26 14:27:13 +0000

    [WASI] Add testcase for environ and clock functions

--- test/host/wasi/wasi.cpp
+++ test/host/wasi/wasi.cpp
@@ -6,69 +6,500 @@
 #include <vector>
 using namespace std::literals;
 
+namespace {
+void writeDummyMemoryContent(
+    SSVM::Runtime::Instance::MemoryInstance &MemInst) noexcept {
+  std::fill_n(MemInst.getPointer<uint8_t *>(0), 0xa5, 64);
+}
+__wasi_errno_t convertErrno(int SysErrno) {
+  switch (SysErrno) {
+  case 0:
+    return __WASI_ESUCCESS;
+  case E2BIG:
+    return __WASI_E2BIG;
+  case EACCES:
+    return __WASI_EACCES;
+  case EADDRINUSE:
+    return __WASI_EADDRINUSE;
+  case EADDRNOTAVAIL:
+    return __WASI_EADDRNOTAVAIL;
+  case EAFNOSUPPORT:
+    return __WASI_EAFNOSUPPORT;
+  case EAGAIN:
+    return __WASI_EAGAIN;
+  case EALREADY:
+    return __WASI_EALREADY;
+  case EBADF:
+    return __WASI_EBADF;
+  case EBADMSG:
+    return __WASI_EBADMSG;
+  case EBUSY:
+    return __WASI_EBUSY;
+  case ECANCELED:
+    return __WASI_ECANCELED;
+  case ECHILD:
+    return __WASI_ECHILD;
+  case ECONNABORTED:
+    return __WASI_ECONNABORTED;
+  case ECONNREFUSED:
+    return __WASI_ECONNREFUSED;
+  case ECONNRESET:
+    return __WASI_ECONNRESET;
+  case EDEADLK:
+    return __WASI_EDEADLK;
+  case EDESTADDRREQ:
+    return __WASI_EDESTADDRREQ;
+  case EDOM:
+    return __WASI_EDOM;
+  case EDQUOT:
+    return __WASI_EDQUOT;
+  case EEXIST:
+    return __WASI_EEXIST;
+  case EFAULT:
+    return __WASI_EFAULT;
+  case EFBIG:
+    return __WASI_EFBIG;
+  case EHOSTUNREACH:
+    return __WASI_EHOSTUNREACH;
+  case EIDRM:
+    return __WASI_EIDRM;
+  case EILSEQ:
+    return __WASI_EILSEQ;
+  case EINPROGRESS:
+    return __WASI_EINPROGRESS;
+  case EINTR:
+    return __WASI_EINTR;
+  case EINVAL:
+    return __WASI_EINVAL;
+  case EIO:
+    return __WASI_EIO;
+  case EISCONN:
+    return __WASI_EISCONN;
+  case EISDIR:
+    return __WASI_EISDIR;
+  case ELOOP:
+    return __WASI_ELOOP;
+  case EMFILE:
+    return __WASI_EMFILE;
+  case EMLINK:
+    return __WASI_EMLINK;
+  case EMSGSIZE:
+    return __WASI_EMSGSIZE;
+  case EMULTIHOP:
+    return __WASI_EMULTIHOP;
+  case ENAMETOOLONG:
+    return __WASI_ENAMETOOLONG;
+  case ENETDOWN:
+    return __WASI_ENETDOWN;
+  case ENETRESET:
+    return __WASI_ENETRESET;
+  case ENETUNREACH:
+    return __WASI_ENETUNREACH;
+  case ENFILE:
+    return __WASI_ENFILE;
+  case ENOBUFS:
+    return __WASI_ENOBUFS;
+  case ENODEV:
+    return __WASI_ENODEV;
+  case ENOENT:
+    return __WASI_ENOENT;
+  case ENOEXEC:
+    return __WASI_ENOEXEC;
+  case ENOLCK:
+    return __WASI_ENOLCK;
+  case ENOLINK:
+    return __WASI_ENOLINK;
+  case ENOMEM:
+    return __WASI_ENOMEM;
+  case ENOMSG:
+    return __WASI_ENOMSG;
+  case ENOPROTOOPT:
+    return __WASI_ENOPROTOOPT;
+  case ENOSPC:
+    return __WASI_ENOSPC;
+  case ENOSYS:
+    return __WASI_ENOSYS;
+  case ENOTCONN:
+    return __WASI_ENOTCONN;
+  case ENOTDIR:
+    return __WASI_ENOTDIR;
+  case ENOTEMPTY:
+    return __WASI_ENOTEMPTY;
+  case ENOTRECOVERABLE:
+    return __WASI_ENOTRECOVERABLE;
+  case ENOTSOCK:
+    return __WASI_ENOTSOCK;
+  case ENOTSUP:
+    return __WASI_ENOTSUP;
+  case ENOTTY:
+    return __WASI_ENOTTY;
+  case ENXIO:
+    return __WASI_ENXIO;
+  case EOVERFLOW:
+    return __WASI_EOVERFLOW;
+  case EOWNERDEAD:
+    return __WASI_EOWNERDEAD;
+  case EPERM:
+    return __WASI_EPERM;
+  case EPIPE:
+    return __WASI_EPIPE;
+  case EPROTO:
+    return __WASI_EPROTO;
+  case EPROTONOSUPPORT:
+    return __WASI_EPROTONOSUPPORT;
+  case EPROTOTYPE:
+    return __WASI_EPROTOTYPE;
+  case ERANGE:
+    return __WASI_ERANGE;
+  case EROFS:
+    return __WASI_EROFS;
+  case ESPIPE:
+    return __WASI_ESPIPE;
+  case ESRCH:
+    return __WASI_ESRCH;
+  case ESTALE:
+    return __WASI_ESTALE;
+  case ETIMEDOUT:
+    return __WASI_ETIMEDOUT;
+  case ETXTBSY:
+    return __WASI_ETXTBSY;
+  case EXDEV:
+    return __WASI_EXDEV;
+  default:
+    __builtin_unreachable();
+  }
+}
+
+uint64_t convertTimespec(const timespec &Timespec) noexcept {
+  return Timespec.tv_sec * UINT64_C(1000000000) + Timespec.tv_nsec;
+}
+
+} // namespace
+
 TEST(WasiTest, Args) {
   SSVM::Host::WasiEnvironment Env;
   SSVM::Runtime::Instance::MemoryInstance MemInst(SSVM::AST::Limit(1));
 
+  SSVM::Host::WasiArgsSizesGet WasiArgsSizesGet(Env);
+  SSVM::Host::WasiArgsGet WasiArgsGet(Env);
+  std::array<SSVM::ValVariant, 1> Errno;
+
+  // args: test\0
+  Env.init({}, "test"s, {}, {});
+  writeDummyMemoryContent(MemInst);
+  EXPECT_TRUE(WasiArgsSizesGet.run(
+      &MemInst, std::array<SSVM::ValVariant, 2>{UINT32_C(0), UINT32_C(4)},
+      Errno));
+  EXPECT_EQ(SSVM::retrieveValue<int32_t>(Errno[0]), __WASI_ESUCCESS);
+  EXPECT_EQ(*MemInst.getPointer<const uint32_t *>(0), UINT32_C(1));
+  EXPECT_EQ(*MemInst.getPointer<const uint32_t *>(4), UINT32_C(5));
+
+  EXPECT_TRUE(WasiArgsGet.run(
+      &MemInst, std::array<SSVM::ValVariant, 2>{UINT32_C(0), UINT32_C(8)},
+      Errno));
+  EXPECT_EQ(SSVM::retrieveValue<int32_t>(Errno[0]), __WASI_ESUCCESS);
+  EXPECT_EQ(*MemInst.getPointer<const uint32_t *>(0), UINT32_C(8));
+  EXPECT_EQ(*MemInst.getPointer<const uint32_t *>(4), UINT32_C(0));
+  EXPECT_STREQ(MemInst.getPointer<const char *>(8), "test");
+  Env.fini();
+
+  // args: test\0 abc\0
+  Env.init({}, "test"s, std::array{"abc"s}, {});
+  writeDummyMemoryContent(MemInst);
+  EXPECT_TRUE(WasiArgsSizesGet.run(
+      &MemInst, std::array<SSVM::ValVariant, 2>{UINT32_C(0), UINT32_C(4)},
+      Errno));
+  EXPECT_EQ(SSVM::retrieveValue<int32_t>(Errno[0]), __WASI_ESUCCESS);
+  EXPECT_EQ(*MemInst.getPointer<const uint32_t *>(0), UINT32_C(2));
+  EXPECT_EQ(*MemInst.getPointer<const uint32_t *>(4), UINT32_C(9));
+
+  EXPECT_TRUE(WasiArgsGet.run(
+      &MemInst, std::array<SSVM::ValVariant, 2>{UINT32_C(0), UINT32_C(12)},
+      Errno));
+  EXPECT_EQ(SSVM::retrieveValue<int32_t>(Errno[0]), __WASI_ESUCCESS);
+  EXPECT_EQ(*MemInst.getPointer<const uint32_t *>(0), UINT32_C(12));
+  EXPECT_EQ(*MemInst.getPointer<const uint32_t *>(4), UINT32_C(17));
+  EXPECT_EQ(*MemInst.getPointer<const uint32_t *>(8), UINT32_C(0));
+  EXPECT_STREQ(MemInst.getPointer<const char *>(12), "test");
+  EXPECT_STREQ(MemInst.getPointer<const char *>(17), "abc");
+  Env.fini();
+
+  // args: test\0 \0
+  Env.init({}, "test"s, std::array{""s}, {});
+  writeDummyMemoryContent(MemInst);
+  EXPECT_TRUE(WasiArgsSizesGet.run(
+      &MemInst, std::array<SSVM::ValVariant, 2>{UINT32_C(0), UINT32_C(4)},
+      Errno));
+  EXPECT_EQ(SSVM::retrieveValue<int32_t>(Errno[0]), __WASI_ESUCCESS);
+  EXPECT_EQ(*MemInst.getPointer<const uint32_t *>(0), UINT32_C(2));
+  EXPECT_EQ(*MemInst.getPointer<const uint32_t *>(4), UINT32_C(6));
+
+  EXPECT_TRUE(WasiArgsGet.run(
+      &MemInst, std::array<SSVM::ValVariant, 2>{UINT32_C(0), UINT32_C(12)},
+      Errno));
+  EXPECT_EQ(SSVM::retrieveValue<int32_t>(Errno[0]), __WASI_ESUCCESS);
+  EXPECT_EQ(*MemInst.getPointer<const uint32_t *>(0), UINT32_C(12));
+  EXPECT_EQ(*MemInst.getPointer<const uint32_t *>(4), UINT32_C(17));
+  EXPECT_EQ(*MemInst.getPointer<const uint32_t *>(8), UINT32_C(0));
+  EXPECT_STREQ(MemInst.getPointer<const char *>(12), "test");
+  EXPECT_STREQ(MemInst.getPointer<const char *>(17), "");
+  Env.fini();
+}
+
+TEST(WasiTest, Envs) {
+  SSVM::Host::WasiEnvironment Env;
+  SSVM::Runtime::Instance::MemoryInstance MemInst(SSVM::AST::Limit(1));
+
+  SSVM::Host::WasiEnvironSizesGet WasiEnvironSizesGet(Env);
+  SSVM::Host::WasiEnvironGet WasiEnvironGet(Env);
+  std::array<SSVM::ValVariant, 1> Errno;
+
+  // envs:
+  Env.init({}, "test"s, {}, {});
+  writeDummyMemoryContent(MemInst);
+  EXPECT_TRUE(WasiEnvironSizesGet.run(
+      &MemInst, std::array<SSVM::ValVariant, 2>{UINT32_C(0), UINT32_C(4)},
+      Errno));
+  EXPECT_EQ(SSVM::retrieveValue<int32_t>(Errno[0]), __WASI_ESUCCESS);
+  EXPECT_EQ(*MemInst.getPointer<const uint32_t *>(0), UINT32_C(0));
+  EXPECT_EQ(*MemInst.getPointer<const uint32_t *>(4), UINT32_C(0));
+
+  EXPECT_TRUE(WasiEnvironGet.run(
+      &MemInst, std::array<SSVM::ValVariant, 2>{UINT32_C(0), UINT32_C(8)},
+      Errno));
+  EXPECT_EQ(SSVM::retrieveValue<int32_t>(Errno[0]), __WASI_ESUCCESS);
+  EXPECT_EQ(*MemInst.getPointer<const uint32_t *>(0), UINT32_C(0));
+  Env.fini();
+
+  // envs: a=b\0
+  Env.init({}, "test"s, {}, std::array{"a=b"s});
+  writeDummyMemoryContent(MemInst);
+  EXPECT_TRUE(WasiEnvironSizesGet.run(
+      &MemInst, std::array<SSVM::ValVariant, 2>{UINT32_C(0), UINT32_C(4)},
+      Errno));
+  EXPECT_EQ(SSVM::retrieveValue<int32_t>(Errno[0]), __WASI_ESUCCESS);
+  EXPECT_EQ(*MemInst.getPointer<const uint32_t *>(0), UINT32_C(1));
+  EXPECT_EQ(*MemInst.getPointer<const uint32_t *>(4), UINT32_C(4));
+
+  EXPECT_TRUE(WasiEnvironGet.run(
+      &MemInst, std::array<SSVM::ValVariant, 2>{UINT32_C(0), UINT32_C(8)},
+      Errno));
+  EXPECT_EQ(SSVM::retrieveValue<int32_t>(Errno[0]), __WASI_ESUCCESS);
+  EXPECT_EQ(*MemInst.getPointer<const uint32_t *>(0), UINT32_C(8));
+  EXPECT_EQ(*MemInst.getPointer<const uint32_t *>(4), UINT32_C(0));
+  EXPECT_STREQ(MemInst.getPointer<const char *>(8), "a=b");
+  Env.fini();
+
+  // envs: a=b\0 TEST=TEST=Test\0
+  Env.init({}, "test"s, {}, std::array{"a=b"s, "TEST=TEST=TEST"s});
+  writeDummyMemoryContent(MemInst);
+  EXPECT_TRUE(WasiEnvironSizesGet.run(
+      &MemInst, std::array<SSVM::ValVariant, 2>{UINT32_C(0), UINT32_C(4)},
+      Errno));
+  EXPECT_EQ(SSVM::retrieveValue<int32_t>(Errno[0]), __WASI_ESUCCESS);
+  EXPECT_EQ(*MemInst.getPointer<const uint32_t *>(0), UINT32_C(2));
+  EXPECT_EQ(*MemInst.getPointer<const uint32_t *>(4), UINT32_C(19));
+
+  EXPECT_TRUE(WasiEnvironGet.run(
+      &MemInst, std::array<SSVM::ValVariant, 2>{UINT32_C(0), UINT32_C(12)},
+      Errno));
+  EXPECT_EQ(SSVM::retrieveValue<int32_t>(Errno[0]), __WASI_ESUCCESS);
+  EXPECT_EQ(*MemInst.getPointer<const uint32_t *>(0), UINT32_C(12));
+  EXPECT_EQ(*MemInst.getPointer<const uint32_t *>(4), UINT32_C(16));
+  EXPECT_STREQ(MemInst.getPointer<const char *>(12), "a=b");
+  EXPECT_STREQ(MemInst.getPointer<const char *>(16), "TEST=TEST=TEST");
+  Env.fini();
+}
+
+TEST(WasiTest, ClockRes) {
+  SSVM::Host::WasiEnvironment Env;
+  SSVM::Runtime::Instance::MemoryInstance MemInst(SSVM::AST::Limit(1));
+
+  SSVM::Host::WasiClockResGet WasiClockResGet(Env);
+  std::array<SSVM::ValVariant, 1> Errno;
+  timespec Timespec;
+
+  // realtime clock
   {
-    SSVM::Host::WasiArgsSizesGet WasiArgsSizesGet(Env);
-    SSVM::Host::WasiArgsGet WasiArgsGet(Env);
-    std::vector<SSVM::ValVariant> Rets(1);
+    int SysErrno = 0;
+    if (clock_getres(CLOCK_REALTIME, &Timespec) != 0) {
+      SysErrno = errno;
+    }
 
-    // args: test\0
     Env.init({}, "test"s, {}, {});
-    EXPECT_TRUE(WasiArgsSizesGet.run(
-        &MemInst, std::array<SSVM::ValVariant, 2>{UINT32_C(0), UINT32_C(4)},
-        Rets));
-    EXPECT_EQ(SSVM::retrieveValue<uint32_t>(Rets[0]), UINT32_C(0));
-    EXPECT_EQ(*MemInst.getPointer<const uint32_t *>(0), UINT32_C(1));
-    EXPECT_EQ(*MemInst.getPointer<const uint32_t *>(4), UINT32_C(5));
+    writeDummyMemoryContent(MemInst);
+    EXPECT_TRUE(WasiClockResGet.run(
+        &MemInst,
+        std::array<SSVM::ValVariant, 2>{__WASI_CLOCK_REALTIME, UINT32_C(0)},
+        Errno));
+    EXPECT_EQ(SSVM::retrieveValue<int32_t>(Errno[0]), convertErrno(SysErrno));
+    if (SysErrno == 0) {
+      const uint64_t Res = convertTimespec(Timespec);
+      EXPECT_EQ(*MemInst.getPointer<const uint64_t *>(0), Res);
+    }
+    Env.fini();
+  }
 
-    EXPECT_TRUE(WasiArgsGet.run(
-        &MemInst, std::array<SSVM::ValVariant, 2>{UINT32_C(0), UINT32_C(8)},
-        Rets));
-    EXPECT_EQ(SSVM::retrieveValue<uint32_t>(Rets[0]), UINT32_C(0));
-    EXPECT_EQ(*MemInst.getPointer<const uint32_t *>(0), UINT32_C(8));
-    EXPECT_EQ(*MemInst.getPointer<const uint32_t *>(4), UINT32_C(0));
-    EXPECT_STREQ(MemInst.getPointer<const char *>(8), "test");
+  // monotonic clock
+  {
+    int SysErrno = 0;
+    if (clock_getres(CLOCK_MONOTONIC, &Timespec) != 0) {
+      SysErrno = errno;
+    }
 
-    // args: test\0 abc\0
-    Env.init({}, "test"s, std::array{"abc"s}, {});
-    EXPECT_TRUE(WasiArgsSizesGet.run(
-        &MemInst, std::array<SSVM::ValVariant, 2>{UINT32_C(0), UINT32_C(4)},
-        Rets));
-    EXPECT_EQ(SSVM::retrieveValue<uint32_t>(Rets[0]), UINT32_C(0));
-    EXPECT_EQ(*MemInst.getPointer<const uint32_t *>(0), UINT32_C(2));
-    EXPECT_EQ(*MemInst.getPointer<const uint32_t *>(4), UINT32_C(9));
+    Env.init({}, "test"s, {}, {});
+    writeDummyMemoryContent(MemInst);
+    EXPECT_TRUE(WasiClockResGet.run(
+        &MemInst,
+        std::array<SSVM::ValVariant, 2>{__WASI_CLOCK_MONOTONIC, UINT32_C(0)},
+        Errno));
+    EXPECT_EQ(SSVM::retrieveValue<int32_t>(Errno[0]), convertErrno(SysErrno));
+    if (SysErrno == 0) {
+      const uint64_t Res = convertTimespec(Timespec);
+      EXPECT_EQ(*MemInst.getPointer<const uint64_t *>(0), Res);
+    }
+    Env.fini();
+  }
 
-    EXPECT_TRUE(WasiArgsGet.run(
-        &MemInst, std::array<SSVM::ValVariant, 2>{UINT32_C(0), UINT32_C(12)},
-        Rets));
-    EXPECT_EQ(SSVM::retrieveValue<uint32_t>(Rets[0]), UINT32_C(0));
-    EXPECT_EQ(*MemInst.getPointer<const uint32_t *>(0), UINT32_C(12));
-    EXPECT_EQ(*MemInst.getPointer<const uint32_t *>(4), UINT32_C(17));
-    EXPECT_EQ(*MemInst.getPointer<const uint32_t *>(8), UINT32_C(0));
-    EXPECT_STREQ(MemInst.getPointer<const char *>(12), "test");
-    EXPECT_STREQ(MemInst.getPointer<const char *>(17), "abc");
+  // process cputime clock
+  {
+    int SysErrno = 0;
+    if (clock_getres(CLOCK_PROCESS_CPUTIME_ID, &Timespec) != 0) {
+      SysErrno = errno;
+    }
 
-    // args: test\0 \0
-    Env.init({}, "test"s, std::array{""s}, {});
-    EXPECT_TRUE(WasiArgsSizesGet.run(
-        &MemInst, std::array<SSVM::ValVariant, 2>{UINT32_C(0), UINT32_C(4)},
-        Rets));
-    EXPECT_EQ(SSVM::retrieveValue<uint32_t>(Rets[0]), UINT32_C(0));
-    EXPECT_EQ(*MemInst.getPointer<const uint32_t *>(0), UINT32_C(2));
-    EXPECT_EQ(*MemInst.getPointer<const uint32_t *>(4), UINT32_C(6));
+    Env.init({}, "test"s, {}, {});
+    writeDummyMemoryContent(MemInst);
+    EXPECT_TRUE(
+        WasiClockResGet.run(&MemInst,
+                            std::array<SSVM::ValVariant, 2>{
+                                __WASI_CLOCK_PROCESS_CPUTIME_ID, UINT32_C(0)},
+                            Errno));
+    EXPECT_EQ(SSVM::retrieveValue<int32_t>(Errno[0]), convertErrno(SysErrno));
+    if (SysErrno == 0) {
+      const uint64_t Res = convertTimespec(Timespec);
+      EXPECT_EQ(*MemInst.getPointer<const uint64_t *>(0), Res);
+    }
+    Env.fini();
+  }
 
-    EXPECT_TRUE(WasiArgsGet.run(
-        &MemInst, std::array<SSVM::ValVariant, 2>{UINT32_C(0), UINT32_C(12)},
-        Rets));
-    EXPECT_EQ(SSVM::retrieveValue<uint32_t>(Rets[0]), UINT32_C(0));
-    EXPECT_EQ(*MemInst.getPointer<const uint32_t *>(0), UINT32_C(12));
-    EXPECT_EQ(*MemInst.getPointer<const uint32_t *>(4), UINT32_C(17));
-    EXPECT_EQ(*MemInst.getPointer<const uint32_t *>(8), UINT32_C(0));
-    EXPECT_STREQ(MemInst.getPointer<const char *>(12), "test");
-    EXPECT_STREQ(MemInst.getPointer<const char *>(17), "");
+  // thread cputime clock
+  {
+    int SysErrno = 0;
+    if (clock_getres(CLOCK_THREAD_CPUTIME_ID, &Timespec) != 0) {
+      SysErrno = errno;
+    }
+
+    Env.init({}, "test"s, {}, {});
+    writeDummyMemoryContent(MemInst);
+    EXPECT_TRUE(
+        WasiClockResGet.run(&MemInst,
+                            std::array<SSVM::ValVariant, 2>{
+                                __WASI_CLOCK_THREAD_CPUTIME_ID, UINT32_C(0)},
+                            Errno));
+    EXPECT_EQ(SSVM::retrieveValue<int32_t>(Errno[0]), convertErrno(SysErrno));
+    if (SysErrno == 0) {
+      const uint64_t Res = convertTimespec(Timespec);
+      EXPECT_EQ(*MemInst.getPointer<const uint64_t *>(0), Res);
+    }
+    Env.fini();
+  }
+}
+
+TEST(WasiTest, ClockTimeGet) {
+  SSVM::Host::WasiEnvironment Env;
+  SSVM::Runtime::Instance::MemoryInstance MemInst(SSVM::AST::Limit(1));
+
+  SSVM::Host::WasiClockTimeGet WasiClockTimeGet(Env);
+  std::array<SSVM::ValVariant, 1> Errno;
+  timespec Timespec;
+
+  // realtime clock
+  {
+    int SysErrno = 0;
+    if (clock_gettime(CLOCK_REALTIME, &Timespec) != 0) {
+      SysErrno = errno;
+    }
+
+    Env.init({}, "test"s, {}, {});
+    writeDummyMemoryContent(MemInst);
+    EXPECT_TRUE(WasiClockTimeGet.run(
+        &MemInst,
+        std::array<SSVM::ValVariant, 3>{__WASI_CLOCK_REALTIME, UINT64_C(0),
+                                        UINT32_C(0)},
+        Errno));
+    EXPECT_EQ(SSVM::retrieveValue<int32_t>(Errno[0]), convertErrno(SysErrno));
+    if (SysErrno == 0) {
+      const uint64_t Time = convertTimespec(Timespec);
+      EXPECT_NEAR(*MemInst.getPointer<const uint64_t *>(0), Time, 1000000);
+    }
+    Env.fini();
+  }
+
+  // monotonic clock
+  {
+    int SysErrno = 0;
+    if (clock_gettime(CLOCK_MONOTONIC, &Timespec) != 0) {
+      SysErrno = errno;
+    }
+
+    Env.init({}, "test"s, {}, {});
+    writeDummyMemoryContent(MemInst);
+    EXPECT_TRUE(WasiClockTimeGet.run(
+        &MemInst,
+        std::array<SSVM::ValVariant, 3>{__WASI_CLOCK_MONOTONIC, UINT64_C(0),
+                                        UINT32_C(0)},
+        Errno));
+    EXPECT_EQ(SSVM::retrieveValue<int32_t>(Errno[0]), convertErrno(SysErrno));
+    if (SysErrno == 0) {
+      const uint64_t Time = convertTimespec(Timespec);
+      EXPECT_NEAR(*MemInst.getPointer<const uint64_t *>(0), Time, 1000000);
+    }
+    Env.fini();
+  }
+
+  // process cputime clock
+  {
+    int SysErrno = 0;
+    if (clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &Timespec) != 0) {
+      SysErrno = errno;
+    }
+
+    Env.init({}, "test"s, {}, {});
+    writeDummyMemoryContent(MemInst);
+    EXPECT_TRUE(WasiClockTimeGet.run(
+        &MemInst,
+        std::array<SSVM::ValVariant, 3>{__WASI_CLOCK_PROCESS_CPUTIME_ID,
+                                        UINT64_C(0), UINT32_C(0)},
+        Errno));
+    EXPECT_EQ(SSVM::retrieveValue<int32_t>(Errno[0]), convertErrno(SysErrno));
+    if (SysErrno == 0) {
+      const uint64_t Time = convertTimespec(Timespec);
+      EXPECT_NEAR(*MemInst.getPointer<const uint64_t *>(0), Time, 1000000);
+    }
+    Env.fini();
+  }
+
+  // thread cputime clock
+  {
+    int SysErrno = 0;
+    if (clock_gettime(CLOCK_THREAD_CPUTIME_ID, &Timespec) != 0) {
+      SysErrno = errno;
+    }
+
+    Env.init({}, "test"s, {}, {});
+    writeDummyMemoryContent(MemInst);
+    EXPECT_TRUE(WasiClockTimeGet.run(
+        &MemInst,
+        std::array<SSVM::ValVariant, 3>{__WASI_CLOCK_THREAD_CPUTIME_ID,
+                                        UINT64_C(0), UINT32_C(0)},
+        Errno));
+    EXPECT_EQ(SSVM::retrieveValue<int32_t>(Errno[0]), convertErrno(SysErrno));
+    if (SysErrno == 0) {
+      const uint64_t Time = convertTimespec(Timespec);
+      EXPECT_NEAR(*MemInst.getPointer<const uint64_t *>(0), Time, 1000000);
+    }
+    Env.fini();
   }
 }
 

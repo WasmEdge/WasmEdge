5ad58e9556e86bbe86db31de4d5999c1b904b750
YiYing He <yiying@secondstate.io>
2021-01-13 05:39:59 +0800

    [Test] Fix gcc warnings in file manager test.

--- test/loader/filemgrTest.cpp
+++ test/loader/filemgrTest.cpp
@@ -49,7 +49,7 @@ TEST(FileManagerTest, File__ReadByte) {
   /// 2. Test unsigned char reading.
   SSVM::Expect<uint8_t> ReadByte;
   ASSERT_TRUE(FMgr.setPath("filemgrTestData/readByteTest.bin"));
-  EXPECT_EQ(0, FMgr.getOffset());
+  EXPECT_EQ(0U, FMgr.getOffset());
   ASSERT_TRUE(ReadByte = FMgr.readByte());
   EXPECT_EQ(0x00, ReadByte.value());
   ASSERT_TRUE(ReadByte = FMgr.readByte());
@@ -71,14 +71,14 @@ TEST(FileManagerTest, File__ReadByte) {
   ASSERT_TRUE(ReadByte = FMgr.readByte());
   EXPECT_EQ(0x88, ReadByte.value());
   ASSERT_FALSE(ReadByte = FMgr.readByte());
-  EXPECT_EQ(10, FMgr.getOffset());
+  EXPECT_EQ(10U, FMgr.getOffset());
 }
 
 TEST(FileManagerTest, File__ReadBytes) {
   /// 3. Test unsigned char list reading.
   SSVM::Expect<std::vector<uint8_t>> ReadBytes;
   ASSERT_TRUE(FMgr.setPath("filemgrTestData/readByteTest.bin"));
-  EXPECT_EQ(0, FMgr.getOffset());
+  EXPECT_EQ(0U, FMgr.getOffset());
   ASSERT_TRUE(ReadBytes = FMgr.readBytes(1));
   EXPECT_EQ(0x00, ReadBytes.value()[0]);
   ASSERT_TRUE(ReadBytes = FMgr.readBytes(2));
@@ -94,14 +94,14 @@ TEST(FileManagerTest, File__ReadBytes) {
   EXPECT_EQ(0x79, ReadBytes.value()[2]);
   EXPECT_EQ(0x88, ReadBytes.value()[3]);
   ASSERT_FALSE(ReadBytes = FMgr.readBytes(1));
-  EXPECT_EQ(10, FMgr.getOffset());
+  EXPECT_EQ(10U, FMgr.getOffset());
 }
 
 TEST(FileManagerTest, File__ReadUnsigned32) {
   /// 4. Test unsigned 32bit integer decoding.
   SSVM::Expect<uint32_t> ReadNum;
   ASSERT_TRUE(FMgr.setPath("filemgrTestData/readU32Test.bin"));
-  EXPECT_EQ(0, FMgr.getOffset());
+  EXPECT_EQ(0U, FMgr.getOffset());
   ASSERT_TRUE(ReadNum = FMgr.readU32());
   EXPECT_EQ(UINT32_C(0), ReadNum.value());
   ASSERT_TRUE(ReadNum = FMgr.readU32());
@@ -123,14 +123,14 @@ TEST(FileManagerTest, File__ReadUnsigned32) {
   ASSERT_TRUE(ReadNum = FMgr.readU32());
   EXPECT_EQ(891055U, ReadNum.value());
   ASSERT_FALSE(ReadNum = FMgr.readU32());
-  EXPECT_EQ(36, FMgr.getOffset());
+  EXPECT_EQ(36U, FMgr.getOffset());
 }
 
 TEST(FileManagerTest, File__ReadUnsigned64) {
   /// 5. Test unsigned 64bit integer decoding.
   SSVM::Expect<uint64_t> ReadNum;
   ASSERT_TRUE(FMgr.setPath("filemgrTestData/readU64Test.bin"));
-  EXPECT_EQ(0, FMgr.getOffset());
+  EXPECT_EQ(0U, FMgr.getOffset());
   ASSERT_TRUE(ReadNum = FMgr.readU64());
   EXPECT_EQ(UINT64_C(0), ReadNum.value());
   ASSERT_TRUE(ReadNum = FMgr.readU64());
@@ -152,14 +152,14 @@ TEST(FileManagerTest, File__ReadUnsigned64) {
   ASSERT_TRUE(ReadNum = FMgr.readU64());
   EXPECT_EQ(891055U, ReadNum.value());
   ASSERT_FALSE(ReadNum = FMgr.readU64());
-  EXPECT_EQ(69, FMgr.getOffset());
+  EXPECT_EQ(69U, FMgr.getOffset());
 }
 
 TEST(FileManagerTest, File__ReadSigned32) {
   /// 6. Test signed 32bit integer decoding.
   SSVM::Expect<int32_t> ReadNum;
   ASSERT_TRUE(FMgr.setPath("filemgrTestData/readS32Test.bin"));
-  EXPECT_EQ(0, FMgr.getOffset());
+  EXPECT_EQ(0U, FMgr.getOffset());
   ASSERT_TRUE(ReadNum = FMgr.readS32());
   EXPECT_EQ(0, ReadNum.value());
   ASSERT_TRUE(ReadNum = FMgr.readS32());
@@ -181,14 +181,14 @@ TEST(FileManagerTest, File__ReadSigned32) {
   ASSERT_TRUE(ReadNum = FMgr.readS32());
   EXPECT_EQ(891055, ReadNum.value());
   ASSERT_FALSE(ReadNum = FMgr.readS32());
-  EXPECT_EQ(30, FMgr.getOffset());
+  EXPECT_EQ(30U, FMgr.getOffset());
 }
 
 TEST(FileManagerTest, File__ReadSigned64) {
   /// 7. Test signed 64bit integer decoding.
   SSVM::Expect<int64_t> ReadNum;
   ASSERT_TRUE(FMgr.setPath("filemgrTestData/readS64Test.bin"));
-  EXPECT_EQ(0, FMgr.getOffset());
+  EXPECT_EQ(0U, FMgr.getOffset());
   ASSERT_TRUE(ReadNum = FMgr.readS64());
   EXPECT_EQ(0, ReadNum.value());
   ASSERT_TRUE(ReadNum = FMgr.readS64());
@@ -210,7 +210,7 @@ TEST(FileManagerTest, File__ReadSigned64) {
   ASSERT_TRUE(ReadNum = FMgr.readS64());
   EXPECT_EQ(7124932496753367824LL, ReadNum.value());
   ASSERT_FALSE(ReadNum = FMgr.readS64());
-  EXPECT_EQ(63, FMgr.getOffset());
+  EXPECT_EQ(63U, FMgr.getOffset());
 }
 
 TEST(FileManagerTest, File__ReadFloat32) {
@@ -227,7 +227,7 @@ TEST(FileManagerTest, File__ReadFloat32) {
   ///   9.  -1.0 / 0.0 : -inf
   SSVM::Expect<float> ReadNum;
   ASSERT_TRUE(FMgr.setPath("filemgrTestData/readF32Test.bin"));
-  EXPECT_EQ(0, FMgr.getOffset());
+  EXPECT_EQ(0U, FMgr.getOffset());
   ASSERT_TRUE(ReadNum = FMgr.readF32());
   EXPECT_EQ(+0.0f, ReadNum.value());
   ASSERT_TRUE(ReadNum = FMgr.readF32());
@@ -247,7 +247,7 @@ TEST(FileManagerTest, File__ReadFloat32) {
   ASSERT_TRUE(ReadNum = FMgr.readF32());
   EXPECT_TRUE(std::isinf(ReadNum.value()));
   ASSERT_FALSE(ReadNum = FMgr.readF32());
-  EXPECT_EQ(36, FMgr.getOffset());
+  EXPECT_EQ(36U, FMgr.getOffset());
 }
 
 TEST(FileManagerTest, File__ReadFloat64) {
@@ -264,7 +264,7 @@ TEST(FileManagerTest, File__ReadFloat64) {
   ///   9.  -1.0 / 0.0 : -inf
   SSVM::Expect<double> ReadNum;
   ASSERT_TRUE(FMgr.setPath("filemgrTestData/readF64Test.bin"));
-  EXPECT_EQ(0, FMgr.getOffset());
+  EXPECT_EQ(0U, FMgr.getOffset());
   ASSERT_TRUE(ReadNum = FMgr.readF64());
   EXPECT_EQ(+0.0f, ReadNum.value());
   ASSERT_TRUE(ReadNum = FMgr.readF64());
@@ -284,14 +284,14 @@ TEST(FileManagerTest, File__ReadFloat64) {
   ASSERT_TRUE(ReadNum = FMgr.readF64());
   EXPECT_TRUE(std::isinf(ReadNum.value()));
   ASSERT_FALSE(ReadNum = FMgr.readF64());
-  EXPECT_EQ(72, FMgr.getOffset());
+  EXPECT_EQ(72U, FMgr.getOffset());
 }
 
 TEST(FileManagerTest, File__ReadName) {
   /// 10. Test utf-8 string reading.
   SSVM::Expect<std::string> ReadStr;
   ASSERT_TRUE(FMgr.setPath("filemgrTestData/readNameTest.bin"));
-  EXPECT_EQ(0, FMgr.getOffset());
+  EXPECT_EQ(0U, FMgr.getOffset());
   ASSERT_TRUE(ReadStr = FMgr.readName());
   EXPECT_EQ("", ReadStr.value());
   ASSERT_TRUE(ReadStr = FMgr.readName());
@@ -301,7 +301,7 @@ TEST(FileManagerTest, File__ReadName) {
   ASSERT_TRUE(ReadStr = FMgr.readName());
   EXPECT_EQ("Loader", ReadStr.value());
   ASSERT_FALSE(ReadStr = FMgr.readName());
-  EXPECT_EQ(15, FMgr.getOffset());
+  EXPECT_EQ(15U, FMgr.getOffset());
 }
 
 TEST(FileManagerTest, File__ReadUnsigned32TooLong) {
@@ -373,7 +373,7 @@ TEST(FileManagerTest, Vector__ReadByte) {
   SSVM::Expect<uint8_t> ReadByte;
   ASSERT_TRUE(VMgr.setCode(std::array<uint8_t, 10>{
       0x00, 0xFF, 0x1F, 0x2E, 0x3D, 0x4C, 0x5B, 0x6A, 0x79, 0x88}));
-  EXPECT_EQ(0, VMgr.getOffset());
+  EXPECT_EQ(0U, VMgr.getOffset());
   ASSERT_TRUE(ReadByte = VMgr.readByte());
   EXPECT_EQ(0x00, ReadByte.value());
   ASSERT_TRUE(ReadByte = VMgr.readByte());
@@ -395,7 +395,7 @@ TEST(FileManagerTest, Vector__ReadByte) {
   ASSERT_TRUE(ReadByte = VMgr.readByte());
   EXPECT_EQ(0x88, ReadByte.value());
   ASSERT_FALSE(ReadByte = VMgr.readByte());
-  EXPECT_EQ(10, VMgr.getOffset());
+  EXPECT_EQ(10U, VMgr.getOffset());
 }
 
 TEST(FileManagerTest, Vector__ReadBytes) {
@@ -403,7 +403,7 @@ TEST(FileManagerTest, Vector__ReadBytes) {
   SSVM::Expect<std::vector<uint8_t>> ReadBytes;
   ASSERT_TRUE(VMgr.setCode(std::array<uint8_t, 10>{
       0x00, 0xFF, 0x1F, 0x2E, 0x3D, 0x4C, 0x5B, 0x6A, 0x79, 0x88}));
-  EXPECT_EQ(0, VMgr.getOffset());
+  EXPECT_EQ(0U, VMgr.getOffset());
   ASSERT_TRUE(ReadBytes = VMgr.readBytes(1));
   EXPECT_EQ(0x00, ReadBytes.value()[0]);
   ASSERT_TRUE(ReadBytes = VMgr.readBytes(2));
@@ -419,7 +419,7 @@ TEST(FileManagerTest, Vector__ReadBytes) {
   EXPECT_EQ(0x79, ReadBytes.value()[2]);
   EXPECT_EQ(0x88, ReadBytes.value()[3]);
   ASSERT_FALSE(ReadBytes = VMgr.readBytes(1));
-  EXPECT_EQ(10, VMgr.getOffset());
+  EXPECT_EQ(10U, VMgr.getOffset());
 }
 
 TEST(FileManagerTest, Vector__ReadUnsigned32) {
@@ -429,7 +429,7 @@ TEST(FileManagerTest, Vector__ReadUnsigned32) {
       0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0x07, 0x80, 0x80, 0x80, 0x80, 0x08, 0xFF,
       0xFF, 0xFF, 0xFF, 0x0F, 0x84, 0xAD, 0xF4, 0x4E, 0x86, 0x01, 0x9C, 0x8C,
       0xB0, 0xFD, 0x0C, 0xDA, 0x65, 0xF8, 0x94, 0x8C, 0x2F, 0xAF, 0xB1, 0x36}));
-  EXPECT_EQ(0, VMgr.getOffset());
+  EXPECT_EQ(0U, VMgr.getOffset());
   ASSERT_TRUE(ReadNum = VMgr.readU32());
   EXPECT_EQ(UINT32_C(0), ReadNum.value());
   ASSERT_TRUE(ReadNum = VMgr.readU32());
@@ -451,7 +451,7 @@ TEST(FileManagerTest, Vector__ReadUnsigned32) {
   ASSERT_TRUE(ReadNum = VMgr.readU32());
   EXPECT_EQ(891055U, ReadNum.value());
   ASSERT_FALSE(ReadNum = VMgr.readU32());
-  EXPECT_EQ(36, VMgr.getOffset());
+  EXPECT_EQ(36U, VMgr.getOffset());
 }
 
 TEST(FileManagerTest, Vector__ReadUnsigned64) {
@@ -464,7 +464,7 @@ TEST(FileManagerTest, Vector__ReadUnsigned64) {
       0x01, 0xA1, 0xA4, 0xDF, 0xA5, 0xEC, 0xA3, 0xCC, 0xAC, 0xB6, 0x01, 0xF6,
       0xD5, 0xBA, 0xFD, 0x82, 0xF6, 0x07, 0xDA, 0x65, 0xF7, 0x8C, 0xEC, 0xA1,
       0xF4, 0xE7, 0xA1, 0x96, 0xEF, 0x01, 0xAF, 0xB1, 0x36}));
-  EXPECT_EQ(0, VMgr.getOffset());
+  EXPECT_EQ(0U, VMgr.getOffset());
   ASSERT_TRUE(ReadNum = VMgr.readU64());
   EXPECT_EQ(UINT64_C(0), ReadNum.value());
   ASSERT_TRUE(ReadNum = VMgr.readU64());
@@ -486,7 +486,7 @@ TEST(FileManagerTest, Vector__ReadUnsigned64) {
   ASSERT_TRUE(ReadNum = VMgr.readU64());
   EXPECT_EQ(891055U, ReadNum.value());
   ASSERT_FALSE(ReadNum = VMgr.readU64());
-  EXPECT_EQ(69, VMgr.getOffset());
+  EXPECT_EQ(69U, VMgr.getOffset());
 }
 
 TEST(FileManagerTest, Vector__ReadSigned32) {
@@ -496,7 +496,7 @@ TEST(FileManagerTest, Vector__ReadSigned32) {
       0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0x07, 0x80, 0x80, 0x80, 0x80,
       0x78, 0x7F, 0x01, 0x86, 0x01, 0xFE, 0xB1, 0xEE, 0xD9, 0x7E,
       0xDA, 0xE5, 0x00, 0x88, 0xEB, 0xF3, 0x50, 0xAF, 0xB1, 0x36}));
-  EXPECT_EQ(0, VMgr.getOffset());
+  EXPECT_EQ(0U, VMgr.getOffset());
   ASSERT_TRUE(ReadNum = VMgr.readS32());
   EXPECT_EQ(0, ReadNum.value());
   ASSERT_TRUE(ReadNum = VMgr.readS32());
@@ -518,7 +518,7 @@ TEST(FileManagerTest, Vector__ReadSigned32) {
   ASSERT_TRUE(ReadNum = VMgr.readS32());
   EXPECT_EQ(891055, ReadNum.value());
   ASSERT_FALSE(ReadNum = VMgr.readS32());
-  EXPECT_EQ(30, VMgr.getOffset());
+  EXPECT_EQ(30U, VMgr.getOffset());
 }
 
 TEST(FileManagerTest, Vector__ReadSigned64) {
@@ -531,7 +531,7 @@ TEST(FileManagerTest, Vector__ReadSigned64) {
       0xED, 0xA6, 0xC2, 0xFB, 0xE0, 0xA6, 0xB9, 0xDF, 0xF0, 0x00, 0xD3,
       0xB4, 0xA0, 0xA1, 0xC8, 0xFC, 0xE1, 0xAB, 0x80, 0x7F, 0x90, 0xB6,
       0xFC, 0xAC, 0xB3, 0x8B, 0xB6, 0xF0, 0xE2, 0x00}));
-  EXPECT_EQ(0, VMgr.getOffset());
+  EXPECT_EQ(0U, VMgr.getOffset());
   ASSERT_TRUE(ReadNum = VMgr.readS64());
   EXPECT_EQ(0, ReadNum.value());
   ASSERT_TRUE(ReadNum = VMgr.readS64());
@@ -553,7 +553,7 @@ TEST(FileManagerTest, Vector__ReadSigned64) {
   ASSERT_TRUE(ReadNum = VMgr.readS64());
   EXPECT_EQ(7124932496753367824LL, ReadNum.value());
   ASSERT_FALSE(ReadNum = VMgr.readS64());
-  EXPECT_EQ(63, VMgr.getOffset());
+  EXPECT_EQ(63U, VMgr.getOffset());
 }
 
 TEST(FileManagerTest, Vector__ReadFloat32) {
@@ -573,7 +573,7 @@ TEST(FileManagerTest, Vector__ReadFloat32) {
       0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x80, 0x00, 0x00, 0xC0, 0xFF,
       0x00, 0x00, 0xC0, 0xFF, 0x00, 0x00, 0xC0, 0x7F, 0x00, 0x00, 0xC0, 0x7F,
       0x00, 0x00, 0x80, 0xFF, 0x00, 0x00, 0x80, 0x7F, 0x00, 0x00, 0x80, 0xFF}));
-  EXPECT_EQ(0, VMgr.getOffset());
+  EXPECT_EQ(0U, VMgr.getOffset());
   ASSERT_TRUE(ReadNum = VMgr.readF32());
   EXPECT_EQ(+0.0f, ReadNum.value());
   ASSERT_TRUE(ReadNum = VMgr.readF32());
@@ -593,7 +593,7 @@ TEST(FileManagerTest, Vector__ReadFloat32) {
   ASSERT_TRUE(ReadNum = VMgr.readF32());
   EXPECT_TRUE(std::isinf(ReadNum.value()));
   ASSERT_FALSE(ReadNum = VMgr.readF32());
-  EXPECT_EQ(36, VMgr.getOffset());
+  EXPECT_EQ(36U, VMgr.getOffset());
 }
 
 TEST(FileManagerTest, Vector__ReadFloat64) {
@@ -616,7 +616,7 @@ TEST(FileManagerTest, Vector__ReadFloat64) {
       0x00, 0x00, 0xF8, 0x7F, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xF8, 0x7F,
       0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xF0, 0xFF, 0x00, 0x00, 0x00, 0x00,
       0x00, 0x00, 0xF0, 0x7F, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xF0, 0xFF}));
-  EXPECT_EQ(0, VMgr.getOffset());
+  EXPECT_EQ(0U, VMgr.getOffset());
   ASSERT_TRUE(ReadNum = VMgr.readF64());
   EXPECT_EQ(+0.0f, ReadNum.value());
   ASSERT_TRUE(ReadNum = VMgr.readF64());
@@ -636,7 +636,7 @@ TEST(FileManagerTest, Vector__ReadFloat64) {
   ASSERT_TRUE(ReadNum = VMgr.readF64());
   EXPECT_TRUE(std::isinf(ReadNum.value()));
   ASSERT_FALSE(ReadNum = VMgr.readF64());
-  EXPECT_EQ(72, VMgr.getOffset());
+  EXPECT_EQ(72U, VMgr.getOffset());
 }
 
 TEST(FileManagerTest, Vector__ReadName) {
@@ -645,7 +645,7 @@ TEST(FileManagerTest, Vector__ReadName) {
   ASSERT_TRUE(VMgr.setCode(
       std::array<uint8_t, 15>{0x00, 0x04, 0x74, 0x65, 0x73, 0x74, 0x01, 0x20,
                               0x06, 0x4C, 0x6F, 0x61, 0x64, 0x65, 0x72}));
-  EXPECT_EQ(0, VMgr.getOffset());
+  EXPECT_EQ(0U, VMgr.getOffset());
   ASSERT_TRUE(ReadStr = VMgr.readName());
   EXPECT_EQ("", ReadStr.value());
   ASSERT_TRUE(ReadStr = VMgr.readName());
@@ -655,7 +655,7 @@ TEST(FileManagerTest, Vector__ReadName) {
   ASSERT_TRUE(ReadStr = VMgr.readName());
   EXPECT_EQ("Loader", ReadStr.value());
   ASSERT_FALSE(ReadStr = VMgr.readName());
-  EXPECT_EQ(15, VMgr.getOffset());
+  EXPECT_EQ(15U, VMgr.getOffset());
 }
 
 TEST(FileManagerTest, Vector__ReadUnsigned32TooLong) {

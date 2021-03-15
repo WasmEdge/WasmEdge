98d2cdc1b54f69cc609aabc31bd1aac7965dd508
Shen-Ta Hsieh <beststeve@secondstate.io>
2021-03-15 00:29:43 +0000

    [AOT] Add blake3 hash calculator
    * Calculate hash for cache file

---
+++ lib/aot/blake3.cpp
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: Apache-2.0
+#include "aot/blake3.h"
+#include "common/config.h"
+#include "common/defines.h"
+
+namespace SSVM {
+namespace AOT {
+
+namespace {}
+
+Blake3::Blake3() noexcept { blake3_hasher_init(&Hasher); }
+
+void Blake3::update(Span<const Byte> Data) noexcept {
+  blake3_hasher_update(&Hasher, Data.data(), Data.size());
+}
+
+void Blake3::finalize(Span<Byte> Output) noexcept {
+  blake3_hasher_finalize(&Hasher, Output.data(), Output.size());
+}
+
+} // namespace AOT
+} // namespace SSVM

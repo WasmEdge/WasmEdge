6777e5ba92541c7467eed2fb5adac34dddb3f241
Shen-Ta Hsieh <beststeve@secondstate.io>
2021-05-17 08:08:33 +0000

    [Misc] Update boost to 1.76

--- utils/docker/build-manylinux.sh
+++ utils/docker/build-manylinux.sh
@@ -6,10 +6,10 @@ export CC=gcc
 export CXX=g++
 
 cd
-curl -s -L -O --remote-name-all https://dl.bintray.com/boostorg/release/1.75.0/source/boost_1_75_0.tar.bz2
-echo 953db31e016db7bb207f11432bef7df100516eeb746843fa0486a222e3fd49cb boost_1_75_0.tar.bz2 | sha256sum -c
-bzip2 -dc boost_1_75_0.tar.bz2 | tar -xf -
-cmake -Bbuild -GNinja -DCMAKE_BUILD_TYPE=Release -DBUILD_PACKAGE="TGZ;TBZ2;TXZ;TZST;RPM" -DBOOST_INCLUDEDIR=$(pwd)/boost_1_75_0/ /wasmedge
+curl -s -L -O --remote-name-all https://boostorg.jfrog.io/artifactory/main/release/1.76.0/source/boost_1_76_0.tar.bz2
+echo f0397ba6e982c4450f27bf32a2a83292aba035b827a5623a14636ea583318c41 boost_1_76_0.tar.bz2 | sha256sum -c
+bzip2 -dc boost_1_76_0.tar.bz2 | tar -xf -
+cmake -Bbuild -GNinja -DCMAKE_BUILD_TYPE=Release -DBUILD_PACKAGE="TGZ;TBZ2;TXZ;TZST;RPM" -DBOOST_INCLUDEDIR=$(pwd)/boost_1_76_0/ /wasmedge
 cmake --build build
 cmake --build build --target package
 cp -v build/WasmEdge-*.tar.gz /wasmedge/WasmEdge.tar.gz

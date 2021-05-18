023ba0b2f7fb36a36125b60a60d506fcaba8128e
Shen-Ta Hsieh <beststeve@secondstate.io>
2021-05-18 20:49:02 +0000

    [System] Add memory mapped file helper for linux and windows
    * Add allocator helper for windows

--- lib/system/allocator.cpp
+++ lib/system/allocator.cpp
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: Apache-2.0
 #include "system/allocator.h"
+#include "common/defines.h"
 #include "common/errcode.h"
 #include "config.h"
+#include <cassert>
 #include <cctype>
 #include <cstdlib>
 #include <fstream>
@@ -11,8 +13,48 @@
 #include <string>
 #include <utility>
 
+#if WASMEDGE_OS_LINUX || WASMEDGE_OS_MACOS
 #ifdef HAVE_MMAP
+#define ALLOC_MMAP 1
+#else
+#define ALLOC_STUB 1
+#endif
+#elif WASMEDGE_OS_WINDOWS
+#define ALLOC_WINAPI 1
+#endif
+
+#if ALLOC_MMAP
 #include <sys/mman.h>
+#elif MAP_WINAPI
+#include <boost/winapi/basic_types.hpp>
+#include <boost/winapi/page_protection_flags.hpp>
+#if !defined(BOOST_USE_WINDOWS_H)
+extern "C" {
+BOOST_SYMBOL_IMPORT boost::winapi::LPVOID_ BOOST_WINAPI_WINAPI_CC VirtualAlloc(
+    boost::winapi::LPVOID_ lpAddress, boost::winapi::SIZE_T_ dwSize,
+    boost::winapi::DWORD_ flAllocationType, boost::winapi::DWORD_ flProtect);
+BOOST_SYMBOL_IMPORT boost::winapi::BOOL_ BOOST_WINAPI_WINAPI_CC
+VirtualFree(boost::winapi::LPVOID_ lpAddress, boost::winapi::SIZE_T_ dwSize,
+            boost::winapi::DWORD_ dwFreeType);
+}
+#endif
+namespace boost {
+namespace winapi {
+using ::VirtualAlloc;
+using ::VirtualFree;
+#if defined(BOOST_USE_WINDOWS_H)
+BOOST_CONSTEXPR_OR_CONST DWORD_ MEM_COMMIT_ = MEM_COMMIT;
+BOOST_CONSTEXPR_OR_CONST DWORD_ MEM_RESERVE_ = MEM_RESERVE;
+BOOST_CONSTEXPR_OR_CONST DWORD_ MEM_DECOMMIT_ = MEM_DECOMMIT;
+BOOST_CONSTEXPR_OR_CONST DWORD_ MEM_RELEASE_ = MEM_RELEASE;
+#else
+BOOST_CONSTEXPR_OR_CONST DWORD_ MEM_COMMIT_ = 0x00001000;
+BOOST_CONSTEXPR_OR_CONST DWORD_ MEM_RESERVE_ = 0x00002000;
+BOOST_CONSTEXPR_OR_CONST DWORD_ MEM_DECOMMIT_ = 0x00004000;
+BOOST_CONSTEXPR_OR_CONST DWORD_ MEM_RELEASE_ = 0x00008000;
+#endif
+} // namespace winapi
+} // namespace boost
 #endif
 
 namespace WasmEdge {
@@ -24,14 +66,14 @@ static inline constexpr const uint64_t k4G = UINT64_C(0x100000000);
 static inline constexpr const uint64_t k8G = UINT64_C(0x200000000);
 static inline constexpr const uint64_t k12G = k4G + k8G;
 
-#ifdef HAVE_MMAP
+#if ALLOC_MMAP
 static std::mutex Mutex;
 static std::set<std::pair<uintptr_t, uintptr_t>> Regions;
 #endif
 } // namespace
 
 uint8_t *Allocator::allocate(uint32_t PageCount) noexcept {
-#ifdef HAVE_MMAP
+#if ALLOC_MMAP
   /// Parse smaps for available memory region
   std::unique_lock Lock(Mutex);
 
@@ -62,14 +104,30 @@ uint8_t *Allocator::allocate(uint32_t PageCount) noexcept {
     }
   }
   return nullptr;
-#else
+#elif MAP_WINAPI
+  auto Reserved = reinterpret_cast<uint8_t *>(
+      boost::winapi::VirtualAlloc(nullptr, k12G, boost::winapi::MEM_RESERVE_,
+                                  boost::winapi::PAGE_NOACCESS_));
+  if (Reserved == nullptr) {
+    return nullptr;
+  }
+  if (PageCount == 0) {
+    return Reserved + k4G;
+  }
+  auto Pointer = resize(Reserved + k4G, 0, PageCount);
+  if (Pointer == nullptr) {
+    return nullptr;
+  }
+  return Pointer;
+#elif MAP_STUB
   return std::malloc(kPageSize);
 #endif
 }
 
 uint8_t *Allocator::resize(uint8_t *Pointer, uint32_t OldPageCount,
                            uint32_t NewPageCount) noexcept {
-#ifdef HAVE_MMAP
+  assert(NewPageCount > OldPageCount);
+#if ALLOC_MMAP
   if (OldPageCount == 0) {
     if (mmap(Pointer, NewPageCount * kPageSize, PROT_READ | PROT_WRITE,
              MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED, -1, 0) == MAP_FAILED) {
@@ -82,13 +140,21 @@ uint8_t *Allocator::resize(uint8_t *Pointer, uint32_t OldPageCount,
     }
   }
   return Pointer;
-#else
+#elif MAP_WINAPI
+  if (boost::winapi::VirtualAlloc(Pointer + OldPageCount * kPageSize,
+                                  (NewPageCount - OldPageCount) * kPageSize,
+                                  boost::winapi::MEM_COMMIT_,
+                                  boost::winapi::PAGE_READWRITE_) == nullptr) {
+    return nullptr;
+  }
+  return Pointer;
+#elif MAP_STUB
   return std::realloc(Pointer, PageCount * kPageSize);
 #endif
 }
 
 void Allocator::release(uint8_t *Pointer, uint32_t PageCount) noexcept {
-#ifdef HAVE_MMAP
+#if ALLOC_MMAP
   if (Pointer == nullptr) {
     return;
   }
@@ -96,7 +162,9 @@ void Allocator::release(uint8_t *Pointer, uint32_t PageCount) noexcept {
   const auto PaddedBegin = reinterpret_cast<uintptr_t>(Pointer) - k4G;
   Regions.erase(std::pair(PaddedBegin, PaddedBegin + k12G));
   munmap(Pointer, PageCount * kPageSize);
-#else
+#elif MAP_WINAPI
+  boost::winapi::VirtualFree(Pointer - k4G, 0, boost::winapi::MEM_RELEASE_);
+#elif MAP_STUB
   return std::free(Pointer);
 #endif
 }

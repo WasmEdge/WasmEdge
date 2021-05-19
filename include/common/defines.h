bc554f349bb95564415bfd7b456d6b0dc42c6229
Shen-Ta Hsieh <beststeve@secondstate.io>
2021-05-19 11:23:53 +0000

    [MISC] Ensure every platform defines is defined
    Signed-off-by: Shen-Ta Hsieh <beststeve@secondstate.io>

--- include/common/defines.h
+++ include/common/defines.h
@@ -10,17 +10,30 @@
 ///
 //===----------------------------------------------------------------------===//
 #pragma once
-#if defined(_WIN32) || defined(_WIN64) || defined(__WIN32__) ||                \
-    defined(__TOS_WIN__) || defined(__WINDOWS__)
-#define WASMEDGE_OS_WINDOWS 1
-#endif
 
 #if defined(linux) || defined(__linux) || defined(__linux__) ||                \
     defined(__gnu_linux__)
-#define WASMEDGE_OS_LINUX 1
-#endif
 
-#if defined(macintosh) || defined(Macintosh) ||                                \
+#define WASMEDGE_OS_LINUX 1
+#define WASMEDGE_OS_MACOS 0
+#define WASMEDGE_OS_WINDOWS 0
+
+#elif defined(macintosh) || defined(Macintosh) ||                              \
     (defined(__APPLE__) && defined(__MACH__))
+
+#define WASMEDGE_OS_LINUX 0
 #define WASMEDGE_OS_MACOS 1
+#define WASMEDGE_OS_WINDOWS 0
+
+#elif defined(_WIN32) || defined(_WIN64) || defined(__WIN32__) ||              \
+    defined(__TOS_WIN__) || defined(__WINDOWS__)
+
+#define WASMEDGE_OS_LINUX 0
+#define WASMEDGE_OS_MACOS 0
+#define WASMEDGE_OS_WINDOWS 1
+
+#else
+
+#error Unsupported environment!
+
 #endif

elliott-wen/SSVM

[Test] Turn on the assert_malformed tests in core test suite.

test/spec/spectest.cpp
@@ -284,6 +284,13 @@ void SpecTest::run(std::string_view Proposal, std::string_view UnitName) {
       EXPECT_TRUE(false);
     }
   };
+  auto TrapLoad = [&](const std::string &Filename, const std::string &Text) {
+    if (auto Res = onLoad(Filename); Res) {
+      EXPECT_TRUE(false);
+    } else {
+      EXPECT_TRUE(onStringContains(Text, SSVM::ErrCodeStr[Res.error()]));
+    }
+  };
   auto TrapInvoke = [&](const rapidjson::Value &Action,
                         const std::string &Text) {
     const auto ModName = GetModuleName(Action);
@@ -316,7 +323,7 @@ void SpecTest::run(std::string_view Proposal, std::string_view UnitName) {
 
   /// Command processing. Return true for expected result.
   auto RunCommand = [&](const rapidjson::Value &Cmd) {
-    /// Line number in wast: It->GetObject()["line"].GetInt()
+    /// Line number in wast: Cmd["line"].Get<uint32_t>()
     if (const auto Type = Cmd.FindMember("type"s); Type != Cmd.MemberEnd()) {
       switch (resolveCommand(Type->value.Get<std::string>())) {
       case SpecTest::CommandID::Module: {
@@ -372,8 +379,16 @@ void SpecTest::run(std::string_view Proposal, std::string_view UnitName) {
         return;
       }
       case CommandID::AssertMalformed: {
-        /// TODO: Wat is not supported in SSVM yet.
-        /// TODO: Add processing binary cases.
+        const auto &ModType = Cmd["module_type"s].Get<std::string>();
+        if (ModType != "binary") {
+          /// TODO: Wat is not supported in SSVM yet.
+          return;
+        }
+        const auto Filename = (TestsuiteRoot / Proposal / UnitName /
+                               Cmd["filename"s].Get<std::string>())
+                                  .u8string();
+        const auto &Text = Cmd["text"s].Get<std::string>();
+        TrapLoad(Filename, Text);
         return;
       }
       case CommandID::AssertInvalid: {

WasmEdge/WasmEdge

Signed-off-by: YiYing He <yiying@secondstate.io>

test/spec/spectest.cpp
@@ -471,6 +471,13 @@ void SpecTest::run(std::string_view Proposal, std::string_view UnitName) {
       EXPECT_TRUE(false);
     }
   };
+  auto TrapLoad = [&](const std::string &Filename, const std::string &Text) {
+    if (auto Res = onLoad(Filename)) {
+      EXPECT_TRUE(false);
+    } else {
+      EXPECT_TRUE(stringContains(Text, WasmEdge::ErrCodeStr[Res.error()]));
+    }
+  };
   auto TrapInvoke = [&](const rapidjson::Value &Action,
                         const std::string &Text) {
     const auto ModName = GetModuleName(Action);
@@ -559,8 +566,16 @@ void SpecTest::run(std::string_view Proposal, std::string_view UnitName) {
         return;
       }
       case CommandID::AssertMalformed: {
-        /// TODO: Wat is not supported in WasmEdge yet.
-        /// TODO: Add processing binary cases.
+        const auto &ModType = Cmd["module_type"s].Get<std::string>();
+        if (ModType != "binary") {
+          /// TODO: Wat is not supported in WasmEdge yet.
+          return;
+        }
+        const auto Filename = (TestsuiteRoot / Proposal / UnitName /
+                               Cmd["filename"s].Get<std::string>())
+                                  .u8string();
+        const auto &Text = Cmd["text"s].Get<std::string>();
+        TrapLoad(Filename, Text);
         return;
       }
       case CommandID::AssertInvalid: {
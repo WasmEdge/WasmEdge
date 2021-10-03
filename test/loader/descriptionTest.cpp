814f7c97acfbab5900ceebc890cc5ac91b233554
YiYing He <yiying@secondstate.io>
2021-10-03 14:27:18 +0800

    [Test] Move the AST loading tests into loader.
    Signed-off-by: YiYing He <yiying@secondstate.io>

--- test/loader/descriptionTest.cpp
+++ test/loader/descriptionTest.cpp
@@ -1,26 +1,40 @@
 // SPDX-License-Identifier: Apache-2.0
-//===-- wasmedge/test/ast/descriptionTest.cpp - AST description unit tests ===//
+//===-- wasmedge/test/loader/descriptionTest.cpp - Load AST description ---===//
 //
 // Part of the WasmEdge Project.
 //
 //===----------------------------------------------------------------------===//
 ///
 /// \file
-/// This file contents unit tests of AST description nodes, which are ImportDesc
-/// and ExportDesc.
+/// This file contents unit tests of loading AST description nodes, which are
+/// ImportDesc and ExportDesc.
 ///
 //===----------------------------------------------------------------------===//
 
-#include "ast/description.h"
+#include "loader/loader.h"
 
 #include "gtest/gtest.h"
 
 namespace {
 
-WasmEdge::FileMgr Mgr;
 WasmEdge::Configure Conf;
+WasmEdge::Loader::Loader Ldr(Conf);
+std::vector<uint8_t> prefixedVec(const std::vector<uint8_t> &Vec) {
+  std::vector<uint8_t> PrefixVec = {
+      0x00U, 0x61U, 0x73U, 0x6DU, /// Magic
+      0x01U, 0x00U, 0x00U, 0x00U  /// Version
+  };
+  PrefixVec.reserve(PrefixVec.size() + Vec.size());
+  PrefixVec.insert(PrefixVec.end(), Vec.begin(), Vec.end());
+  return PrefixVec;
+}
 
 TEST(DescriptionTest, LoadImportDesc) {
+  std::vector<uint8_t> Vec;
+
+  Conf.removeProposal(WasmEdge::Proposal::ImportExportMutGlobals);
+  WasmEdge::Loader::Loader LdrNoImpMutGlob(Conf);
+
   /// 1. Test load import description.
   ///
   ///   1.  Load invalid empty import description.
@@ -32,38 +46,48 @@ TEST(DescriptionTest, LoadImportDesc) {
   ///   7.  Load import description of global type.
   ///   8.  Load invalid import description of global type without Mut-Globals
   ///       proposal.
-  Mgr.setCode(std::vector<uint8_t>());
-  WasmEdge::AST::ImportDesc Imp1;
-  EXPECT_FALSE(Imp1.loadBinary(Mgr, Conf));
 
-  std::vector<unsigned char> Vec2 = {
+  Vec = {
+      0x02U, /// Import section
+      0x01U, /// Content size = 1
+      0x01U  /// Vector length = 1
+  };
+  EXPECT_FALSE(Ldr.parseModule(prefixedVec(Vec)));
+
+  Vec = {
+      0x02U,       /// Import section
+      0x05U,       /// Content size = 5
+      0x01U,       /// Vector length = 1
       0x00U,       /// Empty module name
       0x00U,       /// Empty external name
       0x00U, 0x00U /// function type and index
   };
-  Mgr.setCode(Vec2);
-  WasmEdge::AST::ImportDesc Imp2;
-  EXPECT_TRUE(Imp2.loadBinary(Mgr, Conf) && Mgr.getRemainSize() == 0);
+  EXPECT_TRUE(Ldr.parseModule(prefixedVec(Vec)));
 
-  std::vector<unsigned char> Vec3 = {
+  Vec = {
+      0x02U,                                           /// Import section
+      0x0FU,                                           /// Content size = 15
+      0x01U,                                           /// Vector length = 1
       0x04U, 0x74U, 0x65U, 0x73U, 0x74U,               /// Module name: test
       0x06U, 0x4CU, 0x6FU, 0x61U, 0x64U, 0x65U, 0x72U, /// External name: Loader
       0x00U, 0x00U /// function type and index
   };
-  Mgr.setCode(Vec3);
-  WasmEdge::AST::ImportDesc Imp3;
-  EXPECT_TRUE(Imp3.loadBinary(Mgr, Conf) && Mgr.getRemainSize() == 0);
+  EXPECT_TRUE(Ldr.parseModule(prefixedVec(Vec)));
 
-  std::vector<unsigned char> Vec4 = {
+  Vec = {
+      0x02U,                                           /// Import section
+      0x0EU,                                           /// Content size = 14
+      0x01U,                                           /// Vector length = 1
       0x04U, 0x74U, 0x65U, 0x73U, 0x74U,               /// Module name: test
       0x06U, 0x4CU, 0x6FU, 0x61U, 0x64U, 0x65U, 0x72U, /// External name: Loader
       0x04U, 0x00U                                     /// Invalid external type
   };
-  Mgr.setCode(Vec4);
-  WasmEdge::AST::ImportDesc Imp4;
-  EXPECT_FALSE(Imp4.loadBinary(Mgr, Conf));
+  EXPECT_FALSE(Ldr.parseModule(prefixedVec(Vec)));
 
-  std::vector<unsigned char> Vec5 = {
+  Vec = {
+      0x02U,                                           /// Import section
+      0x1AU,                                           /// Content size = 26
+      0x01U,                                           /// Vector length = 1
       0x04U, 0x74U, 0x65U, 0x73U, 0x74U,               /// Module name: test
       0x06U, 0x4CU, 0x6FU, 0x61U, 0x64U, 0x65U, 0x72U, /// External name: Loader
       0x01U,                                           /// Table type
@@ -72,11 +96,12 @@ TEST(DescriptionTest, LoadImportDesc) {
       0xF1U, 0xFFU, 0xFFU, 0xFFU, 0x0FU,               /// Min = 4294967281
       0xFFU, 0xFFU, 0xFFU, 0xFFU, 0x0FU                /// Max = 4294967295
   };
-  Mgr.setCode(Vec5);
-  WasmEdge::AST::ImportDesc Imp5;
-  EXPECT_TRUE(Imp5.loadBinary(Mgr, Conf) && Mgr.getRemainSize() == 0);
+  EXPECT_TRUE(Ldr.parseModule(prefixedVec(Vec)));
 
-  std::vector<unsigned char> Vec6 = {
+  Vec = {
+      0x02U,                                           /// Import section
+      0x19U,                                           /// Content size = 25
+      0x01U,                                           /// Vector length = 1
       0x04U, 0x74U, 0x65U, 0x73U, 0x74U,               /// Module name: test
       0x06U, 0x4CU, 0x6FU, 0x61U, 0x64U, 0x65U, 0x72U, /// External name: Loader
       0x02U,                                           /// Memory type
@@ -84,36 +109,34 @@ TEST(DescriptionTest, LoadImportDesc) {
       0xF1U, 0xFFU, 0xFFU, 0xFFU, 0x0FU,               /// Min = 4294967281
       0xFFU, 0xFFU, 0xFFU, 0xFFU, 0x0FU                /// Max = 4294967295
   };
-  Mgr.setCode(Vec6);
-  WasmEdge::AST::ImportDesc Imp6;
-  EXPECT_TRUE(Imp6.loadBinary(Mgr, Conf) && Mgr.getRemainSize() == 0);
+  EXPECT_TRUE(Ldr.parseModule(prefixedVec(Vec)));
 
-  std::vector<unsigned char> Vec7 = {
+  Vec = {
+      0x02U,                                           /// Import section
+      0x10U,                                           /// Content size = 16
+      0x01U,                                           /// Vector length = 1
       0x04U, 0x74U, 0x65U, 0x73U, 0x74U,               /// Module name: test
       0x06U, 0x4CU, 0x6FU, 0x61U, 0x64U, 0x65U, 0x72U, /// External name: Loader
       0x03U,                                           /// Global type
       0x7CU, 0x00U                                     /// Const F64 number type
   };
-  Mgr.setCode(Vec7);
-  WasmEdge::AST::ImportDesc Imp7;
-  EXPECT_TRUE(Imp7.loadBinary(Mgr, Conf) && Mgr.getRemainSize() == 0);
+  EXPECT_TRUE(Ldr.parseModule(prefixedVec(Vec)));
 
-  Conf.removeProposal(WasmEdge::Proposal::ImportExportMutGlobals);
-
-  std::vector<unsigned char> Vec8 = {
+  Vec = {
+      0x02U,                                           /// Import section
+      0x10U,                                           /// Content size = 16
+      0x01U,                                           /// Vector length = 1
       0x04U, 0x74U, 0x65U, 0x73U, 0x74U,               /// Module name: test
       0x06U, 0x4CU, 0x6FU, 0x61U, 0x64U, 0x65U, 0x72U, /// External name: Loader
       0x03U,                                           /// Global type
       0x7CU, 0x01U                                     /// Mut F64 number type
   };
-  Mgr.setCode(Vec8);
-  WasmEdge::AST::ImportDesc Imp8;
-  EXPECT_FALSE(Imp8.loadBinary(Mgr, Conf));
-
-  Conf.addProposal(WasmEdge::Proposal::ImportExportMutGlobals);
+  EXPECT_FALSE(LdrNoImpMutGlob.parseModule(prefixedVec(Vec)));
 }
 
 TEST(DescriptionTest, LoadExportDesc) {
+  std::vector<uint8_t> Vec;
+
   /// 2. Test load export description.
   ///
   ///   1.  Load invalid empty export description.
@@ -121,41 +144,48 @@ TEST(DescriptionTest, LoadExportDesc) {
   ///   3.  Load export description with non-empty module name.
   ///   4.  Load export description with invalid external type.
   ///   5.  Load export description of table type.
-  Mgr.setCode(std::vector<uint8_t>());
-  WasmEdge::AST::ExportDesc Exp1;
-  EXPECT_FALSE(Exp1.loadBinary(Mgr, Conf));
 
-  std::vector<unsigned char> Vec2 = {
+  Vec = {
+      0x07U, /// Export section
+      0x01U, /// Content size = 1
+      0x01U  /// Vector length = 1
+  };
+  EXPECT_FALSE(Ldr.parseModule(prefixedVec(Vec)));
+
+  Vec = {
+      0x07U,       /// Export section
+      0x04U,       /// Content size = 4
+      0x01U,       /// Vector length = 1
       0x00U,       /// Empty external name
       0x00U, 0x00U /// function type and index
   };
-  Mgr.setCode(Vec2);
-  WasmEdge::AST::ExportDesc Exp2;
-  EXPECT_TRUE(Exp2.loadBinary(Mgr, Conf) && Mgr.getRemainSize() == 0);
+  EXPECT_TRUE(Ldr.parseModule(prefixedVec(Vec)));
 
-  std::vector<unsigned char> Vec3 = {
+  Vec = {
+      0x07U,                                           /// Export section
+      0x0AU,                                           /// Content size = 10
+      0x01U,                                           /// Vector length = 1
       0x06U, 0x4CU, 0x6FU, 0x61U, 0x64U, 0x65U, 0x72U, /// External name: Loader
       0x00U, 0x00U /// function type and index
   };
-  Mgr.setCode(Vec3);
-  WasmEdge::AST::ExportDesc Exp3;
-  EXPECT_TRUE(Exp3.loadBinary(Mgr, Conf) && Mgr.getRemainSize() == 0);
+  EXPECT_TRUE(Ldr.parseModule(prefixedVec(Vec)));
 
-  std::vector<unsigned char> Vec4 = {
+  Vec = {
+      0x07U,                                           /// Export section
+      0x0AU,                                           /// Content size = 10
+      0x01U,                                           /// Vector length = 1
       0x06U, 0x4CU, 0x6FU, 0x61U, 0x64U, 0x65U, 0x72U, /// External name: Loader
       0x04U, 0x00U                                     /// Invalid external type
   };
-  Mgr.setCode(Vec4);
-  WasmEdge::AST::ExportDesc Exp4;
-  EXPECT_FALSE(Exp4.loadBinary(Mgr, Conf));
+  EXPECT_FALSE(Ldr.parseModule(prefixedVec(Vec)));
 
-  std::vector<unsigned char> Vec5 = {
+  Vec = {
+      0x07U,                                           /// Export section
+      0x0EU,                                           /// Content size = 14
+      0x01U,                                           /// Vector length = 1
       0x06U, 0x4CU, 0x6FU, 0x61U, 0x64U, 0x65U, 0x72U, /// External name: Loader
       0x01U, 0xFFU, 0xFFU, 0xFFU, 0xFFU, 0x0FU /// Table type and table index
   };
-  Mgr.setCode(Vec5);
-  WasmEdge::AST::ExportDesc Exp5;
-  EXPECT_TRUE(Exp5.loadBinary(Mgr, Conf) && Mgr.getRemainSize() == 0);
+  EXPECT_TRUE(Ldr.parseModule(prefixedVec(Vec)));
 }
-
 } // namespace
--- test/loader/moduleTest.cpp
+++ test/loader/moduleTest.cpp
@@ -1,40 +1,37 @@
 // SPDX-License-Identifier: Apache-2.0
-//===-- wasmedge/test/ast/moduleTest.cpp - AST module unit tests ----------===//
+//===-- wasmedge/test/loader/moduleTest.cpp - Load AST module unit tests --===//
 //
 // Part of the WasmEdge Project.
 //
 //===----------------------------------------------------------------------===//
 ///
 /// \file
-/// This file contents unit tests of AST module node and the main function.
+/// This file contents unit tests of loading AST module node and the main
+/// function.
 ///
 //===----------------------------------------------------------------------===//
 
-#include "ast/module.h"
+#include "loader/loader.h"
 
 #include "gtest/gtest.h"
 
 namespace {
 
-WasmEdge::FileMgr Mgr;
 WasmEdge::Configure Conf;
+WasmEdge::Loader::Loader Ldr(Conf);
 
 TEST(ModuleTest, LoadModule) {
+  std::vector<uint8_t> Vec;
+
   /// 1. Test load empty file
-  Mgr.setCode(std::vector<uint8_t>());
-  WasmEdge::AST::Module Mod1;
-  EXPECT_FALSE(Mod1.loadBinary(Mgr, Conf));
+  EXPECT_FALSE(Ldr.parseModule(Vec));
 
   /// 2. Test load empty module
-  WasmEdge::AST::Module Mod2;
-  std::vector<unsigned char> Vec2 = {0x00U, 0x61U, 0x73U, 0x6DU,
-                                     0x01U, 0x00U, 0x00U, 0x00U};
-  Mgr.setCode(Vec2);
-  EXPECT_TRUE(Mod2.loadBinary(Mgr, Conf) && Mgr.getRemainSize() == 0);
+  Vec = {0x00U, 0x61U, 0x73U, 0x6DU, 0x01U, 0x00U, 0x00U, 0x00U};
+  EXPECT_TRUE(Ldr.parseModule(Vec));
 
   /// 3. Test load module with valid empty sections
-  WasmEdge::AST::Module Mod3;
-  std::vector<unsigned char> Vec3 = {
+  Vec = {
       0x00U, 0x61U, 0x73U, 0x6DU,                      /// Magic
       0x01U, 0x00U, 0x00U, 0x00U,                      /// Version
       0x00U, 0x81U, 0x80U, 0x80U, 0x80U, 0x00U, 0x00U, /// Custom section
@@ -50,12 +47,10 @@ TEST(ModuleTest, LoadModule) {
       0x0AU, 0x81U, 0x80U, 0x80U, 0x80U, 0x00U, 0x00U, /// Code section
       0x0BU, 0x81U, 0x80U, 0x80U, 0x80U, 0x00U, 0x00U  /// Data section
   };
-  Mgr.setCode(Vec3);
-  EXPECT_TRUE(Mod3.loadBinary(Mgr, Conf) && Mgr.getRemainSize() == 0);
+  EXPECT_TRUE(Ldr.parseModule(Vec));
 
   /// 4. Test load module with invalid sections
-  WasmEdge::AST::Module Mod4;
-  std::vector<unsigned char> Vec4 = {
+  Vec = {
       0x00U, 0x61U, 0x73U, 0x6DU,                      /// Magic
       0x01U, 0x00U, 0x00U, 0x00U,                      /// Version
       0x00U, 0x81U, 0x80U, 0x80U, 0x80U, 0x00U, 0x00U, /// Custom section
@@ -72,181 +67,153 @@ TEST(ModuleTest, LoadModule) {
       0x0BU, 0x81U, 0x80U, 0x80U, 0x80U, 0x00U, 0x00U, /// Data section
       0x0DU, 0x81U, 0x80U, 0x80U, 0x80U, 0x00U, 0x00U  /// Invalid section
   };
-  Mgr.setCode(Vec4);
-  EXPECT_FALSE(Mod4.loadBinary(Mgr, Conf));
+  EXPECT_FALSE(Ldr.parseModule(Vec));
 }
 
 TEST(ModuleTest, LoadDataCountSecModule) {
+  Conf.removeProposal(WasmEdge::Proposal::BulkMemoryOperations);
+  Conf.removeProposal(WasmEdge::Proposal::ReferenceTypes);
+  WasmEdge::Loader::Loader LdrNoRefType(Conf);
+  std::vector<uint8_t> Vec;
+
   /// 5. Test load module with invalid datacount section without proposals.
-  WasmEdge::AST::Module Mod5;
-  std::vector<unsigned char> Vec5 = {
+  Vec = {
       0x00U, 0x61U, 0x73U, 0x6DU,       /// Magic
       0x01U, 0x00U, 0x00U, 0x00U,       /// Version
       0x0CU, 0x01U, 0x01U,              /// DataCount section
       0x0BU, 0x03U, 0x01U, 0x01U, 0x00U /// Data section
   };
-  Mgr.setCode(Vec5);
-  Conf.removeProposal(WasmEdge::Proposal::BulkMemoryOperations);
-  Conf.removeProposal(WasmEdge::Proposal::ReferenceTypes);
-  EXPECT_FALSE(Mod5.loadBinary(Mgr, Conf));
-  Conf.addProposal(WasmEdge::Proposal::BulkMemoryOperations);
-  Conf.addProposal(WasmEdge::Proposal::ReferenceTypes);
+  EXPECT_FALSE(LdrNoRefType.parseModule(Vec));
 
   /// 6. Test load module with invalid datacount section.
-  WasmEdge::AST::Module Mod6;
-  std::vector<unsigned char> Vec6 = {
+  Vec = {
       0x00U, 0x61U, 0x73U, 0x6DU, /// Magic
       0x01U, 0x00U, 0x00U, 0x00U, /// Version
       0x0CU, 0x00U                /// DataCount section, miss data count
   };
-  Mgr.setCode(Vec6);
-  EXPECT_FALSE(Mod6.loadBinary(Mgr, Conf));
+  EXPECT_FALSE(Ldr.parseModule(Vec));
 }
 
 TEST(ModuleTest, LoadStartSecModule) {
   /// 7. Test load module with invalid start section.
-  WasmEdge::AST::Module Mod;
   std::vector<unsigned char> Vec = {
       0x00U, 0x61U, 0x73U, 0x6DU, /// Magic
       0x01U, 0x00U, 0x00U, 0x00U, /// Version
       0x08U, 0x00U                /// Start section, miss index
   };
-  Mgr.setCode(Vec);
-  EXPECT_FALSE(Mod.loadBinary(Mgr, Conf));
+  EXPECT_FALSE(Ldr.parseModule(Vec));
 }
 
 TEST(ModuleTest, LoadDupSecModule) {
+  std::vector<uint8_t> Vec;
+
   /// 8. Test load module with duplicated type section.
-  WasmEdge::AST::Module Mod1;
-  std::vector<unsigned char> Vec1 = {
+  Vec = {
       0x00U, 0x61U, 0x73U, 0x6DU, /// Magic
       0x01U, 0x00U, 0x00U, 0x00U, /// Version
       0x01U, 0x01U, 0x00U,        /// Type section
       0x01U, 0x01U, 0x00U         /// Type section duplicated
   };
-  Mgr.setCode(Vec1);
-  EXPECT_FALSE(Mod1.loadBinary(Mgr, Conf));
+  EXPECT_FALSE(Ldr.parseModule(Vec));
 
   /// 9. Test load module with duplicated import section.
-  WasmEdge::AST::Module Mod2;
-  std::vector<unsigned char> Vec2 = {
+  Vec = {
       0x00U, 0x61U, 0x73U, 0x6DU, /// Magic
       0x01U, 0x00U, 0x00U, 0x00U, /// Version
       0x02U, 0x01U, 0x00U,        /// Import section
       0x02U, 0x01U, 0x00U         /// Import section duplicated
   };
-  Mgr.setCode(Vec2);
-  EXPECT_FALSE(Mod2.loadBinary(Mgr, Conf));
+  EXPECT_FALSE(Ldr.parseModule(Vec));
 
   /// 10. Test load module with duplicated function section.
-  WasmEdge::AST::Module Mod3;
-  std::vector<unsigned char> Vec3 = {
+  Vec = {
       0x00U, 0x61U, 0x73U, 0x6DU, /// Magic
       0x01U, 0x00U, 0x00U, 0x00U, /// Version
       0x03U, 0x01U, 0x00U,        /// Function section
       0x03U, 0x01U, 0x00U         /// Function section duplicated
   };
-  Mgr.setCode(Vec3);
-  EXPECT_FALSE(Mod3.loadBinary(Mgr, Conf));
+  EXPECT_FALSE(Ldr.parseModule(Vec));
 
   /// 11. Test load module with duplicated table section.
-  WasmEdge::AST::Module Mod4;
-  std::vector<unsigned char> Vec4 = {
+  Vec = {
       0x00U, 0x61U, 0x73U, 0x6DU, /// Magic
       0x01U, 0x00U, 0x00U, 0x00U, /// Version
       0x04U, 0x01U, 0x00U,        /// Table section
       0x04U, 0x01U, 0x00U         /// Table section duplicated
   };
-  Mgr.setCode(Vec4);
-  EXPECT_FALSE(Mod4.loadBinary(Mgr, Conf));
+  EXPECT_FALSE(Ldr.parseModule(Vec));
 
   /// 12. Test load module with duplicated memory section.
-  WasmEdge::AST::Module Mod5;
-  std::vector<unsigned char> Vec5 = {
+  Vec = {
       0x00U, 0x61U, 0x73U, 0x6DU, /// Magic
       0x01U, 0x00U, 0x00U, 0x00U, /// Version
       0x05U, 0x01U, 0x00U,        /// Memory section
       0x05U, 0x01U, 0x00U         /// Memory section duplicated
   };
-  Mgr.setCode(Vec5);
-  EXPECT_FALSE(Mod5.loadBinary(Mgr, Conf));
+  EXPECT_FALSE(Ldr.parseModule(Vec));
 
   /// 13. Test load module with duplicated global section.
-  WasmEdge::AST::Module Mod6;
-  std::vector<unsigned char> Vec6 = {
+  Vec = {
       0x00U, 0x61U, 0x73U, 0x6DU, /// Magic
       0x01U, 0x00U, 0x00U, 0x00U, /// Version
       0x06U, 0x01U, 0x00U,        /// Global section
       0x06U, 0x01U, 0x00U         /// Global section duplicated
   };
-  Mgr.setCode(Vec6);
-  EXPECT_FALSE(Mod6.loadBinary(Mgr, Conf));
+  EXPECT_FALSE(Ldr.parseModule(Vec));
 
   /// 14. Test load module with duplicated export section.
-  WasmEdge::AST::Module Mod7;
-  std::vector<unsigned char> Vec7 = {
+  Vec = {
       0x00U, 0x61U, 0x73U, 0x6DU, /// Magic
       0x01U, 0x00U, 0x00U, 0x00U, /// Version
       0x07U, 0x01U, 0x00U,        /// Export section
       0x07U, 0x01U, 0x00U         /// Export section duplicated
   };
-  Mgr.setCode(Vec7);
-  EXPECT_FALSE(Mod7.loadBinary(Mgr, Conf));
+  EXPECT_FALSE(Ldr.parseModule(Vec));
 
   /// 15. Test load module with duplicated start section.
-  WasmEdge::AST::Module Mod8;
-  std::vector<unsigned char> Vec8 = {
+  Vec = {
       0x00U, 0x61U, 0x73U, 0x6DU, /// Magic
       0x01U, 0x00U, 0x00U, 0x00U, /// Version
       0x08U, 0x01U, 0x00U,        /// Start section
       0x08U, 0x01U, 0x00U         /// Start section duplicated
   };
-  Mgr.setCode(Vec8);
-  EXPECT_FALSE(Mod8.loadBinary(Mgr, Conf));
+  EXPECT_FALSE(Ldr.parseModule(Vec));
 
   /// 16. Test load module with duplicated element section.
-  WasmEdge::AST::Module Mod9;
-  std::vector<unsigned char> Vec9 = {
+  Vec = {
       0x00U, 0x61U, 0x73U, 0x6DU, /// Magic
       0x01U, 0x00U, 0x00U, 0x00U, /// Version
       0x09U, 0x01U, 0x00U,        /// Element section
       0x09U, 0x01U, 0x00U         /// Element section duplicated
   };
-  Mgr.setCode(Vec9);
-  EXPECT_FALSE(Mod9.loadBinary(Mgr, Conf));
+  EXPECT_FALSE(Ldr.parseModule(Vec));
 
   /// 17. Test load module with duplicated code section.
-  WasmEdge::AST::Module ModA;
-  std::vector<unsigned char> VecA = {
+  Vec = {
       0x00U, 0x61U, 0x73U, 0x6DU, /// Magic
       0x01U, 0x00U, 0x00U, 0x00U, /// Version
       0x0AU, 0x01U, 0x00U,        /// Code section
       0x0AU, 0x01U, 0x00U         /// Code section duplicated
   };
-  Mgr.setCode(VecA);
-  EXPECT_FALSE(ModA.loadBinary(Mgr, Conf));
+  EXPECT_FALSE(Ldr.parseModule(Vec));
 
   /// 18. Test load module with duplicated data section.
-  WasmEdge::AST::Module ModB;
-  std::vector<unsigned char> VecB = {
+  Vec = {
       0x00U, 0x61U, 0x73U, 0x6DU, /// Magic
       0x01U, 0x00U, 0x00U, 0x00U, /// Version
       0x0BU, 0x01U, 0x00U,        /// Data section
       0x0BU, 0x01U, 0x00U         /// Data section duplicated
   };
-  Mgr.setCode(VecB);
-  EXPECT_FALSE(ModB.loadBinary(Mgr, Conf));
+  EXPECT_FALSE(Ldr.parseModule(Vec));
 
   /// 19. Test load module with duplicated datacount section.
-  WasmEdge::AST::Module ModC;
-  std::vector<unsigned char> VecC = {
+  Vec = {
       0x00U, 0x61U, 0x73U, 0x6DU, /// Magic
       0x01U, 0x00U, 0x00U, 0x00U, /// Version
       0x0CU, 0x01U, 0x00U,        /// Datacount section
       0x0CU, 0x01U, 0x00U         /// Datacount section duplicated
   };
-  Mgr.setCode(VecC);
-  EXPECT_FALSE(ModC.loadBinary(Mgr, Conf));
+  EXPECT_FALSE(Ldr.parseModule(Vec));
 }
 
 } // namespace

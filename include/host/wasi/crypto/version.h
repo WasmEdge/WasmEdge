1311d77eb995c97578447c6a1fc4f495b66d8f3a
sonder-joker <highmagic@outlook.com>
2021-10-03 04:12:16 +0000

    [WASI] Implementation Version
    Signed-off-by: sonder-joker <highmagic@outlook.com>

---
+++ include/host/wasi/crypto/version.h
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: Apache-2.0
+#pragma once
+
+#include "wasi/crypto/api.hpp"
+#include <cstdint>
+
+namespace WasmEdge {
+namespace Host {
+namespace WASI {
+namespace Crypto {
+/// Version of a managed key.
+///
+/// A version can be an arbitrary `u64` integer, with the expection of some
+/// reserved values.
+namespace Version {
+
+/// Key doesn't support versioning.
+inline constexpr __wasi_version_t UNSPECIFIED = 0xff00000000000000;
+/// Use the latest version of a key.
+inline constexpr __wasi_version_t LASTEST = 0xff00000000000001;
+/// Perform an operation over all versions of a key
+inline constexpr __wasi_version_t ALL = 0xff00000000000002;
+
+} // namespace Version
+} // namespace Crypto
+} // namespace WASI
+} // namespace Host
+} // namespace WasmEdge

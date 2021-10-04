9a2db23bbc98c13b61180bfd350001cee626995a
YiYing He <yiying@secondstate.io>
2021-10-05 03:37:16 +0800

    [Validator] Apply the new AST node structures.
    Signed-off-by: YiYing He <yiying@secondstate.io>

--- include/validator/validator.h
+++ include/validator/validator.h
@@ -14,7 +14,6 @@
 
 #include "ast/module.h"
 #include "common/configure.h"
-#include "common/errcode.h"
 #include "validator/formchecker.h"
 
 #include <memory>
@@ -33,7 +32,7 @@ public:
 
 private:
   /// Validate AST::Types
-  Expect<void> validate(const Limit &Lim);
+  Expect<void> validate(const AST::Limit &Lim);
   Expect<void> validate(const AST::TableType &Tab);
   Expect<void> validate(const AST::MemoryType &Mem);
   /// GlobalType is always valid.

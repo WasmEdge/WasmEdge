55c9d0aa711a649f1db028c3647a22c6dc301fb1
Antonio Yang <yanganto@gmail.com>
2021-11-11 16:54:41 +0800

    [Rust] Add host function example and implementation
    Signed-off-by: Antonio Yang <yanganto@gmail.com>

--- bindings/rust/wasmedge-sys/src/lib.rs
+++ bindings/rust/wasmedge-sys/src/lib.rs
@@ -1,5 +1,7 @@
 #![deny(rust_2018_idioms, unreachable_pub)]
 
+use std::{cell::RefCell, collections::HashMap};
+
 #[allow(warnings)]
 pub mod wasmedge {
     include!(concat!(env!("OUT_DIR"), "/wasmedge.rs"));
@@ -8,6 +10,7 @@ pub mod wasmedge {
 pub mod config;
 pub mod import_obj;
 pub mod instance;
+pub mod io;
 pub mod module;
 pub mod raw_result;
 pub mod string;
@@ -17,6 +20,9 @@ pub mod vm;
 pub mod wasi;
 
 pub use config::{Config, OptLevel};
+pub use import_obj::ImportObj;
+pub(crate) use io::WasmFnIO;
+pub use io::{I1, I2};
 pub use module::Module;
 pub use raw_result::ErrReport;
 pub use string::{StringBuf, StringRef, WasmEdgeString};
@@ -24,6 +30,13 @@ pub use value::Value;
 pub use version::{full_version, semv_version};
 pub use vm::Vm;
 
+thread_local! {
+    // TODO: allow modify capacity before running
+    #[allow(clippy::type_complexity)]
+    static HOST_FUNCS:
+      RefCell<
+        HashMap<usize, Box<dyn Fn(Vec<Value>) -> Result<Vec<Value>, u8>>>> = RefCell::new(HashMap::with_capacity(500));
+}
 #[cfg(test)]
 mod tests {
     use super::*;

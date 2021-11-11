3dd852eb279d289e39399f3bc989e2ba98edf4ad
Antonio Yang <yanganto@gmail.com>
2021-11-11 11:45:52 +0800

    [Rust] Init Rust function wraper
    Signed-off-by: Antonio Yang <yanganto@gmail.com>

--- bindings/rust/wasmedge-sys/src/instance/function.rs
+++ bindings/rust/wasmedge-sys/src/instance/function.rs
@@ -1,5 +1,22 @@
 use crate::{wasmedge, Value};
 
+use core::ffi::c_void;
+
+extern "C" fn wraper_fn(
+    _real_fn: *mut c_void,
+    _data: *mut c_void,
+    _mem_cxt: *mut wasmedge::WasmEdge_MemoryInstanceContext,
+    _params: *const wasmedge::WasmEdge_Value,
+    _param_len: u32,
+    _returns: *mut wasmedge::WasmEdge_Value,
+    _return_len: u32,
+) -> wasmedge::WasmEdge_Result {
+    wasmedge::WasmEdge_Result {
+        // TODO: binding error code and replace this
+        Code: 0,
+    }
+}
+
 pub trait WasmFnIO {
     fn parameters() -> Vec<Value> {
         Vec::new()
@@ -12,14 +29,35 @@ pub struct Function {
 }
 
 impl Function {
+    /// wasmedge::WasmEdge_FunctionInstanceCreate take C functions
+    /// This may not be implement
     pub fn create<I, O>(_f: Box<dyn std::ops::FnOnce(I) -> O>) -> Self
     where
         I: WasmFnIO,
         O: WasmFnIO,
     {
+        unimplemented!()
+    }
+
+    // TODO:
+    // binding errors and restict the error types
+    /// Binding Rust function (HostFunction) to WasmEdgeFunction
+    pub fn create_bindings<I, O>(real_fn: Box<dyn std::ops::Fn(I) -> Result<O, usize>>) -> Self
+    where
+        I: WasmFnIO,
+        O: WasmFnIO,
+    {
+        let real_fn_ptr =
+            &*real_fn as *const dyn std::ops::Fn(I) -> Result<O, usize> as *mut c_void;
         let t = Type::create(I::parameters(), O::parameters());
         let ctx = unsafe {
-            wasmedge::WasmEdge_FunctionInstanceCreate(t.ctx, None, std::ptr::null_mut(), 0)
+            wasmedge::WasmEdge_FunctionInstanceCreateBinding(
+                t.ctx,
+                Some(wraper_fn),
+                real_fn_ptr,
+                std::ptr::null_mut(),
+                0,
+            )
         };
         Self { ctx }
     }

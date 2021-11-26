e7512c40e00c1eac053a9a5c2f5dfbc97b03af5c
Kenvi Zhu <mrwhite@163.com>
2021-11-27 07:21:39 +0800

    upgrade junit & add exception cases
    Signed-off-by: Kenvi Zhu <mrwhite@163.com>

--- bindings/java/wasmedge-java/src/main/java/org/wasmedge/StatisticsContext.java
+++ bindings/java/wasmedge-java/src/main/java/org/wasmedge/StatisticsContext.java
@@ -1,9 +1,27 @@
 package org.wasmedge;
 
 public class StatisticsContext {
+    private long pointer;
 
     public StatisticsContext() {
 
     }
+
+    private native void nativeInit();
+
+    public native int getInstrCount();
+
+    public native double getInstrPerSecond();
+
+    public native void setCostTable(long[] costTable);
+
+    public native long getTotalCost();
+
+    public void destroy() {
+        delete();
+        pointer = 0;
+    }
+
+    public native void delete();
 
 }
--- bindings/java/wasmedge-java/src/test/java/WasmEdgeVMTest.java
+++ bindings/java/wasmedge-java/src/test/java/WasmEdgeVMTest.java
@@ -3,7 +3,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.wasmedge.ConfigureContext;
 import org.wasmedge.StoreContext;
-import org.wasmedge.VMContext;
 import org.wasmedge.WasmEdge;
 import org.wasmedge.WasmEdgeI32Value;
 import org.wasmedge.WasmEdgeVM;
@@ -11,6 +10,7 @@ import org.wasmedge.WasmEdgeValue;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class WasmEdgeVMTest {
     @Before
@@ -35,9 +35,7 @@ public class WasmEdgeVMTest {
 
     @Test
     public void testRunStepByStep(){
-        System.out.println("Start testing");
         WasmEdgeVM vm = new WasmEdgeVM(new ConfigureContext(), new StoreContext());
-        System.out.println("Start testing");
         vm.loadWasmFromFile("/root/fibonacci.wasm");
         vm.validate();
         vm.instantiate();
@@ -50,6 +48,34 @@ public class WasmEdgeVMTest {
 
         Assert.assertEquals(3, ((WasmEdgeI32Value) returns.get(0)).getValue());
         vm.destroy();
+    }
 
+    @Test(expected = Exception.class)
+    public void testInvalidPath () {
+        WasmEdgeVM vm = new WasmEdgeVM(new ConfigureContext(), new StoreContext());
+        vm.loadWasmFromFile("/root/invalid_path.wasm");
+    }
+
+    @Test(expected = Exception.class)
+    public void testInvalidFuncName() {
+        WasmEdgeVM vm = new WasmEdgeVM(new ConfigureContext(), new StoreContext());
+        vm.loadWasmFromFile("/root/fibonacci.wasm");
+        vm.validate();
+        vm.instantiate();
+        List<WasmEdgeValue> params = new ArrayList<>();
+        params.add(new WasmEdgeI32Value(3));
+
+        List<WasmEdgeValue> returns = new ArrayList<>();
+        returns.add(new WasmEdgeI32Value());
+        vm.execute(UUID.randomUUID().toString(), params, returns);
+    }
+
+
+    @Test(expected = Exception.class)
+    public void testInvalidFlow() {
+        WasmEdgeVM vm = new WasmEdgeVM(new ConfigureContext(), new StoreContext());
+        vm.loadWasmFromFile("/root/fibonacci.wasm");
+        vm.instantiate();
+        vm.validate();
     }
 }

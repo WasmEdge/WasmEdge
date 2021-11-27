510b7bcc8d13acbc4c76c73331e790daedfa1bed
Xin Liu <xin.sam.liu@hotmail.com>
2021-11-27 12:28:52 +0000

    [Rust][feat]: add new APIs

--- bindings/rust/wasmedge-sys/src/module.rs
+++ bindings/rust/wasmedge-sys/src/module.rs
@@ -117,4 +117,55 @@ mod tests {
         let module = result.unwrap();
         assert!(!module.ctx.is_null());
     }
+
+    pub fn load_from_buffer(config: &crate::config::Config, buffer: &[u8]) -> WasmEdgeResult<Self> {
+        let loader_ctx = unsafe { wasmedge::WasmEdge_LoaderCreate(config.ctx) };
+        let mut ctx: *mut wasmedge::WasmEdge_ASTModuleContext = std::ptr::null_mut();
+
+        unsafe {
+            check(wasmedge::WasmEdge_LoaderParseFromBuffer(
+                loader_ctx,
+                &mut ctx as *mut _,
+                buffer.as_ptr(),
+                buffer.len() as u32,
+            ))?;
+        }
+
+        assert!(!ctx.is_null(), "WasmEdge failed to load from buffer!");
+
+        Ok(Self { ctx })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Module;
+    use crate::Config;
+
+    #[test]
+    fn test_module_from_buffer() {
+        let wasm_path = std::path::PathBuf::from(env!("WASMEDGE_SRC_DIR"))
+            .join("test/api/apiTestData/test.wasm");
+        let result = std::fs::read(wasm_path);
+        assert!(result.is_ok());
+        let buf = result.unwrap();
+
+        let conf = Config::default().enable_bulkmemoryoperations(true);
+        assert!(conf.has_bulkmemoryoperations());
+
+        let result = Module::load_from_buffer(&conf, &buf);
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn test_module_from_file() {
+        let path = std::path::PathBuf::from(env!("WASMEDGE_SRC_DIR"))
+            .join("test/api/apiTestData/test.wasm");
+
+        let conf = Config::default().enable_bulkmemoryoperations(true);
+        assert!(conf.has_bulkmemoryoperations());
+
+        let result = Module::load_from_file(&conf, path);
+        assert!(result.is_ok());
+    }
 }

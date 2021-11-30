c56f51dd1158fbe619ac04dad3d1b7704dd33bf2
Xin Liu <sam@secondstate.io>
2021-11-30 08:57:34 +0000

    [Rust][feature](store, statistics, utils): add new modules
    Signed-off-by: Xin Liu <sam@secondstate.io>

---
+++ bindings/rust/wasmedge-sys/src/utils.rs
@@ -0,0 +1,38 @@
+use crate::error::WasmEdgeResult;
+use std::ffi::CString;
+use std::path::Path;
+
+#[cfg(unix)]
+pub fn path_to_cstring(path: &Path) -> WasmEdgeResult<CString> {
+    use std::os::unix::ffi::OsStrExt;
+
+    Ok(CString::new(path.as_os_str().as_bytes())?)
+}
+
+#[cfg(windows)]
+pub fn path_to_cstring(path: &Path) -> WasmEdgeResult<CString> {
+    match path.to_str() {
+        Some(s) => Ok(CString::new(s)?),
+        None => {
+            let message = format!("Couldn't convert path '{}' to UTF-8", path.display());
+            Err(message.into())
+        }
+    }
+}
+
+pub fn vec_string_to_c_array<T: Iterator<Item = E>, E: AsRef<str>>(
+    args: T,
+) -> *const *const std::os::raw::c_char {
+    let mut v = vec![];
+    for arg in args {
+        v.push(string_to_c_char(arg));
+    }
+    let p = v.as_ptr();
+    std::mem::forget(v);
+    p
+}
+
+pub fn string_to_c_char(arg: impl AsRef<str>) -> *const std::os::raw::c_char {
+    let s = CString::new(arg.as_ref()).unwrap();
+    s.as_ptr()
+}

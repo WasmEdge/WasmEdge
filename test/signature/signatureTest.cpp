f47c7ed2aa22e518a84949df1a81f1ad3a5f4bd7
XingziYu <edittriendlyu@gmail.com>
2021-12-01 07:36:47 +0000

    [Wasm-Signature] Add tests
    Signed-off-by: XingZYu <edittriendlyu@gmail.com>

--- test/signature/signatureTest.cpp
+++ test/signature/signatureTest.cpp
@@ -13,19 +13,46 @@
 #include "signature/signature.h"
 
 #include "gtest/gtest.h"
+#include <filesystem>
+#include <ostream>
 
 namespace {
 WasmEdge::Signature::Signature SignatureEngine;
-TEST(SignatureTEST, HELLO) {
+TEST(SignatureTEST, KEYGEN) {
   std::vector<unsigned char> PublicKey;
   std::filesystem::path Base = "signatureTestData";
   std::filesystem::path WasmFile = Base / "hello.wasm";
   std::filesystem::path PrikeyFile = Base / "id_ed25519";
   std::filesystem::path PubkeyFile = Base / "id_ed25519.pub";
-  bool Res;
-  ASSERT_TRUE(SignatureEngine.signWasmFile(WasmFile));
-  EXPECT_TRUE(Res = *(SignatureEngine.verifyWasmFile(WasmFile, PrikeyFile)));
+  testing::internal::CaptureStdout();
+
+  // bool Res;
+  RecordProperty("WasmPath: ", WasmFile);
+  RecordProperty("CurrentPath: ", std::filesystem::current_path());
+  ASSERT_TRUE(std::filesystem::exists(WasmFile));
+  EXPECT_TRUE(SignatureEngine.signWasmFile(WasmFile));
+  ASSERT_TRUE(std::filesystem::exists(PrikeyFile));
+  ASSERT_TRUE(std::filesystem::exists(PubkeyFile));
+
+  // std::string Output = testing::internal::GetCapturedStdout();
+  // RecordProperty("STDOUT: ", Output);
+  // std::ifstream PubKeyFileStream(PubkeyFile, std::ios::binary);
+  // ASSERT_TRUE(PubKeyFileStream.is_open());
+
+  // auto Size = PubKeyFileStream.tellg();
+  // std::string KeyStr(Size, '\0');
+
+  // EXPECT_TRUE(PubKeyFileStream.seekg(0));
+  // EXPECT_TRUE(PubKeyFileStream.read(&KeyStr[0], Size));
+  // RecordProperty("Public Key: ", KeyStr);
+
+  // EXPECT_TRUE(Res = *(SignatureEngine.verifyWasmFile(WasmFile, PrikeyFile)));
+  // RecordProperty("Verify Result: ", Res);
+
+  // RecordProperty("STDOUT: ", Output);
 }
+
+TEST(SignatureTEST, VERIFY) {}
 } // namespace
 
 GTEST_API_ int main(int argc, char **argv) {

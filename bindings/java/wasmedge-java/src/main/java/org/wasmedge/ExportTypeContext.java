3a90fd48f89d961d5610b22848faa84b3ece6bb8
Kenvi Zhu <mrwhite@163.com>
2021-12-13 22:46:44 +0800

    add test cases for export types
    Signed-off-by: Kenvi Zhu <mrwhite@163.com>

--- bindings/java/wasmedge-java/src/main/java/org/wasmedge/ExportTypeContext.java
+++ bindings/java/wasmedge-java/src/main/java/org/wasmedge/ExportTypeContext.java
@@ -1,10 +1,14 @@
 package org.wasmedge;
 
+import org.wasmedge.enums.ExternalType;
+
 public class ExportTypeContext {
     private ASTModuleContext astCtx;
 
     public native String getModuleName();
     public native String getExternalName();
+
+    public native ExternalType getExternalType();
 
     public native FunctionTypeContext getFunctionType();
 

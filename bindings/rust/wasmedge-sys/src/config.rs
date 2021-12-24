3b4bf526c861e272182f0a01d15e6d69130b86ef
Xin Liu <sam@secondstate.io>
2021-12-24 17:56:02 +0800

    [Rust] Added doc comments for Config (#920)
    * [Rust][doc](config): Added doc comments for Config
    * [Rust][refactor](config): Refactored the doc comments
    
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- bindings/rust/wasmedge-sys/src/config.rs
+++ bindings/rust/wasmedge-sys/src/config.rs
@@ -1,18 +1,273 @@
+//! Defines WasmEdge Config struct.
+
 use crate::{
     types::{CompilerOptimizationLevel, CompilerOutputFormat},
     wasmedge, Error, WasmEdgeResult,
 };
 
+/// Struct of WasmEdge Config.
+///
+/// [`Config`] manages the configuration options, which are used in WasmEdge [`Vm`], [`Loader`], [`Validator`],
+/// [`Executor`], and [`Compiler`].
+///
+/// The configuration options are categorized into the following four groups:
+///
+/// - WebAssembly Proposals
+///
+///     This group of options are used to turn on/off the WebAssembly proposals. They are effective to any WasmEdge
+///     context created with [`Config`].
+///     
+///     - `ImportExportMutGlobals` supports mutable imported and exported globals.
+///
+///       Also see [Import/Export Mutable Globals Proposal](https://github.com/WebAssembly/mutable-global/blob/master/proposals/mutable-global/Overview.md#importexport-mutable-globals).
+///
+///     - `NonTrapFloatToIntConversions` supports the non-trapping float-to-int conversion.
+///
+///       Also see [Non-trapping Float-to-int Conversions Proposal](https://github.com/WebAssembly/spec/blob/main/proposals/nontrapping-float-to-int-conversion/Overview.md).
+///
+///     - `SignExtensionOperators` supports new integer instructions for sign-extending 8-bit, 16-bit, and 32-bit values.
+///     
+///       Also see [Sign-extension Operators Proposal](https://github.com/WebAssembly/spec/blob/main/proposals/sign-extension-ops/Overview.md).
+///
+///     - `MultiValue` supports functions and instructions with multiple return values, and blocks with inputs.
+///     
+///       Also see [Multi-value Extension](https://github.com/WebAssembly/spec/blob/main/proposals/multi-value/Overview.md).
+///
+///     - `BulkMemoryOperations` supports bulk memory operations.
+///
+///       Also see [Bulk Memory Operations Proposal](https://github.com/WebAssembly/spec/blob/main/proposals/bulk-memory-operations/Overview.md#motivation-for-bulk-memory-operations).
+///
+///     - `ReferenceTypes` supports reference types.
+///
+///       Also see [Reference Types Proposal](https://github.com/WebAssembly/spec/blob/main/proposals/reference-types/Overview.md).
+///
+///     - `SIMD` supports 128-bit packed SIMD extension to WebAssembly.
+///
+///       Also see [SIMD Proposal](https://github.com/WebAssembly/spec/blob/main/proposals/simd/SIMD.md).
+///  
+///     - `TailCall` supports tail call optimization.
+///
+///       Also see [Tail Call Proposal](https://github.com/WebAssembly/tail-call/blob/master/proposals/tail-call/Overview.md).
+///
+///       Also see [](https://github.com/WebAssembly/tail-call/blob/master/proposals/tail-call/Overview.md).
+///
+///     - `Annotations` supports annotations in WASM text format.
+///
+///       Also see [Annotations Proposal](https://github.com/WebAssembly/annotations/blob/master/proposals/annotations/Overview.md).
+///
+///     - `Memory64` supports 64-bit memory indexes.
+///
+///       Also see [Memory64 Proposal](https://github.com/WebAssembly/memory64/blob/main/proposals/memory64/Overview.md).
+///
+///     - `Threads` supports threading feature.
+///
+///       Also see [Threading Proposal](https://github.com/WebAssembly/threads/blob/main/proposals/threads/Overview.md).
+///
+///     - `ExceptionHandling` supports exception handling.
+///     
+///       Also see [Exception Handling Proposal](https://github.com/WebAssembly/exception-handling/blob/main/proposals/exception-handling/Exceptions.md).
+///
+///     - `FunctionReferences` supports typed function references for WebAssembly.
+///
+///       Also see [Function References Proposal](https://github.com/WebAssembly/function-references/blob/master/proposals/function-references/Overview.md).
+///
+/// - Host Registrations
+///     - `Wasi` turns on the `WASI` support in [`Vm`].
+///     - `WasmEdgeProcess` turns on the `wasmedge_process` support in [`Vm`].
+///     
+///     The two options are only effective to [`Vm`].
+///
+/// - Memory Management
+///     - `maximum_memory_page` limits the page size of [`Memory`]. This option is only effective to [`Executor`] and [`VM`].
+///
+/// - AOT Compilation
+///     The AOT compiler options configure the behavior about optimization level, output format, dump IR,
+///     and generic binary.
+///
+///     - Compiler Optimization Levels
+///         - `O0` performs as many optimizations as possible.
+///         - `O1` optimizes quickly without destroying debuggability.
+///         - `02` optimizes for fast execution as much as possible without triggering significant incremental
+///                compile time or code size growth.
+///         - `O3` optimizes for fast execution as much as possible.
+///         - `Os` optimizes for small code size as much as possible without triggering significant incremental
+///                compile time or execution time slowdowns.
+///         - `Oz` optimizes for small code size as much as possible.
+///
+///     - Compiler Output Formats
+///         - `Native` specifies the output format is native dynamic library (`*.wasm.so`).
+///         - `Wasm` specifies the output format is WebAssembly with AOT compiled codes in custom section (`*.wasm`).
+///     
+///     - `dump_ir` determines if AOT compiler generates IR or not.
+///     - `generic_binary` determines if AOT compiler generates the generic binary or not.
+///     
+///     The configuration options above are only effective to [`Compiler`].
+///
+/// - Runtime Statistics
+///     - `instr_counting` determines if measuring the count of instructions when running a compiled or pure WASM.
+///     - `cost_measuring` determines if measuring the instruction costs when running a compiled or pure WASM.
+///     - `time_measuring` determines if measuring the running time when running a compiled or pure WASM.
+///
+/// API users can first set the options of interest, such as those related to the WebAssembly proposals,
+/// host registrations, AOT compiler options, and etc., then apply the configuration
+/// to create other WasmEdge runtime structs.
 #[derive(Debug)]
 pub struct Config {
     pub(crate) ctx: *mut wasmedge::WasmEdge_ConfigureContext,
 }
-
 impl Drop for Config {
     fn drop(&mut self) {
         if !self.ctx.is_null() {
             unsafe { wasmedge::WasmEdge_ConfigureDelete(self.ctx) };
         }
+    }
+}
+impl Config {
+    /// Creates a new [`Config`].
+    pub fn create() -> WasmEdgeResult<Self> {
+        let ctx = unsafe { wasmedge::WasmEdge_ConfigureCreate() };
+        match ctx.is_null() {
+            true => Err(Error::OperationError(String::from(
+                "fail to create Config instance",
+            ))),
+            false => Ok(Self { ctx }),
+        }
+    }
+
+    /// Enables host registration wasi.
+    pub fn enable_wasi(self) -> Self {
+        unsafe {
+            wasmedge::WasmEdge_ConfigureAddHostRegistration(
+                self.ctx,
+                wasmedge::WasmEdge_HostRegistration_Wasi,
+            )
+        };
+        self
+    }
+
+    /// Checks if host registration wasi turns on or not.
+    pub fn wasi_enabled(&self) -> bool {
+        unsafe {
+            wasmedge::WasmEdge_ConfigureHasHostRegistration(
+                self.ctx,
+                wasmedge::WasmEdge_HostRegistration_Wasi,
+            )
+        }
+    }
+
+    /// Checks if host registration wasmedge process turns on or not.
+    pub fn wasmedge_process_enabled(&self) -> bool {
+        unsafe {
+            wasmedge::WasmEdge_ConfigureHasHostRegistration(
+                self.ctx,
+                wasmedge::WasmEdge_HostRegistration_WasmEdge_Process,
+            )
+        }
+    }
+
+    /// Enables host registration WasmEdge process.
+    pub fn enable_wasmedge_process(self) -> Self {
+        unsafe {
+            wasmedge::WasmEdge_ConfigureAddHostRegistration(
+                self.ctx,
+                wasmedge::WasmEdge_HostRegistration_WasmEdge_Process,
+            )
+        };
+        self
+    }
+
+    /// Sets the maximum number of the memory pages available.
+    pub fn set_max_memory_pages(self, count: u32) -> Self {
+        unsafe { wasmedge::WasmEdge_ConfigureSetMaxMemoryPage(self.ctx, count) };
+        self
+    }
+
+    /// Returns the number of the memory pages available.
+    pub fn get_max_memory_pages(&self) -> u32 {
+        unsafe { wasmedge::WasmEdge_ConfigureGetMaxMemoryPage(self.ctx) }
+    }
+
+    // For AOT compiler
+
+    /// Sets the optimization level of AOT compiler.
+    pub fn set_optimization_level(self, opt_level: CompilerOptimizationLevel) -> Self {
+        unsafe {
+            wasmedge::WasmEdge_ConfigureCompilerSetOptimizationLevel(self.ctx, opt_level as u32)
+        };
+        self
+    }
+
+    /// Returns the optimization level of AOT compiler.
+    pub fn get_optimization_level(&self) -> CompilerOptimizationLevel {
+        let level = unsafe { wasmedge::WasmEdge_ConfigureCompilerGetOptimizationLevel(self.ctx) };
+        level.into()
+    }
+
+    /// Sets the output binary format of AOT compiler.
+    pub fn set_compiler_output_format(self, format: CompilerOutputFormat) -> Self {
+        unsafe { wasmedge::WasmEdge_ConfigureCompilerSetOutputFormat(self.ctx, format as u32) };
+        self
+    }
+
+    /// Returns the output binary format of AOT compiler.
+    pub fn get_compiler_output_format(&self) -> CompilerOutputFormat {
+        let value = unsafe { wasmedge::WasmEdge_ConfigureCompilerGetOutputFormat(self.ctx) };
+        value.into()
+    }
+
+    /// Sets the dump IR option of AOT compiler.
+    pub fn dump_ir(self, flag: bool) -> Self {
+        unsafe { wasmedge::WasmEdge_ConfigureCompilerSetDumpIR(self.ctx, flag) };
+        self
+    }
+
+    /// Checks if the dump IR option turns on or not.
+    pub fn is_dump_ir(&self) -> bool {
+        unsafe { wasmedge::WasmEdge_ConfigureCompilerIsDumpIR(self.ctx) }
+    }
+
+    /// Sets the generic binary option of AOT compiler.
+    pub fn generic_binary(self, flag: bool) -> Self {
+        unsafe { wasmedge::WasmEdge_ConfigureCompilerSetGenericBinary(self.ctx, flag) };
+        self
+    }
+
+    /// Checks if the generic binary option of AOT compiler turns on or not.
+    pub fn is_generic_binary(&self) -> bool {
+        unsafe { wasmedge::WasmEdge_ConfigureCompilerIsGenericBinary(self.ctx) }
+    }
+
+    /// Sets the instruction counting option.
+    pub fn count_instructions(self, flag: bool) -> Self {
+        unsafe { wasmedge::WasmEdge_ConfigureStatisticsSetInstructionCounting(self.ctx, flag) };
+        self
+    }
+
+    /// Checks if the instruction counting option turns on or not.
+    pub fn is_instruction_counting(&self) -> bool {
+        unsafe { wasmedge::WasmEdge_ConfigureStatisticsIsInstructionCounting(self.ctx) }
+    }
+
+    /// Sets the cost measuring option.
+    pub fn measure_cost(self, flag: bool) -> Self {
+        unsafe { wasmedge::WasmEdge_ConfigureStatisticsSetCostMeasuring(self.ctx, flag) };
+        self
+    }
+
+    /// Checks if the cost measuring option turns on or not.
+    pub fn is_cost_measuring(&self) -> bool {
+        unsafe { wasmedge::WasmEdge_ConfigureStatisticsIsCostMeasuring(self.ctx) }
+    }
+
+    /// Sets the time measuring option.
+    pub fn measure_time(self, flag: bool) -> Self {
+        unsafe { wasmedge::WasmEdge_ConfigureStatisticsSetTimeMeasuring(self.ctx, flag) };
+        self
+    }
+
+    /// Checks if the cost measuring option turns on or not.
+    pub fn is_time_measuring(&self) -> bool {
+        unsafe { wasmedge::WasmEdge_ConfigureStatisticsIsTimeMeasuring(self.ctx) }
     }
 }
 
@@ -60,8 +315,11 @@ macro_rules! impl_proposal_config {
         }
     }
 }
-
 impl_proposal_config! {
+    ImportExportMutGlobals,
+    NonTrapFloatToIntConversions,
+    SignExtensionOperators,
+    MultiValue,
     BulkMemoryOperations,
     ReferenceTypes,
     SIMD,
@@ -73,121 +331,87 @@ impl_proposal_config! {
     FunctionReferences,
 }
 
-impl Config {
-    pub fn create() -> WasmEdgeResult<Self> {
-        let ctx = unsafe { wasmedge::WasmEdge_ConfigureCreate() };
-        match ctx.is_null() {
-            true => Err(Error::OperationError(String::from(
-                "fail to create Config instance",
-            ))),
-            false => Ok(Self { ctx }),
-        }
-    }
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    // For Vm
-    pub fn enable_wasi(self) -> Self {
-        unsafe {
-            wasmedge::WasmEdge_ConfigureAddHostRegistration(
-                self.ctx,
-                wasmedge::WasmEdge_HostRegistration_Wasi,
-            )
-        };
-        self
-    }
+    #[test]
+    fn test_config_options() {
+        // create a Config instance
+        let result = Config::create();
+        assert!(result.is_ok());
+        let config = result.unwrap();
 
-    /// Set the maximum number of memory pages available to running modules.
-    pub fn set_max_memory_pages(self, num_pages: u32) -> Self {
-        unsafe { wasmedge::WasmEdge_ConfigureSetMaxMemoryPage(self.ctx, num_pages) };
-        self
-    }
+        // check default settings
+        assert!(!config.has_annotations());
+        assert!(config.has_bulkmemoryoperations());
+        assert!(!config.has_exceptionhandling());
+        assert!(!config.has_functionreferences());
+        assert!(!config.has_memory64());
+        assert!(config.has_referencetypes());
+        assert!(config.has_simd());
+        assert!(!config.has_tailcall());
+        assert!(!config.has_threads());
+        assert!(!config.is_cost_measuring());
+        assert!(!config.is_dump_ir());
+        assert!(!config.is_generic_binary());
+        assert!(!config.is_instruction_counting());
+        assert!(!config.is_time_measuring());
+        assert_eq!(config.get_max_memory_pages(), 65536);
+        assert_eq!(
+            config.get_optimization_level(),
+            CompilerOptimizationLevel::O3,
+        );
+        assert_eq!(
+            config.get_compiler_output_format(),
+            CompilerOutputFormat::Wasm,
+        );
 
-    /// Get the page limit of memory instances.
-    pub fn get_max_memory_pages(&self) -> u32 {
-        unsafe { wasmedge::WasmEdge_ConfigureGetMaxMemoryPage(self.ctx) }
-    }
+        // set options
+        let config = config
+            .enable_annotations(true)
+            .enable_bulkmemoryoperations(false)
+            .enable_exceptionhandling(true)
+            .enable_functionreferences(true)
+            .enable_memory64(true)
+            .enable_referencetypes(false)
+            .enable_simd(false)
+            .enable_tailcall(true)
+            .enable_threads(true)
+            .measure_cost(true)
+            .measure_time(true)
+            .dump_ir(true)
+            .generic_binary(true)
+            .count_instructions(true);
 
-    // For AOT compiler
+        // check new settings
+        assert!(config.has_annotations());
+        assert!(!config.has_bulkmemoryoperations());
+        assert!(config.has_exceptionhandling());
+        assert!(config.has_functionreferences());
+        assert!(config.has_memory64());
+        assert!(!config.has_referencetypes());
+        assert!(!config.has_simd());
+        assert!(config.has_tailcall());
+        assert!(config.has_threads());
+        assert!(config.is_cost_measuring());
+        assert!(config.is_dump_ir());
+        assert!(config.is_generic_binary());
+        assert!(config.is_instruction_counting());
+        assert!(config.is_time_measuring());
 
-    /// Set the optimization level of AOT compiler.
-    pub fn set_optimization_level(self, opt_level: CompilerOptimizationLevel) -> Self {
-        unsafe {
-            wasmedge::WasmEdge_ConfigureCompilerSetOptimizationLevel(self.ctx, opt_level as u32)
-        };
-        self
-    }
-
-    /// Get the optimization level of AOT compiler.
-    pub fn get_optimization_level(&self) -> CompilerOptimizationLevel {
-        let level = unsafe { wasmedge::WasmEdge_ConfigureCompilerGetOptimizationLevel(self.ctx) };
-        level.into()
-    }
-
-    /// Set the output binary format of AOT compiler.
-    pub fn set_compiler_output_format(self, format: CompilerOutputFormat) -> Self {
-        unsafe { wasmedge::WasmEdge_ConfigureCompilerSetOutputFormat(self.ctx, format as u32) };
-        self
-    }
-
-    /// Get the output binary format of AOT compiler.
-    pub fn get_compiler_output_format(&self) -> CompilerOutputFormat {
-        let value = unsafe { wasmedge::WasmEdge_ConfigureCompilerGetOutputFormat(self.ctx) };
-        value.into()
-    }
-
-    /// Set the dump IR boolean value of AOT compiler.
-    pub fn dump_ir(self, enable: bool) -> Self {
-        unsafe { wasmedge::WasmEdge_ConfigureCompilerSetDumpIR(self.ctx, enable) };
-        self
-    }
-
-    /// Get the dump IR option of AOT compiler.
-    pub fn is_dump_ir(&self) -> bool {
-        unsafe { wasmedge::WasmEdge_ConfigureCompilerIsDumpIR(self.ctx) }
-    }
-
-    /// Set the generic binary option of AOT compiler.
-    pub fn generic_binary(self, enable: bool) -> Self {
-        unsafe { wasmedge::WasmEdge_ConfigureCompilerSetGenericBinary(self.ctx, enable) };
-        self
-    }
-
-    /// Get the generic binary option of AOT compiler.
-    pub fn is_generic_binary(&self) -> bool {
-        unsafe { wasmedge::WasmEdge_ConfigureCompilerIsGenericBinary(self.ctx) }
-    }
-
-    /// Enable or disable instruction counting.
-    pub fn count_instructions(self, enable: bool) -> Self {
-        unsafe { wasmedge::WasmEdge_ConfigureStatisticsSetInstructionCounting(self.ctx, enable) };
-        self
-    }
-
-    /// Get the instruction counting option.
-    pub fn is_instruction_counting(&self) -> bool {
-        unsafe { wasmedge::WasmEdge_ConfigureStatisticsIsInstructionCounting(self.ctx) }
-    }
-
-    /// Enable or disable cost cost measuring.
-    pub fn measure_costs(self, enable: bool) -> Self {
-        unsafe { wasmedge::WasmEdge_ConfigureStatisticsSetCostMeasuring(self.ctx, enable) };
-        self
-    }
-
-    /// Get the cost measuring option.
-    pub fn is_cost_measuring(&self) -> bool {
-        unsafe { wasmedge::WasmEdge_ConfigureStatisticsIsCostMeasuring(self.ctx) }
-    }
-
-    /// Set the time measuring option.
-    pub fn measure_time(self, enable: bool) -> Self {
-        unsafe { wasmedge::WasmEdge_ConfigureStatisticsSetTimeMeasuring(self.ctx, enable) };
-        self
-    }
-
-    /// Get the cost measuring option.
-    pub fn is_time_measuring(&self) -> bool {
-        unsafe { wasmedge::WasmEdge_ConfigureStatisticsIsTimeMeasuring(self.ctx) }
+        // set maxmimum memory pages
+        let config = config.set_max_memory_pages(10);
+        assert_eq!(config.get_max_memory_pages(), 10);
+        let config = config.set_optimization_level(CompilerOptimizationLevel::Oz);
+        assert_eq!(
+            config.get_optimization_level(),
+            CompilerOptimizationLevel::Oz
+        );
+        let config = config.set_compiler_output_format(CompilerOutputFormat::Native);
+        assert_eq!(
+            config.get_compiler_output_format(),
+            CompilerOutputFormat::Native,
+        );
     }
 }
-
-// # TODO: WasmEdge_HostRegistration

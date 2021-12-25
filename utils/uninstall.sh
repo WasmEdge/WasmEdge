3ef1fe7a25f5aec8213a14474b085471032699ba
Shreyas Atre <shreyasatre16@gmail.com>
2021-12-25 22:05:02 +0530

    [Utils] Uninstall Script: Added source removals for bash_profile and profile
    * Also added their corresponding CI tests
    
    Signed-off-by: Shreyas Atre <shreyasatre16@gmail.com>

--- utils/uninstall.sh
+++ utils/uninstall.sh
@@ -248,14 +248,17 @@ main() {
     _shell_rc=".""$_shell_""rc"
 
     line_num="$(grep -n ". \"${IPATH}/env\"" "${__HOME__}/${_shell_rc}" | cut -d : -f 1)"
-    
+
     if [ "$line_num" != "" ]; then
         sed -i.wasmedge_backup -e "${line_num}"'d' "${__HOME__}/${_shell_rc}"
+        [[ -f "${__HOME__}/.profile" ]] && line_num="$(grep -n ". \"${IPATH}/env\"" "${__HOME__}/.profile" | cut -d : -f 1)" &&
+            sed -i.wasmedge_backup -e "${line_num}"'d' "${__HOME__}/.profile"
+        [[ -f "${__HOME__}/.bash_profile" ]] && line_num="$(grep -n ". \"${IPATH}/env\"" "${__HOME__}/.bash_profile" | cut -d : -f 1)" &&
+            sed -i.wasmedge_backup -e "${line_num}"'d' "${__HOME__}/.bash_profile"
     else
         echo "${YELLOW}Sourcing not found in ${__HOME__}/${_shell_rc} ${NC}"
         exit 1
     fi
-
 }
 
 main "$@"

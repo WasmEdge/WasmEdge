2a79cc380414aae8f5941472f83819b9c7350534
sonder-joker <highmagic@outlook.com>
2022-01-04 15:01:37 +0800

    [Action] GitHub action
    Signed-off-by: sonder-joker <highmagic@outlook.com>

--- utils/docker/build-manylinux.sh
+++ utils/docker/build-manylinux.sh
@@ -10,7 +10,17 @@ export LDFLAGS=-L/toolchain/lib64
 curl -s -L -O --remote-name-all https://boostorg.jfrog.io/artifactory/main/release/1.76.0/source/boost_1_76_0.tar.bz2
 echo "f0397ba6e982c4450f27bf32a2a83292aba035b827a5623a14636ea583318c41  boost_1_76_0.tar.bz2" | sha256sum -c
 bzip2 -dc boost_1_76_0.tar.bz2 | tar -xf -
-if ! cmake -Bbuild -GNinja -DCMAKE_BUILD_TYPE=Release -DWASMEDGE_BUILD_PACKAGE="TGZ;TBZ2;TXZ;TZST;RPM;DEB" -DBoost_NO_SYSTEM_PATHS=TRUE -DBOOST_INCLUDEDIR=$(pwd)/boost_1_76_0/; then
+curl -s -L -O --remote-name-all https://www.openssl.org/source/openssl-1.1.1m.tar.gz
+echo "f89199be8b23ca45fc7cb9f1d8d3ee67312318286ad030f5316aca6462db6c96 openssl-1.1.1m.tar.gz" | sha256sum -c
+tar -xf openssl-1.1.1m.tar.gz
+cd ./openssl-1.1.1m
+mkdir openssl
+./config --prefix=$(pwd)/openssl --openssldir=$(pwd)/openssl -DOPENSSL_API_COMPAT=0x10100000L
+make
+make test
+make install
+cd ..
+if ! cmake -Bbuild -GNinja -DCMAKE_BUILD_TYPE=Release -DWASMEDGE_BUILD_PACKAGE="TGZ;TBZ2;TXZ;TZST;RPM;DEB" -DBoost_NO_SYSTEM_PATHS=TRUE -DBOOST_INCLUDEDIR=$(pwd)/boost_1_76_0/ -DOPENSSL_ROOT_DIR=$(pwd)/openssl-1.1.1m/openssl; then
     echo === CMakeOutput.log ===
     cat build/CMakeFiles/CMakeOutput.log
     echo === CMakeError.log ===

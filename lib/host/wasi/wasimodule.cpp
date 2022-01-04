447bdf8f4fda956406273748a4985eb3d915a2cd
tcdk <458761603@qq.com>
2022-01-05 04:00:00 +0800

    [WASI] rename getpeeraddr and getlocaladdr
    Signed-off-by: tcdk <458761603@qq.com>

--- lib/host/wasi/wasimodule.cpp
+++ lib/host/wasi/wasimodule.cpp
@@ -69,8 +69,8 @@ WasiModule::WasiModule() : ImportObject("wasi_snapshot_preview1") {
   addHostFunc("sock_shutdown", std::make_unique<WasiSockShutdown>(Env));
   addHostFunc("sock_getsockopt", std::make_unique<WasiSockGetOpt>(Env));
   addHostFunc("sock_setsockopt", std::make_unique<WasiSockSetOpt>(Env));
-  addHostFunc("sock_get_local", std::make_unique<WasiSockGetLocalAddr>(Env));
-  addHostFunc("sock_get_peer", std::make_unique<WasiSockGetPeerAddr>(Env));
+  addHostFunc("sock_getlocaladdr", std::make_unique<WasiSockGetLocalAddr>(Env));
+  addHostFunc("sock_getpeeraddr", std::make_unique<WasiSockGetPeerAddr>(Env));
 }
 
 } // namespace Host

c4358a0d6521f4caa5daedf1c218f641775c86d4
Harinath Nampally <harinath922@gmail.com>
2022-01-05 07:33:32 -0500

    [Code Quality] address review comment for Null pointer dereference check #528
    Signed-off-by: Harinath Nampally <harinath922@gmail.com>

--- lib/executor/engine/variableInstr.cpp
+++ lib/executor/engine/variableInstr.cpp
@@ -38,9 +38,8 @@ Expect<void> Executor::runGlobalGetOp(Runtime::StoreManager &StoreMgr,
 Expect<void> Executor::runGlobalSetOp(Runtime::StoreManager &StoreMgr,
                                       const uint32_t Idx) {
   auto *GlobInst = getGlobInstByIdx(StoreMgr, Idx);
-  if (GlobInst) {
-    GlobInst->getValue() = StackMgr.pop();
-  }
+  assuming(GlobInst);
+  GlobInst->getValue() = StackMgr.pop();
   return {};
 }
 

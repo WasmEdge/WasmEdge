61a8dca9b40aefaf8f75999f38544b5e51fcc6f8
sonder-joker <highmagic@outlook.com>
2022-01-13 16:03:15 +0800

    [WASI] Remove Wrapping add
    Signed-off-by: sonder-joker <highmagic@outlook.com>

--- include/host/wasi_crypto/handles.h
+++ include/host/wasi_crypto/handles.h
@@ -5,88 +5,16 @@
 #include "wasi_crypto/api.hpp"
 
 #include <limits>
+#include <mutex>
 #include <shared_mutex>
 #include <type_traits>
-#include <mutex>
 #include <unordered_map>
 
 namespace WasmEdge {
 namespace Host {
 namespace WASICrypto {
 namespace {
-// from mozilla-central/mfbt/WrappingOperations.h
-template <typename UnsignedType> struct WrapToSignedHelper {
-  static_assert(std::is_unsigned_v<UnsignedType>,
-                "WrapToSigned must be passed an unsigned type");
-
-  using SignedType = std::make_signed_t<UnsignedType>;
-
-  static constexpr SignedType MaxValue =
-      (UnsignedType(1) << (CHAR_BIT * sizeof(SignedType) - 1)) - 1;
-  static constexpr SignedType MinValue = -MaxValue - 1;
-
-  static constexpr UnsignedType MinValueUnsigned =
-      static_cast<UnsignedType>(MinValue);
-  static constexpr UnsignedType MaxValueUnsigned =
-      static_cast<UnsignedType>(MaxValue);
-
-  // Overflow-correctness was proven in bug 1432646 and is explained in the
-  // comment below.  This function is very hot, both at compile time and
-  // runtime, so disable all overflow checking in it.
-
-  static constexpr SignedType compute(UnsignedType AValue) {
-    // This algorithm was originally provided here:
-    // https://stackoverflow.com/questions/13150449/efficient-unsigned-to-signed-cast-avoiding-implementation-defined-behavior
-    //
-    // If the value is in the non-negative signed range, just cast.
-    //
-    // If the value will be negative, compute its delta from the first number
-    // past the max signed integer, then add that to the minimum signed value.
-    //
-    // At the low end: if |u| is the maximum signed value plus one, then it has
-    // the same mathematical value as |MinValue| cast to unsigned form.  The
-    // delta is zero, so the signed form of |u| is |MinValue| -- exactly the
-    // result of adding zero delta to |MinValue|.
-    //
-    // At the high end: if |u| is the maximum *unsigned* value, then it has all
-    // bits set.  |MinValue| cast to unsigned form is purely the high bit set.
-    // So the delta is all bits but high set -- exactly |MaxValue|.  And as
-    // |MinValue = -MaxValue - 1|, we have |MaxValue + (-MaxValue - 1)| to
-    // equal -1.
-    //
-    // Thus the delta below is in signed range, the corresponding cast is safe,
-    // and this computation produces values spanning [MinValue, 0): exactly the
-    // desired range of all negative signed integers.
-    return (AValue <= MaxValueUnsigned)
-               ? static_cast<SignedType>(AValue)
-               : static_cast<SignedType>(AValue - MinValueUnsigned) + MinValue;
-  }
-};
-template <typename UnsignedType>
-constexpr typename WrapToSignedHelper<UnsignedType>::SignedType
-WrapToSigned(UnsignedType AValue) {
-  return WrapToSignedHelper<UnsignedType>::compute(AValue);
-}
-template <typename T> constexpr T ToResult(std::make_unsigned_t<T> AUnsigned) {
-  // We could *always* return WrapToSigned and rely on unsigned conversion to
-  // undo the wrapping when |T| is unsigned, but this seems clearer.
-  return std::is_signed_v<T> ? WrapToSigned(AUnsigned) : AUnsigned;
-}
-
-template <typename T> struct WrappingAddHelper {
-private:
-  using UnsignedT = std::make_unsigned_t<T>;
-
-public:
-  static constexpr T compute(T AX, T AY) {
-    return ToResult<T>(static_cast<UnsignedT>(AX) + static_cast<UnsignedT>(AY));
-  }
-};
-template <typename T> constexpr T WrappingAdd(T AX, T AY) {
-  return WrappingAddHelper<T>::compute(AX, AY);
-}
-
-// from MSVC STL
+// Reference from MSVC STL , It entered the standard library after c++20
 template <class T> [[nodiscard]] constexpr T rotr(T Val, int Rotation) noexcept;
 
 template <class T>
@@ -187,7 +115,7 @@ public:
 
 private:
   HandleType nextHandle(HandleType Handle) {
-    auto AddedValue = WrappingAdd(Handle, 1) << 8;
+    auto AddedValue = (Handle + 1) << 8;
     return rotr(AddedValue | static_cast<HandleType>(TypeId), 8);
   }
 

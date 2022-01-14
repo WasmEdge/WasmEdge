a36b77c97ac05adee01676bde164e7334923733a
sonder-joker <highmagic@outlook.com>
2022-01-14 21:54:30 +0800

    [Test] Add Ecdsa sec form test
    Signed-off-by: sonder-joker <highmagic@outlook.com>

--- test/host/wasi_crypto/asymmetric/signature.cpp
+++ test/host/wasi_crypto/asymmetric/signature.cpp
@@ -87,6 +87,57 @@ TEST(WasiCryptoTest, TestSignaturesEcdsa) {
   EXPECT_TRUE(Ctx.signatureClose(SignatureHandle).has_value());
 }
 
+TEST(WasiCryptoTest, TestSignaturesEcdsaSec) {
+  WasiCryptoContext Ctx;
+
+  std::string_view Alg = "ECDSA_P256_SHA256";
+
+  auto KpHandle =
+      Ctx.keypairGenerate(__WASI_ALGORITHM_TYPE_SIGNATURES, Alg, std::nullopt)
+          .value();
+  auto PkHandle = Ctx.keypairPublickey(KpHandle).value();
+
+  auto const PkSerialized =
+      Ctx.publickeyExport(PkHandle, __WASI_PUBLICKEY_ENCODING_COMPRESSED_SEC)
+          .value();
+  auto Raw = std::vector<uint8_t>(Ctx.arrayOutputLen(PkSerialized).value(), 0);
+  Ctx.arrayOutputPull(PkSerialized, Raw).value();
+  PkHandle = Ctx.publickeyImport(__WASI_ALGORITHM_TYPE_SIGNATURES, Alg, Raw,
+                                 __WASI_PUBLICKEY_ENCODING_RAW)
+                 .value();
+
+  auto const KpSerialized =
+      Ctx.keypairExport(KpHandle, __WASI_KEYPAIR_ENCODING_RAW).value();
+  Raw = std::vector<uint8_t>(Ctx.arrayOutputLen(KpSerialized).value(), 0);
+  Ctx.arrayOutputPull(KpSerialized, Raw).value();
+
+  auto const Kp2Handle =
+      Ctx.keypairImport(__WASI_ALGORITHM_TYPE_SIGNATURES, Alg, Raw,
+                        __WASI_KEYPAIR_ENCODING_RAW)
+          .value();
+  KpHandle = Kp2Handle;
+
+  auto const StateHandle = Ctx.signatureStateOpen(KpHandle).value();
+  EXPECT_TRUE(Ctx.signatureStateUpdate(StateHandle, "test"_u8).has_value());
+  auto const SignatureHandle = Ctx.signatureStateSign(StateHandle).value();
+
+  auto const VerificationStateHandle =
+      Ctx.signatureVerificationStateOpen(PkHandle).value();
+  EXPECT_TRUE(
+      Ctx.signatureVerificationStateUpdate(VerificationStateHandle, "test"_u8)
+          .has_value());
+  EXPECT_TRUE(Ctx.signatureVerificationStateVerify(VerificationStateHandle,
+                                                   SignatureHandle)
+                  .has_value());
+
+  EXPECT_TRUE(
+      Ctx.signatureVerificationStateClose(VerificationStateHandle).has_value());
+  EXPECT_TRUE(Ctx.signatureStateClose(StateHandle).has_value());
+  EXPECT_TRUE(Ctx.keypairClose(KpHandle).has_value());
+  EXPECT_TRUE(Ctx.publickeyClose(PkHandle).has_value());
+  EXPECT_TRUE(Ctx.signatureClose(SignatureHandle).has_value());
+}
+
 TEST(WasiCryptoTest, TestSignaturesEddsa) {
   WasiCryptoContext Ctx;
 

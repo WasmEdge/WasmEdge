b7bc5170d897b33ed69a9353c25d3ff03c97ae5b
Xin Liu <sam@secondstate.io>
2022-01-20 11:33:03 +0000

    [Rust SDK][feature](instance): Added instance module.
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- bindings/rust/wasmedge/src/store.rs
+++ bindings/rust/wasmedge/src/store.rs
@@ -1,4 +1,4 @@
-use crate::{error::WasmEdgeResult, wasmedge};
+use crate::{error::WasmEdgeResult, wasmedge, Func};
 
 #[derive(Debug)]
 pub struct Store {
@@ -8,5 +8,30 @@ impl Store {
     pub fn new() -> WasmEdgeResult<Self> {
         let inner = wasmedge::Store::create()?;
         Ok(Self { inner })
+    }
+
+    /// Returns the names of all registered [modules](crate::Module)
+    pub fn mod_names(&self) -> Option<Vec<String>> {
+        self.inner.reg_module_names()
+    }
+
+    pub fn functions(&self) -> WasmEdgeResult<Vec<Func>> {
+        let mut funcs = Vec::new();
+
+        // funcs
+        if self.inner.func_len() > 0 {
+            let func_names = self.inner.func_names().unwrap();
+            for name in func_names {
+                let func = self.inner.find_func(&name)?;
+                let func = Func {
+                    inner: func,
+                    name: Some(name.clone()),
+                    mod_name: None,
+                };
+                funcs.push(func);
+            }
+        }
+
+        Ok(funcs)
     }
 }

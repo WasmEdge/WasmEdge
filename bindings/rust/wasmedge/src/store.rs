fe9ac1cd07e945ade4ffd466a26a436f2babec1a
Xin Liu <sam@secondstate.io>
2022-01-21 04:50:12 +0000

    [Rust SDK][refactor](store): Refactored the functions method.
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- bindings/rust/wasmedge/src/store.rs
+++ bindings/rust/wasmedge/src/store.rs
@@ -20,6 +20,7 @@ impl<'vm> Store<'vm> {
         self.inner.reg_module_names()
     }
 
+    /// Returns all exported functions in the store, including both registered and non-registered.
     pub fn functions(&self) -> WasmEdgeResult<Vec<Func>> {
         let mut funcs = Vec::new();
 
@@ -30,13 +31,92 @@ impl<'vm> Store<'vm> {
                 let func = self.inner.find_func(&name)?;
                 let func = Func {
                     inner: func,
-                    name: Some(name.clone()),
+                    name: Some(name),
                     mod_name: None,
                 };
                 funcs.push(func);
             }
         }
 
+        // registered funcs
+        if self.inner.reg_module_len() > 0 {
+            let mod_names = self.mod_names().unwrap();
+            for mod_name in mod_names {
+                if self.inner.reg_func_len(&mod_name) > 0 {
+                    let func_names = self.inner.reg_func_names(&mod_name).unwrap();
+                    for name in func_names {
+                        let func = self.inner.find_func_registered(&mod_name, &name)?;
+                        let func = Func {
+                            inner: func,
+                            name: Some(name),
+                            mod_name: Some(mod_name.clone()),
+                        };
+                        funcs.push(func);
+                    }
+                }
+            }
+        }
+
         Ok(funcs)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{ConfigBuilder, SignatureBuilder, ValType, VmBuilder};
+
+    #[test]
+    fn test_store_functions() {
+        // create a Config
+        let config = ConfigBuilder::new()
+            .expect("fail to create a ConfigBuilder")
+            .build();
+
+        // create a Store
+        let result = Store::new();
+        assert!(result.is_ok());
+        let store = result.unwrap();
+
+        // create a Vm context
+        let result = VmBuilder::new()
+            .with_config(&config)
+            .with_store(&store)
+            .build();
+        assert!(result.is_ok());
+        let vm = result.unwrap();
+
+        // register a wasm module from a file
+        let path = std::path::PathBuf::from(env!("WASMEDGE_DIR"))
+            .join("tools/wasmedge/examples/fibonacci.wasm");
+        let result = vm.register_from_file("fib-module", path);
+        assert!(result.is_ok());
+        let vm = result.unwrap();
+
+        // get store
+        let result = vm.store_mut();
+        assert!(result.is_ok());
+        let store = result.unwrap();
+
+        // get all Func instances in the store
+        let result = store.functions();
+        assert!(result.is_ok());
+        let funcs = result.unwrap();
+        assert_eq!(funcs.len(), 1);
+        assert!(funcs[0].registered());
+        assert_eq!(funcs[0].mod_name().unwrap(), "fib-module");
+        assert_eq!(funcs[0].name().unwrap(), "fib");
+        let result = funcs[0].signature();
+        assert!(result.is_ok());
+        let signature = result.unwrap();
+        assert_eq!(
+            signature,
+            SignatureBuilder::new()
+                .with_args([ValType::I32])
+                .with_returns([ValType::I32])
+                .build()
+        );
+
+        dbg!(&signature);
     }
 }

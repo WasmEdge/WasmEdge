0bfe6af9935369bd378a1867533e2106a9deb8ce
Xin Liu <sam@secondstate.io>
2022-01-21 03:40:44 +0000

    [Rust SDK][feature](vm): Added new feature for Vm.
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- bindings/rust/wasmedge/src/lib.rs
+++ bindings/rust/wasmedge/src/lib.rs
@@ -10,3 +10,4 @@ pub use config::{Config, ConfigBuilder};
 pub use instance::{Func, FuncSignature, Module};
 pub use store::Store;
 pub use vm::{Vm, VmBuilder};
+pub use wasmedge_sys::{ValType, Value};
--- bindings/rust/wasmedge/src/vm.rs
+++ bindings/rust/wasmedge/src/vm.rs
@@ -1,11 +1,12 @@
 use crate::{error::WasmEdgeResult, wasmedge, Config, Store};
+use std::{marker::PhantomData, path::Path};
 
 // use crate::{config::Config, error::VmError, module::Module, wasi_conf::WasiConf};
 
 #[derive(Debug)]
 pub struct VmBuilder<'a> {
     config: Option<&'a Config>,
-    store: Option<&'a Store>,
+    store: Option<&'a Store<'a>>,
     vm: Option<wasmedge::Vm>,
 }
 
@@ -56,6 +57,26 @@ impl<'a> VmBuilder<'a> {
 pub struct Vm {
     inner: wasmedge::Vm,
 }
+impl Vm {
+    pub fn register_from_file(
+        self,
+        mod_name: impl AsRef<str>,
+        path: impl AsRef<Path>,
+    ) -> WasmEdgeResult<Self> {
+        let inner = self
+            .inner
+            .register_wasm_from_file(mod_name.as_ref(), path.as_ref())?;
+        Ok(Self { inner })
+    }
+
+    pub fn store_mut(&self) -> WasmEdgeResult<Store> {
+        let inner = self.inner.store_mut()?;
+        Ok(Store {
+            inner,
+            _marker: PhantomData,
+        })
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -67,7 +88,10 @@ mod tests {
         {
             let result = VmBuilder::new().build();
             assert!(result.is_ok());
-            dbg!(result.unwrap());
+            let vm = result.unwrap();
+
+            let result = vm.store_mut();
+            assert!(result.is_ok());
         }
 
         {
@@ -79,6 +103,10 @@ mod tests {
             // create a Vm context
             let result = VmBuilder::new().with_config(&config).build();
             assert!(result.is_ok());
+            let vm = result.unwrap();
+
+            let result = vm.store_mut();
+            assert!(result.is_ok());
         }
 
         {
@@ -89,6 +117,10 @@ mod tests {
 
             // create a Vm context
             let result = VmBuilder::new().with_store(&store).build();
+            assert!(result.is_ok());
+            let vm = result.unwrap();
+
+            let result = vm.store_mut();
             assert!(result.is_ok());
         }
 
@@ -109,6 +141,37 @@ mod tests {
                 .with_store(&store)
                 .build();
             assert!(result.is_ok());
+            let vm = result.unwrap();
+
+            let result = vm.store_mut();
+            assert!(result.is_ok());
         }
+    }
+
+    #[test]
+    fn test_vm_register_wasm_from_file() {
+        // create a Config
+        let config = ConfigBuilder::new()
+            .expect("fail to create a ConfigBuilder")
+            .build();
+
+        // create a Store
+        let result = Store::new();
+        assert!(result.is_ok());
+        let store = result.unwrap();
+
+        // create a Vm context
+        let result = VmBuilder::new()
+            .with_config(&config)
+            .with_store(&store)
+            .build();
+        assert!(result.is_ok());
+        let vm = result.unwrap();
+
+        // register a wasm module from a file
+        let path = std::path::PathBuf::from(env!("WASMEDGE_DIR"))
+            .join("tools/wasmedge/examples/fibonacci.wasm");
+        let result = vm.register_from_file("fib-module", path);
+        assert!(result.is_ok());
     }
 }

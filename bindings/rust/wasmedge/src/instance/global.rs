afe7a3424d62d95c37eba4d94afa1ef0c3cb7ac4
Xin Liu <sam@secondstate.io>
2022-01-24 08:59:18 +0000

    [Rust SDK][feature](global): Added GlobalType.

--- bindings/rust/wasmedge/src/instance/global.rs
+++ bindings/rust/wasmedge/src/instance/global.rs
@@ -7,8 +7,14 @@ pub struct Global {
     pub(crate) mod_name: Option<String>,
 }
 impl Global {
-    pub fn new(val: Value, val_ty: ValType, mutable: Mutability) -> WasmEdgeResult<Self> {
-        unimplemented!()
+    pub fn new(ty: GlobalType, val: Value) -> WasmEdgeResult<Self> {
+        let mut ty = wasmedge::GlobalType::create(ty.ty(), ty.mutability())?;
+        let inner = wasmedge::Global::create(&mut ty, val)?;
+        Ok(Self {
+            inner,
+            name: None,
+            mod_name: None,
+        })
     }
 
     pub fn name(&self) -> Option<&str> {
@@ -29,19 +35,39 @@ impl Global {
         self.mod_name.is_some()
     }
 
-    pub fn value_type(&self) -> ValType {
-        unimplemented!()
-    }
-
-    pub fn mutability(&self) -> Mutability {
-        unimplemented!()
+    pub fn ty(&self) -> WasmEdgeResult<GlobalType> {
+        let gt = self.inner.ty()?;
+        Ok(GlobalType {
+            ty: gt.value_type(),
+            mutability: gt.mutability(),
+        })
     }
 
     pub fn get_value(&self) -> Value {
-        unimplemented!()
+        self.inner.get_value()
     }
 
-    pub fn set_value(&self, val: Value) -> WasmEdgeResult<()> {
-        unimplemented!()
+    pub fn set_value(&mut self, val: Value) -> WasmEdgeResult<()> {
+        self.inner.set_value(val)?;
+        Ok(())
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct GlobalType {
+    ty: ValType,
+    mutability: Mutability,
+}
+impl GlobalType {
+    pub fn new(ty: ValType, mutability: Mutability) -> Self {
+        Self { ty, mutability }
+    }
+
+    pub fn ty(&self) -> ValType {
+        self.ty
+    }
+
+    pub fn mutability(&self) -> Mutability {
+        self.mutability
     }
 }

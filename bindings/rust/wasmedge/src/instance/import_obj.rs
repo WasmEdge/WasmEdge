a4c5b0970f2a48f0ddcf83900774fb24dc4af4eb
Xin Liu <sam@secondstate.io>
2022-01-25 06:12:58 +0000

    [Rust SDK][feature](import_obj): Added APIs for ImportObj.
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- bindings/rust/wasmedge/src/instance/import_obj.rs
+++ bindings/rust/wasmedge/src/instance/import_obj.rs
@@ -1,5 +1,19 @@
-use crate::wasmedge;
+use crate::{wasmedge, Func, Global, Memory, Table};
 
 pub struct ImportObj {
     pub(crate) inner: wasmedge::ImportObj,
+}
+impl ImportObj {
+    pub fn add_func(&mut self, name: impl AsRef<str>, func: &mut Func) {
+        unimplemented!()
+    }
+    pub fn add_global(&mut self, name: impl AsRef<str>, global: &mut Global) {
+        unimplemented!()
+    }
+    pub fn add_memory(&mut self, name: impl AsRef<str>, memory: &mut Memory) {
+        unimplemented!()
+    }
+    pub fn add_table(&mut self, name: impl AsRef<str>, global: &mut Table) {
+        unimplemented!()
+    }
 }

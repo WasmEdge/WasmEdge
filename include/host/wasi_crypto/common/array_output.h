24d67406bb86ba5294242192bd7dbf36987868b3
sonder-joker <highmagic@outlook.com>
2022-01-26 17:18:10 +0000

    [Doc] Add doc for array output
    Signed-off-by: sonder-joker <highmagic@outlook.com>

--- include/host/wasi_crypto/common/array_output.h
+++ include/host/wasi_crypto/common/array_output.h
@@ -1,4 +1,16 @@
 // SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2019-2022 Second State INC
+
+//===-- wasmedge/wasi_crypto/array_output.h - ArrayOutput definition ------===//
+//
+// Part of the WasmEdge Project.
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file contains the class definitions of the WasiCrypto ArrayOutput.
+///
+//===----------------------------------------------------------------------===//
 #pragma once
 
 #include "common/span.h"
@@ -12,12 +24,18 @@ namespace WasmEdge {
 namespace Host {
 namespace WASICrypto {
 namespace Common {
+/// Functions returning arrays whose size is not constant or too large to be
+/// safely allocated on the stack return a handle to an ArrayOutput type.
 class ArrayOutput {
 public:
   ArrayOutput(std::vector<uint8_t> &&Data) : Data(std::move(Data)) {}
 
+  /// Copy the content to the @param Buf buffer
+  /// Multiple calls are possible, the total number of bytes to be read is
+  /// guaranteed to always match data size
   std::tuple<size_t, bool> pull(Span<uint8_t> Buf);
 
+  /// Return data size
   size_t len();
 
 private:

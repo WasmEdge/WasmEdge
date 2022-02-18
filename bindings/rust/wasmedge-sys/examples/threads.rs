4932814fc33ab0623da7e6f764bbab5a2f7d0ec1
Xin Liu <sam@secondstate.io>
2022-02-18 09:33:40 +0000

    [Rust][refactor](example): Refactored the example.
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- bindings/rust/wasmedge-sys/examples/threads.rs
+++ bindings/rust/wasmedge-sys/examples/threads.rs
@@ -27,9 +27,23 @@ fn main() -> Result<(), WasmEdgeError> {
 
     let vm = Arc::new(Mutex::new(vm));
 
+    // compute fib(4) by a child thread
+    let vm_cloned = Arc::clone(&vm);
+    let handle_a = thread::spawn(move || {
+        let vm_child_thread = vm_cloned.lock().expect("fail to lock vm");
+        let returns = vm_child_thread
+            .run_registered_function("extern", "fib", [Value::from_i32(4)])
+            .expect("fail to compute fib(4)");
+
+        let fib4 = returns[0].to_i32();
+        println!("fib(4) by child thread: {}", fib4);
+
+        fib4
+    });
+
     // compute fib(5) by a child thread
     let vm_cloned = Arc::clone(&vm);
-    let handle = thread::spawn(move || {
+    let handle_b = thread::spawn(move || {
         let vm_child_thread = vm_cloned.lock().expect("fail to lock vm");
         let returns = vm_child_thread
             .run_registered_function("extern", "fib", [Value::from_i32(5)])
@@ -41,17 +55,8 @@ fn main() -> Result<(), WasmEdgeError> {
         fib5
     });
 
-    // compute fib(4) by main thread
-    let fib4 = {
-        let vm_main_thread = vm.lock().expect("fail to lock vm");
-        let returns =
-            vm_main_thread.run_registered_function("extern", "fib", [Value::from_i32(4)])?;
-
-        println!("fib(4) by main thread: {}", returns[0].to_i32());
-        returns[0].to_i32()
-    };
-
-    let fib5 = handle.join().unwrap();
+    let fib4 = handle_a.join().unwrap();
+    let fib5 = handle_b.join().unwrap();
 
     // compute fib(6)
     println!("fib(6) = fib(5) + fib(1) = {}", fib5 + fib4);

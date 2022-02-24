3e151a0a55add2c6110d754a34805012e621d17e
sonder-joker <highmagic@outlook.com>
2022-02-24 11:51:22 +0800

    [WASI] Add symmetric Context
    Signed-off-by: sonder-joker <highmagic@outlook.com>

---
+++ lib/host/wasi_crypto/symmetric/ctx.cpp
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2019-2022 Second State INC
+
+#include "host/wasi_crypto/ctx.h"
+#include "host/wasi_crypto/common/options.h"
+#include "wasi_crypto/api.hpp"
+
+namespace WasmEdge {
+namespace Host {
+namespace WasiCrypto {
+
+
+} // namespace WasiCrypto
+} // namespace Host
+} // namespace WasmEdge

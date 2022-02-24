1d1c78eda3a470ce8012be9def2e1dc7f9ebfa98
sonder-joker <highmagic@outlook.com>
2022-02-24 17:03:18 +0800

    [WASI] Add common Context
    Signed-off-by: sonder-joker <highmagic@outlook.com>

---
+++ lib/host/wasi_crypto/common/ctx.cpp
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2019-2022 Second State INC
+
+#include "host/wasi_crypto/ctx.h"
+
+namespace WasmEdge {
+namespace Host {
+namespace WasiCrypto {
+
+} // namespace WasiCrypto
+} // namespace Host
+} // namespace WasmEdge

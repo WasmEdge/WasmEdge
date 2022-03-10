5ef99b4ab28a0e55bce4b3519399077539226f95
YiYing He <yiying@secondstate.io>
2022-03-10 15:53:14 +0800

    [WASI] Add WASI-NN host functions.
    Signed-off-by: YiYing He <yiying@secondstate.io>

---
+++ include/host/wasi_nn/wasinnmodule.h
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2019-2022 Second State INC
+
+#pragma once
+
+#include "host/wasi_nn/wasinncontext.h"
+#include "runtime/importobj.h"
+
+namespace WasmEdge {
+namespace Host {
+
+class WasiNNModule : public Runtime::ImportObject {
+public:
+  WasiNNModule();
+
+private:
+  WasiNNContext Ctx;
+};
+
+} // namespace Host
+} // namespace WasmEdge

7f1e4e231b7b754efd5bb885b1c44084c5758e40
sonder-joker <highmagic@outlook.com>
2022-03-10 22:17:22 +0800

    [WASI] Implement symmetric sha2
    Signed-off-by: sonder-joker <highmagic@outlook.com>

---
+++ include/host/wasi_crypto/symmetric/hash/sha2.h
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2019-2022 Second State INC
+
+//===-- wasi_crypto/symmetric/hash/sha2.h - Sha2 class declaration --------===//
+//
+// Part of the WasmEdge Project.
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file contains the declaration of Sha2 class.
+///
+//===----------------------------------------------------------------------===//
+#pragma once
+
+#include "host/wasi_crypto/symmetric/hash/helper.h"
+#include "host/wasi_crypto/symmetric/options.h"
+#include "host/wasi_crypto/utils/evp_wrapper.h"
+#include "host/wasi_crypto/utils/optional.h"
+
+namespace WasmEdge {
+namespace Host {
+namespace WasiCrypto {
+namespace Symmetric {
+
+template <int ShaNid> class Sha2 {
+public:
+  /// in fact, sha2 key will never produce. This design for remove forward
+  /// declare
+  class Key : public HashKey<Key> {};
+
+  class State : public HashState<Key> {
+  public:
+    State(EvpMdCtxPtr Ctx) noexcept : Ctx(std::move(Ctx)) {}
+
+    static WasiCryptoExpect<State>
+    open(OptionalRef<Options> OptOption) noexcept;
+
+    WasiCryptoExpect<void> absorb(Span<const uint8_t> Data) noexcept;
+
+    WasiCryptoExpect<void> squeeze(Span<uint8_t> Out) noexcept;
+
+  private:
+    std::shared_ptr<EVP_MD_CTX> Ctx;
+  };
+
+private:
+  /// return sha digest size
+  constexpr static size_t getDigestSize() noexcept;
+};
+
+using Sha256 = Sha2<NID_sha256>;
+using Sha512 = Sha2<NID_sha512>;
+using Sha512_256 = Sha2<NID_sha512_256>;
+
+} // namespace Symmetric
+} // namespace WasiCrypto
+} // namespace Host
+} // namespace WasmEdge
--- lib/host/wasi_crypto/symmetric/state.cpp
+++ lib/host/wasi_crypto/symmetric/state.cpp
@@ -2,11 +2,133 @@
 // SPDX-FileCopyrightText: 2019-2022 Second State INC
 
 #include "host/wasi_crypto/symmetric/state.h"
+#include "host/wasi_crypto/utils/error.h"
+#include "wasi_crypto/api.hpp"
 
 namespace WasmEdge {
 namespace Host {
 namespace WasiCrypto {
 namespace Symmetric {
+namespace {
+WasiCryptoExpect<size_t> checkedAdd(size_t A, size_t B) {
+  size_t Res;
+  ensureOrReturn(!__builtin_add_overflow(A, B, &Res),
+                 __WASI_CRYPTO_ERRNO_OVERFLOW);
+  return Res;
+}
+} // namespace
+
+WasiCryptoExpect<size_t> stateOptionsGet(const StateVariant &StateVariant,
+                                         std::string_view Name,
+                                         Span<uint8_t> Value) noexcept {
+  return std::visit(
+      [=](auto &&State) noexcept { return State.optionsGet(Name, Value); },
+      StateVariant);
+}
+
+WasiCryptoExpect<uint64_t> stateOptionsGetU64(const StateVariant &StateVariant,
+                                              std::string_view Name) noexcept {
+  return std::visit(
+      [=](auto &&State) noexcept { return State.optionsGetU64(Name); },
+      StateVariant);
+}
+
+WasiCryptoExpect<void> stateAbsorb(StateVariant &StateVariant,
+                                   Span<const uint8_t> Data) noexcept {
+  return std::visit([=](auto &&State) noexcept { return State.absorb(Data); },
+                    StateVariant);
+}
+
+WasiCryptoExpect<void> stateSqueeze(StateVariant &StateVariant,
+                                    Span<uint8_t> Out) noexcept {
+  return std::visit([=](auto &&State) noexcept { return State.squeeze(Out); },
+                    StateVariant);
+}
+
+WasiCryptoExpect<Tag> stateSqueezeTag(StateVariant &StateVariant) noexcept {
+  return std::visit([=](auto &&State) noexcept { return State.squeezeTag(); },
+                    StateVariant);
+}
+
+WasiCryptoExpect<KeyVariant> stateSqueezeKey(StateVariant &StateVariant,
+                                             Algorithm KeyAlg) noexcept {
+  return std::visit(
+      [=](auto &&State) noexcept {
+        return State.squeezeKey(KeyAlg).map(
+            [](auto &&Key) { return KeyVariant{Key}; });
+      },
+      StateVariant);
+}
+
+WasiCryptoExpect<size_t>
+stateMaxTagLen(const StateVariant &StateVariant) noexcept {
+  return std::visit([=](auto &&State) noexcept { return State.maxTagLen(); },
+                    StateVariant);
+}
+
+WasiCryptoExpect<size_t> stateEncrypt(StateVariant &StateVariant,
+                                      Span<uint8_t> Out,
+                                      Span<const uint8_t> Data) noexcept {
+  return std::visit(
+      [=](auto &&State) noexcept -> WasiCryptoExpect<size_t> {
+        return State.maxTagLen()
+            .and_then([DataSize = Data.size()](size_t TagLen) noexcept {
+              return checkedAdd(DataSize, TagLen);
+            })
+            .and_then([Out, Data, &State](size_t ActualDataLen) noexcept
+                      -> WasiCryptoExpect<size_t> {
+              ensureOrReturn(Out.size() == ActualDataLen,
+                             __WASI_CRYPTO_ERRNO_INVALID_LENGTH);
+              return State.encrypt(Out, Data);
+            });
+      },
+      StateVariant);
+}
+
+WasiCryptoExpect<Tag> stateEncryptDetached(StateVariant &StateVariant,
+                                           Span<uint8_t> Out,
+                                           Span<const uint8_t> Data) noexcept {
+  ensureOrReturn(Data.size() == Out.size(), __WASI_CRYPTO_ERRNO_INVALID_LENGTH);
+  return std::visit(
+      [=](auto &&State) noexcept { return State.encryptDetached(Out, Data); },
+      StateVariant);
+}
+
+WasiCryptoExpect<size_t> stateDecrypt(StateVariant &StateVariant,
+                                      Span<uint8_t> Out,
+                                      Span<const uint8_t> Data) noexcept {
+  return std::visit(
+      [=](auto &&State) noexcept -> WasiCryptoExpect<size_t> {
+        return State.maxTagLen()
+            .and_then([OutSize = Out.size()](size_t TagLen) noexcept {
+              return checkedAdd(OutSize, TagLen);
+            })
+            .and_then([Out, Data, &State](size_t ActualOutLen) noexcept
+                      -> WasiCryptoExpect<size_t> {
+              ensureOrReturn(Data.size() == ActualOutLen,
+                             __WASI_CRYPTO_ERRNO_INVALID_LENGTH);
+              return State.decrypt(Out, Data);
+            });
+      },
+      StateVariant);
+}
+
+WasiCryptoExpect<size_t> stateDecryptDetached(StateVariant &StateVariant,
+                                              Span<uint8_t> Out,
+                                              Span<const uint8_t> Data,
+                                              Span<uint8_t> RawTag) noexcept {
+  ensureOrReturn(Data.size() == Out.size(), __WASI_CRYPTO_ERRNO_INVALID_LENGTH);
+  return std::visit(
+      [=](auto &&State) noexcept {
+        return State.decryptDetached(Out, Data, RawTag);
+      },
+      StateVariant);
+}
+
+WasiCryptoExpect<void> stateRatchet(StateVariant &StateVariant) noexcept {
+  return std::visit([=](auto &&State) noexcept { return State.ratchet(); },
+                    StateVariant);
+}
 
 } // namespace Symmetric
 } // namespace WasiCrypto

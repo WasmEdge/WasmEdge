ca3264234d6c1e7e43b514b4643d6fe419e57a98
sonder-joker <highmagic@outlook.com>
2022-03-10 22:23:02 +0800

    [WASI] Implement Hkdf
    Signed-off-by: sonder-joker <highmagic@outlook.com>

---
+++ include/host/wasi_crypto/symmetric/eae/hkdf.h
@@ -0,0 +1,131 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2019-2022 Second State INC
+
+//===-- wasi_crypto/symmetric/eae/hkdf.h - Hkdf class declaration ---------===//
+//
+// Part of the WasmEdge Project.
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file contains the declaration of Hkdf class.
+///
+//===----------------------------------------------------------------------===//
+#pragma once
+
+#include "host/wasi_crypto/symmetric/eae/helper.h"
+#include "host/wasi_crypto/symmetric/options.h"
+#include "host/wasi_crypto/utils/evp_wrapper.h"
+#include "host/wasi_crypto/utils/optional.h"
+#include "host/wasi_crypto/utils/secret_key.h"
+
+namespace WasmEdge {
+namespace Host {
+namespace WasiCrypto {
+namespace Symmetric {
+
+template <int ShaNid> class Hkdf {
+public:
+  class Expand {
+  public:
+    class Key {
+    public:
+      Key(std::shared_ptr<SecretKey> Data) noexcept : Data(std::move(Data)) {}
+
+      static WasiCryptoExpect<Key> import(Span<const uint8_t> Data) noexcept;
+
+      static WasiCryptoExpect<Key>
+      generate(OptionalRef<Options> Options) noexcept;
+
+      std::vector<uint8_t> exportData() const noexcept { return Data->raw(); }
+
+      const std::vector<uint8_t> &ref() const noexcept { return Data->raw(); }
+
+    private:
+      const std::shared_ptr<SecretKey> Data;
+    };
+
+    class State : public ExpandState<Key> {
+    public:
+      static WasiCryptoExpect<State>
+      open(Key &Key, OptionalRef<Options> OptOption) noexcept;
+
+      State(EvpPkeyCtxPtr Ctx) noexcept : Ctx(std::move(Ctx)) {}
+
+      WasiCryptoExpect<void> absorb(Span<const uint8_t> Data) noexcept;
+
+      WasiCryptoExpect<void> squeeze(Span<uint8_t> Out) noexcept;
+
+    private:
+      const std::shared_ptr<EVP_PKEY_CTX> Ctx;
+    };
+  };
+
+  class Extract {
+  public:
+    class Key {
+    public:
+      Key(std::shared_ptr<SecretKey> Data) noexcept
+          : Data(std::move(Data)) {}
+
+      static WasiCryptoExpect<Key> import(Span<const uint8_t> Data) noexcept;
+
+      static WasiCryptoExpect<Key>
+      generate(OptionalRef<Options> Options) noexcept;
+
+      std::vector<uint8_t> exportData() const noexcept { return Data->raw(); }
+
+      const std::vector<uint8_t> &ref() const noexcept { return Data->raw(); }
+
+    private:
+      const std::shared_ptr<SecretKey> Data;
+    };
+
+    class State : public ExtractState<Key> {
+    public:
+      State(EvpPkeyCtxPtr Ctx) noexcept
+          : Ctx(std::make_shared<Inner>(std::move(Ctx))) {}
+
+      static WasiCryptoExpect<State>
+      open(Key &Key, OptionalRef<Options> OptOption) noexcept;
+
+      /// absorbs the salt of the key(Extract)/info(Expand) information.
+      WasiCryptoExpect<void> absorb(Span<const uint8_t> Data) noexcept;
+
+      /// Extract:
+      /// returns the PRK, whose algorithm type is set to the EXPAND counterpart
+      /// of the EXTRACT operation
+      WasiCryptoExpect<typename Expand::Key> squeezeKey(Algorithm Alg) noexcept;
+
+    private:
+      struct Inner {
+        Inner(EvpPkeyCtxPtr RawCtx) : RawCtx(std::move(RawCtx)) {}
+        std::shared_mutex Mutex;
+        std::vector<uint8_t> Salt;
+        EvpPkeyCtxPtr RawCtx;
+      };
+      const std::shared_ptr<Inner> Ctx;
+    };
+  };
+
+private:
+  constexpr static uint32_t getKeySize() noexcept;
+
+  /// Type erasure to pass clang-tidy check
+  constexpr static void *getShaCtx() noexcept;
+
+  constexpr static Symmetric::Algorithm getExpandAlg() noexcept;
+
+  static WasiCryptoExpect<EvpPkeyCtxPtr>
+  openStateImpl(const std::vector<uint8_t> &Key, int Mode) noexcept;
+};
+
+using HkdfSha256Extract = Hkdf<NID_sha256>::Extract;
+using HkdfSha512Extract = Hkdf<NID_sha512>::Extract;
+using HkdfSha256Expand = Hkdf<NID_sha256>::Expand;
+using HkdfSha512Expand = Hkdf<NID_sha512>::Expand;
+
+} // namespace Symmetric
+} // namespace WasiCrypto
+} // namespace Host
+} // namespace WasmEdge
---
+++ lib/host/wasi_crypto/symmetric/eae/hkdf.cpp
@@ -0,0 +1,144 @@
+// SPDX-License-Identifier: Apache-2.0
+
+#include "host/wasi_crypto/symmetric/eae/hkdf.h"
+#include "host/wasi_crypto/symmetric/alg.h"
+#include "host/wasi_crypto/symmetric/factory.h"
+#include "host/wasi_crypto/utils/error.h"
+#include "host/wasi_crypto/utils/evp_wrapper.h"
+#include "host/wasi_crypto/utils/secret_key.h"
+#include "openssl/kdf.h"
+#include "openssl/rand.h"
+#include "wasi_crypto/api.hpp"
+
+namespace WasmEdge {
+namespace Host {
+namespace WasiCrypto {
+namespace Symmetric {
+
+template <int ShaNid> constexpr uint32_t Hkdf<ShaNid>::getKeySize() noexcept {
+  if constexpr (ShaNid == NID_sha256)
+    return 32;
+  if constexpr (ShaNid == NID_sha512)
+    return 64;
+}
+
+template <int ShaNid> constexpr void *Hkdf<ShaNid>::getShaCtx() noexcept {
+  return static_cast<void *>(const_cast<EVP_MD *>(EVP_get_digestbynid(ShaNid)));
+}
+
+template <int ShaNid>
+constexpr Algorithm Hkdf<ShaNid>::getExpandAlg() noexcept {
+  if constexpr (ShaNid == NID_sha256)
+    return Algorithm::HkdfSha256Expand;
+  if constexpr (ShaNid == NID_sha512)
+    return Algorithm::HkdfSha512Expand;
+}
+
+template <int ShaNid>
+WasiCryptoExpect<typename Hkdf<ShaNid>::Expand::Key>
+Hkdf<ShaNid>::Expand::Key::generate(OptionalRef<Options>) noexcept {
+  return WasiCryptoUnexpect(__WASI_CRYPTO_ERRNO_UNSUPPORTED_FEATURE);
+}
+
+template <int ShaNid>
+WasiCryptoExpect<typename Hkdf<ShaNid>::Expand::Key>
+Hkdf<ShaNid>::Expand::Key::import(Span<const uint8_t> Raw) noexcept {
+  ensureOrReturn(Raw.size() == getKeySize(), __WASI_CRYPTO_ERRNO_INVALID_KEY);
+  return SecretKey::random(getKeySize());
+}
+
+template <int ShaNid>
+WasiCryptoExpect<typename Hkdf<ShaNid>::Expand::State>
+Hkdf<ShaNid>::Expand::State::open(Key &Key, OptionalRef<Options>) noexcept {
+  return openStateImpl(Key.ref(), EVP_PKEY_HKDEF_MODE_EXPAND_ONLY);
+}
+
+template <int ShaNid>
+WasiCryptoExpect<void>
+Hkdf<ShaNid>::Expand::State::absorb(Span<const uint8_t> Data) noexcept {
+  opensslAssuming(
+      EVP_PKEY_CTX_add1_hkdf_info(Ctx.get(), Data.data(), Data.size()));
+  return {};
+}
+
+template <int ShaNid>
+WasiCryptoExpect<void>
+Hkdf<ShaNid>::Expand::State::squeeze(Span<uint8_t> Out) noexcept {
+  ensureOrReturn(
+      EVP_PKEY_derive(Ctx.get(), Out.data(), addressOfTemporary(Out.size())),
+      __WASI_CRYPTO_ERRNO_INVALID_KEY);
+
+  return {};
+}
+
+template <int ShaNid>
+WasiCryptoExpect<typename Hkdf<ShaNid>::Extract::Key>
+Hkdf<ShaNid>::Extract::Key::generate(OptionalRef<Options>) noexcept {
+  return SecretKey::random(getKeySize());
+}
+
+template <int ShaNid>
+WasiCryptoExpect<typename Hkdf<ShaNid>::Extract::Key>
+Hkdf<ShaNid>::Extract::Key::import(Span<const uint8_t> Raw) noexcept {
+  return std::make_shared<SecretKey>(Raw);
+}
+
+template <int ShaNid>
+WasiCryptoExpect<typename Hkdf<ShaNid>::Extract::State>
+Hkdf<ShaNid>::Extract::State::open(Key &Key, OptionalRef<Options>) noexcept {
+  return openStateImpl(Key.ref(), EVP_PKEY_HKDEF_MODE_EXTRACT_ONLY);
+}
+
+template <int ShaNid>
+WasiCryptoExpect<void>
+Hkdf<ShaNid>::Extract::State::absorb(Span<const uint8_t> Data) noexcept {
+  std::unique_lock<std::shared_mutex> Lock{Ctx->Mutex};
+
+  Ctx->Salt.insert(Ctx->Salt.end(), Data.begin(), Data.end());
+  return {};
+}
+
+template <int ShaNid>
+WasiCryptoExpect<typename Hkdf<ShaNid>::Expand::Key>
+Hkdf<ShaNid>::Extract::State::squeezeKey(Symmetric::Algorithm Alg) noexcept {
+  ensureOrReturn(Alg == getExpandAlg(),
+                 __WASI_CRYPTO_ERRNO_UNSUPPORTED_ALGORITHM);
+
+  std::shared_lock<std::shared_mutex> Lock{Ctx->Mutex};
+  opensslAssuming(EVP_PKEY_CTX_set1_hkdf_salt(
+      Ctx->RawCtx.get(), Ctx->Salt.data(), Ctx->Salt.size()));
+
+  auto Data = std::make_shared<SecretKey>(getKeySize());
+
+  size_t ActualOutSize;
+  opensslAssuming(
+      EVP_PKEY_derive(Ctx->RawCtx.get(), Data->raw().data(), &ActualOutSize));
+  ensureOrReturn(ActualOutSize == getKeySize(),
+                 __WASI_CRYPTO_ERRNO_ALGORITHM_FAILURE);
+
+  return Data;
+}
+
+template <int ShaNid>
+WasiCryptoExpect<EvpPkeyCtxPtr>
+Hkdf<ShaNid>::openStateImpl(const std::vector<uint8_t> &Key,
+                            int Mode) noexcept {
+  EvpPkeyCtxPtr Ctx{EVP_PKEY_CTX_new_id(EVP_PKEY_HKDF, nullptr)};
+  opensslAssuming(EVP_PKEY_derive_init(Ctx.get()));
+  opensslAssuming(EVP_PKEY_CTX_set_hkdf_md(Ctx.get(), getShaCtx()));
+  opensslAssuming(EVP_PKEY_CTX_hkdf_mode(Ctx.get(), Mode));
+  ensureOrReturn(EVP_PKEY_CTX_set1_hkdf_key(Ctx.get(), Key.data(), Key.size()),
+                 __WASI_CRYPTO_ERRNO_INVALID_KEY);
+
+  return Ctx;
+}
+
+template class Hkdf<NID_sha256>::Extract;
+template class Hkdf<NID_sha512>::Extract;
+template class Hkdf<NID_sha256>::Expand;
+template class Hkdf<NID_sha512>::Expand;
+
+} // namespace Symmetric
+} // namespace WasiCrypto
+} // namespace Host
+} // namespace WasmEdge

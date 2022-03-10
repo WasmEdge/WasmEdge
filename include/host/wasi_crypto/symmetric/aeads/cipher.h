1352107bc005619c36dbe0d1b756b928101c2523
sonder-joker <highmagic@outlook.com>
2022-03-10 22:27:04 +0800

    [WASI] Implement Cipher
    Signed-off-by: sonder-joker <highmagic@outlook.com>

---
+++ include/host/wasi_crypto/symmetric/aeads/cipher.h
@@ -0,0 +1,153 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2019-2022 Second State INC
+
+//===-- wasi_crypto/symmetric/aeads/cipher.h - Cipher class declaration ---===//
+//
+// Part of the WasmEdge Project.
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file contains the declaration of Cipher class.
+///
+//===----------------------------------------------------------------------===//
+#pragma once
+
+#include "host/wasi_crypto/symmetric/aeads/helper.h"
+#include "host/wasi_crypto/utils/evp_wrapper.h"
+#include "host/wasi_crypto/utils/optional.h"
+#include "host/wasi_crypto/utils/secret_key.h"
+#include "wasi_crypto/api.hpp"
+
+namespace WasmEdge {
+namespace Host {
+namespace WasiCrypto {
+namespace Symmetric {
+
+template <int CipherNid> class Cipher {
+  inline constexpr static size_t NonceSize = 12;
+
+public:
+  class Key {
+  public:
+    Key(std::shared_ptr<SecretKey> Data) noexcept : Data(std::move(Data)) {}
+
+    static WasiCryptoExpect<Key> import(Span<const uint8_t> Data) noexcept;
+
+    static WasiCryptoExpect<Key>
+    generate(OptionalRef<Options> Options) noexcept;
+
+    std::vector<uint8_t> exportData() const noexcept { return Data->raw(); }
+
+    const std::vector<uint8_t> &ref() const noexcept { return Data->raw(); }
+
+  private:
+    const std::shared_ptr<SecretKey> Data;
+  };
+
+  // Nonce = IV,
+  class State : public AEADsState<Key> {
+  public:
+    /// There are four inputs for authenticated encryption:
+    /// @param[in] Key The secret key for encrypt
+    /// @param[in] OptOption `Must` contain an Nonce(Initialization vector).
+    static WasiCryptoExpect<State>
+    open(Key &Key, OptionalRef<Options> OptOption) noexcept;
+
+    State(EvpCipherCtxPtr Ctx, std::array<uint8_t, NonceSize> Nonce) noexcept
+        : Ctx(std::make_shared<Inner>(std::move(Ctx), Nonce)) {}
+
+    WasiCryptoExpect<size_t> optionsGet(std::string_view Name,
+                                        Span<uint8_t> Value) const noexcept;
+
+    /// Absorbs additional data. Multiple calls to absorb() MUST be equivalent
+    /// to a single call with a concatenation of the inputs.
+    ///
+    /// @param Data Additional data
+    /// @return Nothing or WasiCrypto error
+    WasiCryptoExpect<void> absorb(Span<const uint8_t> Data) noexcept;
+
+    /// Return the length required to encode the authentication tag
+    /// and optional padding bytes. The returned length MUST be constant for a
+    /// given algorithm. Guest applications are expected to provide an output
+    /// buffer whose size is the size of the message, plus the max_tag_len()
+    /// output value.
+    ///
+    /// @return the length required to encode the authentication tag
+    /// and optional padding bytes.
+    WasiCryptoExpect<size_t> maxTagLen() const noexcept;
+
+    /// Check Out.size() == Data.size() + maxTagLen(), then call
+    /// encryptUnchecked(Out, Data) or return error if not equal
+    ///
+    /// @param Out The encrypted data text
+    /// @param Data The data to be encrypted
+    /// @return Tag's size or
+    /// `__WASI_CRYPTO_ERRNO_OVERFLOW`/`__WASI_CRYPTO_ERRNO_INVALID_LENGTH`
+    WasiCryptoExpect<size_t> encrypt(Span<uint8_t> Out,
+                                     Span<const uint8_t> Data) noexcept;
+
+    /// Check Out.size() == Data.size(), then call
+    /// encryptDetachedUnchecked(Out, Data) or error if not equal
+    ///
+    /// @param Out The encrypted data text
+    /// @param Data The data to be encrypted
+    /// @return Tag
+    /// or `__WASI_CRYPTO_ERRNO_OVERFLOW`/`__WASI_CRYPTO_ERRNO_INVALID_LENGTH`
+    WasiCryptoExpect<Tag> encryptDetached(Span<uint8_t> Out,
+                                          Span<const uint8_t> Data) noexcept;
+
+    /// Check Out.size() = Data.size() + maxTagLen(), then call
+    /// decryptDetachedUnchecked(Out, Data) or error if not equal
+    ///
+    /// @param Out The decrypted data text
+    /// @param Data The data to be decrypted
+    /// @return Size or
+    /// `__WASI_CRYPTO_ERRNO_OVERFLOW`/`__WASI_CRYPTO_ERRNO_INVALID_LENGTH`
+    WasiCryptoExpect<size_t> decrypt(Span<uint8_t> Out,
+                                     Span<const uint8_t> Data) noexcept;
+
+    /// Check Out.size() == Data.size(), then call
+    /// encryptDetachedUnchecked(Out, Data) or error if not equal
+    ///
+    /// @param Out The decrypted data text
+    /// @param Data The data to be decrypted
+    /// @return Size or
+    /// `__WASI_CRYPTO_ERRNO_OVERFLOW`/`__WASI_CRYPTO_ERRNO_INVALID_LENGTH`
+    WasiCryptoExpect<size_t>
+    decryptDetached(Span<uint8_t> Out, Span<const uint8_t> Data,
+                    Span<const uint8_t> RawTag) noexcept;
+
+  private:
+    WasiCryptoExpect<size_t> encryptImpl(Span<uint8_t> Out, Span<uint8_t> Tag,
+                                         Span<const uint8_t> Data) noexcept;
+
+    WasiCryptoExpect<size_t> decryptImpl(Span<uint8_t> Out,
+                                         Span<const uint8_t> Data,
+                                         Span<const uint8_t> RawTag) noexcept;
+    struct Inner {
+      Inner(EvpCipherCtxPtr RawCtx,
+            std::array<uint8_t, NonceSize> Nonce) noexcept
+          : RawCtx(std::move(RawCtx)), Nonce(Nonce) {}
+      EvpCipherCtxPtr RawCtx;
+      std::array<uint8_t, NonceSize> Nonce;
+    };
+    const std::shared_ptr<Inner> Ctx;
+  };
+
+private:
+  enum Mode { Unchanged = -1, Decrypt = 0, Encrypt = 1 };
+
+  constexpr static size_t getKeySize() noexcept;
+
+  constexpr static size_t getTagSize() noexcept;
+};
+
+using Aes128Gcm = Cipher<NID_aes_128_gcm>;
+using Aes256Gcm = Cipher<NID_aes_256_gcm>;
+using ChaCha20Poly1305 = Cipher<NID_chacha20_poly1305>;
+
+} // namespace Symmetric
+} // namespace WasiCrypto
+} // namespace Host
+} // namespace WasmEdge
---
+++ lib/host/wasi_crypto/symmetric/aeads/cipher.cpp
@@ -0,0 +1,200 @@
+// SPDX-License-Identifier: Apache-2.0
+
+#include "host/wasi_crypto/symmetric/aeads/cipher.h"
+#include "host/wasi_crypto/utils/error.h"
+#include "host/wasi_crypto/utils/evp_wrapper.h"
+#include "wasi_crypto/api.hpp"
+
+#include <cstdint>
+
+namespace WasmEdge {
+namespace Host {
+namespace WasiCrypto {
+namespace Symmetric {
+using namespace std::literals;
+
+template <int CipherNid>
+constexpr size_t Cipher<CipherNid>::getKeySize() noexcept {
+  switch (CipherNid) {
+  case NID_aes_128_gcm:
+    return 16;
+  case NID_aes_256_gcm:
+    return 32;
+  case NID_chacha20_poly1305:
+    return 32;
+  }
+}
+
+template <int CipherNid>
+WasiCryptoExpect<size_t> Cipher<CipherNid>::State::maxTagLen() const noexcept {
+  return getTagSize();
+}
+
+template <int CipherNid>
+constexpr size_t Cipher<CipherNid>::getTagSize() noexcept {
+  return 16;
+}
+
+template <int CipherNid>
+WasiCryptoExpect<typename Cipher<CipherNid>::Key>
+Cipher<CipherNid>::Key::generate(OptionalRef<Options>) noexcept {
+  return SecretKey::random(getKeySize());
+}
+
+template <int CipherNid>
+WasiCryptoExpect<typename Cipher<CipherNid>::Key>
+Cipher<CipherNid>::Key::import(Span<const uint8_t> Raw) noexcept {
+  return std::make_shared<SecretKey>(Raw);
+}
+
+template <int CipherNid>
+WasiCryptoExpect<typename Cipher<CipherNid>::State>
+Cipher<CipherNid>::State::open(Key &Key,
+                               OptionalRef<Options> OptOption) noexcept {
+  ensureOrReturn(OptOption, __WASI_CRYPTO_ERRNO_NONCE_REQUIRED);
+
+  std::array<uint8_t, NonceSize> Nonce;
+  if (auto Res = OptOption->get("nonce"sv, Nonce); !Res) {
+    return WasiCryptoUnexpect(__WASI_CRYPTO_ERRNO_INVALID_NONCE);
+  } else {
+    ensureOrReturn(*Res == NonceSize, __WASI_CRYPTO_ERRNO_INVALID_NONCE);
+  }
+
+  ensureOrReturn(getKeySize() == Key.ref().size(),
+                 __WASI_CRYPTO_ERRNO_INVALID_HANDLE);
+
+  EvpCipherCtxPtr Ctx{EVP_CIPHER_CTX_new()};
+  opensslAssuming(EVP_CipherInit_ex(Ctx.get(), EVP_get_cipherbynid(CipherNid),
+                                    nullptr, Key.ref().data(), Nonce.data(),
+                                    Mode::Unchanged));
+
+  return State{std::move(Ctx), Nonce};
+}
+
+template <int CipherNid>
+WasiCryptoExpect<size_t>
+Cipher<CipherNid>::State::optionsGet(std::string_view Name,
+                                     Span<uint8_t> Value) const noexcept {
+  ensureOrReturn(Name == "nonce"sv, __WASI_CRYPTO_ERRNO_UNSUPPORTED_OPTION);
+  ensureOrReturn(NonceSize <= Value.size(), __WASI_CRYPTO_ERRNO_OVERFLOW);
+  /// Not work, alway return pointer to '0', so replace with a member
+  // const uint8_t *Nonce = EVP_CIPHER_CTX_original_iv(Ctx.get());
+  // std::copy(Nonce, Nonce + getNonceSize(), Value.begin());
+  std::copy(Ctx->Nonce.begin(), Ctx->Nonce.end(), Value.begin());
+  return NonceSize;
+}
+
+/// https://wiki.openssl.org/index.php/EVP_Authenticated_Encryption_and_Decryption
+template <int CipherNid>
+WasiCryptoExpect<void>
+Cipher<CipherNid>::State::absorb(Span<const uint8_t> Data) noexcept {
+  ensureOrReturn(Data.size() <= INT_MAX, __WASI_CRYPTO_ERRNO_ALGORITHM_FAILURE);
+
+  int ActualOutputSize;
+  int DataSize = static_cast<int>(Data.size());
+  opensslAssuming(EVP_CipherUpdate(Ctx->RawCtx.get(), nullptr,
+                                   &ActualOutputSize, Data.data(), DataSize));
+  ensureOrReturn(ActualOutputSize == DataSize,
+                 __WASI_CRYPTO_ERRNO_ALGORITHM_FAILURE);
+
+  return {};
+}
+
+template <int CipherNid>
+WasiCryptoExpect<size_t>
+Cipher<CipherNid>::State::encrypt(Span<uint8_t> Out,
+                                  Span<const uint8_t> Data) noexcept {
+  return encryptImpl(Out.first(Data.size()), Out.last(getTagSize()), Data);
+}
+
+template <int CipherNid>
+WasiCryptoExpect<Tag>
+Cipher<CipherNid>::State::encryptDetached(Span<uint8_t> Out,
+                                          Span<const uint8_t> Data) noexcept {
+  std::vector<uint8_t> Tag(getTagSize());
+  if (auto Res = encryptImpl(Out, Tag, Data); !Res) {
+    return WasiCryptoUnexpect(Res);
+  }
+  return Tag;
+}
+
+template <int CipherNid>
+WasiCryptoExpect<size_t>
+Cipher<CipherNid>::State::encryptImpl(Span<uint8_t> Out, Span<uint8_t> Tag,
+                                      Span<const uint8_t> Data) noexcept {
+  opensslAssuming(EVP_CipherInit_ex(Ctx->RawCtx.get(), nullptr, nullptr,
+                                    nullptr, nullptr, Mode::Encrypt));
+
+  ensureOrReturn(Data.size() <= INT_MAX, __WASI_CRYPTO_ERRNO_ALGORITHM_FAILURE);
+
+  int ActualOutSize;
+  opensslAssuming(EVP_CipherUpdate(Ctx->RawCtx.get(), Out.data(),
+                                   &ActualOutSize, Data.data(),
+                                   static_cast<int>(Data.size())));
+
+  ensureOrReturn(static_cast<size_t>(ActualOutSize) == Out.size(),
+                 __WASI_CRYPTO_ERRNO_ALGORITHM_FAILURE);
+
+  ensureOrReturn(EVP_CipherFinal_ex(Ctx->RawCtx.get(), nullptr, &ActualOutSize),
+                 __WASI_CRYPTO_ERRNO_INTERNAL_ERROR);
+  ensureOrReturn(ActualOutSize == 0, __WASI_CRYPTO_ERRNO_ALGORITHM_FAILURE);
+
+  opensslAssuming(EVP_CIPHER_CTX_ctrl(Ctx->RawCtx.get(), EVP_CTRL_AEAD_GET_TAG,
+                                      static_cast<int>(getTagSize()),
+                                      Tag.data()));
+
+  return Out.size() + getTagSize();
+}
+
+template <int CipherNid>
+WasiCryptoExpect<size_t>
+Cipher<CipherNid>::State::decrypt(Span<uint8_t> Out,
+                                  Span<const uint8_t> Data) noexcept {
+  return decryptImpl(Out, Data.first(Out.size()), Data.last(getTagSize()));
+}
+
+template <int CipherNid>
+WasiCryptoExpect<size_t>
+Cipher<CipherNid>::State::decryptDetached(Span<uint8_t> Out,
+                                          Span<const uint8_t> Data,
+                                          Span<const uint8_t> RawTag) noexcept {
+  return decryptImpl(Out, Data, RawTag);
+}
+
+template <int CipherNid>
+WasiCryptoExpect<size_t>
+Cipher<CipherNid>::State::decryptImpl(Span<uint8_t> Out,
+                                      Span<const uint8_t> Data,
+                                      Span<const uint8_t> RawTag) noexcept {
+  opensslAssuming(EVP_CipherInit_ex(Ctx->RawCtx.get(), nullptr, nullptr,
+                                    nullptr, nullptr, Mode::Decrypt));
+  int ActualOutSize;
+
+  ensureOrReturn(Data.size() <= INT_MAX, __WASI_CRYPTO_ERRNO_ALGORITHM_FAILURE);
+  int DataSize = static_cast<int>(Data.size());
+  opensslAssuming(EVP_CipherUpdate(Ctx->RawCtx.get(), Out.data(),
+                                   &ActualOutSize, Data.data(), DataSize));
+  ensureOrReturn(ActualOutSize == DataSize,
+                 __WASI_CRYPTO_ERRNO_ALGORITHM_FAILURE);
+
+  opensslAssuming(EVP_CIPHER_CTX_ctrl(Ctx->RawCtx.get(), EVP_CTRL_AEAD_SET_TAG,
+                                      static_cast<int>(getTagSize()),
+                                      const_cast<uint8_t *>(RawTag.data())));
+
+  if (!EVP_CipherFinal_ex(Ctx->RawCtx.get(), nullptr, &ActualOutSize)) {
+    std::fill(Out.begin(), Out.end(), 0);
+    return WasiCryptoUnexpect(__WASI_CRYPTO_ERRNO_INVALID_TAG);
+  }
+  ensureOrReturn(ActualOutSize == 0, __WASI_CRYPTO_ERRNO_ALGORITHM_FAILURE);
+
+  return Out.size();
+}
+
+template class Cipher<NID_aes_128_gcm>;
+template class Cipher<NID_aes_256_gcm>;
+template class Cipher<NID_chacha20_poly1305>;
+
+} // namespace Symmetric
+} // namespace WasiCrypto
+} // namespace Host
+} // namespace WasmEdge

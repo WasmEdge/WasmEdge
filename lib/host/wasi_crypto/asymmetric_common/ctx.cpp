37357ba06a1e9db84a47a74fb15b19b338a48127
sonder-joker <highmagic@outlook.com>
2022-03-10 17:17:33 +0800

    [WASI] Implement asymmetric common KeyPair
    Signed-off-by: sonder-joker <highmagic@outlook.com>

--- lib/host/wasi_crypto/asymmetric_common/ctx.cpp
+++ lib/host/wasi_crypto/asymmetric_common/ctx.cpp
@@ -55,6 +55,41 @@ Context::publickeyFromSecretkey(__wasi_secretkey_t SkHandle) noexcept {
       });
 }
 
+WasiCryptoExpect<__wasi_array_output_t>
+Context::keypairExport(__wasi_keypair_t KpHandle,
+                       __wasi_keypair_encoding_e_t KeypairEncoding) noexcept {
+  return KeyPairManager.get(KpHandle)
+      .and_then([=](auto &&Kp) noexcept {
+        return AsymmetricCommon::kpExportData(Kp, KeypairEncoding);
+      })
+      .and_then([this](auto &&Data) noexcept {
+        return ArrayOutputManger.registerManager(std::move(Data));
+      });
+}
+
+WasiCryptoExpect<__wasi_publickey_t>
+Context::keypairPublickey(__wasi_keypair_t KpHandle) noexcept {
+  return KeyPairManager.get(KpHandle)
+      .and_then(AsymmetricCommon::kpPublicKey)
+      .and_then([this](auto &&Pk) noexcept {
+        return PublicKeyManager.registerManager(std::move(Pk));
+      });
+}
+
+WasiCryptoExpect<__wasi_secretkey_t>
+Context::keypairSecretkey(__wasi_keypair_t KpHandle) noexcept {
+  return KeyPairManager.get(KpHandle)
+      .and_then(AsymmetricCommon::kpSecretKey)
+      .and_then([this](auto &&Pk) noexcept {
+        return SecretKeyManager.registerManager(std::move(Pk));
+      });
+}
+
+WasiCryptoExpect<void>
+Context::keypairClose(__wasi_keypair_t KpHandle) noexcept {
+  return KeyPairManager.close(KpHandle);
+}
+
 } // namespace WasiCrypto
 } // namespace Host
 } // namespace WasmEdge

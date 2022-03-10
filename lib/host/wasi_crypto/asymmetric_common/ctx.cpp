14482076eba1b5aa79bf8333197a2f691b303965
sonder-joker <highmagic@outlook.com>
2022-03-10 18:07:14 +0800

    [WASI] Implement asymmetric common PublicKey
    Signed-off-by: sonder-joker <highmagic@outlook.com>

--- lib/host/wasi_crypto/asymmetric_common/ctx.cpp
+++ lib/host/wasi_crypto/asymmetric_common/ctx.cpp
@@ -6,6 +6,29 @@
 namespace WasmEdge {
 namespace Host {
 namespace WasiCrypto {
+
+WasiCryptoExpect<__wasi_array_output_t>
+Context::publickeyExport(__wasi_publickey_t PkHandle,
+                         __wasi_publickey_encoding_e_t PkEncoding) noexcept {
+  return PublicKeyManager.get(PkHandle)
+      .and_then([=](auto &&Pk) {
+        return AsymmetricCommon::pkExportData(Pk, PkEncoding);
+      })
+      .and_then([this](auto &&Data) {
+        return ArrayOutputManger.registerManager(std::move(Data));
+      });
+}
+
+WasiCryptoExpect<void>
+Context::publickeyVerify(__wasi_publickey_t PkHandle) noexcept {
+  return PublicKeyManager.get(PkHandle).and_then(AsymmetricCommon::pkVerify);
+}
+
+WasiCryptoExpect<void>
+Context::publickeyClose(__wasi_publickey_t PkHandle) noexcept {
+  return PublicKeyManager.close(PkHandle);
+}
+
 } // namespace WasiCrypto
 } // namespace Host
 } // namespace WasmEdge
---
+++ lib/host/wasi_crypto/asymmetric_common/publickey.cpp
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2019-2022 Second State INC
+
+#include "host/wasi_crypto/asymmetric_common/publickey.h"
+
+namespace WasmEdge {
+namespace Host {
+namespace WasiCrypto {
+namespace AsymmetricCommon {
+
+WasiCryptoExpect<std::vector<uint8_t>>
+pkExportData(const PkVariant &PkVariant,
+             __wasi_publickey_encoding_e_t Encoding) noexcept {
+  return std::visit([=](auto &&Pk) noexcept { return Pk.exportData(Encoding); },
+                    PkVariant);
+}
+
+WasiCryptoExpect<void> pkVerify(const PkVariant &PkVariant) noexcept {
+  return std::visit([=](auto &&Pk) noexcept { return Pk.verify(); }, PkVariant);
+}
+
+} // namespace AsymmetricCommon
+} // namespace WasiCrypto
+} // namespace Host
+} // namespace WasmEdge

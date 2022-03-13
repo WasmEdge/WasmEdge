d787a6790ba0de371308d8d2e212cfe40f5300c2
sonder-joker <highmagic@outlook.com>
2022-03-13 21:01:26 +0800

    [WASI] Implement signature Sig
    Signed-off-by: sonder-joker <highmagic@outlook.com>

---
+++ include/host/wasi_crypto/signatures/signatures.h
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2019-2022 Second State INC
+
+//===-- wasi_crypto/signatures/registed.h - Signatures implement ----------===//
+//
+// Part of the WasmEdge Project.
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file contains implementation of signatures.
+///
+//===----------------------------------------------------------------------===//
+#pragma once
+
+#include "host/wasi_crypto/signatures/registed.h"
+#include "host/wasi_crypto/utils/error.h"
+
+#include <variant>
+
+namespace WasmEdge {
+namespace Host {
+namespace WasiCrypto {
+namespace Signatures {
+
+using SigVariant = RegistedAlg::SigVariant;
+
+WasiCryptoExpect<std::vector<uint8_t>>
+sigExportData(const SigVariant &SigVariant,
+              __wasi_signature_encoding_e_t Encoding) noexcept;
+
+} // namespace Signatures
+} // namespace WasiCrypto
+} // namespace Host
+} // namespace WasmEdge
---
+++ lib/host/wasi_crypto/signatures/signatures.cpp
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2019-2022 Second State INC
+
+#include "host/wasi_crypto/signatures/signatures.h"
+
+namespace WasmEdge {
+namespace Host {
+namespace WasiCrypto {
+namespace Signatures {
+
+WasiCryptoExpect<std::vector<uint8_t>>
+sigExportData(const SigVariant &SigVariant,
+              __wasi_signature_encoding_e_t Encoding) noexcept {
+  return std::visit(
+      [=](auto &&Sig) noexcept { return Sig.exportData(Encoding); },
+      SigVariant);
+}
+
+} // namespace Signatures
+} // namespace WasiCrypto
+} // namespace Host
+} // namespace WasmEdge

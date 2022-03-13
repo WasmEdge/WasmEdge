a0bc2c6893daa641c397ca17218e128563d39fee
sonder-joker <highmagic@outlook.com>
2022-03-13 20:49:19 +0800

    [WASI] Add signatures Context
    Signed-off-by: sonder-joker <highmagic@outlook.com>

---
+++ lib/host/wasi_crypto/signatures/ctx.cpp
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: Apache-2.0
+
+#include "host/wasi_crypto/ctx.h"
+#include "host/wasi_crypto/signatures/signatures.h"
+
+namespace WasmEdge {
+namespace Host {
+namespace WasiCrypto {
+
+} // namespace WasiCrypto
+} // namespace Host
+} // namespace WasmEdge

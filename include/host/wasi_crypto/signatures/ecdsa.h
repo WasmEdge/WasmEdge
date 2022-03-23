92e0276c2a9ac143d418eab15c79fbdc5491c48e
sonder-joker <highmagic@outlook.com>
2022-03-23 20:33:39 +0800

    [WASi] Implement signatures Ecdsa
    Signed-off-by: sonder-joker <highmagic@outlook.com>

---
+++ include/host/wasi_crypto/signatures/ecdsa.h
@@ -0,0 +1,200 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2019-2022 Second State INC
+
+//===-- wasi_crypto/signatures/ecdsa.h - Ecdsa algorithm implement --------===//
+//
+// Part of the WasmEdge Project.
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file contains the definition of ecdsa relative algorithm
+///
+//===----------------------------------------------------------------------===//
+#pragma once
+
+#include "common/span.h"
+#include "host/wasi_crypto/signatures/alg.h"
+#include "host/wasi_crypto/signatures/options.h"
+#include "host/wasi_crypto/utils/error.h"
+#include "host/wasi_crypto/utils/evp_wrapper.h"
+#include "host/wasi_crypto/utils/optional.h"
+
+namespace WasmEdge {
+namespace Host {
+namespace WasiCrypto {
+namespace Signatures {
+
+template <int CurveNid> class Ecdsa {
+public:
+  class Signature {
+  public:
+    Signature(std::vector<uint8_t> &&Data) noexcept : Data(std::move(Data)) {}
+
+    static WasiCryptoExpect<Signature>
+    import(Span<const uint8_t> Encoded,
+           __wasi_signature_encoding_e_t Encoding) noexcept;
+
+    WasiCryptoExpect<std::vector<uint8_t>>
+    exportData(__wasi_signature_encoding_e_t Encoding) const noexcept;
+
+    auto &ref() { return Data; }
+
+  private:
+    const std::vector<uint8_t> Data;
+  };
+
+  class SignState {
+  public:
+    SignState(EvpMdCtxPtr Ctx) noexcept : Ctx(std::move(Ctx)) {}
+
+    WasiCryptoExpect<void> update(Span<const uint8_t> Input) noexcept;
+
+    WasiCryptoExpect<Signature> sign() noexcept;
+
+  private:
+    std::shared_ptr<EVP_MD_CTX> Ctx;
+  };
+
+  class VerificationState {
+  public:
+    VerificationState(EvpMdCtxPtr Ctx) noexcept : Ctx(std::move(Ctx)) {}
+
+    WasiCryptoExpect<void> update(Span<const uint8_t> Input) noexcept;
+
+    WasiCryptoExpect<void> verify(Signature &Sig) noexcept;
+
+  private:
+    std::shared_ptr<EVP_MD_CTX> Ctx;
+  };
+
+  class PublicKey {
+  public:
+    PublicKey(EvpPkeyPtr Ctx) noexcept : Ctx(std::move(Ctx)) {}
+
+    static WasiCryptoExpect<PublicKey>
+    import(Span<const uint8_t> Encoded,
+           __wasi_publickey_encoding_e_t Encoding) noexcept;
+
+    WasiCryptoExpect<std::vector<uint8_t>>
+    exportData(__wasi_publickey_encoding_e_t Encoding) const noexcept;
+
+    WasiCryptoExpect<void> verify() const noexcept;
+
+    WasiCryptoExpect<VerificationState> openVerificationState() noexcept;
+
+  private:
+    static WasiCryptoExpect<PublicKey> importPkcs8(Span<const uint8_t> EncodeSd,
+                                                   bool Compressed) noexcept;
+
+    static WasiCryptoExpect<PublicKey> importPem(Span<const uint8_t> Encoded,
+                                                 bool Compressed) noexcept;
+
+    static WasiCryptoExpect<PublicKey> importSec(Span<const uint8_t> Encoded,
+                                                 bool Compressed) noexcept;
+
+    static WasiCryptoExpect<EvpPkeyPtr> checkValid(EvpPkeyPtr Ctx,
+                                                   bool Compressed) noexcept;
+
+    WasiCryptoExpect<std::vector<uint8_t>>
+    exportSec(bool Compressed) const noexcept;
+
+    WasiCryptoExpect<std::vector<uint8_t>>
+    exportPem(bool Compressed) const noexcept;
+
+    WasiCryptoExpect<std::vector<uint8_t>>
+    exportPkcs8(bool Compressed) const noexcept;
+
+    std::shared_ptr<EVP_PKEY> Ctx;
+  };
+
+  class KeyPair;
+
+  class SecretKey {
+  public:
+    SecretKey(EvpPkeyPtr Ctx) noexcept : Ctx(std::move(Ctx)) {}
+
+    static WasiCryptoExpect<SecretKey>
+    import(Span<const uint8_t> Encoded,
+           __wasi_secretkey_encoding_e_t Encoding) noexcept;
+
+    WasiCryptoExpect<std::vector<uint8_t>>
+    exportData(__wasi_secretkey_encoding_e_t Encoding) const noexcept;
+
+    WasiCryptoExpect<PublicKey> publicKey() const noexcept;
+
+    WasiCryptoExpect<KeyPair> toKeyPair(PublicKey &Pk) noexcept;
+
+  private:
+    static WasiCryptoExpect<SecretKey>
+    importPkcs8(Span<const uint8_t> Encoded) noexcept;
+
+    static WasiCryptoExpect<SecretKey>
+    importPem(Span<const uint8_t> Encoded) noexcept;
+
+    static WasiCryptoExpect<SecretKey>
+    importRaw(Span<const uint8_t> Encoded) noexcept;
+
+    static WasiCryptoExpect<EvpPkeyPtr> checkValid(EvpPkeyPtr Ctx) noexcept;
+
+    WasiCryptoExpect<std::vector<uint8_t>> exportPkcs8() const noexcept;
+
+    WasiCryptoExpect<std::vector<uint8_t>> exportPem() const noexcept;
+
+    WasiCryptoExpect<std::vector<uint8_t>> exportRaw() const noexcept;
+
+    std::shared_ptr<EVP_PKEY> Ctx;
+  };
+
+  class KeyPair {
+  public:
+    KeyPair(EvpPkeyPtr Ctx) noexcept : Ctx(std::move(Ctx)) {}
+
+    static WasiCryptoExpect<KeyPair>
+    generate(OptionalRef<Options> Options) noexcept;
+
+    static WasiCryptoExpect<KeyPair>
+    import(Span<const uint8_t> Encoded,
+           __wasi_keypair_encoding_e_t Encoding) noexcept;
+
+    static WasiCryptoExpect<EvpPkeyPtr> checkValid(EvpPkeyPtr Ctx,
+                                                   bool Compressed) noexcept;
+
+    WasiCryptoExpect<std::vector<uint8_t>>
+    exportData(__wasi_keypair_encoding_e_t Encoding) const noexcept;
+
+    WasiCryptoExpect<PublicKey> publicKey() const noexcept;
+
+    WasiCryptoExpect<SecretKey> secretKey() const noexcept;
+
+    WasiCryptoExpect<SignState> openSignState() noexcept;
+
+  private:
+    static WasiCryptoExpect<KeyPair> importPkcs8(Span<const uint8_t> Encoded,
+                                                 bool Compressed) noexcept;
+
+    static WasiCryptoExpect<KeyPair> importPem(Span<const uint8_t> Encoded,
+                                               bool Compressed) noexcept;
+
+    static WasiCryptoExpect<KeyPair>
+    importRaw(Span<const uint8_t> Encoded) noexcept;
+
+    static WasiCryptoExpect<EvpPkeyPtr> checkValid(EvpPkeyPtr Ctx) noexcept;
+
+    WasiCryptoExpect<std::vector<uint8_t>> exportPkcs8() const noexcept;
+
+    WasiCryptoExpect<std::vector<uint8_t>> exportPem() const noexcept;
+
+    WasiCryptoExpect<std::vector<uint8_t>> exportRaw() const noexcept;
+
+    std::shared_ptr<EVP_PKEY> Ctx;
+  };
+};
+
+using EcdsaP256 = Ecdsa<NID_X9_62_prime256v1>;
+using EcdsaK256 = Ecdsa<NID_secp256k1>;
+
+} // namespace Signatures
+} // namespace WasiCrypto
+} // namespace Host
+} // namespace WasmEdge
---
+++ lib/host/wasi_crypto/signatures/ecdsa.cpp
@@ -0,0 +1,513 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2019-2022 Second State INC
+
+#include "host/wasi_crypto/signatures/ecdsa.h"
+#include "host/wasi_crypto/utils/error.h"
+#include "host/wasi_crypto/utils/evp_wrapper.h"
+#include "host/wasi_crypto/utils/optional.h"
+#include "openssl/pem.h"
+#include "wasi_crypto/api.hpp"
+#include <openssl/ec.h>
+
+namespace WasmEdge {
+namespace Host {
+namespace WasiCrypto {
+namespace Signatures {
+
+namespace {
+inline const size_t UnCompressedPkSize = 65;
+inline const size_t CompressedPkSize = 33;
+constexpr size_t getRawPkSize(bool Compressed) {
+  return Compressed ? CompressedPkSize : UnCompressedPkSize;
+}
+
+inline const size_t SkSize = 32;
+
+constexpr point_conversion_form_t getForm(bool Compressed) noexcept {
+  return Compressed ? POINT_CONVERSION_COMPRESSED
+                    : POINT_CONVERSION_UNCOMPRESSED;
+}
+
+} // namespace
+// raw secret scalar encoded as big endian, SEC-1, compressed SEC-1, unencrypted
+// PKCS#8, PEM-encoded unencrypted PKCS#8
+
+template <int CurveNid>
+WasiCryptoExpect<typename Ecdsa<CurveNid>::PublicKey>
+Ecdsa<CurveNid>::PublicKey::import(
+    Span<const uint8_t> Encoded,
+    __wasi_publickey_encoding_e_t Encoding) noexcept {
+  switch (Encoding) {
+  case __WASI_PUBLICKEY_ENCODING_PKCS8:
+    return importPkcs8(Encoded, false);
+  case __WASI_PUBLICKEY_ENCODING_COMPRESSED_PKCS8:
+    return importPkcs8(Encoded, true);
+  case __WASI_PUBLICKEY_ENCODING_PEM:
+    return importPem(Encoded, false);
+  case __WASI_PUBLICKEY_ENCODING_COMPRESSED_PEM:
+    return importPem(Encoded, true);
+  case __WASI_PUBLICKEY_ENCODING_SEC:
+    return importSec(Encoded, false);
+  case __WASI_PUBLICKEY_ENCODING_COMPRESSED_SEC:
+    return importSec(Encoded, true);
+  default:
+    return WasiCryptoUnexpect(__WASI_CRYPTO_ERRNO_UNSUPPORTED_ENCODING);
+  }
+}
+
+template <int CurveNid>
+WasiCryptoExpect<std::vector<uint8_t>> Ecdsa<CurveNid>::PublicKey::exportData(
+    __wasi_publickey_encoding_e_t Encoding) const noexcept {
+  switch (Encoding) {
+  case __WASI_PUBLICKEY_ENCODING_SEC:
+    return exportSec(false);
+  case __WASI_PUBLICKEY_ENCODING_COMPRESSED_SEC:
+    return exportSec(true);
+  case __WASI_PUBLICKEY_ENCODING_PEM:
+    return exportPem(false);
+  case __WASI_PUBLICKEY_ENCODING_COMPRESSED_PEM:
+    return exportPem(true);
+  case __WASI_PUBLICKEY_ENCODING_PKCS8:
+    return exportPkcs8(false);
+  case __WASI_PUBLICKEY_ENCODING_COMPRESSED_PKCS8:
+    return exportPkcs8(true);
+  default:
+    return WasiCryptoUnexpect(__WASI_CRYPTO_ERRNO_UNSUPPORTED_ENCODING);
+  }
+}
+
+template <int CurveNid>
+WasiCryptoExpect<void> Ecdsa<CurveNid>::PublicKey::verify() const noexcept {
+  return WasiCryptoUnexpect(__WASI_CRYPTO_ERRNO_NOT_IMPLEMENTED);
+}
+
+template <int CurveNid>
+WasiCryptoExpect<typename Ecdsa<CurveNid>::VerificationState>
+Ecdsa<CurveNid>::PublicKey::openVerificationState() noexcept {
+  EvpMdCtxPtr SignCtx{EVP_MD_CTX_create()};
+  opensslAssuming(EVP_DigestVerifyInit(SignCtx.get(), nullptr, EVP_sha256(),
+                                       nullptr, Ctx.get()));
+  return SignCtx;
+}
+
+template <int CurveNid>
+WasiCryptoExpect<EvpPkeyPtr>
+Ecdsa<CurveNid>::PublicKey::checkValid(EvpPkeyPtr Ctx, bool) noexcept {
+  ensureOrReturn(Ctx, __WASI_CRYPTO_ERRNO_INVALID_KEY);
+  EC_KEY *EcCtx = EVP_PKEY_get0_EC_KEY(Ctx.get());
+  ensureOrReturn(EcCtx, __WASI_CRYPTO_ERRNO_INVALID_KEY);
+
+  const EC_GROUP *Group = EC_KEY_get0_group(EcCtx);
+  ensureOrReturn(Group, __WASI_CRYPTO_ERRNO_INVALID_KEY);
+  ensureOrReturn(EC_GROUP_get_curve_name(Group) == CurveNid,
+                 __WASI_CRYPTO_ERRNO_INVALID_KEY);
+  // don't check: only get POINT_CONVERSION_UNCOMPRESSED even compressed
+  // ensureOrReturn(getForm(Compressed) ==
+  //                    EC_GROUP_get_point_conversion_form(Group),
+  //                __WASI_CRYPTO_ERRNO_INVALID_KEY);
+  return {std::move(Ctx)};
+}
+
+template <int CurveNid>
+WasiCryptoExpect<typename Ecdsa<CurveNid>::PublicKey>
+Ecdsa<CurveNid>::PublicKey::importPkcs8(Span<const uint8_t> Encoded,
+                                        bool Compressed) noexcept {
+  return checkValid(EvpPkeyPtr{d2iPUBKEY(Encoded)}, Compressed);
+}
+
+template <int CurveNid>
+WasiCryptoExpect<typename Ecdsa<CurveNid>::PublicKey>
+Ecdsa<CurveNid>::PublicKey::importPem(Span<const uint8_t> Encoded,
+                                      bool Compressed) noexcept {
+  return checkValid(EvpPkeyPtr{pemReadPUBKEY(Encoded)}, Compressed);
+}
+
+template <int CurveNid>
+WasiCryptoExpect<typename Ecdsa<CurveNid>::PublicKey>
+Ecdsa<CurveNid>::PublicKey::importSec(Span<const uint8_t> Encoded,
+                                      bool Compressed) noexcept {
+  EcKeyPtr EcCtx{EC_KEY_new_by_curve_name(CurveNid)};
+  EcPointPtr Pk{EC_POINT_new(EC_KEY_get0_group(EcCtx.get()))};
+  ensureOrReturn(EC_POINT_oct2point(EC_KEY_get0_group(EcCtx.get()), Pk.get(),
+                                    Encoded.data(), Encoded.size(), nullptr),
+                 __WASI_CRYPTO_ERRNO_INVALID_KEY);
+  opensslAssuming(EC_KEY_set_public_key(EcCtx.get(), Pk.get()));
+
+  EvpPkeyPtr Ctx{EVP_PKEY_new()};
+  opensslAssuming(EVP_PKEY_set1_EC_KEY(Ctx.get(), EcCtx.get()));
+
+  return checkValid(std::move(Ctx), Compressed);
+}
+
+template <int CurveNid>
+WasiCryptoExpect<std::vector<uint8_t>>
+Ecdsa<CurveNid>::PublicKey::exportSec(bool Compressed) const noexcept {
+  EC_KEY *EcCtx = EVP_PKEY_get0_EC_KEY(Ctx.get());
+  std::vector<uint8_t> Res(getRawPkSize(Compressed));
+  opensslAssuming(EC_POINT_point2oct(
+      EC_KEY_get0_group(EcCtx), EC_KEY_get0_public_key(EcCtx),
+      getForm(Compressed), Res.data(), Res.size(), nullptr));
+  return Res;
+}
+
+template <int CurveNid>
+WasiCryptoExpect<std::vector<uint8_t>>
+Ecdsa<CurveNid>::PublicKey::exportPem(bool Compressed) const noexcept {
+  EC_KEY_set_conv_form(EVP_PKEY_get0_EC_KEY(Ctx.get()), getForm(Compressed));
+
+  return pemWritePUBKEY(Ctx.get());
+}
+
+template <int CurveNid>
+WasiCryptoExpect<std::vector<uint8_t>>
+Ecdsa<CurveNid>::PublicKey::exportPkcs8(bool Compressed) const noexcept {
+  EC_KEY_set_conv_form(EVP_PKEY_get0_EC_KEY(Ctx.get()), getForm(Compressed));
+
+  return i2dPUBKEY(Ctx.get());
+}
+
+template <int CurveNid>
+WasiCryptoExpect<typename Ecdsa<CurveNid>::SecretKey>
+Ecdsa<CurveNid>::SecretKey::import(
+    Span<const uint8_t> Encoded,
+    __wasi_secretkey_encoding_e_t Encoding) noexcept {
+  switch (Encoding) {
+  case __WASI_SECRETKEY_ENCODING_RAW:
+    return importRaw(Encoded);
+  case __WASI_SECRETKEY_ENCODING_PKCS8:
+    return importPkcs8(Encoded);
+  case __WASI_SECRETKEY_ENCODING_PEM:
+    return importPem(Encoded);
+  default:
+    return WasiCryptoUnexpect(__WASI_CRYPTO_ERRNO_UNSUPPORTED_ENCODING);
+  }
+}
+
+template <int CurveNid>
+WasiCryptoExpect<EvpPkeyPtr>
+Ecdsa<CurveNid>::SecretKey::checkValid(EvpPkeyPtr Ctx) noexcept {
+  ensureOrReturn(Ctx, __WASI_CRYPTO_ERRNO_INVALID_KEY);
+  EC_KEY *EcCtx = EVP_PKEY_get0_EC_KEY(Ctx.get());
+  ensureOrReturn(EcCtx, __WASI_CRYPTO_ERRNO_INVALID_KEY);
+
+  const EC_GROUP *Group = EC_KEY_get0_group(EcCtx);
+  ensureOrReturn(Group, __WASI_CRYPTO_ERRNO_INVALID_KEY);
+  ensureOrReturn(EC_GROUP_get_curve_name(Group) == CurveNid,
+                 __WASI_CRYPTO_ERRNO_INVALID_KEY);
+  return {std::move(Ctx)};
+}
+
+template <int CurveNid>
+WasiCryptoExpect<typename Ecdsa<CurveNid>::SecretKey>
+Ecdsa<CurveNid>::SecretKey::importPkcs8(Span<const uint8_t> Encoded) noexcept {
+  return checkValid(EvpPkeyPtr{d2iPrivateKey(Encoded)});
+}
+
+template <int CurveNid>
+WasiCryptoExpect<typename Ecdsa<CurveNid>::SecretKey>
+Ecdsa<CurveNid>::SecretKey::importPem(Span<const uint8_t> Encoded) noexcept {
+  return checkValid(EvpPkeyPtr{pemReadPrivateKey(Encoded)});
+}
+
+template <int CurveNid>
+WasiCryptoExpect<typename Ecdsa<CurveNid>::SecretKey>
+Ecdsa<CurveNid>::SecretKey::importRaw(Span<const uint8_t> Encoded) noexcept {
+  EcKeyPtr EcCtx{EC_KEY_new_by_curve_name(CurveNid)};
+  ensureOrReturn(Encoded.size() == SkSize, __WASI_CRYPTO_ERRNO_INVALID_KEY);
+
+  BnPtr Sk{
+      BN_bin2bn(Encoded.data(), static_cast<int>(Encoded.size()), nullptr)};
+  ensureOrReturn(EC_KEY_set_private_key(EcCtx.get(), Sk.get()),
+                 __WASI_CRYPTO_ERRNO_INVALID_KEY);
+
+  EvpPkeyPtr Ctx{EVP_PKEY_new()};
+  opensslAssuming(EVP_PKEY_set1_EC_KEY(Ctx.get(), EcCtx.get()));
+
+  return Ctx;
+}
+
+template <int CurveNid>
+WasiCryptoExpect<typename Ecdsa<CurveNid>::KeyPair>
+Ecdsa<CurveNid>::SecretKey::toKeyPair(PublicKey &) noexcept {
+  return WasiCryptoUnexpect(__WASI_CRYPTO_ERRNO_NOT_IMPLEMENTED);
+}
+
+template <int CurveNid>
+WasiCryptoExpect<std::vector<uint8_t>> Ecdsa<CurveNid>::SecretKey::exportData(
+    __wasi_secretkey_encoding_e_t Encoding) const noexcept {
+  switch (Encoding) {
+  case __WASI_SECRETKEY_ENCODING_RAW:
+    return exportRaw();
+  case __WASI_SECRETKEY_ENCODING_PKCS8:
+    return exportPkcs8();
+  case __WASI_SECRETKEY_ENCODING_PEM:
+    return exportPem();
+  default:
+    return WasiCryptoUnexpect(__WASI_CRYPTO_ERRNO_UNSUPPORTED_ENCODING);
+  }
+}
+
+template <int CurveNid>
+WasiCryptoExpect<std::vector<uint8_t>>
+Ecdsa<CurveNid>::SecretKey::exportPkcs8() const noexcept {
+  return i2dPrivateKey(Ctx.get());
+}
+
+template <int CurveNid>
+WasiCryptoExpect<std::vector<uint8_t>>
+Ecdsa<CurveNid>::SecretKey::exportPem() const noexcept {
+  return pemWritePrivateKey(Ctx.get());
+}
+
+template <int CurveNid>
+WasiCryptoExpect<std::vector<uint8_t>>
+Ecdsa<CurveNid>::SecretKey::exportRaw() const noexcept {
+  /// must equal to SkSize, not check.
+  const BIGNUM *Sk = EC_KEY_get0_private_key(EVP_PKEY_get0_EC_KEY(Ctx.get()));
+  std::vector<uint8_t> Res(SkSize);
+  opensslAssuming(BN_bn2bin(Sk, Res.data()));
+
+  return Res;
+}
+
+template <int CurveNid>
+WasiCryptoExpect<typename Ecdsa<CurveNid>::PublicKey>
+Ecdsa<CurveNid>::SecretKey::publicKey() const noexcept {
+  BioPtr B{BIO_new(BIO_s_mem())};
+  opensslAssuming(i2d_PUBKEY_bio(B.get(), Ctx.get()));
+
+  EVP_PKEY *Res = nullptr;
+  opensslAssuming(d2i_PUBKEY_bio(B.get(), &Res));
+
+  return EvpPkeyPtr{Res};
+}
+
+template <int CurveNid>
+WasiCryptoExpect<typename Ecdsa<CurveNid>::KeyPair>
+Ecdsa<CurveNid>::KeyPair::generate(OptionalRef<Options>) noexcept {
+  EvpPkeyCtxPtr ParamCtx{EVP_PKEY_CTX_new_id(EVP_PKEY_EC, nullptr)};
+  EVP_PKEY_keygen_init(ParamCtx.get());
+  EVP_PKEY_CTX_set_ec_paramgen_curve_nid(ParamCtx.get(), CurveNid);
+
+  EVP_PKEY *Key = nullptr;
+  opensslAssuming(EVP_PKEY_keygen(ParamCtx.get(), &Key));
+
+  return EvpPkeyPtr{Key};
+}
+
+template <int CurveNid>
+WasiCryptoExpect<EvpPkeyPtr>
+Ecdsa<CurveNid>::KeyPair::checkValid(EvpPkeyPtr Ctx, bool) noexcept {
+  ensureOrReturn(Ctx, __WASI_CRYPTO_ERRNO_INVALID_KEY);
+  EC_KEY *EcCtx = EVP_PKEY_get0_EC_KEY(Ctx.get());
+  ensureOrReturn(EcCtx, __WASI_CRYPTO_ERRNO_INVALID_KEY);
+
+  /// curve id check
+  const EC_GROUP *Group = EC_KEY_get0_group(EcCtx);
+  ensureOrReturn(Group, __WASI_CRYPTO_ERRNO_INVALID_KEY);
+  ensureOrReturn(EC_GROUP_get_curve_name(Group) == CurveNid,
+                 __WASI_CRYPTO_ERRNO_INVALID_KEY);
+  /// have public key
+  ensureOrReturn(EC_KEY_get0_public_key(EcCtx),
+                 __WASI_CRYPTO_ERRNO_INVALID_KEY);
+  return {std::move(Ctx)};
+}
+
+template <int CurveNid>
+WasiCryptoExpect<typename Ecdsa<CurveNid>::KeyPair>
+Ecdsa<CurveNid>::KeyPair::import(
+    Span<const uint8_t> Encoded,
+    __wasi_keypair_encoding_e_t Encoding) noexcept {
+  switch (Encoding) {
+  case __WASI_KEYPAIR_ENCODING_RAW:
+    return importRaw(Encoded);
+  case __WASI_KEYPAIR_ENCODING_PKCS8:
+    return importPkcs8(Encoded, false);
+  case __WASI_KEYPAIR_ENCODING_PEM:
+    return importPem(Encoded, false);
+  case __WASI_KEYPAIR_ENCODING_COMPRESSED_PKCS8:
+    return importPkcs8(Encoded, true);
+  case __WASI_KEYPAIR_ENCODING_COMPRESSED_PEM:
+    return importPem(Encoded, true);
+  default:
+    return WasiCryptoUnexpect(__WASI_CRYPTO_ERRNO_UNSUPPORTED_ENCODING);
+  }
+}
+
+template <int CurveNid>
+WasiCryptoExpect<std::vector<uint8_t>> Ecdsa<CurveNid>::KeyPair::exportData(
+    __wasi_keypair_encoding_e_t Encoding) const noexcept {
+  switch (Encoding) {
+  case __WASI_KEYPAIR_ENCODING_RAW:
+    return exportRaw();
+  case __WASI_KEYPAIR_ENCODING_PKCS8:
+    return exportPkcs8();
+  case __WASI_KEYPAIR_ENCODING_PEM:
+    return exportPem();
+  default:
+    return WasiCryptoUnexpect(__WASI_CRYPTO_ERRNO_UNSUPPORTED_ENCODING);
+  }
+}
+
+template <int CurveNid>
+WasiCryptoExpect<typename Ecdsa<CurveNid>::PublicKey>
+Ecdsa<CurveNid>::KeyPair::publicKey() const noexcept {
+  BioPtr B{BIO_new(BIO_s_mem())};
+  opensslAssuming(i2d_PUBKEY_bio(B.get(), Ctx.get()));
+
+  EVP_PKEY *Res = nullptr;
+  opensslAssuming(d2i_PUBKEY_bio(B.get(), &Res));
+
+  return EvpPkeyPtr{Res};
+}
+
+template <int CurveNid>
+WasiCryptoExpect<typename Ecdsa<CurveNid>::SecretKey>
+Ecdsa<CurveNid>::KeyPair::secretKey() const noexcept {
+  BioPtr B{BIO_new(BIO_s_mem())};
+  opensslAssuming(i2d_PrivateKey_bio(B.get(), Ctx.get()));
+
+  EVP_PKEY *Res = nullptr;
+  opensslAssuming(d2i_PrivateKey_bio(B.get(), &Res));
+
+  return EvpPkeyPtr{Res};
+}
+
+template <int CurveNid>
+WasiCryptoExpect<typename Ecdsa<CurveNid>::SignState>
+Ecdsa<CurveNid>::KeyPair::openSignState() noexcept {
+  EvpMdCtxPtr SignCtx{EVP_MD_CTX_create()};
+  opensslAssuming(EVP_DigestSignInit(SignCtx.get(), nullptr, EVP_sha256(),
+                                     nullptr, Ctx.get()));
+
+  return SignCtx;
+}
+
+template <int CurveNid>
+WasiCryptoExpect<typename Ecdsa<CurveNid>::KeyPair>
+Ecdsa<CurveNid>::KeyPair::importPkcs8(Span<const uint8_t> Encoded,
+                                      bool Compressed) noexcept {
+  return checkValid(EvpPkeyPtr{d2iPrivateKey(Encoded)}, Compressed);
+}
+
+template <int CurveNid>
+WasiCryptoExpect<typename Ecdsa<CurveNid>::KeyPair>
+Ecdsa<CurveNid>::KeyPair::importPem(Span<const uint8_t> Encoded,
+                                    bool Compressed) noexcept {
+  return checkValid(EvpPkeyPtr{pemReadPrivateKey(Encoded)}, Compressed);
+}
+
+template <int CurveNid>
+WasiCryptoExpect<typename Ecdsa<CurveNid>::KeyPair>
+Ecdsa<CurveNid>::KeyPair::importRaw(Span<const uint8_t> Encoded) noexcept {
+  ensureOrReturn(Encoded.size() == SkSize, __WASI_CRYPTO_ERRNO_INVALID_KEY);
+  EcKeyPtr EcCtx{EC_KEY_new_by_curve_name(CurveNid)};
+  BnPtr Sk{
+      BN_bin2bn(Encoded.data(), static_cast<int>(Encoded.size()), nullptr)};
+  ensureOrReturn(EC_KEY_set_private_key(EcCtx.get(), Sk.get()),
+                 __WASI_CRYPTO_ERRNO_INVALID_KEY);
+
+  /// calculate Pk and set
+  EcPointPtr Pk{EC_POINT_new(EC_KEY_get0_group(EcCtx.get()))};
+  opensslAssuming(EC_POINT_mul(EC_KEY_get0_group(EcCtx.get()), Pk.get(),
+                               Sk.get(), nullptr, nullptr, nullptr));
+  opensslAssuming(EC_KEY_set_public_key(EcCtx.get(), Pk.get()));
+
+  EvpPkeyPtr Ctx{EVP_PKEY_new()};
+  opensslAssuming(EVP_PKEY_set1_EC_KEY(Ctx.get(), EcCtx.get()));
+  ensureOrReturn(Ctx, __WASI_CRYPTO_ERRNO_INVALID_KEY);
+
+  return Ctx;
+}
+
+template <int CurveNid>
+WasiCryptoExpect<std::vector<uint8_t>>
+Ecdsa<CurveNid>::KeyPair::exportPkcs8() const noexcept {
+  return i2dPrivateKey(Ctx.get());
+}
+
+template <int CurveNid>
+WasiCryptoExpect<std::vector<uint8_t>>
+Ecdsa<CurveNid>::KeyPair::exportPem() const noexcept {
+  return pemWritePrivateKey(Ctx.get());
+}
+
+template <int CurveNid>
+WasiCryptoExpect<std::vector<uint8_t>>
+Ecdsa<CurveNid>::KeyPair::exportRaw() const noexcept {
+  const BIGNUM *Sk = EC_KEY_get0_private_key(EVP_PKEY_get0_EC_KEY(Ctx.get()));
+  std::vector<uint8_t> Res(SkSize);
+  opensslAssuming(BN_bn2bin(Sk, Res.data()));
+
+  return Res;
+}
+
+template <int CurveNid>
+WasiCryptoExpect<typename Ecdsa<CurveNid>::Signature>
+Ecdsa<CurveNid>::Signature::import(
+    Span<const uint8_t> Encoded,
+    __wasi_signature_encoding_e_t Encoding) noexcept {
+  switch (Encoding) {
+  case __WASI_SIGNATURE_ENCODING_RAW:
+    return std::vector<uint8_t>{Encoded.begin(), Encoded.end()};
+  case __WASI_SIGNATURE_ENCODING_DER:
+    return WasiCryptoUnexpect(__WASI_CRYPTO_ERRNO_NOT_IMPLEMENTED);
+  default:
+    assumingUnreachable();
+  }
+}
+
+template <int CurveNid>
+WasiCryptoExpect<std::vector<uint8_t>> Ecdsa<CurveNid>::Signature::exportData(
+    __wasi_signature_encoding_e_t Encoding) const noexcept {
+  switch (Encoding) {
+  case __WASI_SIGNATURE_ENCODING_RAW:
+    return Data;
+  case __WASI_SIGNATURE_ENCODING_DER:
+    return WasiCryptoUnexpect(__WASI_CRYPTO_ERRNO_NOT_IMPLEMENTED);
+  default:
+    assumingUnreachable();
+  }
+}
+
+template <int CurveNid>
+WasiCryptoExpect<void>
+Ecdsa<CurveNid>::SignState::update(Span<const uint8_t> Data) noexcept {
+  opensslAssuming(EVP_DigestSignUpdate(Ctx.get(), Data.data(), Data.size()));
+  return {};
+}
+
+template <int CurveNid>
+WasiCryptoExpect<typename Ecdsa<CurveNid>::Signature>
+Ecdsa<CurveNid>::SignState::sign() noexcept {
+  size_t Size;
+  opensslAssuming(EVP_DigestSignFinal(Ctx.get(), nullptr, &Size));
+
+  std::vector<uint8_t> Res(Size);
+  opensslAssuming(EVP_DigestSignFinal(Ctx.get(), Res.data(), &Size));
+
+  return Res;
+}
+
+template <int CurveNid>
+WasiCryptoExpect<void>
+Ecdsa<CurveNid>::VerificationState::update(Span<const uint8_t> Data) noexcept {
+  opensslAssuming(EVP_DigestVerifyUpdate(Ctx.get(), Data.data(), Data.size()));
+  return {};
+}
+
+template <int CurveNid>
+WasiCryptoExpect<void>
+Ecdsa<CurveNid>::VerificationState::verify(Signature &Sig) noexcept {
+  ensureOrReturn(
+      EVP_DigestVerifyFinal(Ctx.get(), Sig.ref().data(), Sig.ref().size()),
+      __WASI_CRYPTO_ERRNO_VERIFICATION_FAILED);
+  return {};
+}
+
+template class Ecdsa<NID_X9_62_prime256v1>;
+template class Ecdsa<NID_secp256k1>;
+} // namespace Signatures
+} // namespace WasiCrypto
+} // namespace Host
+} // namespace WasmEdge

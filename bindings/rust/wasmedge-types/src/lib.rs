0982af0e678838adc90881046a34f233000082be
Xin Liu <sam@secondstate.io>
2022-03-29 11:53:38 +0000

    [rust-types][doc]: Added the necessary artifacts for publish.
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- bindings/rust/wasmedge-types/src/lib.rs
+++ bindings/rust/wasmedge-types/src/lib.rs
@@ -1,3 +1,9 @@
+//! The [wasmedge-types](https://crates.io/crates/wasmedge-types) crate defines a group of common data structures used by both [wasmedge-rs](https://crates.io/crates/wasmedge-sdk) and [wasmedge-sys](https://crates.io/crates/wasmedge-sys) crates.
+//!
+//! See also
+//!
+//! * [WasmEdge Runtime](https://wasmedge.org/)
+
 /// Defines WasmEdge reference types.
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub enum RefType {

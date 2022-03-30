0f1cb4fb725ea4797b4e04cf4a38e976a673657e
Xin Liu <sam@secondstate.io>
2022-03-30 02:15:09 +0000

    [Rust SDK][doc](readme): Added readme.
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- bindings/rust/wasmedge/src/lib.rs
+++ bindings/rust/wasmedge/src/lib.rs
@@ -1,3 +1,13 @@
+//! # Overview
+//!
+//! The [wasmedge-rs](https://crates.io/crates/wasmedge-sdk) crate defines a group of high-level Rust APIs, which are used to build up business applications.
+//!
+//! See also
+//!
+//! * [wasmedge-sys: WasmEdge Low-level Rust APIs](https://crates.io/crates/wasmedge-sys)
+//! * [WasmEdge Runtime](https://wasmedge.org/)
+//! * [WasmEdge C API Documentation](https://github.com/WasmEdge/WasmEdge/blob/master/docs/c_api.md)
+
 use wasmedge_sys as sys;
 use wasmedge_types;
 

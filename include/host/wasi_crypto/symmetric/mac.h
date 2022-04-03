e42c2a198f1c7227f1b9f40872cb136998c3c3ba
sonder-joker <highmagic@outlook.com>
2022-04-03 17:36:16 +0800

    [WASI] Remove redundant empty line.
    Signed-off-by: sonder-joker <highmagic@outlook.com>

--- include/host/wasi_crypto/symmetric/mac.h
+++ include/host/wasi_crypto/symmetric/mac.h
@@ -118,8 +118,6 @@ public:
     WasiCryptoExpect<void> absorb(Span<const uint8_t> Data) noexcept;
 
     /// authenticates the input received up to the function call.
-    ///
-    ///
     /// If finalization is required, the implementation MUST duplicate the
     /// internal state and apply the finalization on the copy, leaving the state
     /// unchanged from the guest perspective.

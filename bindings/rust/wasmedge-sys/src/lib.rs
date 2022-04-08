f89c9eb37cbb85f042d1fdd21656a5e1a1530aa0
Xin Liu <sam@secondstate.io>
2022-04-08 10:04:43 +0000

    [Rust][doc](lib): Added an example in doc comment.
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- bindings/rust/wasmedge-sys/src/lib.rs
+++ bindings/rust/wasmedge-sys/src/lib.rs
@@ -5,7 +5,71 @@
 //!
 //! Most of this documentation is generated from the C API. Until all parts of the documentation have been reviewed there will be incongruities with the actual Rust API.
 //!
-//! See also
+//! ## Usage
+//!
+//! A quick-start example below is using `wasmedge-sys` to run a WebAssembly module written with its WAT format (textual format):
+//!
+//! ```rust
+//! use wasmedge_sys::{Vm, WasmValue};
+//! use wasmedge_types::wat2wasm;
+//!
+//! fn main() -> Result<(), Box<dyn std::error::Error>> {
+//!     // read the wasm bytes
+//!     let wasm_bytes = wat2wasm(
+//!         br#"
+//!         (module
+//!             (export "fib" (func $fib))
+//!             (func $fib (param $n i32) (result i32)
+//!              (if
+//!               (i32.lt_s
+//!                (get_local $n)
+//!                (i32.const 2)
+//!               )
+//!               (return
+//!                (i32.const 1)
+//!               )
+//!              )
+//!              (return
+//!               (i32.add
+//!                (call $fib
+//!                 (i32.sub
+//!                  (get_local $n)
+//!                  (i32.const 2)
+//!                 )
+//!                )
+//!                (call $fib
+//!                 (i32.sub
+//!                  (get_local $n)
+//!                  (i32.const 1)
+//!                 )
+//!                )
+//!               )
+//!              )
+//!             )
+//!            )
+//!         "#)?;
+//!
+//!     // create a Vm instance
+//!     let mut vm = Vm::create(None, None)?;
+//!
+//!     // register the wasm bytes
+//!     let module_name = "extern-module";
+//!     vm.register_wasm_from_buffer(module_name, &wasm_bytes)?;
+//!
+//!     // run the exported function named "fib"
+//!     let func_name = "fib";
+//!     let result = vm.run_registered_function(module_name, func_name, //! [WasmValue::from_i32(5)])?;
+//!
+//!     assert_eq!(result.len(), 1);
+//!     assert_eq!(result[0].to_i32(), 8);
+//!
+//!     Ok(())
+//! }
+//!
+//! ```
+//!
+//! ## See also
+//!
 //! - [WasmEdge Runtime](https://wasmedge.org/)
 //! - [WasmEdge C API Documentation](https://github.com/WasmEdge/WasmEdge/blob/master/docs/c_api.md)
 

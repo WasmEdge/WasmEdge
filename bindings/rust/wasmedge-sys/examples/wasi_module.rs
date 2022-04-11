2b17d7fc72499caa48ba43b3debb211f8d009729
Xin Liu <sam@secondstate.io>
2022-04-11 03:57:21 +0000

    [Rust][refactor](examples): Refactored wasi_module example.
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- bindings/rust/wasmedge-sys/examples/wasi_module.rs
+++ bindings/rust/wasmedge-sys/examples/wasi_module.rs
@@ -4,20 +4,17 @@ use wasmedge_sys::{
 };
 use wasmedge_types::ValType;
 
+#[cfg_attr(test, test)]
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // [WasiModule] implements [ImportInstance](crate::ImportInstance) trait, therefore it can be used to register function, table, memory and global instances.
     {
         // create a Config context, not enable wasi and wasmedge_process options.
-        let result = Config::create();
-        assert!(result.is_ok());
-        let mut config = result.unwrap();
+        let mut config = Config::create()?;
         config.bulk_memory_operations(true);
         assert!(config.bulk_memory_operations_enabled());
 
         // create a Vm context with the given Config and Store
-        let result = Vm::create(Some(config), None);
-        assert!(result.is_ok());
-        let mut vm = result.unwrap();
+        let mut vm = Vm::create(Some(config), None)?;
 
         // get the Wasi module
         let result = vm.wasi_import_module_mut();
@@ -30,9 +27,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         // *** try to add a Wasi module.
 
         // create a Wasi module
-        let result = WasiModule::create(None, None, None);
-        assert!(result.is_ok());
-        let mut import_wasi = result.unwrap();
+        let mut import_wasi = WasiModule::create(None, None, None)?;
 
         // a function to import
         fn real_add(inputs: Vec<WasmValue>) -> Result<Vec<WasmValue>, u8> {
@@ -58,16 +53,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
 
         // add host function
-        let result = FuncType::create(vec![ValType::I32; 2], vec![ValType::I32]);
-        assert!(result.is_ok());
-        let func_ty = result.unwrap();
-        let result = Function::create(&func_ty, Box::new(real_add), 0);
-        assert!(result.is_ok());
-        let host_func = result.unwrap();
+        let func_ty = FuncType::create(vec![ValType::I32; 2], vec![ValType::I32])?;
+        let host_func = Function::create(&func_ty, Box::new(real_add), 0)?;
         import_wasi.add_func("add", host_func);
 
-        let result = vm.register_wasm_from_import(ImportObject::Wasi(import_wasi));
-        assert!(result.is_ok());
+        // register the Wasi module
+        vm.register_wasm_from_import(ImportObject::Wasi(import_wasi))?;
 
         // get the Wasi module
         let result = vm.wasi_import_module_mut();
@@ -78,49 +69,35 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         );
 
         // get store from vm
-        let result = vm.store_mut();
-        assert!(result.is_ok());
-        let mut store = result.unwrap();
-
-        // check registered modules
+        let mut store = vm.store_mut()?;
         assert_eq!(store.module_len(), 1);
         let result = store.module_names();
         assert!(result.is_some());
         assert_eq!(result.unwrap(), ["wasi_snapshot_preview1"]);
 
         // get wasi module instance
-        let result = store.module("wasi_snapshot_preview1");
-        assert!(result.is_ok());
-        let _wasi_instance = result.unwrap();
+        let _wasi_instance = store.module("wasi_snapshot_preview1")?;
     }
 
     // A [WasiModule] can be created implicitly inside a [Vm](crate::Vm) by passing the [Vm](crate::Vm) a [config](crate::Config) argument in which the `wasi` option is enabled.
     {
         // create a Config context
-        let result = Config::create();
-        assert!(result.is_ok());
-        let mut config = result.unwrap();
+        let mut config = Config::create()?;
         config.bulk_memory_operations(true);
         assert!(config.bulk_memory_operations_enabled());
         config.wasi(true);
         assert!(config.wasi_enabled());
 
         // create a Vm context with the given Config and Store
-        let result = Vm::create(Some(config), None);
-        assert!(result.is_ok());
-        let mut vm = result.unwrap();
+        let mut vm = Vm::create(Some(config), None)?;
 
         // get the Wasi module
-        let result = vm.wasi_import_module_mut();
-        assert!(result.is_ok());
-        let _wasi_instance = result.unwrap();
+        let _wasi_instance = vm.wasi_import_module_mut()?;
 
         // *** try to add another Wasi module, that causes error.
 
         // create a Wasi module
-        let result = WasiModule::create(None, None, None);
-        assert!(result.is_ok());
-        let import_wasi = result.unwrap();
+        let import_wasi = WasiModule::create(None, None, None)?;
 
         let result = vm.register_wasm_from_import(ImportObject::Wasi(import_wasi));
         assert!(result.is_err());

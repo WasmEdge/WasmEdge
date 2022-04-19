1c30d4e163b3855253c61a801eaa406ba4e82252
Xin Liu <sam@secondstate.io>
2022-04-19 09:20:48 +0000

    [Rust][doc](lib): Added logo for rustdoc.
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- bindings/rust/wasmedge-sys/src/lib.rs
+++ bindings/rust/wasmedge-sys/src/lib.rs
@@ -1,3 +1,8 @@
+#![doc(
+    html_logo_url = "https://github.com/wasmedge.png",
+    html_favicon_url = "https://wasmedge.org/img/wasmedge-horizontal-white.svg"
+)]
+
 //! # Rust WasmEdge bindings
 //! This library contains safe Rust bindings for WasmEdge, a lightweight, high-performance, and extensible WebAssembly runtime for cloud native, edge, and decentralized applications.
 //!

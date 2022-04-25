110b69ec883f2a6287389052ceffc512738a1f36
Xin Liu <sam@secondstate.io>
2022-04-25 12:10:47 +0000

    [Rust SDK][refactor](lib): Updated the urls for the logo and favicon.
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- bindings/rust/wasmedge/src/lib.rs
+++ bindings/rust/wasmedge/src/lib.rs
@@ -1,6 +1,6 @@
 #![doc(
-    html_logo_url = "https://github.com/wasmedge.png",
-    html_favicon_url = "https://wasmedge.org/img/wasmedge-horizontal-white.svg"
+    html_logo_url = "https://github.com/cncf/artwork/blob/master/projects/wasm-edge-runtime/icon/color/wasm-edge-runtime-icon-color.png?raw=true",
+    html_favicon_url = "https://raw.githubusercontent.com/cncf/artwork/49169bdbc88a7ce3c4a722c641cc2d548bd5c340/projects/wasm-edge-runtime/icon/color/wasm-edge-runtime-icon-color.svg"
 )]
 
 //! # Overview

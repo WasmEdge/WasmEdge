cee89140ce83174ac98efb23d7368f7c482e59e0
YiYing He <yiying@secondstate.io>
2022-05-12 03:53:53 +0800

    [Test] Add the extended-const proposal tests.
    Signed-off-by: YiYing He <yiying@secondstate.io>

--- tools/wasmedge/wasmedgec.cpp
+++ tools/wasmedge/wasmedgec.cpp
@@ -64,6 +64,8 @@ int main(int Argc, const char *Argv[]) {
       PO::Description("Enable Multiple memories proposal"sv));
   PO::Option<PO::Toggle> PropTailCall(
       PO::Description("Enable Tail-call proposal"sv));
+  PO::Option<PO::Toggle> PropExtendConst(
+      PO::Description("Enable Extended-const proposal"sv));
   PO::Option<PO::Toggle> PropAll(PO::Description("Enable all features"sv));
 
   auto Parser = PO::ArgumentParser();
@@ -86,6 +88,7 @@ int main(int Argc, const char *Argv[]) {
            .add_option("disable-simd"sv, PropSIMD)
            .add_option("enable-multi-memory"sv, PropMultiMem)
            .add_option("enable-tail-call"sv, PropTailCall)
+           .add_option("enable-extended-const"sv, PropExtendConst)
            .add_option("enable-all"sv, PropAll)
            .parse(Argc, Argv)) {
     return EXIT_FAILURE;
@@ -123,9 +126,13 @@ int main(int Argc, const char *Argv[]) {
   if (PropTailCall.value()) {
     Conf.addProposal(WasmEdge::Proposal::TailCall);
   }
+  if (PropExtendConst.value()) {
+    Conf.addProposal(WasmEdge::Proposal::ExtendedConst);
+  }
   if (PropAll.value()) {
     Conf.addProposal(WasmEdge::Proposal::MultiMemories);
     Conf.addProposal(WasmEdge::Proposal::TailCall);
+    Conf.addProposal(WasmEdge::Proposal::ExtendedConst);
   }
 
   std::filesystem::path InputPath = std::filesystem::absolute(WasmName.value());
--- tools/wasmedge/wasmedger.cpp
+++ tools/wasmedge/wasmedger.cpp
@@ -68,6 +68,8 @@ int main(int Argc, const char *Argv[]) {
       PO::Description("Enable Multiple memories proposal"sv));
   PO::Option<PO::Toggle> PropTailCall(
       PO::Description("Enable Tail-call proposal"sv));
+  PO::Option<PO::Toggle> PropExtendConst(
+      PO::Description("Enable Extended-const proposal"sv));
   PO::Option<PO::Toggle> PropAll(PO::Description("Enable all features"sv));
 
   PO::Option<PO::Toggle> ConfEnableInstructionCounting(PO::Description(
@@ -116,6 +118,7 @@ int main(int Argc, const char *Argv[]) {
       .add_option("disable-simd"sv, PropSIMD)
       .add_option("enable-multi-memory"sv, PropMultiMem)
       .add_option("enable-tail-call"sv, PropTailCall)
+      .add_option("enable-extended-const"sv, PropExtendConst)
       .add_option("enable-all"sv, PropAll)
       .add_option("time-limit"sv, TimeLim)
       .add_option("gas-limit"sv, GasLim)
@@ -160,9 +163,13 @@ int main(int Argc, const char *Argv[]) {
   if (PropTailCall.value()) {
     Conf.addProposal(WasmEdge::Proposal::TailCall);
   }
+  if (PropExtendConst.value()) {
+    Conf.addProposal(WasmEdge::Proposal::ExtendedConst);
+  }
   if (PropAll.value()) {
     Conf.addProposal(WasmEdge::Proposal::MultiMemories);
     Conf.addProposal(WasmEdge::Proposal::TailCall);
+    Conf.addProposal(WasmEdge::Proposal::ExtendedConst);
   }
 
   std::optional<std::chrono::system_clock::time_point> Timeout;

bb0624909a9ed488704aa2e19cf0d1c189274f06
Gustavo Ye <gusye@mail.ustc.edu.cn>
2022-05-12 00:00:44 +0800

    [WASI] move backend flag into Ctx (#1440)
    Signed-off-by: Gustavo Ye <gusye@mail.ustc.edu.cn>

--- lib/host/wasi_nn/wasinnmodule.cpp
+++ lib/host/wasi_nn/wasinnmodule.cpp
@@ -8,8 +8,6 @@ namespace WasmEdge {
 namespace Host {
 
 WasiNNModule::WasiNNModule() : ModuleInstance("wasi_ephemeral_nn") {
-  Ctx.BackendsMapping.emplace("OpenVINO",
-                              static_cast<WASINN::GraphEncoding>(0));
 
   addHostFunc("load", std::make_unique<WasiNNLoad>(Ctx));
   addHostFunc("init_execution_context",

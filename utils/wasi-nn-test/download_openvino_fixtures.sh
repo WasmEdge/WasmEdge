61a7b12b2befbc7a43a39c87f93f30a4d54625fd
Jianbai Ye <jianbaiye@outlook.com>
2022-05-31 14:08:51 +0800

    chore: move download link
    Signed-off-by: Jianbai Ye <jianbaiye@outlook.com>

--- utils/wasi-nn-test/download_openvino_fixtures.sh
+++ utils/wasi-nn-test/download_openvino_fixtures.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 DOWNLOAD_TO=$1
-
-FIXTURE=https://raw.githubusercontent.com/gusye1234/WasmEdge-WASINN-examples/master/assets/mobilenet-base-example
+FIXTURE=https://github.com/intel/openvino-rs/raw/v0.3.3/crates/openvino/tests/fixtures/mobilenet/
 
 if [ ! -f $DOWNLOAD_TO/mobilenet.bin ]; then
     wget -q --no-clobber --directory-prefix=$DOWNLOAD_TO $FIXTURE/mobilenet.bin

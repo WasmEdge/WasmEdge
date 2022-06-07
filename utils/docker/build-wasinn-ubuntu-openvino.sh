e0cc781f35dd1011b302c3de0db9e631a6cb70bf
YiYing He <yiying@secondstate.io>
2022-06-07 11:47:55 +0800

    [Test] Update the WASI-NN test scripts.
    Signed-off-by: YiYing He <yiying@secondstate.io>

--- utils/docker/build-wasinn-ubuntu-openvino.sh
+++ utils/docker/build-wasinn-ubuntu-openvino.sh
@@ -4,13 +4,18 @@
 
 set -e
 echo "Installing OpenVINO with version ${OPENVINO_VERSION}"
-curl -sSL https://apt.repos.intel.com/openvino/$OPENVINO_YEAR/GPG-PUB-KEY-INTEL-OPENVINO-$OPENVINO_YEAR >./GPG-PUB-KEY-INTEL-OPENVINO-$OPENVINO_YEAR
-apt-key add ./GPG-PUB-KEY-INTEL-OPENVINO-$OPENVINO_YEAR
-echo "deb https://apt.repos.intel.com/openvino/$OPENVINO_YEAR all main" | tee /etc/apt/sources.list.d/intel-openvino-$OPENVINO_YEAR.list
+curl -sSL https://apt.repos.intel.com/openvino/$OPENVINO_YEAR/GPG-PUB-KEY-INTEL-OPENVINO-$OPENVINO_YEAR | gpg --dearmor > /usr/share/keyrings/GPG-PUB-KEY-INTEL-OPENVINO-$OPENVINO_YEAR.gpg
+echo "deb [signed-by=/usr/share/keyrings/GPG-PUB-KEY-INTEL-OPENVINO-$OPENVINO_YEAR.gpg] https://apt.repos.intel.com/openvino/$OPENVINO_YEAR all main" | tee /etc/apt/sources.list.d/intel-openvino-$OPENVINO_YEAR.list
 apt update
 apt install -y intel-openvino-runtime-ubuntu20-$OPENVINO_VERSION
 source /opt/intel/openvino_2021/bin/setupvars.sh
 ldconfig
-
-cmake -Bbuild -GNinja -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DWASMEDGE_BUILD_TESTS=ON -DWASMEDGE_WASINN_BUILD_OPENVINO=ON .
+git config --global --add safe.directory $(pwd)
+if ! cmake -Bbuild -GNinja -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DWASMEDGE_BUILD_TESTS=ON -DWASMEDGE_WASINN_BUILD_OPENVINO=ON .; then
+    echo === CMakeOutput.log ===
+    cat build/CMakeFiles/CMakeOutput.log
+    echo === CMakeError.log ===
+    cat build/CMakeFiles/CMakeError.log
+    exit 1
+fi
 cmake --build build

60de1d06294ec8f39d702be3a235798882ebcedd
YiYing He <yiying@secondstate.io>
2022-07-12 04:03:41 +0800

    [Utils] Separate the openVINO installation and WasmEdge building.
    Signed-off-by: YiYing He <yiying@secondstate.io>

--- utils/wasi-nn/build-wasinn-ubuntu-openvino.sh
+++ utils/wasi-nn/build-wasinn-ubuntu-openvino.sh
@@ -2,22 +2,6 @@
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 # SPDX-FileCopyrightText: 2019-2022 Second State INC
 
-if [[ ! -v "${OPENVINO_VERSION}" ]]; then
-  OPENVINO_VERSION="2021.4.582"
-fi
-if [[ ! -v "${OPENVINO_YEAR}" ]]; then
-  OPENVINO_YEAR="2021"
-fi
-if [[ ! -v "${CMAKE_BUILD_TYPE}" ]]; then
-  CMAKE_BUILD_TYPE=Release
-fi
-
-set -e
-echo "Installing OpenVINO with version ${OPENVINO_VERSION}"
-curl -sSL https://apt.repos.intel.com/openvino/$OPENVINO_YEAR/GPG-PUB-KEY-INTEL-OPENVINO-$OPENVINO_YEAR | gpg --dearmor > /usr/share/keyrings/GPG-PUB-KEY-INTEL-OPENVINO-$OPENVINO_YEAR.gpg
-echo "deb [signed-by=/usr/share/keyrings/GPG-PUB-KEY-INTEL-OPENVINO-$OPENVINO_YEAR.gpg] https://apt.repos.intel.com/openvino/$OPENVINO_YEAR all main" | tee /etc/apt/sources.list.d/intel-openvino-$OPENVINO_YEAR.list
-apt update
-apt install -y intel-openvino-runtime-ubuntu20-$OPENVINO_VERSION
 source /opt/intel/openvino_2021/bin/setupvars.sh
 ldconfig
 git config --global --add safe.directory $(pwd)

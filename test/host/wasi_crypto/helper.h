745ca9d73a31ebb8aaca22fce376efa71dca2700
sonder-joker <highmagic@outlook.com>
2022-07-13 00:58:51 +0800

    [Test] Add the wasi-crypto tests.
    Signed-off-by: sonder-joker <highmagic@outlook.com>

---
+++ test/host/wasi_crypto/helper.h
@@ -0,0 +1,339 @@
+#pragma once
+
+#include "common/enum_types.h"
+#include "common/span.h"
+#include "common/types.h"
+#include "helper.h"
+#include "host/wasi_crypto/asymmetric_common/func.h"
+#include "host/wasi_crypto/common/func.h"
+#include "host/wasi_crypto/ctx.h"
+#include "host/wasi_crypto/utils/error.h"
+#include "runtime/instance/memory.h"
+#include "wasi_crypto/api.hpp"
+#include "gtest/gtest.h"
+
+#include <algorithm>
+#include <cstdint>
+#include <queue>
+#include <string>
+#include <string_view>
+#include <sys/types.h>
+#include <type_traits>
+#include <utility>
+#include <vector>
+
+#define WASI_CRYPTO_EXPECT_SUCCESS(Expr, Function)                             \
+  auto &&Expr##__Result = Function;                                            \
+  EXPECT_TRUE(Expr##__Result)                                                  \
+      << "Wasi Crypto Error code: " << Errno[0].get<int32_t>();                \
+  auto &&Expr = Expr##__Result.value()
+
+#define WASI_CRYPTO_EXPECT_FAILURE(Function, ErrorCode)                        \
+  do {                                                                         \
+    auto Result = Function;                                                    \
+    EXPECT_FALSE(Result) << "The function result should be error but success"; \
+    EXPECT_EQ(Result.error(), ErrorCode);                                      \
+  } while (0)
+
+#define WASI_CRYPTO_EXPECT_TRUE(Function)                                      \
+  EXPECT_TRUE(Function) << "Wasi Crypto Error code: " << Errno[0].get<int32_t>()
+
+namespace WasmEdge {
+namespace Host {
+namespace WasiCrypto {
+
+std::vector<uint8_t> operator"" _u8(const char *Str, std::size_t Len);
+
+std::vector<uint8_t> operator"" _u8v(const char *Str, std::size_t Len);
+
+/// designed for test
+class WasiCryptoTest : public ::testing::Test {
+protected:
+  void writeDummyMemoryContent();
+
+  void writeString(std::string_view String, uint32_t Ptr);
+
+  void writeSpan(Span<const uint8_t> Content, uint32_t Ptr);
+
+  void writeOptKey(std::optional<int32_t> OptKey, uint32_t Ptr);
+
+  void writeOptOptions(std::optional<__wasi_options_t> OptOptions,
+                       uint32_t Ptr);
+
+  ///-------------------------------------------common---------------------------------------
+
+  WasiCryptoExpect<__wasi_size_t>
+  arrayOutputLen(__wasi_array_output_t ArrayOutputHandle);
+
+  WasiCryptoExpect<__wasi_size_t>
+  arrayOutputPull(__wasi_array_output_t ArrayOutputHandle, Span<uint8_t> Buf);
+
+  WasiCryptoExpect<__wasi_options_t>
+  optionsOpen(__wasi_algorithm_type_e_t AlgType);
+
+  WasiCryptoExpect<void> optionsClose(__wasi_options_t OptionsHandle);
+
+  WasiCryptoExpect<void> optionsSet(__wasi_options_t OptionsHandle,
+                                    std::string_view Name,
+                                    Span<const uint8_t> Value);
+
+  WasiCryptoExpect<void> optionsSetU64(__wasi_options_t OptionsHandle,
+                                       std::string_view Name, uint64_t Value);
+
+  /// Not support, Buf place must on page.
+  //   WasiCryptoExpect<void> optionsSetGuestBuffer(__wasi_options_t
+  //   OptionsHandle,
+  //                                                std::string_view Name,
+  //                                                Span<uint8_t> Buf);
+
+  WasiCryptoExpect<__wasi_secrets_manager_t>
+  secretsManagerOpen(std::optional<__wasi_options_t> OptOptionsHandle);
+
+  WasiCryptoExpect<void>
+  secretsManagerClose(__wasi_secrets_manager_t SecretsManagerHandle);
+
+  WasiCryptoExpect<void>
+  secretsManagerInvalidate(__wasi_secrets_manager_t SecretsManagerHandle,
+                           Span<const uint8_t> KeyId, __wasi_version_t Version);
+
+  ///-------------------------------------------symmetric---------------------------------------
+
+  WasiCryptoExpect<__wasi_symmetric_key_t>
+  symmetricKeyGenerate(std::string_view Alg,
+                       std::optional<__wasi_options_t> OptOptionsHandle);
+
+  WasiCryptoExpect<__wasi_symmetric_key_t>
+  symmetricKeyImport(std::string_view Alg, Span<const uint8_t> Raw);
+
+  WasiCryptoExpect<__wasi_array_output_t>
+  symmetricKeyExport(__wasi_symmetric_key_t KeyHandle);
+
+  WasiCryptoExpect<void> symmetricKeyClose(__wasi_symmetric_key_t KeyHandle);
+
+  WasiCryptoExpect<__wasi_symmetric_key_t>
+  symmetricKeyGenerateManaged(__wasi_secrets_manager_t SecretsManagerHandle,
+                              std::string_view Alg,
+                              std::optional<__wasi_options_t> OptOptionsHandle);
+
+  WasiCryptoExpect<void>
+  symmetricKeyStoreManaged(__wasi_secrets_manager_t SecretsManagerHandle,
+                           __wasi_symmetric_key_t KeyHandle,
+                           Span<uint8_t> KeyId);
+
+  WasiCryptoExpect<__wasi_version_t>
+  symmetricKeyReplaceManaged(__wasi_secrets_manager_t SecretsManagerHandle,
+                             __wasi_symmetric_key_t OldKeyHandle,
+                             __wasi_symmetric_key_t NewKeyHandle);
+
+  WasiCryptoExpect<std::tuple<size_t, __wasi_version_t>>
+  symmetricKeyId(__wasi_symmetric_key_t KeyHandle, Span<uint8_t> KeyId);
+
+  WasiCryptoExpect<__wasi_symmetric_key_t>
+  symmetricKeyFromId(__wasi_secrets_manager_t SecretsManagerHandle,
+                     Span<uint8_t> KeyId, __wasi_version_t KeyVersion);
+
+  WasiCryptoExpect<__wasi_symmetric_state_t>
+  symmetricStateOpen(std::string_view Alg,
+                     std::optional<__wasi_symmetric_key_t> OptKeyHandle,
+                     std::optional<__wasi_options_t> OptOptionsHandle);
+
+  WasiCryptoExpect<__wasi_size_t>
+  symmetricStateOptionsGet(__wasi_symmetric_state_t StateHandle,
+                           std::string_view Name, Span<uint8_t> Value);
+
+  WasiCryptoExpect<uint64_t>
+  symmetricStateOptionsGetU64(__wasi_symmetric_state_t StateHandle,
+                              std::string_view Name);
+
+  WasiCryptoExpect<void>
+  symmetricStateClose(__wasi_symmetric_state_t StateHandle);
+
+  WasiCryptoExpect<void>
+  symmetricStateAbsorb(__wasi_symmetric_state_t StateHandle,
+                       Span<const uint8_t> Data);
+
+  WasiCryptoExpect<__wasi_symmetric_state_t>
+  symmetricStateClone(__wasi_symmetric_state_t StateHandle);
+
+  WasiCryptoExpect<void>
+  symmetricStateSqueeze(__wasi_symmetric_state_t StateHandle,
+                        Span<uint8_t> Out);
+
+  WasiCryptoExpect<__wasi_symmetric_tag_t>
+  symmetricStateSqueezeTag(__wasi_symmetric_state_t StateHandle);
+
+  WasiCryptoExpect<__wasi_symmetric_key_t>
+  symmetricStateSqueezeKey(__wasi_symmetric_state_t StateHandle,
+                           std::string_view Alg);
+
+  WasiCryptoExpect<__wasi_size_t>
+  symmetricStateMaxTagLen(__wasi_symmetric_state_t StateHandle);
+
+  WasiCryptoExpect<__wasi_size_t>
+  symmetricStateEncrypt(__wasi_symmetric_state_t StateHandle, Span<uint8_t> Out,
+                        Span<const uint8_t> Data);
+
+  WasiCryptoExpect<__wasi_symmetric_tag_t>
+  symmetricStateEncryptDetached(__wasi_symmetric_state_t StateHandle,
+                                Span<uint8_t> Out, Span<const uint8_t> Data);
+
+  WasiCryptoExpect<__wasi_size_t>
+  symmetricStateDecrypt(__wasi_symmetric_state_t StateHandle, Span<uint8_t> Out,
+                        Span<const uint8_t> Data);
+
+  WasiCryptoExpect<__wasi_size_t>
+  symmetricStateDecryptDetached(__wasi_symmetric_state_t StateHandle,
+                                Span<uint8_t> Out, Span<const uint8_t> Data,
+                                Span<uint8_t> RawTag);
+
+  WasiCryptoExpect<void>
+  symmetricStateRatchet(__wasi_symmetric_state_t StateHandle);
+
+  WasiCryptoExpect<__wasi_size_t>
+  symmetricMaxTagLen(__wasi_symmetric_tag_t TagHandle);
+
+  WasiCryptoExpect<__wasi_size_t>
+  symmetricTagPull(__wasi_symmetric_tag_t TagHandle, Span<uint8_t> Buf);
+
+  WasiCryptoExpect<void> symmetricTagVerify(__wasi_symmetric_tag_t TagHandle,
+                                            Span<const uint8_t> RawTag);
+
+  WasiCryptoExpect<void> symmetricTagClose(__wasi_symmetric_tag_t TagHandle);
+
+  ///-------------------------------------------asymmetric_common---------------------------------------
+
+  WasiCryptoExpect<__wasi_keypair_t>
+  keypairGenerate(__wasi_algorithm_type_e_t AlgType, std::string_view Alg,
+                  std::optional<__wasi_options_t> OptOptionsHandle);
+
+  WasiCryptoExpect<__wasi_keypair_t>
+  keypairImport(__wasi_algorithm_type_e_t AlgType, std::string_view Alg,
+                Span<const uint8_t> Encoded,
+                __wasi_keypair_encoding_e_t Encoding);
+
+  WasiCryptoExpect<__wasi_keypair_t>
+  keypairGenerateManaged(__wasi_secrets_manager_t SecretsManagerHandle,
+                         __wasi_algorithm_type_e_t AlgType,
+                         std::string_view Alg,
+                         std::optional<__wasi_options_t> OptOptionsHandle);
+
+  WasiCryptoExpect<void>
+  keypairStoreManaged(__wasi_secrets_manager_t SecretsManagerHandle,
+                      __wasi_keypair_t KpHandle, Span<uint8_t> KpId);
+
+  WasiCryptoExpect<__wasi_version_t>
+  keypairReplaceManaged(__wasi_secrets_manager_t SecretsManagerHandle,
+                        __wasi_keypair_t OldKpHandle,
+                        __wasi_keypair_t NewKpHandle);
+
+  WasiCryptoExpect<std::tuple<size_t, __wasi_version_t>>
+  keypairId(__wasi_keypair_t KpHandle, Span<uint8_t> KpId);
+
+  WasiCryptoExpect<__wasi_keypair_t>
+  keypairFromId(__wasi_secrets_manager_t SecretsManagerHandle,
+                Span<const uint8_t> KpId, __wasi_version_t KpIdVersion);
+
+  WasiCryptoExpect<__wasi_keypair_t>
+  keypairFromPkAndSk(__wasi_publickey_t PkHandle, __wasi_secretkey_t SkHandle);
+
+  WasiCryptoExpect<__wasi_array_output_t>
+  keypairExport(__wasi_keypair_t KpHandle,
+                __wasi_keypair_encoding_e_t Encoding);
+
+  WasiCryptoExpect<__wasi_publickey_t>
+  keypairPublickey(__wasi_keypair_t KpHandle);
+
+  WasiCryptoExpect<__wasi_secretkey_t>
+  keypairSecretkey(__wasi_keypair_t KpHandle);
+
+  WasiCryptoExpect<void> keypairClose(__wasi_keypair_t KpHandle);
+
+  WasiCryptoExpect<__wasi_publickey_t>
+  publickeyImport(__wasi_algorithm_type_e_t AlgType, std::string_view Alg,
+                  Span<const uint8_t> Encoded,
+                  __wasi_publickey_encoding_e_t Encoding);
+
+  WasiCryptoExpect<__wasi_array_output_t>
+  publickeyExport(__wasi_publickey_t PkHandle,
+                  __wasi_publickey_encoding_e_t Encoding);
+
+  WasiCryptoExpect<void> publickeyVerify(__wasi_publickey_t PkHandle);
+
+  WasiCryptoExpect<__wasi_publickey_t>
+  publickeyFromSecretkey(__wasi_secretkey_t SkHandle);
+
+  WasiCryptoExpect<void> publickeyClose(__wasi_publickey_t PkHandle);
+
+  WasiCryptoExpect<__wasi_secretkey_t>
+  secretkeyImport(__wasi_algorithm_type_e_t AlgType, std::string_view Alg,
+                  Span<const uint8_t> Encoded,
+                  __wasi_secretkey_encoding_e_t Encoding);
+
+  WasiCryptoExpect<__wasi_array_output_t>
+  secretkeyExport(__wasi_secretkey_t SkHandle,
+                  __wasi_secretkey_encoding_e_t Encoding);
+
+  WasiCryptoExpect<void> secretkeyClose(__wasi_secretkey_t SkHandle);
+
+  ///------------------------------------------key_exchange-------------------------------------
+
+  WasiCryptoExpect<__wasi_array_output_t> kxDh(__wasi_kx_publickey_t PkHandle,
+                                               __wasi_kx_secretkey_t SkHandle);
+
+  WasiCryptoExpect<std::tuple<__wasi_array_output_t, __wasi_array_output_t>>
+  kxEncapsulate(__wasi_kx_publickey_t PkHandle);
+
+  WasiCryptoExpect<__wasi_array_output_t>
+  kxDecapsulate(__wasi_kx_secretkey_t SkHandle,
+                Span<const uint8_t> EncapsulatedSecret);
+
+  ///-------------------------------------------signature---------------------------------------
+
+  WasiCryptoExpect<__wasi_array_output_t>
+  signatureExport(__wasi_signature_t SigHandle,
+                  __wasi_signature_encoding_e_t Encoding);
+
+  WasiCryptoExpect<__wasi_signature_t>
+  signatureImport(std::string_view Alg, Span<const uint8_t> Encoded,
+                  __wasi_signature_encoding_e_t Encoding);
+
+  WasiCryptoExpect<void> signatureClose(__wasi_signature_t SigHandle);
+
+  WasiCryptoExpect<__wasi_signature_state_t>
+  signatureStateOpen(__wasi_signature_keypair_t KpHandle);
+
+  WasiCryptoExpect<void>
+  signatureStateUpdate(__wasi_signature_state_t StateHandle,
+                       Span<const uint8_t> Input);
+
+  WasiCryptoExpect<__wasi_signature_t>
+  signatureStateSign(__wasi_signature_state_t StateHandle);
+
+  WasiCryptoExpect<void>
+  signatureStateClose(__wasi_signature_state_t StateHandle);
+
+  WasiCryptoExpect<__wasi_signature_verification_state_t>
+  signatureVerificationStateOpen(__wasi_signature_publickey_t PkHandle);
+
+  WasiCryptoExpect<void> signatureVerificationStateUpdate(
+      __wasi_signature_verification_state_t StateHandle,
+      Span<const uint8_t> Input);
+
+  WasiCryptoExpect<void> signatureVerificationStateVerify(
+      __wasi_signature_verification_state_t StateHandle,
+      __wasi_signature_t SigHandle);
+
+  WasiCryptoExpect<void> signatureVerificationStateClose(
+      __wasi_signature_verification_state_t StateHandle);
+
+  int32_t InvaildHandle = 9999;
+  WasmEdge::Runtime::Instance::MemoryInstance MemInst{
+      WasmEdge::AST::MemoryType(1)};
+  std::array<WasmEdge::ValVariant, 1> Errno;
+
+  Context Ctx;
+};
+
+} // namespace WasiCrypto
+} // namespace Host
+} // namespace WasmEdge
---
+++ test/host/wasi_crypto/signatures.cpp
@@ -0,0 +1,113 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2019-2022 Second State INC
+
+#include "helper.h"
+#include "host/wasi_crypto/asymmetric_common/func.h"
+#include "host/wasi_crypto/common/func.h"
+#include "host/wasi_crypto/signatures/func.h"
+
+#include <cstdint>
+#include <gtest/gtest.h>
+#include <optional>
+#include <string_view>
+
+namespace WasmEdge {
+namespace Host {
+namespace WasiCrypto {
+using namespace std::literals;
+
+TEST_F(WasiCryptoTest, Signatures) {
+  // use generate data sign and verfiy
+  auto SigTest = [this](__wasi_algorithm_type_e_t AlgType,
+                        std::string_view Alg) {
+    SCOPED_TRACE(Alg);
+    WASI_CRYPTO_EXPECT_SUCCESS(KpHandle,
+                               keypairGenerate(AlgType, Alg, std::nullopt));
+    WASI_CRYPTO_EXPECT_SUCCESS(StateHandle, signatureStateOpen(KpHandle));
+    WASI_CRYPTO_EXPECT_TRUE(signatureStateUpdate(StateHandle, "test"_u8));
+    WASI_CRYPTO_EXPECT_TRUE(signatureStateUpdate(StateHandle, "test"_u8));
+    WASI_CRYPTO_EXPECT_SUCCESS(SigHandle, signatureStateSign(StateHandle));
+    WASI_CRYPTO_EXPECT_TRUE(signatureStateClose(StateHandle));
+
+    WASI_CRYPTO_EXPECT_SUCCESS(PkHandle, keypairPublickey(KpHandle));
+    WASI_CRYPTO_EXPECT_SUCCESS(VerifictionStateHandle,
+                               signatureVerificationStateOpen(PkHandle));
+    WASI_CRYPTO_EXPECT_TRUE(
+        signatureVerificationStateUpdate(VerifictionStateHandle, "test"_u8));
+    WASI_CRYPTO_EXPECT_TRUE(
+        signatureVerificationStateUpdate(VerifictionStateHandle, "test"_u8));
+    WASI_CRYPTO_EXPECT_TRUE(
+        signatureVerificationStateVerify(VerifictionStateHandle, SigHandle));
+    WASI_CRYPTO_EXPECT_TRUE(
+        signatureVerificationStateClose(VerifictionStateHandle));
+  };
+  SigTest(__WASI_ALGORITHM_TYPE_SIGNATURES, "ECDSA_P256_SHA256"sv);
+  SigTest(__WASI_ALGORITHM_TYPE_SIGNATURES, "ECDSA_K256_SHA256"sv);
+  SigTest(__WASI_ALGORITHM_TYPE_SIGNATURES, "Ed25519"sv);
+  SigTest(__WASI_ALGORITHM_TYPE_SIGNATURES, "RSA_PKCS1_2048_SHA256"sv);
+  SigTest(__WASI_ALGORITHM_TYPE_SIGNATURES, "RSA_PKCS1_2048_SHA384"sv);
+  SigTest(__WASI_ALGORITHM_TYPE_SIGNATURES, "RSA_PKCS1_2048_SHA512"sv);
+  SigTest(__WASI_ALGORITHM_TYPE_SIGNATURES, "RSA_PKCS1_3072_SHA384"sv);
+  SigTest(__WASI_ALGORITHM_TYPE_SIGNATURES, "RSA_PKCS1_3072_SHA512"sv);
+  SigTest(__WASI_ALGORITHM_TYPE_SIGNATURES, "RSA_PKCS1_4096_SHA512"sv);
+  SigTest(__WASI_ALGORITHM_TYPE_SIGNATURES, "RSA_PSS_2048_SHA256"sv);
+  SigTest(__WASI_ALGORITHM_TYPE_SIGNATURES, "RSA_PSS_2048_SHA384"sv);
+  SigTest(__WASI_ALGORITHM_TYPE_SIGNATURES, "RSA_PSS_2048_SHA512"sv);
+  SigTest(__WASI_ALGORITHM_TYPE_SIGNATURES, "RSA_PSS_3072_SHA384"sv);
+  SigTest(__WASI_ALGORITHM_TYPE_SIGNATURES, "RSA_PSS_3072_SHA512"sv);
+  SigTest(__WASI_ALGORITHM_TYPE_SIGNATURES, "RSA_PSS_4096_SHA512"sv);
+
+  auto SigEncodingTest =
+      [this](
+          std::string_view Alg,
+          std::map<__wasi_signature_encoding_e_t, std::vector<uint8_t>> Data) {
+        SCOPED_TRACE(Alg);
+        for (auto &[Encoding, Sig] : Data) {
+          SCOPED_TRACE(Encoding);
+          WASI_CRYPTO_EXPECT_SUCCESS(SigHandle,
+                                     signatureImport(Alg, Sig, Encoding));
+          WASI_CRYPTO_EXPECT_SUCCESS(ExportSigHandle,
+                                     signatureExport(SigHandle, Encoding));
+          WASI_CRYPTO_EXPECT_SUCCESS(ExportSigSize,
+                                     arrayOutputLen(ExportSigHandle));
+          std::vector<uint8_t> ExportSig(ExportSigSize);
+          WASI_CRYPTO_EXPECT_TRUE(arrayOutputPull(ExportSigHandle, ExportSig));
+          EXPECT_EQ(Sig, ExportSig);
+        }
+      };
+  SigEncodingTest(
+      "ECDSA_K256_SHA256"sv,
+      {{__WASI_SIGNATURE_ENCODING_RAW,
+        "9D92E9FDCA3DDF2E1DDCA1E3B7A79A250B6E4AFFCABF5F9FF4D960B152AB8300E9EB978BD3DA89C42BBFE5A2C2AEB0AF1DD178FB4BCD0833B587D118F59BBB4D"_u8v},
+       {__WASI_SIGNATURE_ENCODING_DER,
+        "30460221009d92e9fdca3ddf2e1ddca1e3b7a79a250b6e4affcabf5f9f"
+        "f4d960b152ab8300022100e9eb978bd3da89c42bbfe5a2c2aeb0af1dd1"
+        "78fb4bcd0833b587d118f59bbb4d"_u8v}});
+  SigEncodingTest(
+      "ECDSA_P256_SHA256"sv,
+      {{__WASI_SIGNATURE_ENCODING_RAW,
+        "80D5D4769AE4F3998DD6B8B01177DE855204122A361F2189F9567C806DE2673E2FBFD3FF018338875B1D144F583EB6E8DC16CF6EEB2BB5C19A3202464ABB58BD"_u8v},
+       {__WASI_SIGNATURE_ENCODING_DER,
+        "304502210080d5d4769ae4f3998dd6b8b01177de855204122a361f2189"
+        "f9567c806de2673e02202fbfd3ff018338875b1d144f583eb6e8dc16cf"
+        "6eeb2bb5c19a3202464abb58bd"_u8v}});
+  SigEncodingTest(
+      "Ed25519"sv,
+      {{__WASI_SIGNATURE_ENCODING_RAW,
+        "d4fbdb52bfa726b44d1786a8c0d171c3e62ca83c9e5bbe63de0bb2483f8fd6cc1429ab72cafc41ab56af02ff8fcc43b99bfe4c7ae940f60f38ebaa9d311c4007"_u8v}});
+  SigEncodingTest(
+      "RSA_PSS_2048_SHA256"sv,
+      {{__WASI_SIGNATURE_ENCODING_RAW,
+        "4f01e0c12b08625ecac89a69231906edf826380f37c959a96690d046316d68ff"
+        "ce9d5c471694fcebfc6b45534864689256e4fc81c78e583f675d0c94b4496474"
+        "51e81beff01a11a516d5e5ce3f1a910437cb8a3a5096b19fb15f4524a35b23d8"
+        "9cdba12cf5b71aac1047b28c562df7c5542c34ce23a182cf7e0e231934b17294"
+        "799d44877a1d68ef1b8f073619b7618e6b7c22db20030d98cf591ffc3d4da5f5"
+        "8613ecd5ecfc3b40a1d02f40891ca43695cd4c088b05a8054c89c595a47e2748"
+        "16f35384226f74459ee63e25a1bfc03c360490552ec38343f8ace502f065303b"
+        "00bc0ec320711b211fde92e57feb9013c3609342495ec0d7cabdec21e54acc38"_u8v}});
+}
+
+} // namespace WasiCrypto
+} // namespace Host
+} // namespace WasmEdge

4d47d856df3be673dd37be6396f886422fb02724
Xin Liu <sam@secondstate.io>
2022-07-20 09:34:37 +0800

    [Rust][refactor](utils): Refactored code.
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- bindings/rust/wasmedge-sys/src/utils.rs
+++ bindings/rust/wasmedge-sys/src/utils.rs
@@ -13,21 +13,9 @@ use std::{
     path::Path,
 };
 
-#[cfg(unix)]
 pub(crate) fn path_to_cstring(path: &Path) -> WasmEdgeResult<CString> {
     use std::os::unix::ffi::OsStrExt;
     Ok(CString::new(path.as_os_str().as_bytes())?)
-}
-
-#[cfg(windows)]
-pub(crate) fn path_to_cstring(path: &Path) -> WasmEdgeResult<CString> {
-    match path.to_str() {
-        Some(s) => Ok(CString::new(s)?),
-        None => {
-            let message = format!("Couldn't convert path '{}' to UTF-8", path.display());
-            Err(message.into())
-        }
-    }
 }
 
 /// Logs the debug information.

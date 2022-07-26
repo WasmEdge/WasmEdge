b7a6ffb2be0eb146d6ad00115bb992174eb9fd31
wcy <564429983@qq.com>
2022-07-26 17:57:01 +0800

    [RISCV64] Fix the format.
    Signed-off-by: wcy <564429983@qq.com>

--- lib/system/allocator.cpp
+++ lib/system/allocator.cpp
@@ -59,7 +59,8 @@ static inline constexpr const uint64_t k12G = UINT64_C(0x300000000);
 
 [[gnu::visibility("default")]] uint8_t *
 Allocator::allocate(uint32_t PageCount) noexcept {
-#if defined(HAVE_MMAP) && defined(__x86_64__) || defined(__aarch64__) || defined(__riscv)
+#if defined(HAVE_MMAP) && defined(__x86_64__) || defined(__aarch64__) ||       \
+    defined(__riscv)
   auto Reserved = reinterpret_cast<uint8_t *>(
       mmap(nullptr, k12G, PROT_NONE,
            MAP_PRIVATE | MAP_ANONYMOUS | MAP_NORESERVE, -1, 0));
@@ -103,7 +104,8 @@ Allocator::allocate(uint32_t PageCount) noexcept {
 Allocator::resize(uint8_t *Pointer, uint32_t OldPageCount,
                   uint32_t NewPageCount) noexcept {
   assuming(NewPageCount > OldPageCount);
-#if defined(HAVE_MMAP) && defined(__x86_64__) || defined(__aarch64__) || defined(__riscv)
+#if defined(HAVE_MMAP) && defined(__x86_64__) || defined(__aarch64__) ||       \
+    defined(__riscv)
   if (mmap(Pointer + OldPageCount * kPageSize,
            (NewPageCount - OldPageCount) * kPageSize, PROT_READ | PROT_WRITE,
            MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED, -1, 0) == MAP_FAILED) {

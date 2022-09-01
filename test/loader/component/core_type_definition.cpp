d425699cde224f3a91ce40bf3e9fdbc53f63da13
Lîm Tsú-thuàn <dannypsnl@secondstate.io>
2022-09-01 12:33:59 +0800

    [Verify] core type definition
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>

---
+++ test/loader/component/core_type_definition.cpp
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2019-2022 Second State INC
+
+//=== Load component model: Type
+//
+// Part of the WasmEdge Project.
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file contents unit tests of loading file as AST component nodes.
+///
+//===----------------------------------------------------------------------===//
+
+#include "loader/loader.h"
+
+#include <cstdint>
+#include <gtest/gtest.h>
+#include <vector>
+
+namespace {
+
+using namespace WasmEdge::AST;
+
+WasmEdge::Configure Conf;
+WasmEdge::Loader::Loader Ldr{Conf};
+
+TEST(ComponentTest, CoreTypeDefinition) {
+  Conf.addProposal(WasmEdge::Proposal::ComponentModel);
+
+  std::filesystem::path path{"testData/core-type-definition.wasm"};
+  auto Comp = Ldr.parseComponent(path);
+  EXPECT_TRUE(Comp);
+}
+
+} // namespace

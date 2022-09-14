b7c3d3cfee3530707f129010ac68d74797ecb873
YiYing He <yiying@secondstate.io>
2022-09-14 21:49:59 +0800

    [WASI] Refine the WASI-NN pytorch scripts
    Signed-off-by: YiYing He <yiying@secondstate.io>

--- utils/wasi-nn/install-pytorch.sh
+++ utils/wasi-nn/install-pytorch.sh
@@ -6,6 +6,7 @@ if [[ ! -n ${PYTORCH_VERSION} ]]; then
   PYTORCH_VERSION="1.8.2"
 fi
 
+<<<<<<< HEAD
 if [[ ! -n ${PYTORCH_INSTALL_TO} ]]; then
   PYTORCH_INSTALL_TO=.
 fi
@@ -28,4 +29,17 @@ if [ ! -d ${PYTORCH_INSTALL_TO}/libtorch ]; then
     echo "${PYTORCH_SHA} ${PYTORCH_LINK}-shared-with-deps-${PYTORCH_VERSION}%2Bcpu.zip" | sha256sum -c
     unzip -q "${PYTORCH_LINK}-shared-with-deps-${PYTORCH_VERSION}%2Bcpu.zip" -d ${PYTORCH_INSTALL_TO}
     rm -f "${PYTORCH_LINK}-shared-with-deps-${PYTORCH_VERSION}%2Bcpu.zip"
+=======
+if [[ $# -lt 1 ]]; then
+  DOWNLOAD_TO=.
+else
+  DOWNLOAD_TO=$1
+fi
+
+if [ ! -d $DOWNLOAD_TO/libtorch ]; then
+    curl -s -L -O --remote-name-all https://download.pytorch.org/libtorch/lts/1.8/cpu/libtorch-cxx11-abi-shared-with-deps-${PYTORCH_VERSION}%2Bcpu.zip
+    echo "b76d6dd4380e2233ce6f7654e672e13aae7c871231d223a4267ef018dcbfb616 libtorch-cxx11-abi-shared-with-deps-${PYTORCH_VERSION}%2Bcpu.zip" | sha256sum -c
+    unzip -q "libtorch-cxx11-abi-shared-with-deps-${PYTORCH_VERSION}%2Bcpu.zip" -d $DOWNLOAD_TO
+    rm -f "libtorch-cxx11-abi-shared-with-deps-${PYTORCH_VERSION}%2Bcpu.zip"
+>>>>>>> af9e22d8 ([WASI] Refine the WASI-NN pytorch scripts)
 fi

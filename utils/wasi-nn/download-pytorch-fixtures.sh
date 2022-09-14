745c9adff164fca4dbdef894e87e97529f2c7c8a
YiYing He <yiying@secondstate.io>
2022-09-14 21:49:59 +0800

    [WASI] Refine the WASI-NN pytorch scripts
    Signed-off-by: YiYing He <yiying@secondstate.io>

--- utils/wasi-nn/download-pytorch-fixtures.sh
+++ utils/wasi-nn/download-pytorch-fixtures.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+# SPDX-FileCopyrightText: 2019-2022 Second State INC
 
 DOWNLOAD_TO=$1
-FIXTURE=https://github.com/gusye1234/torchscript_fixtures/raw/main/mobilenet
-
+FIXTURE=https://github.com/second-state/WasmEdge-WASINN-examples/raw/master/pytorch-mobilenet-image/
 if [ ! -f $DOWNLOAD_TO/mobilenet.pt ]; then
     wget -q --no-clobber --directory-prefix=$DOWNLOAD_TO $FIXTURE/mobilenet.pt
 fi
 
-if [ ! -f $DOWNLOAD_TO/image-1-3-244-244.rgb ]; then
-    wget -q --no-clobber --directory-prefix=$DOWNLOAD_TO $FIXTURE/image-1-3-244-244.rgb
+if [ ! -f $DOWNLOAD_TO/image-1x3x224x224.rgb ]; then
+    wget -q --no-clobber --directory-prefix=$DOWNLOAD_TO $FIXTURE/image-1x3x224x224.rgb
 fi

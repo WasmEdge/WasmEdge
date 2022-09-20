fc754a06bb7f1026b6147f4cc4210acbd86d3ab4
Lîm Tsú-thuàn <dannypsnl@secondstate.io>
2022-09-20 14:58:30 +0800

    [Component] create module instance based on export list
    The problem is from binary definition, I cannot see how it stores sort:idx. Thus, the current implementation about `exportXxx` is wrong since the sort is not in the immediate module instance but somewhere else. On the other hand, the immediate module instance is correct semantic.
    
    To fix this problem, I will propose a test and see how the tool generates reference to sort(like a function instantiate in another module), and check what should be added into the internal interface to interact with these.
    
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>

--- lib/executor/instantiate/component/core_instance.cpp
+++ lib/executor/instantiate/component/core_instance.cpp
@@ -39,11 +39,52 @@ Executor::instantiateCore(Runtime::StoreManager &StoreMgr,
           return {};
         } else if constexpr (std::is_same_v<T, AST::CoreInstance::Export>) {
           for (auto &Export : Arg.getExports()) {
+            auto *Mod = new Runtime::Instance::ModuleInstance{""};
+            CompInst.addCoreInstance(Mod);
+
             std::string_view Name = Export.getName();
-            AST::SortIndex Idx = Export.getExtern();
-            Idx.getSort();
-            Idx.getIndex();
-            // TODO: what to do here?
+            AST::SortIndex Extern = Export.getExtern();
+            auto I = Extern.getIndex();
+            switch (Extern.getSort()) {
+            case AST::Sort::CoreFunc:
+              Mod->exportFunction(Name, I);
+              break;
+            case AST::Sort::Table:
+              Mod->exportTable(Name, I);
+              break;
+            case AST::Sort::Memory:
+              Mod->exportMemory(Name, I);
+              break;
+            case AST::Sort::Global:
+              Mod->exportGlobal(Name, I);
+              break;
+            case AST::Sort::CoreType:
+              // NOTE:
+              // type is added to core:sort in anticipation of the type-imports
+              // proposal. Until that proposal, core modules won't be able to
+              // actually import or export types, however, the type sort is
+              // allowed as part of outer aliases (below).
+              spdlog::error(
+                  ErrInfo::InfoAST(ASTNodeAttr::CompSec_CoreInstance));
+              spdlog::error(ErrInfo::InfoAST(ASTNodeAttr::Component));
+              return Unexpect(ErrCode::Value::JunkSection);
+            case AST::Sort::Module:
+            case AST::Sort::CoreInstance:
+              // NOTE:
+              // module and instance are added to core:sort in anticipation of
+              // the module-linking proposal, which would add these types to
+              // Core WebAssembly. Until then, they are useful for aliases
+              // (below).
+              spdlog::error(
+                  ErrInfo::InfoAST(ASTNodeAttr::CompSec_CoreInstance));
+              spdlog::error(ErrInfo::InfoAST(ASTNodeAttr::Component));
+              return Unexpect(ErrCode::Value::JunkSection);
+            default:
+              spdlog::error(
+                  ErrInfo::InfoAST(ASTNodeAttr::CompSec_CoreInstance));
+              spdlog::error(ErrInfo::InfoAST(ASTNodeAttr::Component));
+              return Unexpect(ErrCode::Value::Unreachable);
+            }
           }
           return {};
         } else {

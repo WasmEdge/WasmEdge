93c4ef9376c1879e41d797dc49c7a334cb277f24
Shen-Ta Hsieh <beststeve@secondstate.io>
2022-09-21 08:27:16 +0800

    [Test] Add fuzzing test tools
    Signed-off-by: Shen-Ta Hsieh <beststeve@secondstate.io>

---
+++ include/driver/fuzzPO.h
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2019-2022 Second State INC
+
+//===-- wasmedge/driver/fuzzPO.h - Fuzz PO entrypoint ---------------------===//
+//
+// Part of the WasmEdge Project.
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file contents the entrypoint for the fuzz PO executable.
+///
+//===----------------------------------------------------------------------===//
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+
+namespace WasmEdge {
+namespace Driver {
+
+int FuzzPO(const uint8_t *Data, size_t Size) noexcept;
+
+} // namespace Driver
+} // namespace WasmEdge
---
+++ include/driver/fuzzTool.h
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2019-2022 Second State INC
+
+//===-- wasmedge/driver/fuzzTool.h - Fuzz Tool entrypoint -----------------===//
+//
+// Part of the WasmEdge Project.
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file contents the entrypoint for the fuzz tool executable.
+///
+//===----------------------------------------------------------------------===//
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+
+namespace WasmEdge {
+namespace Driver {
+
+int FuzzTool(const uint8_t *Data, size_t Size) noexcept;
+
+} // namespace Driver
+} // namespace WasmEdge

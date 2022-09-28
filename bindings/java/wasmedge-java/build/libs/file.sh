ee547a3cfdc4182bbcd4f1f4a136a0f2722dcf1f
yyy1000 <992364620@qq.com>
2022-09-28 17:06:54 +0800

    remove redundant files
    Signed-off-by: yyy1000 <992364620@qq.com>

--- bindings/java/wasmedge-java/build/libs/file.sh
+++
@@ -1,7 +0,0 @@
-export CLASSPATH=$CLASSPATH:./wasmedge-java.jar
-cd ~/wasmedge/bindings/java/wasmedge-jni
-rm -r build
-mkdir build && cd build
-cmake ..
-make
-make install
--- bindings/java/wasmedge-java/build/libs/test.c
+++
@@ -1,6 +0,0 @@
-#include <wasmedge/wasmedge.h>
-#include <stdio.h>
-int main() {
-  printf("WasmEdge version: %s\n", WasmEdge_VersionGet());
-  return 0;
-}

4459f267ec9664043572ea20b46f599832a97233
Xin Liu <sam@secondstate.io>
2022-10-11 15:00:35 +0800

    refactor(rust-macro)!: Changed the argument type of the expand_host_func func
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- bindings/rust/wasmedge-macro/src/lib.rs
+++ bindings/rust/wasmedge-macro/src/lib.rs
@@ -33,7 +33,7 @@ fn expand_host_func(item_fn: &syn::ItemFn) -> syn::Result<proc_macro2::TokenStre
     let wrapper_fn_name_literal = wrapper_fn_name_ident.to_string();
     // arguments of wrapper function
     let wrapper_fn_inputs: syn::punctuated::Punctuated<FnArg, syn::token::Comma> = parse_quote!(
-        frame: &wasmedge_sys::CallingFrame,
+        frame: &CallingFrame,
         args: Vec<wasmedge_sys::WasmValue>,
         data: *mut std::os::raw::c_void
     );

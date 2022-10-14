590c0833bed8329d569d99714dd3708e3b36abcb
Shen-Ta Hsieh <beststeve@secondstate.io>
2022-10-14 10:48:25 +0800

    [Loader] Reject unreasonable long vector sizes
    Signed-off-by: Shen-Ta Hsieh <beststeve@secondstate.io>

--- lib/loader/ast/segment.cpp
+++ lib/loader/ast/segment.cpp
@@ -193,6 +193,10 @@ Expect<void> Loader::loadSegment(AST::ElementSegment &ElemSeg) {
                           ASTNodeAttr::Seg_Element);
     } else {
       VecCnt = *Res;
+      if (VecCnt / 2 > FMgr.getRemainSize()) {
+        return logLoadError(ErrCode::Value::IntegerTooLong,
+                            FMgr.getLastOffset(), ASTNodeAttr::Seg_Element);
+      }
     }
     ElemSeg.getInitExprs().clear();
     ElemSeg.getInitExprs().reserve(VecCnt);
@@ -229,6 +233,11 @@ Expect<void> Loader::loadSegment(AST::CodeSegment &CodeSeg) {
   uint32_t VecCnt = 0;
   if (auto Res = FMgr.readU32()) {
     VecCnt = *Res;
+    if (VecCnt / 2 > FMgr.getRemainSize()) {
+      return logLoadError(ErrCode::Value::IntegerTooLong, FMgr.getLastOffset(),
+                          ASTNodeAttr::Seg_Code);
+    }
+
     CodeSeg.getLocals().clear();
     CodeSeg.getLocals().reserve(VecCnt);
   } else {
@@ -343,6 +352,10 @@ Expect<void> Loader::loadSegment(AST::DataSegment &DataSeg) {
     uint32_t VecCnt = 0;
     if (auto Res = FMgr.readU32()) {
       VecCnt = *Res;
+      if (VecCnt / 2 > FMgr.getRemainSize()) {
+        return logLoadError(ErrCode::Value::IntegerTooLong,
+                            FMgr.getLastOffset(), ASTNodeAttr::Seg_Data);
+      }
     } else {
       return logLoadError(Res.error(), FMgr.getLastOffset(),
                           ASTNodeAttr::Seg_Data);

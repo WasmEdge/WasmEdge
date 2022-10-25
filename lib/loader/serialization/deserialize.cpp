3031f4d0f47471b0b6406a8891ba67333e2ec2c9
Omkar Acharekar <omkaracharekar12@gmail.com>
2022-10-25 10:38:35 +0530

    Data Count Section

---
+++ lib/loader/serialization/deserialize.cpp
@@ -0,0 +1,12 @@
+#include "ast/module.h"
+#include "loader/loader.h"
+
+namespace WasmEdge {
+
+namespace Serializer {
+
+std::vector<uint8_t> Serializer::deserialize(std::vector<uint8_t>) {}
+
+} // namespace Serializer
+
+} // namespace WasmEdge

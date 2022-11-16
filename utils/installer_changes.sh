b5c8c6b75e0c43c8c47a32af9814991a9ffb9040
Shreyas Atre <shreyasatre16@gmail.com>
2022-11-16 22:23:15 +0530

    [Utils] Installer Changes CI: Fix arguments expansion
    Signed-off-by: Shreyas Atre <shreyasatre16@gmail.com>

--- utils/installer_changes.sh
+++ utils/installer_changes.sh
@@ -8,10 +8,10 @@ NC=$'\e[0m' # No Color
 test_diff_env() {
     local _path_=$1
     local _common_args_=$2
-    echo "Testing path: $_path_"
-    bash ./utils/install.sh.old -p "$_path_" "$_common_args_"
+    echo "Testing path: $_path_ args: $_common_args_"
+    bash ./utils/install.sh.old -p "$_path_" $_common_args_
     cp "$_path_"/env "$HOME"/env.old
-    INSTALL_PY_URL="https://raw.githubusercontent.com/SAtacker/WasmEdge/fix_python_installer/utils/install.py" bash ./utils/install.sh -p "$_path_" "$_common_args_"
+    INSTALL_PY_URL="https://raw.githubusercontent.com/SAtacker/WasmEdge/fix_python_installer/utils/install.py" bash ./utils/install.sh -p "$_path_" $_common_args_
     cp "$_path_"/env "$HOME"/env
     diff -u \
         <(sed '1,/Please/d' "$HOME"/env.old | sed -e 's/\/\//\//g' |
@@ -42,7 +42,7 @@ test_diff_env "$HOME"/new_folder
 test_diff_env /usr/local
 test_diff_env /usr
 
-test_diff_env "$HOME"/.wasmedge -e all
-test_diff_env "$HOME"/new_folder -e all
-test_diff_env /usr/local -e all
-test_diff_env /usr -e all
+test_diff_env "$HOME"/.wasmedge "-e all"
+test_diff_env "$HOME"/new_folder "-e all"
+test_diff_env /usr/local "-e all"
+test_diff_env /usr "-e all"

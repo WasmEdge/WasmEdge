92049619526f3c631e3934da27ba6e69c6514e6e
Little Willy <641902082@qq.com>
2022-12-05 12:21:54 +0800

    remove the added C API function
    Signed-off-by: Little Willy <641902082@qq.com>

--- lib/api/wasmedge.cpp
+++ lib/api/wasmedge.cpp
@@ -1167,17 +1167,6 @@ WasmEdge_GlobalTypeGetValType(const WasmEdge_GlobalTypeContext *Cxt) {
   return WasmEdge_ValType_I32;
 }
 
-WASMEDGE_CAPI_EXPORT WasmEdge_FullValType
-WasmEdge_GlobalTypeGetFullValType(const WasmEdge_GlobalTypeContext *Cxt) {
-  if (Cxt) {
-    return fromGlobTypeCxt(Cxt)->getValType().asCStruct();
-  }
-  return WasmEdge_FullValType{
-      .TypeCode = WasmEdge_ValType_I32,
-      .Ext = WasmEdge_ValTypeExt(),
-  };
-}
-
 WASMEDGE_CAPI_EXPORT enum WasmEdge_Mutability
 WasmEdge_GlobalTypeGetMutability(const WasmEdge_GlobalTypeContext *Cxt) {
   if (Cxt) {

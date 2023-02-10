b84d8f8f72c61e5f99ed54312dc3d41b1da5b2f3
YiYing He <yiying@secondstate.io>
2023-02-10 13:58:57 +0800

    [Host] Mock the host modules of plug-ins.
    Signed-off-by: YiYing He <yiying@secondstate.io>

---
+++ include/host/mock/wasi_nn_module.h
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2019-2022 Second State INC
+
+#pragma once
+
+#include "host/mock/wasi_nn_func.h"
+#include "runtime/instance/module.h"
+
+namespace WasmEdge {
+namespace Host {
+
+class WasiNNModuleMock : public Runtime::Instance::ModuleInstance {
+public:
+  WasiNNModuleMock() : Runtime::Instance::ModuleInstance("wasi_ephemeral_nn") {
+    addHostFunc("load", std::make_unique<WasiNNMock::Load>());
+    addHostFunc("init_execution_context",
+                std::make_unique<WasiNNMock::InitExecCtx>());
+    addHostFunc("set_input", std::make_unique<WasiNNMock::SetInput>());
+    addHostFunc("get_output", std::make_unique<WasiNNMock::GetOuput>());
+    addHostFunc("compute", std::make_unique<WasiNNMock::Compute>());
+  }
+};
+
+} // namespace Host
+} // namespace WasmEdge
---
+++ include/host/mock/wasmedge_process_func.h
@@ -0,0 +1,108 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2019-2022 Second State INC
+
+#pragma once
+
+#include "common/errcode.h"
+#include "host/mock/log.h"
+#include "runtime/callingframe.h"
+#include "runtime/hostfunc.h"
+
+namespace WasmEdge {
+namespace Host {
+namespace WasmEdgeProcessMock {
+
+using namespace std::literals;
+
+class SetProgName : public Runtime::HostFunction<SetProgName> {
+public:
+  Expect<void> body(const Runtime::CallingFrame &, uint32_t, uint32_t) {
+    printPluginMock("WasmEdge-Process"sv);
+    return Unexpect(ErrCode::Value::HostFuncError);
+  }
+};
+
+class AddArg : public Runtime::HostFunction<AddArg> {
+public:
+  Expect<void> body(const Runtime::CallingFrame &, uint32_t, uint32_t) {
+    printPluginMock("WasmEdge-Process"sv);
+    return Unexpect(ErrCode::Value::HostFuncError);
+  }
+};
+
+class AddEnv : public Runtime::HostFunction<AddEnv> {
+public:
+  Expect<void> body(const Runtime::CallingFrame &, uint32_t, uint32_t, uint32_t,
+                    uint32_t) {
+    printPluginMock("WasmEdge-Process"sv);
+    return Unexpect(ErrCode::Value::HostFuncError);
+  }
+};
+
+class AddStdIn : public Runtime::HostFunction<AddStdIn> {
+public:
+  Expect<void> body(const Runtime::CallingFrame &, uint32_t, uint32_t) {
+    printPluginMock("WasmEdge-Process"sv);
+    return Unexpect(ErrCode::Value::HostFuncError);
+  }
+};
+
+class SetTimeOut : public Runtime::HostFunction<SetTimeOut> {
+public:
+  Expect<void> body(const Runtime::CallingFrame &, uint32_t) {
+    printPluginMock("WasmEdge-Process"sv);
+    return Unexpect(ErrCode::Value::HostFuncError);
+  }
+};
+
+class Run : public Runtime::HostFunction<Run> {
+public:
+  Expect<uint32_t> body(const Runtime::CallingFrame &) {
+    printPluginMock("WasmEdge-Process"sv);
+    return Unexpect(ErrCode::Value::HostFuncError);
+  }
+};
+
+class GetExitCode : public Runtime::HostFunction<GetExitCode> {
+public:
+  Expect<uint32_t> body(const Runtime::CallingFrame &) {
+    printPluginMock("WasmEdge-Process"sv);
+    return Unexpect(ErrCode::Value::HostFuncError);
+  }
+};
+
+class GetStdOutLen : public Runtime::HostFunction<GetStdOutLen> {
+public:
+  Expect<uint32_t> body(const Runtime::CallingFrame &) {
+    printPluginMock("WasmEdge-Process"sv);
+    return Unexpect(ErrCode::Value::HostFuncError);
+  }
+};
+
+class GetStdOut : public Runtime::HostFunction<GetStdOut> {
+public:
+  Expect<void> body(const Runtime::CallingFrame &, uint32_t) {
+    printPluginMock("WasmEdge-Process"sv);
+    return Unexpect(ErrCode::Value::HostFuncError);
+  }
+};
+
+class GetStdErrLen : public Runtime::HostFunction<GetStdErrLen> {
+public:
+  Expect<uint32_t> body(const Runtime::CallingFrame &) {
+    printPluginMock("WasmEdge-Process"sv);
+    return Unexpect(ErrCode::Value::HostFuncError);
+  }
+};
+
+class GetStdErr : public Runtime::HostFunction<GetStdErr> {
+public:
+  Expect<void> body(const Runtime::CallingFrame &, uint32_t) {
+    printPluginMock("WasmEdge-Process"sv);
+    return Unexpect(ErrCode::Value::HostFuncError);
+  }
+};
+
+} // namespace WasmEdgeProcessMock
+} // namespace Host
+} // namespace WasmEdge

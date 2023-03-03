1d072737641153785c7add369bb9e38310a2e420
xxchan <xxchan22f@gmail.com>
2023-03-03 10:57:16 +0100

    [Docs] fix: wasmedge-rs -> wasmedge-sdk (#2332)
    Signed-off-by: xxchan <xxchan22f@gmail.com>

--- bindings/rust/wasmedge-types/src/lib.rs
+++ bindings/rust/wasmedge-types/src/lib.rs
@@ -1,4 +1,4 @@
-//! The [wasmedge-types](https://crates.io/crates/wasmedge-types) crate defines a group of common data structures used by both [wasmedge-rs](https://crates.io/crates/wasmedge-sdk) and [wasmedge-sys](https://crates.io/crates/wasmedge-sys) crates.
+//! The [wasmedge-types](https://crates.io/crates/wasmedge-types) crate defines a group of common data structures used by both [wasmedge-sdk](https://crates.io/crates/wasmedge-sdk) and [wasmedge-sys](https://crates.io/crates/wasmedge-sys) crates.
 //!
 //! See also
 //!

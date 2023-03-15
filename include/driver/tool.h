5787459812efb09625c93675f4f14f2ede75bfc6
michael1017 <michael881017@gmail.com>
2023-03-15 06:27:48 +0000

    create runtime tool options struct
    Signed-off-by: michael1017 <michael881017@gmail.com>

--- include/driver/tool.h
+++ include/driver/tool.h
@@ -12,9 +12,126 @@
 ///
 //===----------------------------------------------------------------------===//
 #pragma once
+#include "plugin/plugin.h"
+#include "po/argument_parser.h"
+#include <string_view>
 
 namespace WasmEdge {
 namespace Driver {
+
+struct DriverToolOptions {
+  DriverToolOptions()
+    : SoName(PO::Description("Wasm or so file"sv),PO::MetaVar("WASM_OR_SO"sv)),
+      Args(PO::Description("Execution arguments"sv), PO::MetaVar("ARG"sv)),
+      Reactor(PO::Description("Enable reactor mode. Reactor mode calls `_initialize` if exported."sv)),
+      Dir(
+        PO::Description(
+            "Binding directories into WASI virtual filesystem. Each directory "
+            "can be specified as --dir `host_path`. You can also map a guest "
+            "directory to a host directory by --dir `guest_path:host_path`, "
+            "where `guest_path` specifies the path that will correspond to "
+            "`host_path` for calls like `fopen` in the guest."sv),
+        PO::MetaVar("PREOPEN_DIRS"sv)),
+      Env(
+        PO::Description(
+            "Environ variables. Each variable can be specified as --env `NAME=VALUE`."sv),
+        PO::MetaVar("ENVS"sv)),
+      PropMutGlobals(PO::Description("Disable Import/Export of mutable globals proposal"sv)),
+      PropNonTrapF2IConvs(PO::Description("Disable Non-trapping float-to-int conversions proposal"sv)),
+      PropSignExtendOps(PO::Description("Disable Sign-extension operators proposal"sv)),
+      PropMultiValue(PO::Description("Disable Multi-value proposal"sv)),
+      PropBulkMemOps(PO::Description("Disable Bulk memory operations proposal"sv)),
+      PropRefTypes(PO::Description("Disable Reference types proposal"sv)),
+      PropSIMD(PO::Description("Disable SIMD proposal"sv)),
+      PropMultiMem(PO::Description("Enable Multiple memories proposal"sv)),
+      PropTailCall(PO::Description("Enable Tail-call proposal"sv)),
+      PropExtendConst(PO::Description("Enable Extended-const proposal"sv)),
+      PropThreads(PO::Description("Enable Threads proposal"sv)),
+      PropAll(PO::Description("Enable all features"sv)),
+      ConfEnableInstructionCounting(PO::Description("Enable generating code for counting Wasm instructions executed."sv)),
+      ConfEnableGasMeasuring(PO::Description("Enable generating code for counting gas burned during execution."sv)),
+      ConfEnableTimeMeasuring(PO::Description("Enable generating code for counting time during execution."sv)),
+      ConfEnableAllStatistics(PO::Description(
+        "Enable generating code for all statistics options include instruction counting, gas measuring, and execution time"sv)),
+      ConfForceInterpreter(PO::Description("Forcibly run WASM in interpreter mode."sv)),
+      TimeLim(
+        PO::Description(
+            "Limitation of maximum time(in milliseconds) for execution, default value is 0 for no limitations"sv),
+        PO::MetaVar("TIMEOUT"sv), PO::DefaultValue<uint64_t>(0)),
+      GasLim(
+        PO::Description(
+            "Limitation of execution gas. Upper bound can be specified as --gas-limit `GAS_LIMIT`."sv),
+        PO::MetaVar("GAS_LIMIT"sv)),
+      MemLim(
+        PO::Description(
+            "Limitation of pages(as size of 64 KiB) in every memory instance. Upper bound can be specified as --memory-page-limit `PAGE_COUNT`."sv),
+        PO::MetaVar("PAGE_COUNT"sv)),
+      ForbiddenPlugins(PO::Description("List of plugins to ignore."sv), PO::MetaVar("NAMES"sv))
+    {}
+
+  PO::Option<std::string> SoName;
+  PO::List<std::string> Args;
+  PO::Option<PO::Toggle> Reactor;
+  PO::List<std::string> Dir;
+  PO::List<std::string> Env;
+  PO::Option<PO::Toggle> PropMutGlobals;
+  PO::Option<PO::Toggle> PropNonTrapF2IConvs;
+  PO::Option<PO::Toggle> PropSignExtendOps;
+  PO::Option<PO::Toggle> PropMultiValue;
+  PO::Option<PO::Toggle> PropBulkMemOps;
+  PO::Option<PO::Toggle> PropRefTypes;
+  PO::Option<PO::Toggle> PropSIMD;
+  PO::Option<PO::Toggle> PropMultiMem;
+  PO::Option<PO::Toggle> PropTailCall;
+  PO::Option<PO::Toggle> PropExtendConst;
+  PO::Option<PO::Toggle> PropThreads;
+  PO::Option<PO::Toggle> PropAll;
+  PO::Option<PO::Toggle> ConfEnableInstructionCounting;
+  PO::Option<PO::Toggle> ConfEnableGasMeasuring;
+  PO::Option<PO::Toggle> ConfEnableTimeMeasuring;
+  PO::Option<PO::Toggle> ConfEnableAllStatistics;
+  PO::Option<PO::Toggle> ConfForceInterpreter;
+  PO::Option<uint64_t> TimeLim;
+  PO::List<int> GasLim;
+  PO::List<int> MemLim;
+  PO::List<std::string> ForbiddenPlugins;
+
+  void add_option(PO::ArgumentParser &Parser) noexcept {
+
+    Parser.add_option(SoName)
+      .add_option(Args)
+      .add_option("reactor"sv, Reactor)
+      .add_option("dir"sv, Dir)
+      .add_option("env"sv, Env)
+      .add_option("enable-instruction-count"sv, ConfEnableInstructionCounting)
+      .add_option("enable-gas-measuring"sv, ConfEnableGasMeasuring)
+      .add_option("enable-time-measuring"sv, ConfEnableTimeMeasuring)
+      .add_option("enable-all-statistics"sv, ConfEnableAllStatistics)
+      .add_option("force-interpreter"sv, ConfForceInterpreter)
+      .add_option("disable-import-export-mut-globals"sv, PropMutGlobals)
+      .add_option("disable-non-trap-float-to-int"sv, PropNonTrapF2IConvs)
+      .add_option("disable-sign-extension-operators"sv, PropSignExtendOps)
+      .add_option("disable-multi-value"sv, PropMultiValue)
+      .add_option("disable-bulk-memory"sv, PropBulkMemOps)
+      .add_option("disable-reference-types"sv, PropRefTypes)
+      .add_option("disable-simd"sv, PropSIMD)
+      .add_option("enable-multi-memory"sv, PropMultiMem)
+      .add_option("enable-tail-call"sv, PropTailCall)
+      .add_option("enable-extended-const"sv, PropExtendConst)
+      .add_option("enable-threads"sv, PropThreads)
+      .add_option("enable-all"sv, PropAll)
+      .add_option("time-limit"sv, TimeLim)
+      .add_option("gas-limit"sv, GasLim)
+      .add_option("memory-page-limit"sv, MemLim)
+      .add_option("forbidden-plugin"sv, ForbiddenPlugins);
+    
+    for (const auto &Path : Plugin::Plugin::getDefaultPluginPaths()) {
+      Plugin::Plugin::load(Path);
+    }
+    Plugin::Plugin::addPluginOptions(Parser);
+
+  }
+};
 
 int Tool(int Argc, const char *Argv[]) noexcept;
 
--- lib/driver/runtimeTool.cpp
+++ lib/driver/runtimeTool.cpp
@@ -8,8 +8,6 @@
 #include "common/version.h"
 #include "driver/tool.h"
 #include "host/wasi/wasimodule.h"
-#include "plugin/plugin.h"
-#include "po/argument_parser.h"
 #include "vm/vm.h"
 
 #include <chrono>
@@ -30,113 +28,12 @@ int Tool(int Argc, const char *Argv[]) noexcept {
   std::ios::sync_with_stdio(false);
   Log::setInfoLoggingLevel();
 
-  for (const auto &Path : Plugin::Plugin::getDefaultPluginPaths()) {
-    Plugin::Plugin::load(Path);
-  }
-
-  PO::Option<std::string> SoName(PO::Description("Wasm or so file"sv),
-                                 PO::MetaVar("WASM_OR_SO"sv));
-  PO::List<std::string> Args(PO::Description("Execution arguments"sv),
-                             PO::MetaVar("ARG"sv));
-
-  PO::Option<PO::Toggle> Reactor(PO::Description(
-      "Enable reactor mode. Reactor mode calls `_initialize` if exported."sv));
-
-  PO::List<std::string> Dir(
-      PO::Description(
-          "Binding directories into WASI virtual filesystem. Each directory "
-          "can be specified as --dir `host_path`. You can also map a guest "
-          "directory to a host directory by --dir `guest_path:host_path`, "
-          "where `guest_path` specifies the path that will correspond to "
-          "`host_path` for calls like `fopen` in the guest."sv),
-      PO::MetaVar("PREOPEN_DIRS"sv));
-
-  PO::List<std::string> Env(
-      PO::Description(
-          "Environ variables. Each variable can be specified as --env `NAME=VALUE`."sv),
-      PO::MetaVar("ENVS"sv));
-
-  PO::Option<PO::Toggle> PropMutGlobals(
-      PO::Description("Disable Import/Export of mutable globals proposal"sv));
-  PO::Option<PO::Toggle> PropNonTrapF2IConvs(PO::Description(
-      "Disable Non-trapping float-to-int conversions proposal"sv));
-  PO::Option<PO::Toggle> PropSignExtendOps(
-      PO::Description("Disable Sign-extension operators proposal"sv));
-  PO::Option<PO::Toggle> PropMultiValue(
-      PO::Description("Disable Multi-value proposal"sv));
-  PO::Option<PO::Toggle> PropBulkMemOps(
-      PO::Description("Disable Bulk memory operations proposal"sv));
-  PO::Option<PO::Toggle> PropRefTypes(
-      PO::Description("Disable Reference types proposal"sv));
-  PO::Option<PO::Toggle> PropSIMD(PO::Description("Disable SIMD proposal"sv));
-  PO::Option<PO::Toggle> PropMultiMem(
-      PO::Description("Enable Multiple memories proposal"sv));
-  PO::Option<PO::Toggle> PropTailCall(
-      PO::Description("Enable Tail-call proposal"sv));
-  PO::Option<PO::Toggle> PropExtendConst(
-      PO::Description("Enable Extended-const proposal"sv));
-  PO::Option<PO::Toggle> PropThreads(
-      PO::Description("Enable Threads proposal"sv));
-  PO::Option<PO::Toggle> PropAll(PO::Description("Enable all features"sv));
-
-  PO::Option<PO::Toggle> ConfEnableInstructionCounting(PO::Description(
-      "Enable generating code for counting Wasm instructions executed."sv));
-  PO::Option<PO::Toggle> ConfEnableGasMeasuring(PO::Description(
-      "Enable generating code for counting gas burned during execution."sv));
-  PO::Option<PO::Toggle> ConfEnableTimeMeasuring(PO::Description(
-      "Enable generating code for counting time during execution."sv));
-  PO::Option<PO::Toggle> ConfEnableAllStatistics(PO::Description(
-      "Enable generating code for all statistics options include instruction counting, gas measuring, and execution time"sv));
-  PO::Option<PO::Toggle> ConfForceInterpreter(
-      PO::Description("Forcibly run WASM in interpreter mode."sv));
-
-  PO::Option<uint64_t> TimeLim(
-      PO::Description(
-          "Limitation of maximum time(in milliseconds) for execution, default value is 0 for no limitations"sv),
-      PO::MetaVar("TIMEOUT"sv), PO::DefaultValue<uint64_t>(0));
-
-  PO::List<int> GasLim(
-      PO::Description(
-          "Limitation of execution gas. Upper bound can be specified as --gas-limit `GAS_LIMIT`."sv),
-      PO::MetaVar("GAS_LIMIT"sv));
-
-  PO::List<int> MemLim(
-      PO::Description(
-          "Limitation of pages(as size of 64 KiB) in every memory instance. Upper bound can be specified as --memory-page-limit `PAGE_COUNT`."sv),
-      PO::MetaVar("PAGE_COUNT"sv));
-
-  PO::List<std::string> ForbiddenPlugins(
-      PO::Description("List of plugins to ignore."sv), PO::MetaVar("NAMES"sv));
-
+  
   auto Parser = PO::ArgumentParser();
-  Parser.add_option(SoName)
-      .add_option(Args)
-      .add_option("reactor"sv, Reactor)
-      .add_option("dir"sv, Dir)
-      .add_option("env"sv, Env)
-      .add_option("enable-instruction-count"sv, ConfEnableInstructionCounting)
-      .add_option("enable-gas-measuring"sv, ConfEnableGasMeasuring)
-      .add_option("enable-time-measuring"sv, ConfEnableTimeMeasuring)
-      .add_option("enable-all-statistics"sv, ConfEnableAllStatistics)
-      .add_option("force-interpreter"sv, ConfForceInterpreter)
-      .add_option("disable-import-export-mut-globals"sv, PropMutGlobals)
-      .add_option("disable-non-trap-float-to-int"sv, PropNonTrapF2IConvs)
-      .add_option("disable-sign-extension-operators"sv, PropSignExtendOps)
-      .add_option("disable-multi-value"sv, PropMultiValue)
-      .add_option("disable-bulk-memory"sv, PropBulkMemOps)
-      .add_option("disable-reference-types"sv, PropRefTypes)
-      .add_option("disable-simd"sv, PropSIMD)
-      .add_option("enable-multi-memory"sv, PropMultiMem)
-      .add_option("enable-tail-call"sv, PropTailCall)
-      .add_option("enable-extended-const"sv, PropExtendConst)
-      .add_option("enable-threads"sv, PropThreads)
-      .add_option("enable-all"sv, PropAll)
-      .add_option("time-limit"sv, TimeLim)
-      .add_option("gas-limit"sv, GasLim)
-      .add_option("memory-page-limit"sv, MemLim)
-      .add_option("forbidden-plugin"sv, ForbiddenPlugins);
+  struct DriverToolOptions opt;
+  
+  opt.add_option(Parser);
 
-  Plugin::Plugin::addPluginOptions(Parser);
 
   if (!Parser.parse(stdout, Argc, Argv)) {
     return EXIT_FAILURE;
@@ -147,40 +44,40 @@ int Tool(int Argc, const char *Argv[]) noexcept {
   }
 
   Configure Conf;
-  if (PropMutGlobals.value()) {
+  if (opt.PropMutGlobals.value()) {
     Conf.removeProposal(Proposal::ImportExportMutGlobals);
   }
-  if (PropNonTrapF2IConvs.value()) {
+  if (opt.PropNonTrapF2IConvs.value()) {
     Conf.removeProposal(Proposal::NonTrapFloatToIntConversions);
   }
-  if (PropSignExtendOps.value()) {
+  if (opt.PropSignExtendOps.value()) {
     Conf.removeProposal(Proposal::SignExtensionOperators);
   }
-  if (PropMultiValue.value()) {
+  if (opt.PropMultiValue.value()) {
     Conf.removeProposal(Proposal::MultiValue);
   }
-  if (PropBulkMemOps.value()) {
+  if (opt.PropBulkMemOps.value()) {
     Conf.removeProposal(Proposal::BulkMemoryOperations);
   }
-  if (PropRefTypes.value()) {
+  if (opt.PropRefTypes.value()) {
     Conf.removeProposal(Proposal::ReferenceTypes);
   }
-  if (PropSIMD.value()) {
+  if (opt.PropSIMD.value()) {
     Conf.removeProposal(Proposal::SIMD);
   }
-  if (PropMultiMem.value()) {
+  if (opt.PropMultiMem.value()) {
     Conf.addProposal(Proposal::MultiMemories);
   }
-  if (PropTailCall.value()) {
+  if (opt.PropTailCall.value()) {
     Conf.addProposal(Proposal::TailCall);
   }
-  if (PropExtendConst.value()) {
+  if (opt.PropExtendConst.value()) {
     Conf.addProposal(Proposal::ExtendedConst);
   }
-  if (PropThreads.value()) {
+  if (opt.PropThreads.value()) {
     Conf.addProposal(Proposal::Threads);
   }
-  if (PropAll.value()) {
+  if (opt.PropAll.value()) {
     Conf.addProposal(Proposal::MultiMemories);
     Conf.addProposal(Proposal::TailCall);
     Conf.addProposal(Proposal::ExtendedConst);
@@ -188,44 +85,44 @@ int Tool(int Argc, const char *Argv[]) noexcept {
   }
 
   std::optional<std::chrono::system_clock::time_point> Timeout;
-  if (TimeLim.value() > 0) {
+  if (opt.TimeLim.value() > 0) {
     Timeout = std::chrono::system_clock::now() +
-              std::chrono::milliseconds(TimeLim.value());
+              std::chrono::milliseconds(opt.TimeLim.value());
   }
-  if (GasLim.value().size() > 0) {
+  if (opt.GasLim.value().size() > 0) {
     Conf.getStatisticsConfigure().setCostMeasuring(true);
     Conf.getStatisticsConfigure().setCostLimit(
-        static_cast<uint32_t>(GasLim.value().back()));
+        static_cast<uint32_t>(opt.GasLim.value().back()));
   }
-  if (MemLim.value().size() > 0) {
+  if (opt.MemLim.value().size() > 0) {
     Conf.getRuntimeConfigure().setMaxMemoryPage(
-        static_cast<uint32_t>(MemLim.value().back()));
+        static_cast<uint32_t>(opt.MemLim.value().back()));
   }
-  if (ConfEnableAllStatistics.value()) {
+  if (opt.ConfEnableAllStatistics.value()) {
     Conf.getStatisticsConfigure().setInstructionCounting(true);
     Conf.getStatisticsConfigure().setCostMeasuring(true);
     Conf.getStatisticsConfigure().setTimeMeasuring(true);
   } else {
-    if (ConfEnableInstructionCounting.value()) {
+    if (opt.ConfEnableInstructionCounting.value()) {
       Conf.getStatisticsConfigure().setInstructionCounting(true);
     }
-    if (ConfEnableGasMeasuring.value()) {
+    if (opt.ConfEnableGasMeasuring.value()) {
       Conf.getStatisticsConfigure().setCostMeasuring(true);
     }
-    if (ConfEnableTimeMeasuring.value()) {
+    if (opt.ConfEnableTimeMeasuring.value()) {
       Conf.getStatisticsConfigure().setTimeMeasuring(true);
     }
   }
-  if (ConfForceInterpreter.value()) {
+  if (opt.ConfForceInterpreter.value()) {
     Conf.getRuntimeConfigure().setForceInterpreter(true);
   }
 
-  for (const auto &Name : ForbiddenPlugins.value()) {
+  for (const auto &Name : opt.ForbiddenPlugins.value()) {
     Conf.addForbiddenPlugins(Name);
   }
 
   Conf.addHostRegistration(HostRegistration::Wasi);
-  const auto InputPath = std::filesystem::absolute(SoName.value());
+  const auto InputPath = std::filesystem::absolute(opt.SoName.value());
   VM::VM VM(Conf);
 
   Host::WasiModule *WasiMod = dynamic_cast<Host::WasiModule *>(
@@ -259,20 +156,20 @@ int Tool(int Argc, const char *Argv[]) noexcept {
 
     // if HasStart but not Valid, insert _start to enter reactor mode
     if (HasStart && !Valid) {
-      Args.value().insert(Args.value().begin(), "_start");
+      opt.Args.value().insert(opt.Args.value().begin(), "_start");
     }
 
     return HasStart && Valid;
   };
 
-  bool EnterCommandMode = !Reactor.value() && HasValidCommandModStartFunc();
+  bool EnterCommandMode = !opt.Reactor.value() && HasValidCommandModStartFunc();
 
   WasiMod->getEnv().init(
-      Dir.value(),
+      opt.Dir.value(),
       InputPath.filename()
           .replace_extension(std::filesystem::u8path("wasm"sv))
           .u8string(),
-      Args.value(), Env.value());
+      opt.Args.value(), opt.Env.value());
 
   if (EnterCommandMode) {
     // command mode
@@ -291,12 +188,12 @@ int Tool(int Argc, const char *Argv[]) noexcept {
     }
   } else {
     // reactor mode
-    if (Args.value().empty()) {
+    if (opt.Args.value().empty()) {
       std::cerr
           << "A function name is required when reactor mode is enabled.\n";
       return EXIT_FAILURE;
     }
-    const auto &FuncName = Args.value().front();
+    const auto &FuncName = opt.Args.value().front();
 
     using namespace std::literals::string_literals;
     const auto InitFunc = "_initialize"s;
@@ -328,31 +225,31 @@ int Tool(int Argc, const char *Argv[]) noexcept {
     std::vector<ValVariant> FuncArgs;
     std::vector<ValType> FuncArgTypes;
     for (size_t I = 0;
-         I < FuncType.getParamTypes().size() && I + 1 < Args.value().size();
+         I < FuncType.getParamTypes().size() && I + 1 < opt.Args.value().size();
          ++I) {
       switch (FuncType.getParamTypes()[I]) {
       case ValType::I32: {
         const uint32_t Value =
-            static_cast<uint32_t>(std::stol(Args.value()[I + 1]));
+            static_cast<uint32_t>(std::stol(opt.Args.value()[I + 1]));
         FuncArgs.emplace_back(Value);
         FuncArgTypes.emplace_back(ValType::I32);
         break;
       }
       case ValType::I64: {
         const uint64_t Value =
-            static_cast<uint64_t>(std::stoll(Args.value()[I + 1]));
+            static_cast<uint64_t>(std::stoll(opt.Args.value()[I + 1]));
         FuncArgs.emplace_back(Value);
         FuncArgTypes.emplace_back(ValType::I64);
         break;
       }
       case ValType::F32: {
-        const float Value = std::stof(Args.value()[I + 1]);
+        const float Value = std::stof(opt.Args.value()[I + 1]);
         FuncArgs.emplace_back(Value);
         FuncArgTypes.emplace_back(ValType::F32);
         break;
       }
       case ValType::F64: {
-        const double Value = std::stod(Args.value()[I + 1]);
+        const double Value = std::stod(opt.Args.value()[I + 1]);
         FuncArgs.emplace_back(Value);
         FuncArgTypes.emplace_back(ValType::F64);
         break;
@@ -362,11 +259,11 @@ int Tool(int Argc, const char *Argv[]) noexcept {
         break;
       }
     }
-    if (FuncType.getParamTypes().size() + 1 < Args.value().size()) {
+    if (FuncType.getParamTypes().size() + 1 < opt.Args.value().size()) {
       for (size_t I = FuncType.getParamTypes().size() + 1;
-           I < Args.value().size(); ++I) {
+           I < opt.Args.value().size(); ++I) {
         const uint64_t Value =
-            static_cast<uint64_t>(std::stoll(Args.value()[I]));
+            static_cast<uint64_t>(std::stoll(opt.Args.value()[I]));
         FuncArgs.emplace_back(Value);
         FuncArgTypes.emplace_back(ValType::I64);
       }

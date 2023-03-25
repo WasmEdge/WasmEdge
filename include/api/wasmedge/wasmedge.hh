78b46789fec1b0bce2ff75bd18972289e3fca9b0
Subhradeep <chakrabortysubhradeep556@gmail.com>
2023-03-25 20:32:21 +0530

    Add ASTModule, FunctionType, ValType, ImportType and ExportType
    Signed-off-by: Subhradeep <chakrabortysubhradeep556@gmail.com>

--- include/api/wasmedge/wasmedge.hh
+++ include/api/wasmedge/wasmedge.hh
@@ -34,6 +34,7 @@
 
 #include "string"
 #include "vector"
+#include "memory"
 
 namespace WasmEdge {
   enum WASMEDGE_CPP_API_EXPORT ErrCategory {
@@ -94,6 +95,17 @@ namespace WasmEdge {
     Level_Oz
   };
 
+  enum WASMEDGE_CPP_API_EXPORT ValType {
+    None = 0x40,
+    I32 = 0x7F,
+    I64 = 0x7E,
+    F32 = 0x7D,
+    F64 = 0x7C,
+    V128 = 0x7B,
+    FuncRef = 0x70,
+    ExternRef = 0x6F
+  };
+
   // TODO: should Version get its own namespace?
   WASMEDGE_CPP_API_EXPORT std::string VersionGet();
   WASMEDGE_CPP_API_EXPORT uint32_t VersionGetMajor();
@@ -104,6 +116,11 @@ namespace WasmEdge {
   WASMEDGE_CPP_API_EXPORT void LogSetErrorLevel();
   WASMEDGE_CPP_API_EXPORT void LogSetDebugLevel();
   WASMEDGE_CPP_API_EXPORT void LogOff();
+
+  class WASMEDGE_CPP_API_EXPORT Value {
+  public:
+    // TODO
+  };
 
   class WASMEDGE_CPP_API_EXPORT String {
   public:
@@ -140,6 +157,8 @@ namespace WasmEdge {
     Result(const uint32_t Code);
   };
 
+  // >>>>>>>> WasmEdge Data Structures >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
   class WASMEDGE_CPP_API_EXPORT Limit {
   public:
     Limit(bool HasMax, bool Shared, uint32_t Min, uint32_t Max)
@@ -156,6 +175,33 @@ namespace WasmEdge {
 
     bool IsEqual(const Limit Lim);
   };
+
+  class WASMEDGE_CPP_API_EXPORT FunctionType {
+  public:
+    FunctionType(const std::vector<ValType> &ParamList,
+                const uint32_t ParamLen,
+                const std::vector<ValType> &ReturnList,
+                const uint32_t ReturnLen);
+    ~FunctionType();
+
+    uint32_t GetParametersLength();
+    std::vector<ValType> GetParameters();
+
+    uint32_t GetReturnsLength();
+    std::vector<ValType> GetReturns();
+  private:
+    // TODO
+  };
+
+  class WASMEDGE_CPP_API_EXPORT ImportType {
+    // TODO
+  };
+
+  class WASMEDGE_CPP_API_EXPORT ExportType {
+    // TODO
+  };
+
+  // <<<<<<<< WasmEdge Data Structures <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
   class WASMEDGE_CPP_API_EXPORT ConfigureContext {
   public:
@@ -227,7 +273,7 @@ namespace WasmEdge {
     double GetInstrPerSecond();
     uint64_t GetTotalCost();
 
-    void SetCostTable(uint64_t *CostArr, const uint32_t Len);
+    void SetCostTable(std::vector<uint64_t> CostArr, const uint32_t Len);
     void SetCostLimit(const uint64_t Limit);
     void Clear();
 
@@ -235,38 +281,38 @@ namespace WasmEdge {
     // TODO
   };
 
-  // >>>>>>>> WasmEdge Loader class >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  // >>>>>>>> WasmEdge Loader class >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
   class WASMEDGE_CPP_API_EXPORT Loader {
   public:
-    Loader();
+    Loader(const ConfigureContext &ConfCxt);
     ~Loader();
 
-    Result ParseFromFIle(ASTModuleContext **Module, const std::string Path);
-    Result ParseFromBuffer(ASTModuleContext **Module,
-                          const uint8_t *Buf, const uint32_t BufLen);
+    Result ParseFromFile(std::unique_ptr<ASTModule> Module, const std::string Path);
+    Result ParseFromBuffer(std::unique_ptr<ASTModule> Module,
+                          const std::vector<uint8_t> Buf, const uint32_t BufLen);
 
   private:
     // LoaderContext LoaderCxt; // TODO
   };
 
-  // <<<<<<<< WasmEdge Loader CLass <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+  // <<<<<<<< WasmEdge Loader CLass <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
-  // >>>>>>>> WasmEdge Validator class >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  // >>>>>>>> WasmEdge Validator class >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
   class WASMEDGE_CPP_API_EXPORT Validator {
   public:
     Validator(ConfigureContext &ConfCxt);
     ~Validator() = default;
 
-    Result Validate(const ASTModuleContext &ASTCxt);
+    Result Validate(const ASTModule &ASTCxt);
   private:
     // ValidatorContext ValidatorCxt; // TODO
   };
 
-  // <<<<<<<< WasmEdge Validator Class <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+  // <<<<<<<< WasmEdge Validator Class <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
-  // >>>>>>>> WasmEdge Executor class >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  // >>>>>>>> WasmEdge Executor class >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
   class WASMEDGE_CPP_API_EXPORT Executor {
   public:
@@ -274,19 +320,19 @@ namespace WasmEdge {
               StatisticsContext & StatCxt);
     ~Executor() = default;
 
-    Result Instantiate(ModuleInstance **ModuleCxt,
-                        Store &StoreCxt,
-                        const ASTModuleContext & ASTCxt);
+    Result Instantiate(std::unique_ptr<ModuleInstance> ModuleCxt,
+                      Store &StoreCxt,
+                      const ASTModule &ASTCxt);
 
-    Result Register(ModuleInstance **ModuleCxt,
+    Result Register(std::unique_ptr<ModuleInstance> ModuleCxt,
                     Store &StoreCxt,
-                    const ASTModuleContext & ASTCxt,
+                    const ASTModule &ASTCxt,
                     String ModuleName);
 
     Result RegisterImport(Store &StoreCxt,
                           const ModuleInstance &ImportCxt);
 
-    Result Invoke(const FunctionInstanceContext &FuncCxt,
+    Result Invoke(const FunctionInstance &FuncCxt,
                   const std::vector<String> &Params, const uint32_t ParamLen,
                   std::vector<Value> &Returns, const uint32_t ReturnLen);
 
@@ -294,7 +340,7 @@ namespace WasmEdge {
     // ExecutorContext ExecutorCxt; // TODO
   };
 
-  // <<<<<<<< WasmEdge Executor Class <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+  // <<<<<<<< WasmEdge Executor Class <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
   class WASMEDGE_CPP_API_EXPORT Store {
   public:
@@ -309,19 +355,46 @@ namespace WasmEdge {
     // StoreContext StoreCxt; // TODO
   };
 
+  // >>>>>>>> WasmEdge Instances >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
   class WASMEDGE_CPP_API_EXPORT ModuleInstance {
   public:
     ModuleInstance(const String ModuleName);
     ModuleInstance(const std::vector<const std::string> Args,
-                    const uint32_t ArgLen,
-                    const std::vector<const std::string> Envs,
-                    const uint32_t EnvLen,
-                    const std::vector<const std::string> Preopens,
-                    const uint32_t PreopenLen);
+                  const uint32_t ArgLen,
+                  const std::vector<const std::string> Envs,
+                  const uint32_t EnvLen,
+                  const std::vector<const std::string> Preopens,
+                  const uint32_t PreopenLen);
     ~ModuleInstance();
 
     // TODO
   };
+
+  class WASMEDGE_CPP_API_EXPORT FunctionInstance {
+  public:
+    // TODO
+  };
+
+
+
+  // <<<<<<<< WasmEdge Instances <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+  class WASMEDGE_CPP_API_EXPORT ASTModule {
+  public:
+    ASTModule();
+    ~ASTModule();
+
+    uint32_t ListImportsLength();
+    uint32_t ListImports(const std::vector<ImportType> &Imports,
+                        const uint32_t Len);
+
+    uint32_t ListExportsLength();
+    uint32_t ListExports(const std::vector<ExportType> &Exports,
+                        const uint32_t Len);
+  private:
+    // TODO
+  };  
 }
 
 #endif // WASMEDGE_CPP_API_HH

372faaaa808d0c45bb943cff57d3fc78a7b3926a
Xin Liu <sam@secondstate.io>
2023-03-27 16:37:03 +0800

    feat(rust-sys): derive Clone trait for Executor and Store
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- bindings/rust/wasmedge-sys/src/executor.rs
+++ bindings/rust/wasmedge-sys/src/executor.rs
@@ -8,11 +8,12 @@ use crate::{
     Config, Engine, FuncRef, Function, ImportObject, Instance, Module, Statistics, Store,
     WasmEdgeResult, WasmValue,
 };
+use std::sync::Arc;
 
 /// Defines an execution environment for both pure WASM and compiled WASM.
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Executor {
-    pub(crate) inner: InnerExecutor,
+    pub(crate) inner: Arc<InnerExecutor>,
     pub(crate) registered: bool,
 }
 impl Executor {
@@ -48,7 +49,7 @@ impl Executor {
         match ctx.is_null() {
             true => Err(Box::new(WasmEdgeError::ExecutorCreate)),
             false => Ok(Executor {
-                inner: InnerExecutor(ctx),
+                inner: Arc::new(InnerExecutor(ctx)),
                 registered: false,
             }),
         }
@@ -320,7 +321,7 @@ impl Executor {
 }
 impl Drop for Executor {
     fn drop(&mut self) {
-        if !self.registered && !self.inner.0.is_null() {
+        if !self.registered && Arc::strong_count(&self.inner) == 1 && !self.inner.0.is_null() {
             unsafe { ffi::WasmEdge_ExecutorDelete(self.inner.0) }
         }
     }

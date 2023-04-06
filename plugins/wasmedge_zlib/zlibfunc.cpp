ea8c47edd15be7e7693d67065d38f84b4d27c9b4
fathomless <digonto.pvt.2021@gmail.com>
2023-04-06 21:33:07 +0530

    Added deflateInit_ & inflateInit_

--- plugins/wasmedge_zlib/zlibfunc.cpp
+++ plugins/wasmedge_zlib/zlibfunc.cpp
@@ -22,7 +22,7 @@ struct Wasm_z_stream {
   uint32_t zfree;
   uint32_t opaque;
 
-  int32_t data_type;
+  int32_t data_type; // +ve & 2's complement in memory so int ~ uint
 
   uint32_t adler;
   uint32_t reserved;
@@ -35,63 +35,87 @@ Expect<int32_t>
 WasmEdgeZlibDeflateInit_::body(const Runtime::CallingFrame &Frame,
                                uint32_t ZStreamPtr, int32_t Level,
                                uint32_t VersionPtr, uint32_t StreamSize) {
-  const auto HostZStreamIt = Env.ZStreamMap.get(ZStreamPtr);
-  if (HostZStreamIt == Env.ZStreamMap.end()) {
-    return Unexpect(ErrCode::Value::HostFuncError);
-  }
-  auto HostZStream = *HostZStreamIt;
 
   auto *MemInst = Frame.getMemoryByIndex(0);
-  return -1;
+  if (MemInst == nullptr) {
+    return Unexpect(ErrCode::Value::HostFuncError);
+  }
+  Wasm_z_stream *WasmZStream = MemInst->getPointer<Wasm_z_stream *>(ZStreamPtr);
+  if (StreamSize != 56) {
+    spdlog::error("[WasmEdge Zlib] WASM sizeof(z_stream) != 56 but {}",
+                  StreamSize);
+    return Unexpect(ErrCode::Value::HostFuncError);
+  }
+
+  const char *WasmZlibVersion = MemInst->getPointer<const char *>(VersionPtr);
+  if (WasmZlibVersion[0] != ZLIB_VERSION[0]) {
+    spdlog::error("[WasmEdge Zlib] Major Zlib version of Host ({}) & Wasm ({}) "
+                  "does not match",
+                  ZLIB_VERSION[0], WasmZlibVersion[0]);
+    return Unexpect(ErrCode::Value::HostFuncError);
+  }
+
+  auto HostZStream = std::make_unique<z_stream>();
+
+  HostZStream.get()->zalloc = Z_NULL;
+  HostZStream.get()->zfree = Z_NULL;
+  HostZStream.get()->opaque =
+      Z_NULL; // ignore opaque since zmalloc and zfree was ignored
+
+  const auto z_res =
+      deflateInit_(HostZStream.get(), Level, ZLIB_VERSION, sizeof(z_stream));
+
+  Env.ZStreamMap.emplace(std::make_pair(ZStreamPtr, std::move(HostZStream)));
+
+  return static_cast<int32_t>(z_res);
 }
 
 Expect<int32_t>
 WasmEdgeZlibInflateInit_::body(const Runtime::CallingFrame &Frame,
                                uint32_t ZStreamPtr, uint32_t VersionPtr,
                                uint32_t StreamSize) {
-  const auto HostZStreamIt = Env.ZStreamMap.get(ZStreamPtr);
-  if (HostZStreamIt == Env.ZStreamMap.end()) {
-    return Unexpect(ErrCode::Value::HostFuncError);
-  }
-  auto HostZStream = *HostZStreamIt;
 
   auto *MemInst = Frame.getMemoryByIndex(0);
-  return -1;
-}
-
-Expect<int32_t>
-WasmEdgeZlibDeflateInit::body(const Runtime::CallingFrame &Frame,
-                              uint32_t ZStreamPtr, int32_t Level) {
-  const auto HostZStreamIt = Env.ZStreamMap.get(ZStreamPtr);
-  if (HostZStreamIt == Env.ZStreamMap.end()) {
+  if (MemInst == nullptr) {
     return Unexpect(ErrCode::Value::HostFuncError);
   }
-  auto HostZStream = *HostZStreamIt;
-
-  auto *MemInst = Frame.getMemoryByIndex(0);
-  return -1;
-}
-
-Expect<int32_t>
-WasmEdgeZlibInflateInit::body(const Runtime::CallingFrame &Frame,
-                              uint32_t ZStreamPtr) {
-  const auto HostZStreamIt = Env.ZStreamMap.get(ZStreamPtr);
-  if (HostZStreamIt == Env.ZStreamMap.end()) {
+  Wasm_z_stream *WasmZStream = MemInst->getPointer<Wasm_z_stream *>(ZStreamPtr);
+  if (StreamSize != 56) {
+    spdlog::error("[WasmEdge Zlib] WASM sizeof(z_stream) != 56 but {}",
+                  StreamSize);
     return Unexpect(ErrCode::Value::HostFuncError);
   }
-  auto HostZStream = *HostZStreamIt;
 
-  auto *MemInst = Frame.getMemoryByIndex(0);
-  return -1;
+  const char *WasmZlibVersion = MemInst->getPointer<const char *>(VersionPtr);
+  if (WasmZlibVersion[0] != ZLIB_VERSION[0]) {
+    spdlog::error("[WasmEdge Zlib] Major Zlib version of Host ({}) & Wasm ({}) "
+                  "does not match",
+                  ZLIB_VERSION[0], WasmZlibVersion[0]);
+    return Unexpect(ErrCode::Value::HostFuncError);
+  }
+
+  auto HostZStream = std::make_unique<z_stream>();
+
+  HostZStream.get()->zalloc = Z_NULL;
+  HostZStream.get()->zfree = Z_NULL;
+  HostZStream.get()->opaque =
+      Z_NULL; // ignore opaque since zmalloc and zfree was ignored
+
+  const auto z_res =
+      inflateInit_(HostZStream.get(), ZLIB_VERSION, sizeof(z_stream));
+
+  Env.ZStreamMap.emplace(std::make_pair(ZStreamPtr, std::move(HostZStream)));
+
+  return static_cast<int32_t>(z_res);
 }
 
 Expect<int32_t> WasmEdgeZlibDeflate::WasmEdgeZlibDeflate::body(
     const Runtime::CallingFrame &Frame, uint32_t ZStreamPtr, int32_t Flush) {
-  const auto HostZStreamIt = Env.ZStreamMap.get(ZStreamPtr);
+  const auto HostZStreamIt = Env.ZStreamMap.find(ZStreamPtr);
   if (HostZStreamIt == Env.ZStreamMap.end()) {
     return Unexpect(ErrCode::Value::HostFuncError);
   }
-  auto HostZStream = *HostZStreamIt;
+  auto HostZStream = HostZStreamIt->second.get();
 
   auto *MemInst = Frame.getMemoryByIndex(0);
   return -1;
@@ -99,11 +123,11 @@ Expect<int32_t> WasmEdgeZlibDeflate::WasmEdgeZlibDeflate::body(
 
 Expect<int32_t> WasmEdgeZlibInflate::body(const Runtime::CallingFrame &Frame,
                                           uint32_t ZStreamPtr, int32_t Flush) {
-  const auto HostZStreamIt = Env.ZStreamMap.get(ZStreamPtr);
+  const auto HostZStreamIt = Env.ZStreamMap.find(ZStreamPtr);
   if (HostZStreamIt == Env.ZStreamMap.end()) {
     return Unexpect(ErrCode::Value::HostFuncError);
   }
-  auto HostZStream = *HostZStreamIt;
+  auto HostZStream = HostZStreamIt->second.get();
 
   auto *MemInst = Frame.getMemoryByIndex(0);
 

3e1d86d74bd043b364cfee9e0046beef1fc249a1
Sarrah Bastawala <sarrah.basta@gmail.com>
2023-04-30 09:27:07 +0530

    initial commit for plugin wasi_ocr
    Signed-off-by: Sarrah Bastawala <sarrah.basta@gmail.com>

---
+++ plugins/wasi_ocr/wasiocrfunc.h
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2023 Second State INC
+
+#pragma once
+
+#include "runtime/callingframe.h"
+#include "wasiocrbase.h"
+
+#include <cstdint>
+
+namespace WasmEdge {
+namespace Host {
+
+class WasiOCRNumOfExtractions : public WasiOCR<WasiOCRNumOfExtractions> {
+public:
+  WasiOCRNumOfExtractions(WASIOCR::WasiOCREnvironment &HostEnv) : WasiOCR(HostEnv) {}
+  Expect<uint32_t> body(const Runtime::CallingFrame &, uint32_t ImagePathPtr,
+                      uint32_t ImagePathLen); //, uint32_t NumOfExtractionsPtr);
+};
+
+class WasiOCRGetOutput : public WasiOCR<WasiOCRGetOutput> {
+public:
+  WasiOCRGetOutput(WASIOCR::WasiOCREnvironment &HostEnv) : WasiOCR(HostEnv) {}
+  Expect<uint32_t> body(const Runtime::CallingFrame &, uint32_t OutBufferPtr,
+                        uint32_t OutBufferMaxSize);
+};
+
+}
+}

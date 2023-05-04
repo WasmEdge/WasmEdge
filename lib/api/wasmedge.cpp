225e39499c203c794632d963a2fe73896c4f6f7d
YiYing He <yiying@secondstate.io>
2023-05-05 06:04:01 +0800

    [API] Add the async invoke API of executor.
    Signed-off-by: YiYing He <yiying@secondstate.io>
    Signed-off-by: Sarrah Bastawala <sarrah.basta@gmail.com>

--- lib/api/wasmedge.cpp
+++ lib/api/wasmedge.cpp
@@ -1641,6 +1641,19 @@ WasmEdge_ExecutorInvoke(WasmEdge_ExecutorContext *Cxt,
       FuncCxt);
 }
 
+WASMEDGE_CAPI_EXPORT WasmEdge_Async *
+WasmEdge_ExecutorAsyncInvoke(WasmEdge_ExecutorContext *Cxt,
+                             const WasmEdge_FunctionInstanceContext *FuncCxt,
+                             const WasmEdge_Value *Params,
+                             const uint32_t ParamLen) {
+  if (Cxt && FuncCxt) {
+    auto ParamPair = genParamPair(Params, ParamLen);
+    return new WasmEdge_Async(fromExecutorCxt(Cxt)->asyncInvoke(
+        fromFuncCxt(FuncCxt), ParamPair.first, ParamPair.second));
+  }
+  return nullptr;
+}
+
 WASMEDGE_CAPI_EXPORT void
 WasmEdge_ExecutorDelete(WasmEdge_ExecutorContext *Cxt) {
   delete fromExecutorCxt(Cxt);

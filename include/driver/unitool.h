1414d5bd90edb27e8c2554541786fc6051287b1b
michael1017 <40065278+michael1017@users.noreply.github.com>
2023-05-25 15:04:12 +0800

    [Tools] Unified WasmEdge tools Prerelease (#2363)
    * create runtime tool options struct
    
    Signed-off-by: michael1017 <michael881017@gmail.com>
    
    * add unified wasmedge tool and remove wasmedgec
    
    Signed-off-by: michael1017 <michael881017@gmail.com>
    
    * fix subcommand usage hint
    
    Signed-off-by: michael1017 <michael881017@gmail.com>
    
    * document update
    
    Signed-off-by: michael1017 <michael881017@gmail.com>
    
    * recover wasmedgec
    
    Signed-off-by: michael1017 <michael881017@gmail.com>
    
    * fix dco check
    
    Signed-off-by: michael1017 <michael881017@gmail.com>
    
    ---------
    
    Signed-off-by: michael1017 <michael881017@gmail.com>
    Signed-off-by: Sarrah Bastawala <sarrah.basta@gmail.com>

---
+++ include/driver/unitool.h
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2019-2022 Second State INC
+
+//===-- wasmedge/driver/unitool.h - UniTool entrypoint ------------------===//
+//
+// Part of the WasmEdge Project.
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file contents the entrypoint for the wasmedge unified tool executable.
+///
+//===----------------------------------------------------------------------===//
+#pragma once
+
+namespace WasmEdge {
+namespace Driver {
+
+enum class ToolType : char { All, Compiler, Tool };
+
+int UniTool(int Argc, const char *Argv[], const ToolType ToolSelect) noexcept;
+
+} // namespace Driver
+} // namespace WasmEdge
---
+++ lib/driver/uniTool.cpp
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2019-2022 Second State INC
+
+#include "driver/unitool.h"
+#include "common/log.h"
+#include "driver/compiler.h"
+#include "driver/tool.h"
+#include "po/argument_parser.h"
+
+#include <iostream>
+#include <string_view>
+
+namespace WasmEdge {
+namespace Driver {
+
+int UniTool(int Argc, const char *Argv[], const ToolType ToolSelect) noexcept {
+  using namespace std::literals;
+
+  std::ios::sync_with_stdio(false);
+  Log::setInfoLoggingLevel();
+
+  auto Parser = PO::ArgumentParser();
+
+  PO::SubCommand ToolSubCommand(
+      PO::Description("Wasmedge runtime tool subcommand"sv));
+  PO::SubCommand CompilerSubCommand(
+      PO::Description("Wasmedge compiler subcommand"sv));
+  struct DriverToolOptions ToolOptions;
+  struct DriverCompilerOptions CompilerOptions;
+
+  // Construct Parser Subcommands and Options
+  if (ToolSelect == ToolType::All) {
+    ToolOptions.add_option(Parser);
+
+    Parser.begin_subcommand(CompilerSubCommand, "compile"sv);
+    CompilerOptions.add_option(Parser);
+    Parser.end_subcommand();
+
+    Parser.begin_subcommand(ToolSubCommand, "run"sv);
+    ToolOptions.add_option(Parser);
+    Parser.end_subcommand();
+  } else if (ToolSelect == ToolType::Tool) {
+    ToolOptions.add_option(Parser);
+  } else if (ToolSelect == ToolType::Compiler) {
+    CompilerOptions.add_option(Parser);
+  } else {
+    return EXIT_FAILURE;
+  }
+
+  // Parse
+  if (!Parser.parse(stdout, Argc, Argv)) {
+    return EXIT_FAILURE;
+  }
+  if (Parser.isVersion()) {
+    std::cout << Argv[0] << " version "sv << kVersionString << '\n';
+    return EXIT_SUCCESS;
+  }
+
+  // Forward Results
+  if (ToolSubCommand.is_selected() || ToolSelect == ToolType::Tool) {
+    return Tool(ToolOptions);
+  } else if (CompilerSubCommand.is_selected() ||
+             ToolSelect == ToolType::Compiler) {
+    return Compiler(CompilerOptions);
+  } else {
+    return Tool(ToolOptions);
+  }
+}
+} // namespace Driver
+} // namespace WasmEdge

9615d7b6a44ea11cea9986e913fc5a77b3fb3350
Saikat Dey <saikatdey2100@gmail.com>
2023-06-06 06:38:29 +0530

    Added zlib plugin WasmEdgeZlibInflateInit_ implementation.
    Signed-off-by: Saikat Dey <saikatdey2100@gmail.com>

--- plugins/wasmedge_zlib/zlibfunc.cpp
+++ plugins/wasmedge_zlib/zlibfunc.cpp
@@ -51,5 +51,47 @@ WasmEdgeZlibDeflateInit_::body(const Runtime::CallingFrame &Frame,
   return static_cast<int32_t>(z_res);
 }
 
+Expect<int32_t>
+WasmEdgeZlibInflateInit_::body(const Runtime::CallingFrame &Frame,
+                               uint32_t ZStreamPtr, uint32_t VersionPtr,
+                               uint32_t StreamSize) {
+
+  auto *MemInst = Frame.getMemoryByIndex(0);
+  if (MemInst == nullptr) {
+    return Unexpect(ErrCode::Value::HostFuncError);
+  }
+  // Wasm_z_stream *WasmZStream = MemInst->getPointer<Wasm_z_stream
+  // *>(ZStreamPtr);
+  if (StreamSize != 56) {
+    spdlog::error("[WasmEdge Zlib] [WasmEdgeZlibInflateInit_] WASM "
+                  "sizeof(z_stream) != 56 but {}",
+                  StreamSize);
+    return Unexpect(ErrCode::Value::HostFuncError);
+  }
+
+  const char *WasmZlibVersion = MemInst->getPointer<const char *>(VersionPtr);
+  if (WasmZlibVersion[0] != ZLIB_VERSION[0]) {
+    spdlog::error("[WasmEdge Zlib] [WasmEdgeZlibInflateInit_] Major Zlib "
+                  "version of Host ({}) & Wasm ({}) "
+                  "does not match",
+                  ZLIB_VERSION[0], WasmZlibVersion[0]);
+    return Unexpect(ErrCode::Value::HostFuncError);
+  }
+
+  auto HostZStream = std::make_unique<z_stream>();
+
+  HostZStream.get()->zalloc = Z_NULL;
+  HostZStream.get()->zfree = Z_NULL;
+  HostZStream.get()->opaque =
+      Z_NULL; // ignore opaque since zmalloc and zfree was ignored
+
+  const auto z_res =
+      inflateInit_(HostZStream.get(), ZLIB_VERSION, sizeof(z_stream));
+
+  Env.ZStreamMap.emplace(std::make_pair(ZStreamPtr, std::move(HostZStream)));
+
+  return static_cast<int32_t>(z_res);
+}
+
 } // namespace Host
 } // namespace WasmEdge

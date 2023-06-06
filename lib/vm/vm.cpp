02f07011035c70db844d1470a52c5e5910e752fd
XingziYu <edittriendlyu@gmail.com>
2023-06-06 14:03:00 +0800

    [Misc] Tiny fix
    Signed-off-by: XingziYu <edittriendlyu@gmail.com>

--- lib/vm/vm.cpp
+++ lib/vm/vm.cpp
@@ -24,7 +24,7 @@ createPluginModule(std::string_view PName, std::string_view MName) {
       return Module->create();
     }
   }
-  spdlog::debug("Plugin: {} , module name: {} not found. Mock instead.", PName,
+  spdlog::debug("Plugin: {} , module name: {} not found. Mock instead."sv, PName,
                 MName);
   return std::make_unique<T>();
 }

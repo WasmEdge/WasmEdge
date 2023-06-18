480aaa3b0a2210b38408b4b1e734385045109268
officeyutong <yt.xyxx@gmail.com>
2023-06-18 08:32:41 +0000

    Remove clang installing scripts
    Signed-off-by: officeyutong <yt.xyxx@gmail.com>

--- utils/wasm_bpf/install-clang.sh
+++
@@ -1,8 +0,0 @@
-curl -L https://github.com/llvm/llvm-project/releases/download/llvmorg-16.0.6/llvm-project-16.0.6.src.tar.xz -O
-tar --xz -xvf llvm-project-16.0.6.src.tar.xz > /dev/null
-cd llvm-project-16.0.6.src
-cmake -S llvm -B build -DCMAKE_BUILD_TYPE=Release
-cmake --build build -j
-cd build
-make install
-clang --version 

c74505d1cc0785cb78c6eba3afc346c05be913d3
Saikat Dey <saikatdey2100@gmail.com>
2023-06-29 23:32:58 +0530

    Added in-code documentation to every field in Wasm_z_stream.
    Signed-off-by: Saikat Dey <saikatdey2100@gmail.com>

--- plugins/wasmedge_zlib/zlibenv.h
+++ plugins/wasmedge_zlib/zlibenv.h
@@ -16,26 +16,42 @@
  *
  */
 struct Wasm_z_stream {
+  /* [Wasm Offset] next input byte */
   uint32_t next_in;
+  /* number of bytes available at next_in */
   uint32_t avail_in;
+  /* total number of input bytes read so far */
   uint32_t total_in;
 
+  /* [Wasm Offset] next output byte will go here */
   uint32_t next_out;
+  /* remaining free space at next_out */
   uint32_t avail_out;
+  /* total number of bytes output so far */
   uint32_t total_out;
 
+  /* [Wasm Offset] last error message, NULL if no error */
   uint32_t msg;
+  /* [Wasm Offset] not visible by applications */
   uint32_t state;
 
+  /* used to allocate the internal state */
   uint32_t zalloc;
+  /* used to free the internal state */
   uint32_t zfree;
+  /* [Wasm Offset] private data object passed to zalloc and zfree */
   uint32_t opaque;
 
-  int32_t data_type; // +ve & 2's complement in memory so int ~ uint
+  /* best guess about the data type: binary or text
+                                           for deflate, or the decoding state
+     for inflate */
+  int32_t data_type;
 
+  /* Adler-32 or CRC-32 value of the uncompressed data */
   uint32_t adler;
+  /* reserved for future use */
   uint32_t reserved;
-}; // 56 bytes
+};
 
 static_assert(sizeof(Wasm_z_stream) == 56, "Wasm_z_stream should be 56 bytes");
 

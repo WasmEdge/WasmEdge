9917899cdc152f6ab2271718f8e0a7380912d81f
Divyanshu GUpta <divyanshugupta585@gmail.com>
2023-07-01 17:34:38 +0530

    --
    Signed-off-by: Divyanshu GUpta <divyanshugupta585@gmail.com>

--- utils/wasi-nn/install-openvino.sh
+++ utils/wasi-nn/install-openvino.sh
@@ -11,10 +11,10 @@ fi
 
 set -e
 echo "Installing OpenVINO with version ${OPENVINO_VERSION}"
-curl -sSL https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB | gpg --dearmor > /usr/share/keyrings/GPG-PUB-KEY-INTEL-OPENVINO-$OPENVINO_YEAR.gpg
+wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
 apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
-echo "deb [signed-by=/usr/share/keyrings/GPG-PUB-KEY-INTEL-OPENVINO-$OPENVINO_YEAR.gpg] https://apt.repos.intel.com/openvino/$OPENVINO_YEAR ubuntu20 main" | tee /etc/apt/sources.list.d/intel-openvino-$OPENVINO_YEAR.list
+echo "deb https://apt.repos.intel.com/openvino/2023 ubuntu20 main" | tee /etc/apt/sources.list.d/intel-openvino-2023.list
 apt update
-apt install openvino
+apt install openvino-${OPENVINO_VERSION}
 source /opt/intel/openvino_2023/bin/setupvars.sh
 ldconfig

c4fbd68fcbaefad4530806d8b060cf68e3f1fb42
Divyanshu GUpta <divyanshugupta585@gmail.com>
2023-07-01 16:30:50 +0530

    removed sudo
    Signed-off-by: Divyanshu GUpta <divyanshugupta585@gmail.com>

--- utils/wasi-nn/install-openvino.sh
+++ utils/wasi-nn/install-openvino.sh
@@ -12,10 +12,11 @@ fi
 set -e
 echo "Installing OpenVINO with version ${OPENVINO_VERSION}"
 wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
-sudo apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
+apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
 echo "deb https://apt.repos.intel.com/openvino/2023 ubuntu22 main" | sudo tee /etc/apt/sources.list.d/intel-openvino-2023.list
-sudo apt update
+apt update
 apt-cache search openvino
-sudo apt install openvino
+apt install openvino
+apt list --installed | grep openvino
 source /opt/intel/openvino_2021/bin/setupvars.sh
 ldconfig

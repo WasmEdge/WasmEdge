256d758155226c54f39b90b29444ad78806c27ce
Lîm Tsú-thuàn <dannypsnl@secondstate.io>
2023-07-03 16:43:22 +0800

    add test
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>

---
+++ test/plugins/wasmedge_opencvmini/wasmedge_opencvmini.cpp
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2019-2022 Second State INC
+
+#include "common/defines.h"
+#include "opencvmini_func.h"
+#include "opencvmini_module.h"
+#include "runtime/instance/module.h"
+
+#include <algorithm>
+#include <array>
+#include <cstdint>
+#include <gtest/gtest.h>
+#include <string>
+#include <vector>
+
+namespace {
+WasmEdge::Runtime::Instance::ModuleInstance *createModule() {
+  using namespace std::literals::string_view_literals;
+  WasmEdge::Plugin::Plugin::load(std::filesystem::u8path(
+      "../../../plugins/wasmedge_opencvmini/"
+      "libwasmedgePluginWasmEdgeOpencvmini" WASMEDGE_LIB_EXTENSION));
+  if (const auto *Plugin =
+          WasmEdge::Plugin::Plugin::find("wasmedge_opencvmini"sv)) {
+    if (const auto *Module = Plugin->findModule("wasmedge_opencvmini"sv)) {
+      return Module->create().release();
+    }
+  }
+  return nullptr;
+}
+} // namespace
+
+// TODO: unit tests for every functions.
+
+TEST(WasmEdgeOpecvminiTest, Module) {
+  // Create the wasmedge_opencvmini module instance.
+  auto *ImgMod =
+      dynamic_cast<WasmEdge::Host::WasmEdgeOpecvminiModule *>(createModule());
+  EXPECT_FALSE(ImgMod == nullptr);
+  EXPECT_EQ(ImgMod->getFuncExportNum(), 2U);
+  EXPECT_NE(ImgMod->findFuncExports("wasmedge_opencvmini_imdecode"), nullptr);
+  delete ImgMod;
+}
+
+GTEST_API_ int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}

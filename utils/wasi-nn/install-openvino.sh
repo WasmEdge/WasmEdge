62be1eb1639829f36ff9ebb99942d19abec13e9e
Bahadır <44382157+bahadirbklg@users.noreply.github.com>
2023-07-10 22:38:49 +0300

    Version Update install-openvino.sh
    links and  packages has been changed. there are no package named "intel-openvino-runtime-ubuntu20- **2023** "
    
    Signed-off-by: Bahadır <44382157+bahadirbklg@users.noreply.github.com>

--- utils/wasi-nn/install-openvino.sh
+++ utils/wasi-nn/install-openvino.sh
@@ -3,17 +3,28 @@
 # SPDX-FileCopyrightText: 2019-2022 Second State INC
 
 if [[ ! -v "${OPENVINO_VERSION}" ]]; then
-  OPENVINO_VERSION="2021.4.582"
+  OPENVINO_VERSION="2023.0.1"
 fi
 if [[ ! -v "${OPENVINO_YEAR}" ]]; then
-  OPENVINO_YEAR="2021"
+  OPENVINO_YEAR="2023"
 fi
 
+if [["${OPENVINO_YEAR}" -eq "2021"]]; then
 set -e
 echo "Installing OpenVINO with version ${OPENVINO_VERSION}"
 curl -sSL https://apt.repos.intel.com/openvino/$OPENVINO_YEAR/GPG-PUB-KEY-INTEL-OPENVINO-$OPENVINO_YEAR | gpg --dearmor > /usr/share/keyrings/GPG-PUB-KEY-INTEL-OPENVINO-$OPENVINO_YEAR.gpg
 echo "deb [signed-by=/usr/share/keyrings/GPG-PUB-KEY-INTEL-OPENVINO-$OPENVINO_YEAR.gpg] https://apt.repos.intel.com/openvino/$OPENVINO_YEAR all main" | tee /etc/apt/sources.list.d/intel-openvino-$OPENVINO_YEAR.list
 apt update
 apt install -y intel-openvino-runtime-ubuntu20-$OPENVINO_VERSION
+fi
+
+if [["${OPENVINO_YEAR}" -eq "2023"]]; then
+set -e
+echo "Installing OpenVINO with version ${OPENVINO_VERSION}"
+curl -sSL https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB | apt-key add
+echo "deb https://apt.repos.intel.com/openvino/$OPENVINO_YEAR all main" | sudo tee /etc/apt/sources.list.d/intel-openvino-$OPENVINO_YEAR.list
+apt update
+apt install -y openvino-$OPENVINO_VERSION
+fi
 source /opt/intel/openvino_2021/bin/setupvars.sh
 ldconfig

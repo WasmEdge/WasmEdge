80e22ba36bdb930d07d55b09e700ce0b9f3cc53a
Lîm Tsú-thuàn <dannypsnl@secondstate.io>
2023-07-10 16:17:14 +0800

    build and limit build thread
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>

---
+++ utils/opencvmini/install-opencvmini.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+# SPDX-FileCopyrightText: 2019-2023 Second State INC
+
+wget -O opencv.zip https://github.com/opencv/opencv/archive/4.x.zip
+
+unzip opencv.zip
+
+mkdir -p build && cd build
+# Configure
+cmake ../opencv-4.x
+# Build
+cmake --build . -j2
+# Install to system
+sudo cmake --install .

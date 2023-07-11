3ef9c14364779cf4037c76a53c404e73c60af451
Lîm Tsú-thuàn <dannypsnl@secondstate.io>
2023-07-11 18:04:01 +0800

    remove contrib
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>

--- utils/opencvmini/install-opencvmini.sh
+++ utils/opencvmini/install-opencvmini.sh
@@ -3,18 +3,13 @@
 # SPDX-FileCopyrightText: 2019-2023 Second State INC
 
 wget -O opencv.zip https://github.com/opencv/opencv/archive/4.x.zip
-wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/4.x.zip
 
 unzip opencv.zip
-unzip opencv_contrib.zip
 mv opencv-4.x opencv
-mv opencv_contrib-4.x opencv_contrib
 
 mkdir -p opencv/build && cd opencv/build
 # Configure
-cmake -GNinja \
-  -DOPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
-  ..
+cmake -GNinja ..
 # Build
 cmake --build .
 # Install to system

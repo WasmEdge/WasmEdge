e4a839410d11c7b04f58997961af0d772eef111a
Lîm Tsú-thuàn <dannypsnl@secondstate.io>
2023-07-20 11:45:03 +0800

    install certain version
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>

--- utils/opencvmini/install-opencvmini.sh
+++ utils/opencvmini/install-opencvmini.sh
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 # SPDX-FileCopyrightText: 2019-2023 Second State INC
 
-wget -O opencv.zip https://github.com/opencv/opencv/archive/4.x.zip
+wget -O opencv.zip https://github.com/opencv/opencv/archive/refs/tags/4.8.0.zip
 
 unzip opencv.zip
 mv opencv-4.x opencv

106cfa109bf46f8af90a07ec65534be415cbeac3
Lîm Tsú-thuàn <dannypsnl@secondstate.io>
2023-08-10 05:58:29 +0800

    [CI] pre-build image with necessary dependency for extension (#2731)
    * [CI] try to pre-build image with necessary dependency for extension
    
    related with #2729
    
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>
    
    * revert ci changes
    
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>
    
    * rename dockerfile and tag
    
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>
    
    * revert ci changes
    
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>
    
    * share same installation script
    
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>
    
    * share the same dockerfile for plugin-deps
    
    Since the base image can be assigned by --build-arg
    
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>
    
    * Assign opencv version in dockerfile via environment variable
    
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>
    
    * Fix, must use version variable
    
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>
    
    * build image plugins-deps in actions
    
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>
    
    ---------
    
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>

--- utils/docker/build.sh
+++ utils/docker/build.sh
@@ -25,14 +25,18 @@ function docker_build
 }
 
 # Build all images.
-docker_build Dockerfile.base            ubuntu-base
-docker_build Dockerfile.ci-image-base   ci-image-base
-docker_build Dockerfile.build-clang     ubuntu-build-clang  \
+docker_build Dockerfile.base                  ubuntu-base
+docker_build Dockerfile.ci-image-base         ci-image-base
+docker_build Dockerfile.build-clang           ubuntu-build-clang              \
     --build-arg "BASE=${NAME}:ubuntu-base"
-docker_build Dockerfile.build-clang     latest              \
+docker_build Dockerfile.build-clang           latest                          \
     --build-arg "BASE=${NAME}:ubuntu-base"
-docker_build Dockerfile.build-gcc       ubuntu-build-gcc    \
+docker_build Dockerfile.build-gcc             ubuntu-build-gcc                \
     --build-arg "BASE=${NAME}:ubuntu-base"
+docker_build Dockerfile.build-plugins-deps    ubuntu-build-clang-plugins-deps \
+    --build-arg "BASE=${NAME}:ubuntu-build-clang"
+docker_build Dockerfile.build-plugins-deps    ubuntu-build-gcc-plugins-deps   \
+    --build-arg "BASE=${NAME}:ubuntu-build-gcc"
 
 # Remove intermediate images.
 for NAME_TAG in "${INTERMEDIATES[@]}"; do

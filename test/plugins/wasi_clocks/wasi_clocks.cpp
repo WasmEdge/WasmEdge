fa0bd9eaec71d064506fe7a34a1df8ecb8ae00e7
Lîm Tsú-thuàn <dannypsnl@secondstate.io>
2023-09-01 14:04:03 +0800

    Add CI & test
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>

---
+++ test/plugins/wasi_clocks/wasi_clocks.cpp
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2019-2022 Second State INC
+
+#include "common/defines.h"
+#include "func.h"
+#include "module.h"
+#include "runtime/instance/module.h"
+
+#include <algorithm>
+#include <array>
+#include <cstdint>
+#include <gtest/gtest.h>
+#include <string>
+#include <vector>
+
+namespace {
+WasmEdge::Runtime::Instance::ModuleInstance *createModule() {
+  using namespace std::literals::string_view_literals;
+  WasmEdge::Plugin::Plugin::load(std::filesystem::u8path(
+      "../../../plugins/wasi_clocks/"
+      "libwasmedgePluginWasiClocks" WASMEDGE_LIB_EXTENSION));
+  if (const auto *Plugin =
+          WasmEdge::Plugin::Plugin::find("wasmedge_wasi_clocks"sv)) {
+    if (const auto *Module = Plugin->findModule("wasmedge_wasi_clocks"sv)) {
+      return Module->create().release();
+    }
+  }
+  return nullptr;
+}
+} // namespace
+
+TEST(WasmedgeWasiClocksTests, Module) {
+  // Create the wasmedge_wasi_clocks module instance.
+  auto *ImgMod =
+      dynamic_cast<WasmEdge::Host::WasmEdgeWasiClocksModule *>(createModule());
+  EXPECT_FALSE(ImgMod == nullptr);
+  EXPECT_EQ(ImgMod->getFuncExportNum(), 7U);
+  EXPECT_NE(ImgMod->findFuncExports("wasi_clocks_monotonic_clock_now"),
+            nullptr);
+  EXPECT_NE(ImgMod->findFuncExports("wasi_clocks_monotonic_clock_resolution"),
+            nullptr);
+  EXPECT_NE(ImgMod->findFuncExports("wasi_clocks_wall_clock_now"), nullptr);
+  EXPECT_NE(ImgMod->findFuncExports("wasi_clocks_wall_clock_resolution"),
+            nullptr);
+  delete ImgMod;
+}
+
+GTEST_API_ int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}

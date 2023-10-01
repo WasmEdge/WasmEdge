4272037ac99a469fb9ebe90af9d5d2500152e1ed
hydai <z54981220@gmail.com>
2023-10-01 15:07:35 +0800

    [CI] Fix the broken installation of OpenVINO
    Signed-off-by: hydai <z54981220@gmail.com>

--- utils/wasi-nn/install-openvino.sh
+++ utils/wasi-nn/install-openvino.sh
@@ -3,10 +3,10 @@
 # SPDX-FileCopyrightText: 2019-2022 Second State INC
 
 set -e
-echo "Installing OpenVINO with version 2023.0.0"
+echo "Installing OpenVINO with version 2023.1.0"
 wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
 apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
 echo "deb https://apt.repos.intel.com/openvino/2023 ubuntu20 main" | tee /etc/apt/sources.list.d/intel-openvino-2023.list
 apt update
-apt-get -y install openvino-2023.0.2
+apt-get -y install openvino-2023.1.0
 ldconfig

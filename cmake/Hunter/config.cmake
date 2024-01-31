acc4d93f6050b97d0cfbb0b54dae78ef1eff2ce3
turuslan <turuslan.devbox@gmail.com>
2024-01-31 14:06:27 +0300

    [qdrvm] hunter

---
+++ cmake/Hunter/config.cmake
@@ -0,0 +1,6 @@
+
+hunter_config(
+    LLVM
+    VERSION 16.0.1
+    CMAKE_ARGS LLVM_ENABLE_PROJECTS=compiler-rt
+)
---
+++ lib/loader/ast/component/component_type.cpp
@@ -0,0 +1,3 @@
+// "libwasmedge.a" is merged from objects extracted from libraries.
+// "ar" can't extract duplicate "type.cpp.o" from "libwasmedgeLoader.a".
+#include "./type.cpp"

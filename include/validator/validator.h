58dbacba8783f96b2a010297f6f7f3ae0b8390d7
YiYing He <yiying@secondstate.io>
2024-02-05 16:05:08 +0800

    [Validator] GC proposal validator.
    Signed-off-by: YiYing He <yiying@secondstate.io>

--- include/validator/validator.h
+++ include/validator/validator.h
@@ -37,6 +37,7 @@ public:
 
 private:
   /// Validate AST::Types
+  Expect<void> validate(const AST::SubType &Type);
   Expect<void> validate(const AST::Limit &Lim);
   Expect<void> validate(const AST::TableType &Tab);
   Expect<void> validate(const AST::MemoryType &Mem);

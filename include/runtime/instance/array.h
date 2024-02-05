89feb4a8879cada84237d91c03bd78668a6b815b
YiYing He <yiying@secondstate.io>
2024-02-05 16:06:32 +0800

    [Runtime] Runtime structure of GC proposal.
    Signed-off-by: YiYing He <yiying@secondstate.io>

---
+++ include/runtime/instance/array.h
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2019-2022 Second State INC
+
+//===-- wasmedge/runtime/instance/array.h - Array Instance definition -----===//
+//
+// Part of the WasmEdge Project.
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file contains the array instance definition in store manager.
+///
+//===----------------------------------------------------------------------===//
+#pragma once
+
+#include "ast/type.h"
+#include "common/span.h"
+#include "common/types.h"
+#include "runtime/instance/composite.h"
+
+#include <vector>
+
+namespace WasmEdge {
+namespace Runtime {
+namespace Instance {
+
+class ArrayInstance : public CompositeBase {
+public:
+  ArrayInstance() = delete;
+  ArrayInstance(const ModuleInstance *Mod, const uint32_t Idx,
+                const uint32_t Size) noexcept
+      : CompositeBase(Mod, Idx), RefCount(1),
+        Data(Size, static_cast<uint128_t>(0)) {
+    assuming(ModInst);
+  }
+  ArrayInstance(const ModuleInstance *Mod, const uint32_t Idx,
+                const uint32_t Size, const ValVariant &Init) noexcept
+      : CompositeBase(Mod, Idx), RefCount(1), Data(Size, Init) {
+    assuming(ModInst);
+  }
+  ArrayInstance(const ModuleInstance *Mod, const uint32_t Idx,
+                std::vector<ValVariant> &&Init) noexcept
+      : CompositeBase(Mod, Idx), RefCount(1), Data(std::move(Init)) {
+    assuming(ModInst);
+  }
+
+  /// Get field data in array instance.
+  ValVariant &getData(uint32_t Idx) noexcept { return Data[Idx]; }
+  const ValVariant &getData(uint32_t Idx) const noexcept { return Data[Idx]; }
+
+  /// Get full array.
+  Span<ValVariant> getArray() noexcept { return Data; }
+  Span<const ValVariant> getArray() const noexcept { return Data; }
+
+  /// Get array length.
+  uint32_t getLength() const noexcept {
+    return static_cast<uint32_t>(Data.size());
+  }
+
+  /// Get reference count.
+  uint32_t getRefCount() const noexcept { return RefCount; }
+
+  /// Get boundary index.
+  uint32_t getBoundIdx() const noexcept {
+    return std::max(static_cast<uint32_t>(Data.size()), UINT32_C(1)) -
+           UINT32_C(1);
+  }
+
+private:
+  /// \name Data of array instance.
+  /// @{
+  uint32_t RefCount;
+  std::vector<ValVariant> Data;
+  /// @}
+};
+
+} // namespace Instance
+} // namespace Runtime
+} // namespace WasmEdge

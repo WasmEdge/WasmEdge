3a04e9fe228a5d807e4a9ead17c172f619c3645d
kamilsa <kamilsa16@gmail.com>
2024-02-05 12:15:42 +0600

    Update llvm and lld to 17.0.6

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,6 +1,20 @@
+set(ARCHITECTURE "${CMAKE_SYSTEM_PROCESSOR}")
+
+if(ARCHITECTURE MATCHES "^(aarch64.*|AARCH64.*|arm.*|ARM.*)")
+    set(ARCHITECTURE AArch64)
+elseif(ARCHITECTURE MATCHES "^(x86_64.*|AMD64.*|i386.*|i686.*)")
+    set(ARCHITECTURE X86)
+elseif(ARCHITECTURE MATCHES "^(riscv.*)")
+    set(ARCHITECTURE RISCV)
+else()
+    message(WARNING "Unknown architecture: ${ARCHITECTURE}, using all architectures to build LLVM")
+    set(ARCHITECTURE AArch64;X86;RISCV)
+endif()
 
 hunter_config(
     LLVM
-    VERSION 16.0.1
-    CMAKE_ARGS LLVM_ENABLE_PROJECTS=compiler-rt
+    VERSION 17.0.6
+    CMAKE_ARGS
+        LLVM_ENABLE_PROJECTS=lld;clang
+        LLVM_TARGETS_TO_BUILD=${ARCHITECTURE};BPF
 )

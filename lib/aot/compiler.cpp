44451ccaad914fe0b890ae0966dc878cd18fb798
turuslan <turuslan.devbox@gmail.com>
2024-02-19 12:43:49 +0300

    rename wasm file
    Signed-off-by: turuslan <turuslan.devbox@gmail.com>

--- lib/aot/compiler.cpp
+++ lib/aot/compiler.cpp
@@ -5468,7 +5468,13 @@ Expect<void> outputWasmLibrary(const std::filesystem::path &OutputPath,
 
   spdlog::info("output start");
 
-  std::ofstream OS(OutputPath, std::ios_base::binary);
+  std::filesystem::path OutputPathTmp(OutputPath);
+  OutputPathTmp.replace_extension("%%%%%%%%%%.wasm");
+  OutputPathTmp = createTemp(OutputPathTmp);
+  if (OutputPathTmp.empty()) {
+    return WasmEdge::Unexpect(WasmEdge::ErrCode::Value::IllegalPath);
+  }
+  std::ofstream OS(OutputPathTmp, std::ios_base::binary);
   if (!OS) {
     spdlog::error("output failed.");
     return Unexpect(ErrCode::Value::IllegalPath);
@@ -5481,6 +5487,7 @@ Expect<void> outputWasmLibrary(const std::filesystem::path &OutputPath,
 
   std::error_code Error;
   std::filesystem::remove(SharedObjectName, Error);
+  std::filesystem::rename(OutputPathTmp, OutputPath);
   return {};
 }
 

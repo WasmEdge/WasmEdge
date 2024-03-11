b3682e4bf51f8243867a4c582b5c93530cd3fc47
Hrushi20 <hrushikesh20thegreat@gmail.com>
2024-03-11 23:42:38 +0530

    credit for sample_video in test
    Signed-off-by: Hrushi20 <hrushikesh20thegreat@gmail.com>

--- utils/ffmpeg/download-ffmpeg-sample-video.sh
+++ utils/ffmpeg/download-ffmpeg-sample-video.sh
@@ -2,6 +2,8 @@
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 # SPDX-FileCopyrightText: 2019-2022 Second State INC
 
+# The below video used is sourced from an ffmpeg-libav-tutorial repository.
+# Source: https://github.com/leandromoreira/ffmpeg-libav-tutorial/blob/master/LICENSE.
 TODIR=$1
 SAMPLE_VIDEO=https://raw.githubusercontent.com/Hrushi20/rust-ffmpeg/master/assets/bunny.mp4
 if [[ $# -eq 0 ]]; then

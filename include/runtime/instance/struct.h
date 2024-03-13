459ecd16715b7c3894776255320cdd8ca64e075a
Yi-Ying He <yiying@secondstate.io>
2024-03-13 17:54:58 +0800

    [Runtime] Due to tracing GC, remove ref count. (#3274)
    Signed-off-by: YiYing He <yiying@secondstate.io>

--- include/runtime/instance/struct.h
+++ include/runtime/instance/struct.h
@@ -29,13 +29,12 @@ public:
   StructInstance() = delete;
   StructInstance(const ModuleInstance *Mod, const uint32_t Idx,
                  const uint32_t MemberCnt) noexcept
-      : CompositeBase(Mod, Idx), RefCount(1),
-        Data(MemberCnt, static_cast<uint128_t>(0)) {
+      : CompositeBase(Mod, Idx), Data(MemberCnt, static_cast<uint128_t>(0)) {
     assuming(ModInst);
   }
   StructInstance(const ModuleInstance *Mod, const uint32_t Idx,
                  std::vector<ValVariant> &&Init) noexcept
-      : CompositeBase(Mod, Idx), RefCount(1), Data(std::move(Init)) {
+      : CompositeBase(Mod, Idx), Data(std::move(Init)) {
     assuming(ModInst);
   }
 
@@ -43,13 +42,9 @@ public:
   ValVariant &getField(uint32_t Idx) noexcept { return Data[Idx]; }
   const ValVariant &getField(uint32_t Idx) const noexcept { return Data[Idx]; }
 
-  /// Get reference count.
-  uint32_t getRefCount() const noexcept { return RefCount; }
-
 private:
   /// \name Data of struct instance.
   /// @{
-  uint32_t RefCount;
   std::vector<ValVariant> Data;
   /// @}
 };

45ebf4cd0a51afc97d8826eddc085c9d0cc840c8
hydai <z54981220@gmail.com>
2024-03-19 08:30:44 +0800

    [WASI-NN] ggml: downgrade to b2334 to fix the embedding issue
    Signed-off-by: hydai <z54981220@gmail.com>

--- plugins/wasi_nn/ggml.cpp
+++ plugins/wasi_nn/ggml.cpp
@@ -348,7 +348,7 @@ Expect<ErrNo> getEmbedding(WasiNNEnvironment &Env,
   llama_context_params ContextParams = llama_context_default_params();
   ContextParams.n_ctx = GraphRef.CtxSize;
   ContextParams.n_batch = GraphRef.BatchSize;
-  ContextParams.embeddings = GraphRef.Embedding;
+  ContextParams.embedding = GraphRef.Embedding;
   auto *LlamaContext =
       llama_new_context_with_model(GraphRef.LlamaModel, ContextParams);
 

1d1834fcdd9b4db420f6efe3bd74285a02fe48f6
Ramya <ramyapgk1887@gmail.com>
2024-03-26 17:50:14 +0530

    corrected error in types.h file Signed-off-by: Ramya <ramyapgk1887@gmail.com>

--- plugins/wasi_nn/types.h
+++ plugins/wasi_nn/types.h
@@ -35,7 +35,7 @@ enum class Backend : uint8_t {
   TensorflowLite = 4,
   Autodetect = 5,
   GGML = 6,
-  WHISPER = 8;
+  WHISPER = 8
 };
 
 #define FOR_EACH_BACKEND(F)                                                    \

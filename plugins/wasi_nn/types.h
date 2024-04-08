c30c2e57acec23bdc3f52a9784ff742300c7d131
Ramya <ramyapgk1887@gmail.com>
2024-04-08 23:56:06 +0530

    clang formatted Signed-off-by: Ramya <ramyapgk1887@gmail.com>

--- plugins/wasi_nn/types.h
+++ plugins/wasi_nn/types.h
@@ -35,11 +35,12 @@ enum class Backend : uint8_t {
   TensorflowLite = 4,
   Autodetect = 5,
   GGML = 6,
-  WHISPER=7
-  };
+  WHISPER = 7
+};
 
 #define FOR_EACH_BACKEND(F)                                                    \
-  F(OpenVINO) F(ONNX) F(Tensorflow) F(PyTorch) F(TensorflowLite) F(GGML) F(WHISPER)
+  F(OpenVINO)                                                                  \
+  F(ONNX) F(Tensorflow) F(PyTorch) F(TensorflowLite) F(GGML) F(WHISPER)
 
 struct TensorData {
   Span<uint32_t> Dimension;
--- plugins/wasi_nn/whispercpp.cpp
+++ plugins/wasi_nn/whispercpp.cpp
@@ -1,56 +1,70 @@
 // SPDX-License-Identifier: Apache-2.0
- // SPDX-FileCopyrightText: 2019-2022 Second State INC
+// SPDX-FileCopyrightText: 2019-2022 Second State INC
 
- #include "whispercpp.h"
- #include "wasinnenv.h"
+#include "whispercpp.h"
+#include "wasinnenv.h"
 
- #ifdef WASMEDGE_PLUGIN_WASI_NN_BACKEND_WHISPER
- //#include "simdjson.h"
- #include <algorithm>
- #include <common.h>
- #include "whisper.h"
- #include <cstdlib>
- #include <sstream>
- #endif
+#ifdef WASMEDGE_PLUGIN_WASI_NN_BACKEND_WHISPER
+// #include "simdjson.h"
+#include "whisper.h"
+#include <algorithm>
+#include <common.h>
+#include <cstdlib>
+#include <sstream>
+#endif
 
- namespace WasmEdge::Host::WASINN::WHISPER {
- #ifdef WASMEDGE_PLUGIN_WASI_NN_BACKEND_WHISPER
+namespace WasmEdge::Host::WASINN::WHISPER {
+#ifdef WASMEDGE_PLUGIN_WASI_NN_BACKEND_WHISPER
 
- Expect<ErrNo> load([[maybe_unused]]WasiNNEnvironment &Env, [[maybe_unused]]Span<const Span<uint8_t>> Builders, [[maybe_unused]]Device Device, [[maybe_unused]]uint32_t &GraphId) noexcept {
-   //Env.NNGraph.emplace_back(Backend::WHISPER);
-   return ErrNo::Success;
- }
+Expect<ErrNo> load([[maybe_unused]] WasiNNEnvironment &Env,
+                   [[maybe_unused]] Span<const Span<uint8_t>> Builders,
+                   [[maybe_unused]] Device Device,
+                   [[maybe_unused]] uint32_t &GraphId) noexcept {
+  // Env.NNGraph.emplace_back(Backend::WHISPER);
+  return ErrNo::Success;
+}
 
- Expect<WASINN::ErrNo> initExecCtx([[maybe_unused]]WASINN::WasiNNEnvironment &,[[maybe_unused]] uint32_t,
-                                   uint32_t &) noexcept {
-   return WASINN::ErrNo::Success;
- }
- Expect<WASINN::ErrNo> setInput([[maybe_unused]]WASINN::WasiNNEnvironment &, [[maybe_unused]]uint32_t, [[maybe_unused]]uint32_t,
-                                [[maybe_unused]]const TensorData &) noexcept {
-   return WASINN::ErrNo::Success;
- }
- Expect<WASINN::ErrNo> getOutput([[maybe_unused]]WASINN::WasiNNEnvironment &,[[maybe_unused]] uint32_t,[[maybe_unused]] uint32_t,
-                                 [[maybe_unused]]Span<uint8_t>, [[maybe_unused]]uint32_t &) noexcept {
-   return WASINN::ErrNo::Success;
- }
- Expect<WASINN::ErrNo> getOutputSingle([[maybe_unused]]WASINN::WasiNNEnvironment &Env,
-                                       [[maybe_unused]]uint32_t ContextId,[[maybe_unused]]uint32_t Index,
-                                       [[maybe_unused]]Span<uint8_t> OutBuffer,
-                                       [[maybe_unused]]uint32_t &BytesWritten) noexcept {
-   return WASINN::ErrNo::Success;
- }
- Expect<WASINN::ErrNo> compute([[maybe_unused]]WASINN::WasiNNEnvironment &,[[maybe_unused]] uint32_t) noexcept {
-   return WASINN::ErrNo::Success;
- }
- Expect<WASINN::ErrNo> computeSingle([[maybe_unused]]WASINN::WasiNNEnvironment &Env,
-                                     [[maybe_unused]]uint32_t ContextId) noexcept {
-   return WASINN::ErrNo::Success;
- }
- Expect<WASINN::ErrNo> finiSingle([[maybe_unused]]WASINN::WasiNNEnvironment &Env,
-                                  [[maybe_unused]]uint32_t ContextId) noexcept {
-   return WASINN::ErrNo::Success;
- }
- #else
+Expect<WASINN::ErrNo> initExecCtx([[maybe_unused]] WASINN::WasiNNEnvironment &,
+                                  [[maybe_unused]] uint32_t,
+                                  uint32_t &) noexcept {
+  return WASINN::ErrNo::Success;
+}
+Expect<WASINN::ErrNo> setInput([[maybe_unused]] WASINN::WasiNNEnvironment &,
+                               [[maybe_unused]] uint32_t,
+                               [[maybe_unused]] uint32_t,
+                               [[maybe_unused]] const TensorData &) noexcept {
+  return WASINN::ErrNo::Success;
+}
+Expect<WASINN::ErrNo> getOutput([[maybe_unused]] WASINN::WasiNNEnvironment &,
+                                [[maybe_unused]] uint32_t,
+                                [[maybe_unused]] uint32_t,
+                                [[maybe_unused]] Span<uint8_t>,
+                                [[maybe_unused]] uint32_t &) noexcept {
+  return WASINN::ErrNo::Success;
+}
+Expect<WASINN::ErrNo>
+getOutputSingle([[maybe_unused]] WASINN::WasiNNEnvironment &Env,
+                [[maybe_unused]] uint32_t ContextId,
+                [[maybe_unused]] uint32_t Index,
+                [[maybe_unused]] Span<uint8_t> OutBuffer,
+                [[maybe_unused]] uint32_t &BytesWritten) noexcept {
+  return WASINN::ErrNo::Success;
+}
+Expect<WASINN::ErrNo> compute([[maybe_unused]] WASINN::WasiNNEnvironment &,
+                              [[maybe_unused]] uint32_t) noexcept {
+  return WASINN::ErrNo::Success;
+}
+Expect<WASINN::ErrNo>
+computeSingle([[maybe_unused]] WASINN::WasiNNEnvironment &Env,
+              [[maybe_unused]] uint32_t ContextId) noexcept {
+  return WASINN::ErrNo::Success;
+}
+Expect<WASINN::ErrNo>
+finiSingle([[maybe_unused]] WASINN::WasiNNEnvironment &Env,
+           [[maybe_unused]] uint32_t ContextId) noexcept {
+  return WASINN::ErrNo::Success;
+}
+#else
 namespace {
 Expect<ErrNo> reportBackendNotSupported() noexcept {
   spdlog::error("[WASI-NN] whisper backend is not built. use "
@@ -78,5 +92,5 @@ Expect<ErrNo> compute(WasiNNEnvironment &, uint32_t) noexcept {
   return reportBackendNotSupported();
 }
 
- #endif
- } // namespace WasmEdge::Host::WASINN::WHISPER
+#endif
+} // namespace WasmEdge::Host::WASINN::WHISPER

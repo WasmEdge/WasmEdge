e6163045124c9df5b8f3679c09198194f18eec71
hydai <z54981220@gmail.com>
2024-05-24 17:31:53 +0800

    [Misc] Fix typo to avoid linter failure
    Signed-off-by: hydai <z54981220@gmail.com>

--- test/expected/catch.hpp
+++ test/expected/catch.hpp
@@ -12478,7 +12478,7 @@ namespace Catch {
             }
             void close() override {
                 TrackerBase::close();
-                // Generator interface only finds out if it has another item on atual move
+                // Generator interface only finds out if it has another item on actual move
                 if (m_runState == CompletedSuccessfully && m_generator->next()) {
                     m_children.clear();
                     m_runState = Executing;

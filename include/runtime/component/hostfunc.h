421015ccd72b198499844bd98782b9917b881e29
Lîm Tsú-thuàn <dannypsnl@secondstate.io>
2024-06-20 02:59:53 +0800

    [Component] good internal typing
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>

--- include/runtime/component/hostfunc.h
+++ include/runtime/component/hostfunc.h
@@ -26,6 +26,7 @@ public:
 
   /// Getter of function type.
   const AST::FunctionType &getFuncType() const noexcept { return FuncType; }
+  AST::FunctionType &getFuncType() noexcept { return FuncType; }
 
 protected:
   AST::FunctionType FuncType;
@@ -33,6 +34,8 @@ protected:
 
 template <typename T> class HostFunction : public HostFunctionBase {
 public:
+  HostFunction() : HostFunctionBase() { initializeFuncType(); }
+
   Expect<void> run(Span<const ValInterface> Args,
                    Span<ValInterface> Rets) override {
     using F = FuncTraits<decltype(&T::body)>;
@@ -70,6 +73,7 @@ protected:
   }
 
   void initializeFuncType() {
+    auto &FuncType = getFuncType();
     using F = FuncTraits<decltype(&T::body)>;
     using ArgsT = typename F::ArgsT;
     FuncType.getParamTypes().reserve(F::ArgsN);

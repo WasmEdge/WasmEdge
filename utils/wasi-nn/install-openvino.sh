21c7bd98c2e4cfaa664a8cbd8c354fbb492615db
Pishyi Yehor <elmira-avant@proton.me>
2024-06-20 20:31:52 +0200

    Fixes
    Signed-off-by: Pishyi Yehor <elmira-avant@proton.me>

--- utils/wasi-nn/install-openvino.sh
+++ utils/wasi-nn/install-openvino.sh
@@ -6,7 +6,7 @@ set -e
 echo "Installing OpenVINO with version 2024.2.0"
 wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
 apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
-echo "deb https://apt.repos.intel.com/openvino/2024 ubuntu20 main" | sudo tee /etc/apt/sources.list.d/intel-openvino-2024.list
+echo "deb https://apt.repos.intel.com/openvino/2024 ubuntu20 main" | tee /etc/apt/sources.list.d/intel-openvino-2024.list
 apt update
 apt-get -y install openvino-2024.2.0
 ldconfig

a6330dfcd21a9b7119c3b60593672ff2a7561587
Lîm Tsú-thuàn <dannypsnl@secondstate.io>
2024-06-26 15:06:30 +0800

    [Plugin] impl wasi-poll poll-oneoff
    * update hostfunc's fromTuple to update Span of ValInterface
    * add emplace helper
    * fix `Wit` type converter, it still convert `vector` rather than `List`
    
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>

--- plugins/wasi_poll/func.cpp
+++ plugins/wasi_poll/func.cpp
@@ -8,7 +8,23 @@
 namespace WasmEdge {
 namespace Host {
 
-Expect<void> Drop::body(Pollable) { return {}; }
+bool isPollable(Pollable) {
+  // TODO: use a global HashMap to note this
+  return false;
+}
+
+Expect<void> Drop::body(Pollable) {
+  // TODO: ensure this affect the global HashMap
+  return {};
+}
+
+Expect<List<bool>> PollOneoff::body(List<Pollable> In) {
+  std::vector<bool> Res;
+  for (auto P : In.collection()) {
+    Res.push_back(isPollable(P));
+  }
+  return List<bool>(std::move(Res));
+}
 
 } // namespace Host
 } // namespace WasmEdge

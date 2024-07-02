2a46b523528c5b8c6de317f4cbe50044d4e86d63
YiYing He <yiying@secondstate.io>
2024-07-02 16:51:21 +0800

    [Plugin] Correct the module name and refine code of wasmedge_stablediffusion plugin.
    Signed-off-by: YiYing He <yiying@secondstate.io>

--- plugins/wasmedge_stablediffusion/sd_env.cpp
+++ plugins/wasmedge_stablediffusion/sd_env.cpp
@@ -1,6 +1,9 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2019-2022 Second State INC
 
 #include "sd_env.h"
 #include "sd_module.h"
+
 namespace WasmEdge {
 namespace Host {
 namespace {
@@ -33,38 +36,42 @@ EXPORT_GET_DESCRIPTOR(Descriptor)
 } // namespace
 
 namespace StableDiffusion {
+
 uint32_t SDEnviornment::addContext(sd_ctx_t *Ctx) noexcept {
   Contexts.push_back(Ctx);
   return Contexts.size() - 1;
 }
+
 sd_ctx_t *SDEnviornment::getContext(const uint32_t Id) noexcept {
   return Contexts[Id];
 }
-void SBLog(enum sd_log_level_t level, const char *log, void *) {
-  if (!log) {
+
+void SBLog(enum sd_log_level_t Level, const char *Log, void *) {
+  if (!Log) {
     return;
   }
-  std::string levelStr;
-  switch (level) {
+  std::string LevelStr;
+  switch (Level) {
   case SD_LOG_DEBUG:
-    levelStr = "DEBUG";
+    LevelStr = "DEBUG";
     break;
   case SD_LOG_INFO:
-    levelStr = "INFO";
+    LevelStr = "INFO";
     break;
   case SD_LOG_WARN:
-    levelStr = "WARN";
+    LevelStr = "WARN";
     break;
   case SD_LOG_ERROR:
-    levelStr = "ERROR";
+    LevelStr = "ERROR";
     break;
   default:
-    levelStr = "?????";
+    LevelStr = "?????";
     break;
   }
 
-  spdlog::info("[WasmEdge-StableDiffusion] SD-log: [{}] {}", levelStr, log);
+  spdlog::info("[WasmEdge-StableDiffusion] SD-log: [{}] {}", LevelStr, Log);
 }
+
 } // namespace StableDiffusion
 } // namespace Host
 } // namespace WasmEdge

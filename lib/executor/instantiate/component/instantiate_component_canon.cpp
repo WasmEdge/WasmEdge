37f7b7627734f33f4d820c78e570ba03479d9ced
Lîm Tsú-thuàn <dannypsnl@secondstate.io>
2024-07-04 15:15:31 +0800

    [Fix] intro a temporary variable V of variant fix the build issue
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>

--- lib/executor/instantiate/component/instantiate_component_canon.cpp
+++ lib/executor/instantiate/component/instantiate_component_canon.cpp
@@ -225,9 +225,10 @@ public:
         auto Len = Args[PI++];
         std::string_view V =
             Memory->getStringView(Idx.get<uint32_t>(), Len.get<uint32_t>());
-        std::string S{V.begin(), V.end()};
-        // FIXME: This crashed on Linux
-        HigherArgs.push_back(std::move(S));
+        // FIXME: This crashed on Ubuntu/Debian clang
+        ValInterface VI{};
+        VI.emplace<std::string>(std::string(V.begin(), V.end()));
+        HigherArgs.push_back(VI);
         break;
       }
       default:

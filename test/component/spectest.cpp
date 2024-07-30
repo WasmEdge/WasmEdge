e73eb98b299bf28b095f7dd2d4da3323374a1893
Lîm Tsú-thuàn <dannypsnl@secondstate.io>
2024-07-30 12:52:17 +0800

    [Test] add `http.wasm` into `data/`, ensure it's load/validate
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>

--- test/component/spectest.cpp
+++ test/component/spectest.cpp
@@ -11,25 +11,37 @@ namespace {
 
 using namespace WasmEdge;
 
+template <typename T> void assertOk(Expect<T> Res, const char *Message) {
+  if (!Res) {
+    EXPECT_TRUE(false) << Message;
+  }
+}
+
 TEST(Component, LoadAndRun_SimpleBinary) {
   Configure Conf{};
   Conf.addProposal(Proposal::Component);
   VM::VM VM{Conf};
 
-  if (auto Res = VM.loadWasm("data/core.wasm"); !Res) {
-    EXPECT_TRUE(false) << "failed to load component binary";
-  }
+  assertOk(VM.loadWasm("data/core.wasm"), "failed to load component binary");
+  assertOk(VM.validate(), "failed to validate");
+  assertOk(VM.instantiate(), "failed to instantiate");
 
-  VM.validate();
-  VM.instantiate();
-  auto Res = VM.execute("mdup", {ValInterface(ValVariant(100))},
+  uint64_t V = 100;
+  auto Res = VM.execute("mdup", {ValInterface(ValVariant(V))},
                         {ValType(TypeCode::I64)});
-  if (!Res) {
-    EXPECT_TRUE(false) << "failed to execute";
-  }
+  assertOk(Res, "failed to execute");
   std::vector<std::pair<ValInterface, ValType>> Result = *Res;
-  auto Ret = std::get<ValVariant>(Result[0].first).get<int64_t>();
+  auto Ret = std::get<ValVariant>(Result[0].first).get<uint64_t>();
   EXPECT_EQ(Ret, 200);
+}
+
+TEST(Component, Load_HttpBinary) {
+  Configure Conf{};
+  Conf.addProposal(Proposal::Component);
+  VM::VM VM{Conf};
+
+  assertOk(VM.loadWasm("data/http.wasm"), "failed to load component binary");
+  assertOk(VM.validate(), "failed to validate");
 }
 
 } // namespace

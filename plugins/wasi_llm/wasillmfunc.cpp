62e97b49909e522476b57335d1de83d372d75554
Jun Zhang <jun@junz.org>
2024-08-04 19:14:45 +0800

    [WASI-LLM] Add initial CPU backend for wasi_llm
    Signed-off-by: Jun Zhang <jun@junz.org>

--- plugins/wasi_llm/wasillmfunc.cpp
+++ plugins/wasi_llm/wasillmfunc.cpp
@@ -2,8 +2,10 @@
 // SPDX-FileCopyrightText: 2019-2024 Second State INC
 
 #include "wasillmfunc.h"
+#include "common/errcode.h"
 #include "common/spdlog.h"
-
+#include "llmc_fwd.h"
+#include "types.h"
 #include <string>
 #include <string_view>
 
@@ -13,68 +15,110 @@ namespace Host {
 Expect<WASILLM::ErrNo>
 WasiLLMModelCreate::bodyImpl(const Runtime::CallingFrame &Frame,
                              uint32_t CheckPointPath,
-                             uint32_t CheckPointPathLen) {
-  (void)Frame;
-  (void)CheckPointPath;
-  (void)CheckPointPathLen;
-  return WASILLM::ErrNo::InvalidArgument;
+                             uint32_t CheckPointPathLen, uint32_t ModelIdPtr) {
+  auto *MemInst = Frame.getMemoryByIndex(0);
+  if (MemInst == nullptr) {
+    spdlog::error("[WasmEdge-LLM] Memory instance not found."sv);
+    return WASILLM::ErrNo::MissingMemory;
+  }
+  auto CheckPointPathSpan =
+      MemInst->getSpan<char>(CheckPointPath, CheckPointPathLen);
+  if (unlikely(CheckPointPathSpan.size() != CheckPointPathLen)) {
+    spdlog::error(
+        "[WasmEdge-LLM] Failed when accessing the input checkpoint path memory."sv);
+    return WASILLM::ErrNo::MissingMemory;
+  }
+
+  auto *ModelId = MemInst->getPointer<uint32_t *>(ModelIdPtr);
+  if (unlikely(ModelId == nullptr)) {
+    spdlog::error(
+        "[WasmEdge-LLM] Failed when accessing the return model memory."sv);
+    return WASILLM::ErrNo::InvalidArgument;
+  }
+  std::string CheckPointPathStr =
+      std::string(CheckPointPathSpan.begin(),
+                  CheckPointPathSpan.begin() + CheckPointPathSpan.size());
+  GPT2 *Model = gpt2_create(CheckPointPathStr.data());
+  *ModelId = Env.addModel(Model);
+  return WASILLM::ErrNo::Success;
 }
 
-Expect<WASILLM::ErrNo>
-WasiLLMModelFree::bodyImpl(const Runtime::CallingFrame &Frame,
-                           uint32_t ModelPtr) {
-  (void)Frame;
-  (void)ModelPtr;
-  return WASILLM::ErrNo::InvalidArgument;
-}
+Expect<WASILLM::ErrNo> WasiLLMDataLoaderCreate::bodyImpl(
+    const Runtime::CallingFrame &Frame, uint32_t DataPath, uint32_t DataPathLen,
+    uint32_t B, uint32_t T, uint32_t ProcessRank, uint32_t NumProcesses,
+    int32_t ShouldShuffle, uint32_t DataLoaderIdPtr) {
+  auto *MemInst = Frame.getMemoryByIndex(0);
+  if (MemInst == nullptr) {
+    spdlog::error("[WasmEdge-LLM] Memory instance not found."sv);
+    return WASILLM::ErrNo::MissingMemory;
+  }
+  auto DataPathSpan = MemInst->getSpan<char>(DataPath, DataPathLen);
+  if (unlikely(DataPathSpan.size() != DataPathLen)) {
+    spdlog::error(
+        "[WasmEdge-LLM] Failed when accessing the input dataloader path memory."sv);
+    return WASILLM::ErrNo::MissingMemory;
+  }
 
-Expect<WASILLM::ErrNo>
-WasiLLMDataLoaderCreate::bodyImpl(const Runtime::CallingFrame &Frame,
-                                  uint32_t DataPath, uint32_t DataPathLen) {
-  (void)Frame;
-  (void)DataPath;
-  (void)DataPathLen;
-  return WASILLM::ErrNo::InvalidArgument;
-}
+  auto *DataLoaderId = MemInst->getPointer<uint32_t *>(DataLoaderIdPtr);
+  if (unlikely(DataLoaderId == nullptr)) {
+    spdlog::error(
+        "[WasmEdge-LLM] Failed when accessing the return dataloader memory."sv);
+    return WASILLM::ErrNo::InvalidArgument;
+  }
 
-Expect<WASILLM::ErrNo>
-WasiLLMDataLoaderFree::bodyImpl(const Runtime::CallingFrame &Frame,
-                                uint32_t DataLoaderPtr) {
-  (void)Frame;
-  (void)DataLoaderPtr;
-  return WASILLM::ErrNo::InvalidArgument;
+  std::string DataPathStr = std::string(
+      DataPathSpan.begin(), DataPathSpan.begin() + DataPathSpan.size());
+  DataLoader *D = dataloader_create(DataPathStr.data(), B, T, ProcessRank,
+                                    NumProcesses, ShouldShuffle);
+  *DataLoaderId = Env.addDataLoader(D);
+  return WASILLM::ErrNo::Success;
 }
 
 Expect<WASILLM::ErrNo>
 WasiLLMTokenizerCreate::bodyImpl(const Runtime::CallingFrame &Frame,
-                                 uint32_t FilePath, uint32_t FilePathLen) {
-  (void)Frame;
-  (void)FilePath;
-  (void)FilePathLen;
-  return WASILLM::ErrNo::InvalidArgument;
-}
+                                 uint32_t FilePath, uint32_t FilePathLen,
+                                 uint32_t TokenizerIdPtr) {
+  auto *MemInst = Frame.getMemoryByIndex(0);
+  if (MemInst == nullptr) {
+    spdlog::error("[WasmEdge-LLM] Memory instance not found."sv);
+    return WASILLM::ErrNo::MissingMemory;
+  }
+  auto FilePathSpan = MemInst->getSpan<char>(FilePath, FilePathLen);
+  if (unlikely(FilePathSpan.size() != FilePathLen)) {
+    spdlog::error(
+        "[WasmEdge-LLM] Failed when accessing the input tokenizer path memory."sv);
+    return WASILLM::ErrNo::MissingMemory;
+  }
 
-Expect<WASILLM::ErrNo>
-WasiLLMTokenizerFree::bodyImpl(const Runtime::CallingFrame &Frame,
-                               uint32_t TokenizerPtr) {
-  (void)Frame;
-  (void)TokenizerPtr;
-  return WASILLM::ErrNo::InvalidArgument;
+  auto *TokenizerId = MemInst->getPointer<uint32_t *>(TokenizerIdPtr);
+  if (unlikely(TokenizerId == nullptr)) {
+    spdlog::error(
+        "[WasmEdge-LLM] Failed when accessing the return tokenizer memory."sv);
+    return WASILLM::ErrNo::InvalidArgument;
+  }
+  std::string FilePathStr = std::string(
+      FilePathSpan.begin(), FilePathSpan.begin() + FilePathSpan.size());
+  Tokenizer *T = tokenizer_create(FilePathStr.data());
+  *TokenizerId = Env.addTokenizer(T);
+  return WASILLM::ErrNo::Success;
 }
 
 Expect<WASILLM::ErrNo>
 WasiLLMModelTrain::bodyImpl(const Runtime::CallingFrame &Frame,
-                            uint32_t ModelPtr, uint32_t TrainDataLoaderPtr,
-                            uint32_t ValDataLoaderPtr, uint32_t TokenizerPtr,
-                            uint32_t Lr, uint32_t Epoch) {
-  (void)Frame;
-  (void)ModelPtr;
-  (void)TrainDataLoaderPtr;
-  (void)ValDataLoaderPtr;
-  (void)TokenizerPtr;
-  (void)Lr;
-  (void)Epoch;
-  return WASILLM::ErrNo::InvalidArgument;
+                            uint32_t ModelId, uint32_t TrainDataLoaderId,
+                            uint32_t ValDataLoaderId, uint32_t TokenizerId,
+                            uint32_t B, uint32_t T, float Lr, uint32_t Epoch) {
+  auto *MemInst = Frame.getMemoryByIndex(0);
+  if (MemInst == nullptr) {
+    spdlog::error("[WasmEdge-LLM] Memory instance not found."sv);
+    return WASILLM::ErrNo::MissingMemory;
+  }
+  GPT2 *Model = Env.getModel(ModelId);
+  DataLoader *TrainDataLoader = Env.getDataLoader(TrainDataLoaderId);
+  DataLoader *ValDataLoader = Env.getDataLoader(ValDataLoaderId);
+  Tokenizer *Tokenizer = Env.getTokenizer(TokenizerId);
+  gpt2_train(Model, TrainDataLoader, ValDataLoader, Tokenizer, B, T, Lr, Epoch);
+  return WASILLM::ErrNo::Success;
 }
 
 } // namespace Host

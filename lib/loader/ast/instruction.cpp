b14a83118ac4d0fecaf688b508b71eb771c394dc
junxiang Mu <1948535941@qq.com>
2024-08-05 15:30:04 +0800

    change the writing of the conditional judgment statement
    Signed-off-by: junxiang Mu <1948535941@qq.com>

--- lib/loader/ast/instruction.cpp
+++ lib/loader/ast/instruction.cpp
@@ -313,15 +313,18 @@ Expect<void> Loader::loadInstruction(AST::Instruction &Instr) {
     if (auto Res = readU32(Instr.getMemoryAlign()); unlikely(!Res)) {
       return Unexpect(Res);
     }
-    if (Instr.getMemoryAlign() >= 128 ||
-        (Instr.getMemoryAlign() >= 32 &&
-         !Conf.hasProposal(Proposal::MultiMemories))) {
+    if (Instr.getMemoryAlign() >= 128) {
       return logLoadError(ErrCode::Value::InvalidStoreAlignment,
                           FMgr.getLastOffset(), ASTNodeAttr::Instruction);
     } else if (Instr.getMemoryAlign() >= 64) {
-      Instr.getMemoryAlign() -= 64;
-      if (auto Res = readU32(Instr.getTargetIndex()); unlikely(!Res)) {
-        return Unexpect(Res);
+      if (Conf.hasProposal(Proposal::MultiMemories)) {
+        Instr.getMemoryAlign() -= 64;
+        if (auto Res = readU32(Instr.getTargetIndex()); unlikely(!Res)) {
+          return Unexpect(Res);
+        }
+      } else {
+        return logLoadError(ErrCode::Value::InvalidStoreAlignment,
+                            FMgr.getLastOffset(), ASTNodeAttr::Instruction);
       }
     }
     if (auto Res = readU32(Instr.getMemoryOffset()); unlikely(!Res)) {

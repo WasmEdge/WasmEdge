ff1c84875cfe9c9a48b1a5deeecac6dfa812b865
PeterD1524 <53310459+PeterD1524@users.noreply.github.com>
2024-08-05 14:39:18 +0800

    [Docker] install onnxruntime in manylinux_2_28-plugins-deps for WASI-NN Piper CI (#3622)
    * install onnxruntime in manylinux_2_28-plugins-deps for WASI-NN Piper CI
    
    Signed-off-by: PeterD1524 <qaz246135@gmail.com>
    
    * update utils/wasi-nn/install-onnxruntime.sh to support both x86_64 and aarch64
    
    Signed-off-by: PeterD1524 <qaz246135@gmail.com>
    
    ---------
    
    Signed-off-by: PeterD1524 <qaz246135@gmail.com>

--- utils/wasi-nn/install-onnxruntime.sh
+++ utils/wasi-nn/install-onnxruntime.sh
@@ -2,9 +2,18 @@
 
 set -e
 
+case "$(uname -m)" in
+    'x86_64') ARCH='x64' ;;
+    'aarch64') ARCH='aarch64' ;;
+    *)
+        echo 'Cannot determine architecture for onnxruntime' >&2
+        exit 1
+        ;;
+esac
+
 : ${ONNXRUNTIME_VERSION:=1.14.1}
 
-ONNXRUNTIME_NAME="onnxruntime-linux-x64-${ONNXRUNTIME_VERSION}"
+ONNXRUNTIME_NAME="onnxruntime-linux-${ARCH}-${ONNXRUNTIME_VERSION}"
 ONNXRUNTIME_TGZ="${ONNXRUNTIME_NAME}.tgz"
 
 curl -LO "https://github.com/microsoft/onnxruntime/releases/download/v${ONNXRUNTIME_VERSION}/${ONNXRUNTIME_TGZ}"
@@ -12,3 +21,5 @@ tar zxf "${ONNXRUNTIME_TGZ}"
 mv "${ONNXRUNTIME_NAME}/include/"* /usr/local/include/
 mv "${ONNXRUNTIME_NAME}/lib/"* /usr/local/lib/
 rm -rf "${ONNXRUNTIME_TGZ}" "${ONNXRUNTIME_NAME}"
+
+ldconfig

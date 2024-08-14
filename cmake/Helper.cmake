WasmEdge/WasmEdge

commit 0b285f7bc01cc1797997da1e537161d31d7449c1

[Plugin] Initial support for FFmpeg (#2885)
* added ffmpeg to cmake

Signed-off-by: Hrushi20 <hrushikesh20thegreat@gmail.com>

* error.h and avformat.h file functions

Signed-off-by: Hrushi20 <hrushikesh20thegreat@gmail.com>

* fix compile error

Signed-off-by: Hrushi20 <hrushikesh20thegreat@gmail.com>

* avFormatContext, avInputFormat struct fields

Signed-off-by: Hrushi20 <hrushikesh20thegreat@gmail.com>

* av_best_stream, stream, mediaType

Signed-off-by: Hrushi20 <hrushikesh20thegreat@gmail.com>

* avCodec, avCodecParameters, AVCodecID

Signed-off-by: Hrushi20 <hrushikesh20thegreat@gmail.com>

* avRational

Signed-off-by: Hrushi20 <hrushikesh20thegreat@gmail.com>

* AVPixelFormat,AVFrame

Signed-off-by: Hrushi20 <hrushikesh20thegreat@gmail.com>

* clean code, refactor

Signed-off-by: Hrushi20 <hrushikesh20thegreat@gmail.com>

refactor

Signed-off-by: Hrushi20 <hrushikesh20thegreat@gmail.com>

* avfilter, swscale

Signed-off-by: Hrushi20 <hrushikesh20thegreat@gmail.com>

* bug fix

Signed-off-by: Hrushi20 <hrushikesh20thegreat@gmail.com>

* init test

Signed-off-by: Hrushi20 <hrushikesh20thegreat@gmail.com>

* naming plugin

Signed-off-by: Hrushi20 <hrushikesh20thegreat@gmail.com>

* AVSample

Signed-off-by: Hrushi20 <hrushikesh20thegreat@gmail.com>

* AVFrame, ChannelLayout

Signed-off-by: Hrushi20 <hrushikesh20thegreat@gmail.com>

* added index while fetching data AVFrame

Signed-off-by: Hrushi20 <hrushikesh20thegreat@gmail.com>

* AVRational Tests

Signed-off-by: Hrushi20 <hrushikesh20thegreat@gmail.com>

* swscale tests init

Signed-off-by: Hrushi20 <hrushikesh20thegreat@gmail.com>

* SwsFilter, SwsVector

Signed-off-by: Hrushi20 <hrushikesh20thegreat@gmail.com>

swsvector

Signed-off-by: Hrushi20 <hrushikesh20thegreat@gmail.com>

* swscale tests

Signed-off-by: Hrushi20 <hrushikesh20thegreat@gmail.com>

* bug fix

cmake/Helper.cmake
@@ -103,7 +103,7 @@ endif()
 
 if(WIN32)
   add_definitions(-D_CRT_SECURE_NO_WARNINGS -D_ENABLE_EXTENDED_ALIGNED_STORAGE -DNOMINMAX -D_ITERATOR_DEBUG_LEVEL=0)
-  if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+  if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
     list(APPEND WASMEDGE_CFLAGS
       -Wno-c++98-compat
       -Wno-c++98-compat-pedantic
@@ -128,7 +128,7 @@ function(wasmedge_setup_target target)
   set_target_properties(${target} PROPERTIES
     CXX_STANDARD 17
     CXX_EXTENSIONS OFF
-    CXX_VISIBILITY_PRESET hidden
+    #    CXX_VISIBILITY_PRESET hidden
     ENABLE_EXPORTS ON
     POSITION_INDEPENDENT_CODE ON
     VISIBILITY_INLINES_HIDDEN ON
@@ -235,7 +235,7 @@ if((WASMEDGE_LINK_LLVM_STATIC OR WASMEDGE_BUILD_STATIC_LIB) AND WASMEDGE_USE_LLV
       ${LLVM_LIBRARY_DIR}/liblldWasm.a
     )
   endif()
-  if (APPLE AND LLVM_VERSION_MAJOR GREATER_EQUAL 15)
+  if(APPLE AND LLVM_VERSION_MAJOR GREATER_EQUAL 15)
     # For LLVM 15 or greater on MacOS
     find_package(zstd REQUIRED)
     get_filename_component(ZSTD_PATH "${zstd_LIBRARY}" DIRECTORY)

commit f5b20416fc596b5f12bb2661668584cbb34dc5a4

[CMake] Set CXX_VISIBILITY_PRESET to hidden. Regression by 0b285f7b
Signed-off-by: hydai <z54981220@gmail.com>

cmake/Helper.cmake
@@ -140,7 +140,7 @@ function(wasmedge_setup_target target)
   set_target_properties(${target} PROPERTIES
     CXX_STANDARD 17
     CXX_EXTENSIONS OFF
-    #    CXX_VISIBILITY_PRESET hidden
+    CXX_VISIBILITY_PRESET hidden
     ENABLE_EXPORTS ON
     POSITION_INDEPENDENT_CODE ON
     VISIBILITY_INLINES_HIDDEN ON
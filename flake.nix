685024c4ed4eb418d2fee7de815502cfe006ff48
Lîm Tsú-thuàn <dannypsnl@secondstate.io>
2024-08-20 11:04:40 +0800

    [Nix] fix build
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system overlays; };
+        pkgs = import nixpkgs { inherit system; };
         llvmPackages = pkgs.llvmPackages_16;
 
         wasmedge = pkgs.stdenv.mkDerivation {

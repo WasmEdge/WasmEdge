295c5d33210774162c6e3e43232921fca32a7a2d
Lîm Tsú-thuàn <dannypsnl@secondstate.io>
2024-08-21 17:22:33 +0800

    [Nix] share the buildInputs
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>

--- flake.nix
+++ flake.nix
@@ -10,23 +10,24 @@
         pkgs = import nixpkgs { inherit system; };
         llvmPackages = pkgs.llvmPackages_17;
 
+        wasmedge_buildInputs = with pkgs; [
+          cmake
+          llvmPackages.clang-unwrapped
+          llvmPackages.lld
+          llvmPackages.llvm
+          openssl
+          pkg-config
+          libxml2
+          spdlog
+        ] ++ pkgs.lib.optionals (system == "x86_64-darwin" || system == "aarch64-darwin") [
+          pkgs.darwin.apple_sdk.frameworks.Foundation
+        ];
         wasmedge = pkgs.stdenv.mkDerivation {
           name = "wasmedge";
           version = "0.14.0";
           src = ./.;
 
-          buildInputs = with pkgs; [
-            cmake
-            llvmPackages.clang-unwrapped
-            llvmPackages.lld
-            llvmPackages.llvm
-            openssl
-            pkg-config
-            libxml2
-            spdlog
-          ] ++ pkgs.lib.optionals (system == "x86_64-darwin" || system == "aarch64-darwin") [
-            pkgs.darwin.apple_sdk.frameworks.Foundation
-          ];
+          buildInputs = wasmedge_buildInputs;
           cmakeFlags = [
             "-DCMAKE_BUILD_TYPE=Debug"
             "-DWASMEDGE_BUILD_PLUGINS=OFF"
@@ -42,7 +43,7 @@
             wasmedge
             ninja
             gcovr
-          ];
+          ] ++ wasmedge_buildInputs;
 
           LIBCLANG_PATH = "${llvmPackages.libclang.lib}/lib";
         };

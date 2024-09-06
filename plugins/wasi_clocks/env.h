be3d9cc1897697cfd3f7f12c2fdbf9d8915bf45e
Lîm Tsú-thuàn <dannypsnl@secondstate.io>
2024-09-06 08:55:09 +0800

    [Preview2] plugins
    + wasi-io/error
    + wasi:io/streams
    
    + wasi-cli/environment
    + wasi:cli/exit
    + wasi:cli/stdin
    + wasi:cli/stdout
    + wasi:cli/stderr
    
    + wasi:filesystem/types
    + wasi:filesystem/preopens
    
    + wasi:clocks/wall-clock
    
    [Component] named type recording
    
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>

---
+++ plugins/wasi_clocks/env.h
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2019-2024 Second State INC
+#pragma once
+
+#include "plugin/plugin.h"
+
+#include <cstdint>
+#include <vector>
+
+namespace WasmEdge {
+namespace Host {
+
+class WasiClocksEnvironment {};
+
+} // namespace Host
+} // namespace WasmEdge
---
+++ plugins/wasi_filesystem/module.cpp
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2019-2024 Second State INC
+
+#include "module.h"
+#include "func.h"
+
+#include <memory>
+
+namespace WasmEdge {
+namespace Host {
+
+TypesModule::TypesModule() : ComponentInstance("wasi:filesystem/types@0.2.0") {}
+PreopensModule::PreopensModule()
+    : ComponentInstance("wasi:filesystem/preopens@0.2.0") {}
+
+} // namespace Host
+} // namespace WasmEdge

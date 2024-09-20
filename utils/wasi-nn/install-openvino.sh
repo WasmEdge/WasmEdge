9813a69f9f6a7067a217a7df4f780e23816e9796
Yi Huang <yi@secondstate.io>
2024-09-20 21:24:35 +0800

    [Docker] Ubuntu: Clean-up files after installations
    Signed-off-by: Yi Huang <yi@secondstate.io>

--- utils/wasi-nn/install-openvino.sh
+++ utils/wasi-nn/install-openvino.sh
@@ -4,8 +4,10 @@
 
 set -e
 echo "Installing OpenVINO with version 2024.2.0"
-wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
-apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
+KEY_FILE=GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
+wget https://apt.repos.intel.com/intel-gpg-keys/$KEY_FILE && \
+    apt-key add $KEY_FILE && \
+    rm -f $KEY_FILE
 echo "deb https://apt.repos.intel.com/openvino/2024 ubuntu20 main" | tee /etc/apt/sources.list.d/intel-openvino-2024.list
 apt update
 apt-get -y install openvino-2024.2.0

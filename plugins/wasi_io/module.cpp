a569274951d2cf076aba92d741b6dde3a35ce43c
Lîm Tsú-thuàn <dannypsnl@secondstate.io>
2024-10-22 12:43:42 +0800

    [Component] exploring how to provide dtor of resource from plugin
    From binary we can see code like
    
      import [func 2] Import { module: "wasi:io/streams@0.2.0", name:
    "[resource-drop]output-stream", ty: Func(0) }
    
    so we can figure out that every resource related function is export as
    an usual host-function
    
    rename addExport(DefType) to addHostType
    
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>

--- plugins/wasi_io/module.cpp
+++ plugins/wasi_io/module.cpp
@@ -9,53 +9,18 @@
 namespace WasmEdge {
 namespace Host {
 
-// TODO: Use wasi:error/error error to figure out what a resource host function
-// should be.
-class ToDebugString : public WasiIO<ToDebugString> {
-public:
-  ToDebugString(WasiIOEnvironment &HostEnv) : WasiIO(HostEnv) {}
-  using IoError = uint32_t;
-  Expect<void> body(IoError) { return {}; }
-};
-
-class IoError : public AST::Component::ResourceType {
-public:
-  IoError(WasiIOEnvironment &Env)
-      : ResourceType(new Runtime::Instance::ComponentInstance("error")) {
-    // to-debug-string: func() -> string;
-    Impl->addHostFunc("to-debug-string", std::make_unique<ToDebugString>(Env));
-  }
-};
-
 WasiIOErrorModule::WasiIOErrorModule()
     : ComponentInstance("wasi:io/error@0.2.0") {
-  addExport("error", IoError(getEnv()));
+  addHostType("error", IoError(getEnv()));
 }
-
-class InputStream : public AST::Component::ResourceType {
-public:
-  InputStream()
-      : ResourceType(new Runtime::Instance::ComponentInstance("input-stream")) {
-  }
-};
-class OutputStream : public AST::Component::ResourceType {
-public:
-  OutputStream()
-      : ResourceType(
-            new Runtime::Instance::ComponentInstance("output-stream")) {}
-};
-
-class StreamError : public AST::Component::ResourceType {
-public:
-  StreamError()
-      : ResourceType(new Runtime::Instance::ComponentInstance("error")) {}
-};
 
 WasiIOStreamsModule::WasiIOStreamsModule()
     : ComponentInstance("wasi:io/streams@0.2.0") {
-  addExport("input-stream", InputStream());
-  addExport("output-stream", OutputStream());
-  addExport("error", StreamError());
+  addHostType("input-stream", InputStream());
+  addHostType("output-stream", OutputStream());
+  addHostFunc("[resource-drop]output-stream",
+              std::make_unique<DropOutputStream>(Env));
+  addHostType("error", StreamError());
 }
 
 } // namespace Host

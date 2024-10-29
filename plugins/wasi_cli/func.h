91224c4a1213e030e26937c919e3d80bbb67ff81
Lîm Tsú-thuàn <dannypsnl@secondstate.io>
2024-10-29 13:32:40 +0800

    add host functions for wasi:cli/environment
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>

--- plugins/wasi_cli/func.h
+++ plugins/wasi_cli/func.h
@@ -7,5 +7,29 @@
 #include "runtime/callingframe.h"
 
 namespace WasmEdge {
-namespace Host {} // namespace Host
+namespace Host {
+
+class GetEnvironment : public WasiCli<GetEnvironment> {
+public:
+  GetEnvironment(WasiCliEnvironment &HostEnv) : WasiCli(HostEnv) {}
+  // func() -> list<tuple<string, string>>
+  // TODO: fix the type, need to introduce tuple type
+  Expect<void> body() { return {}; }
+};
+
+class GetArguments : public WasiCli<GetArguments> {
+public:
+  GetArguments(WasiCliEnvironment &HostEnv) : WasiCli(HostEnv) {}
+  // get-arguments: func() -> list<string>;
+  Expect<List<std::string>> body();
+};
+
+class InitialCwd : public WasiCli<InitialCwd> {
+public:
+  InitialCwd(WasiCliEnvironment &HostEnv) : WasiCli(HostEnv) {}
+  // initial-cwd: func() -> option<string>;
+  Expect<std::string> body();
+};
+
+} // namespace Host
 } // namespace WasmEdge

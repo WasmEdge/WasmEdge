a34ba7b6985f26b9d15b227d8b8692b0b02b82d5
Lîm Tsú-thuàn <dannypsnl@secondstate.io>
2024-10-30 15:20:28 +0800

    add methods of wasi:io/streams output-stream
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>

--- plugins/wasi_io/module.cpp
+++ plugins/wasi_io/module.cpp
@@ -20,9 +20,17 @@ WasiIOStreamsModule::WasiIOStreamsModule()
   addHostType("input-stream", InputStream());
   addHostFunc("[resource-drop]input-stream",
               std::make_unique<DropInputStream>(Env));
+
   addHostType("output-stream", OutputStream());
+  addHostFunc("[method]output-stream.check-write",
+              std::make_unique<OutputStream_CheckWrite>(Env));
+  addHostFunc("[method]output-stream.write",
+              std::make_unique<OutputStream_Write>(Env));
+  addHostFunc("[method]output-stream.blocking-flush",
+              std::make_unique<OutputStream_BlockingFlush>(Env));
   addHostFunc("[resource-drop]output-stream",
               std::make_unique<DropOutputStream>(Env));
+
   addHostType("error", StreamError());
   addHostFunc("[resource-drop]error", std::make_unique<DropStreamError>(Env));
 }

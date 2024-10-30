f06d5dadc5c7f1f5c887cd7865fcab24ba2f2ae5
Lîm Tsú-thuàn <dannypsnl@secondstate.io>
2024-10-30 15:20:28 +0800

    add methods of wasi:io/streams output-stream
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>

--- plugins/wasi_io/func.h
+++ plugins/wasi_io/func.h
@@ -16,6 +16,21 @@ public:
   Expect<void> body(uint32_t ThisIOError);
 };
 
+class OutputStream_CheckWrite : public WasiIO<OutputStream_CheckWrite> {
+public:
+  OutputStream_CheckWrite(WasiIOEnvironment &HostEnv) : WasiIO(HostEnv) {}
+  Expect<void> body() { return {}; }
+};
+class OutputStream_Write : public WasiIO<OutputStream_Write> {
+public:
+  OutputStream_Write(WasiIOEnvironment &HostEnv) : WasiIO(HostEnv) {}
+  Expect<void> body() { return {}; }
+};
+class OutputStream_BlockingFlush : public WasiIO<OutputStream_BlockingFlush> {
+public:
+  OutputStream_BlockingFlush(WasiIOEnvironment &HostEnv) : WasiIO(HostEnv) {}
+  Expect<void> body() { return {}; }
+};
 class DropOutputStream : public WasiIO<DropOutputStream> {
 public:
   DropOutputStream(WasiIOEnvironment &HostEnv) : WasiIO(HostEnv) {}

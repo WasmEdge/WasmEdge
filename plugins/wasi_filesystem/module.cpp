3c7f1e838f549b54323b5d8ce35728c91fccb9aa
Lîm Tsú-thuàn <dannypsnl@secondstate.io>
2024-11-01 11:35:17 +0800

    complete needed wasi:filesystem/types host functions
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>

--- plugins/wasi_filesystem/module.cpp
+++ plugins/wasi_filesystem/module.cpp
@@ -20,6 +20,17 @@ TypesModule::TypesModule() : ComponentInstance("wasi:filesystem/types@0.2.0") {
   addHostType("descriptor", Descriptor());
   addHostFunc("[method]descriptor.write-via-stream",
               std::make_unique<Descriptor_WriteViaStream>(Env));
+  addHostFunc("[method]descriptor.append-via-stream",
+              std::make_unique<Descriptor_AppendViaStream>(Env));
+  addHostFunc("[method]descriptor.get-type",
+              std::make_unique<Descriptor_GetType>(Env));
+  addHostFunc("[method]descriptor.stat",
+              std::make_unique<Descriptor_Stat>(Env));
+  addHostFunc("[resource-drop]descriptor",
+              std::make_unique<DropDescriptor>(Env));
+
+  addHostFunc("filesystem-error-code",
+              std::make_unique<FilesystemErrorCode>(Env));
 }
 
 PreopensModule::PreopensModule()

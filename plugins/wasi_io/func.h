2fc21f36c9406c25e6b62148aa0fafe1ff42972f
Lîm Tsú-thuàn <dannypsnl@secondstate.io>
2024-11-11 13:10:58 +0800

    [Component] resource.drop is a runtime decision
    Thus, no need to implement anything about it from wasi side
    
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>

--- plugins/wasi_io/func.h
+++ plugins/wasi_io/func.h
@@ -10,12 +10,6 @@
 namespace WasmEdge {
 namespace Host {
 
-class DropError : public WasiIO<DropError> {
-public:
-  DropError(WasiIOEnvironment &HostEnv) : WasiIO(HostEnv) {}
-  Expect<void> body(uint32_t ThisIOError);
-};
-
 class OutputStream_CheckWrite : public WasiIO<OutputStream_CheckWrite> {
 public:
   OutputStream_CheckWrite(WasiIOEnvironment &HostEnv) : WasiIO(HostEnv) {}
@@ -37,23 +31,6 @@ public:
   OutputStream_BlockingWriteAndFlush(WasiIOEnvironment &HostEnv)
       : WasiIO(HostEnv) {}
   Expect<void> body() { return {}; }
-};
-class DropOutputStream : public WasiIO<DropOutputStream> {
-public:
-  DropOutputStream(WasiIOEnvironment &HostEnv) : WasiIO(HostEnv) {}
-  Expect<void> body(uint32_t ThisOutputStream);
-};
-
-class DropInputStream : public WasiIO<DropInputStream> {
-public:
-  DropInputStream(WasiIOEnvironment &HostEnv) : WasiIO(HostEnv) {}
-  Expect<void> body(uint32_t ThisInputStream);
-};
-
-class DropStreamError : public WasiIO<DropStreamError> {
-public:
-  DropStreamError(WasiIOEnvironment &HostEnv) : WasiIO(HostEnv) {}
-  Expect<void> body(uint32_t ThisStreamError);
 };
 
 } // namespace Host

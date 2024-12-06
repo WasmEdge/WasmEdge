dda39d9b7276dd3b81ad31a2c8d9bfe78bebbcea
Lîm Tsú-thuàn <dannypsnl@secondstate.io>
2024-12-06 11:19:06 +0800

    to correctly store higher type like option<T>, need to convert them into index
    The problem is, I cannot sure I should reuse index for the same type or
    not; and if I use these type, e.g. ErrorCode, how can I ensure use-site
    will lookup at the right place (wasi-http plugin component), instead of
    explain these type indicies at their place (use-site component)?
    
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>

--- plugins/wasi_http/interface_types.cpp
+++ plugins/wasi_http/interface_types.cpp
@@ -13,10 +13,15 @@ WasiHttp_Types::WasiHttp_Types() : ComponentInstance("wasi:http/types@0.2.0") {
   using namespace Types;
   addHostType("method", Method::ast());
   addHostType("scheme", Scheme::ast());
-  addHostType("DNS-error-payload", DNSErrorPayload::ast());
-  addHostType("TLS-alert-received-payload", TLSAlertReceivedPayload::ast());
-  addHostType("field-size-payload", FieldSizePayload::ast());
-  addHostType("error-code", ErrorCode::ast());
+  addHostType("DNS-error-payload", DNSErrorPayload::ast(this));
+  auto DNSErrorPayload = getLastTypeIndex();
+  addHostType("TLS-alert-received-payload", TLSAlertReceivedPayload::ast(this));
+  auto TLSAlterReceivedPayload = getLastTypeIndex();
+  addHostType("field-size-payload", FieldSizePayload::ast(this));
+  auto FieldSizePayload = getLastTypeIndex();
+  addHostType("error-code",
+              ErrorCode::ast(this, DNSErrorPayload, TLSAlterReceivedPayload,
+                             FieldSizePayload));
   addHostFunc("http-error-code", std::make_unique<HttpErrorCode>(Env));
   addHostType("header-error", HeaderError::ast());
   addHostType("field-key", FieldKey::ast());

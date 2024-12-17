8ae70b4fbc130ce7ba48d73ae75658299bbd54ea
lhajouji <lhajouji@gmail.com>
2024-12-17 23:41:35 +0100

    Add error message for Windows users in install script
    Signed-off-by: lhajouji <lhajouji@gmail.com>

--- utils/install_v2.sh
+++ utils/install_v2.sh
@@ -12,6 +12,27 @@ YELLOW=$'\e[0;33m'
 NC=$'\e[0m' # No Color
 TMP_DIR="/tmp/wasmedge.$$"
 
+# OS Detection Block
+# This block detects if the script is being run on an unsupported Windows environment.
+# "uname -s" outputs:
+# - 'Linux' on Linux
+# - 'Darwin' on macOS
+# - 'MINGW64_NT-xxxx' or similar on Windows Git Bash
+# - 'Windows_NT' in certain Windows environments
+#
+# If the OS is detected as Windows (Git Bash or native), the script exits early
+# and provides a link for manual installation
+
+OS=$(uname -s)
+
+if [[ "$OS" =~ ^MINGW.* || "$OS" = "Windows_NT" ]]; then
+    echo "Error: This script does not support Windows directly (including Git Bash)."
+    echo "Please download WasmEdge manually from the release page:"
+    echo "https://github.com/WasmEdge/WasmEdge/releases/latest"
+    exit 1
+fi
+
+
 info() {
 	command printf '\e[0;32mInfo\e[0m: %s\n\n' "$1"
 }

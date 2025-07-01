1acffd37c8a7488a93c0148c8708a4e4899957c3
hydai <hydai@secondstate.io>
2025-07-01 13:58:04 +0800

    ci(commitlint): set body-max-line-length to warning level (#4222)
    Dependabot will create long commit message lines, that we have no
    control. Let's make it be a warning at the current stage.
    
    Signed-off-by: hydai <z54981220@gmail.com>

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,3 +1,8 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    // Set to warning level since Dependabot creates long commit message lines
+    // that we have no control.
+    "body-max-line-length": [1, "always", 100],
+  },
 };

55c8cd03c92ccf98d12f2d2945564da90adaef65
grorge <g97116@yahoo.com>
2025-07-10 10:38:20 +0800

    refactor(WASI-NN): add set environ function
    Signed-off-by: grorge <g97116@yahoo.com>

--- plugins/wasi_nn/wasinnfunc.cpp
+++ plugins/wasi_nn/wasinnfunc.cpp
@@ -50,16 +50,20 @@ WASINN::ErrNo metadataToErrNo(
 #endif // #ifdef WASMEDGE_BUILD_WASI_NN_RPC
 } // namespace
 
+const Host::WASI::Environ *
+getEnv(const Runtime::Instance::ModuleInstance *WasiModule) {
+  if (WasiModule != nullptr) {
+    return static_cast<const WasmEdge::Host::WasiModule *>(WasiModule)
+        ->getEnv();
+  }
+  return nullptr;
+}
+
 Expect<WASINN::ErrNo>
 WasiNNLoad::bodyImpl(const Runtime::CallingFrame &Frame, uint32_t BuilderPtr,
                      uint32_t BuilderLen, uint32_t RawEncoding, uint32_t Target,
                      uint32_t GraphIdPtr) {
-  if (static_cast<const WasmEdge::Host::WasiModule *>(Frame.getWASIModule()) !=
-      nullptr) {
-    Env.setEnv(
-        static_cast<const WasmEdge::Host::WasiModule *>(Frame.getWASIModule())
-            ->getEnv());
-  }
+  Env.setEnv(getEnv(Frame.getWASIModule()));
 #ifdef WASMEDGE_BUILD_WASI_NN_RPC
   if (Env.NNRPCChannel != nullptr) {
     // TODO: implement RPC for Load
@@ -125,12 +129,7 @@ WasiNNLoad::bodyImpl(const Runtime::CallingFrame &Frame, uint32_t BuilderPtr,
 Expect<WASINN::ErrNo>
 WasiNNLoadByName::bodyImpl(const Runtime::CallingFrame &Frame, uint32_t NamePtr,
                            uint32_t NameLen, uint32_t GraphIdPtr) {
-  if (static_cast<const WasmEdge::Host::WasiModule *>(Frame.getWASIModule()) !=
-      nullptr) {
-    Env.setEnv(
-        static_cast<const WasmEdge::Host::WasiModule *>(Frame.getWASIModule())
-            ->getEnv());
-  }
+  Env.setEnv(getEnv(Frame.getWASIModule()));
   auto *MemInst = Frame.getMemoryByIndex(0);
   if (MemInst == nullptr) {
     return Unexpect(ErrCode::Value::HostFuncError);
@@ -180,12 +179,7 @@ WasiNNLoadByName::bodyImpl(const Runtime::CallingFrame &Frame, uint32_t NamePtr,
 Expect<WASINN::ErrNo> WasiNNLoadByNameWithConfig::bodyImpl(
     const Runtime::CallingFrame &Frame, uint32_t NamePtr, uint32_t NameLen,
     uint32_t ConfigPtr, uint32_t ConfigLen, uint32_t GraphIdPtr) {
-  if (static_cast<const WasmEdge::Host::WasiModule *>(Frame.getWASIModule()) !=
-      nullptr) {
-    Env.setEnv(
-        static_cast<const WasmEdge::Host::WasiModule *>(Frame.getWASIModule())
-            ->getEnv());
-  }
+  Env.setEnv(getEnv(Frame.getWASIModule()));
   auto *MemInst = Frame.getMemoryByIndex(0);
   if (MemInst == nullptr) {
     return Unexpect(ErrCode::Value::HostFuncError);

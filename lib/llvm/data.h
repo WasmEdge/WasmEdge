0b5a1882171dedc95450b4cede954bd1cbab44ed
Shen-Ta Hsieh(BestSteve) <ibmibmibm.tw@gmail.com>
2025-09-11 18:58:16 +0800

    feat(aot): add support for llvm 21
    Signed-off-by: Shen-Ta Hsieh(BestSteve) <ibmibmibm.tw@gmail.com>

--- lib/llvm/data.h
+++ lib/llvm/data.h
@@ -6,9 +6,20 @@
 #include "llvm/data.h"
 
 struct WasmEdge::LLVM::Data::DataContext {
+#if LLVM_VERSION_MAJOR >= 21
+  LLVM::Context LLContext = LLVM::Context::create();
+  LLVM::Context getLLContext() noexcept { return LLContext; }
+  LLVM::OrcThreadSafeContext getTSContext() noexcept {
+    return LLVM::OrcThreadSafeContext(LLContext);
+  }
+#else
   LLVM::OrcThreadSafeContext TSContext;
+  LLVM::Context getLLContext() noexcept { return TSContext.getContext(); }
+  LLVM::OrcThreadSafeContext getTSContext() noexcept {
+    return std::move(TSContext);
+  }
+#endif
   LLVM::Module LLModule;
   LLVM::TargetMachine TM;
-  DataContext() noexcept : TSContext(), LLModule(LLContext(), "wasm") {}
-  LLVM::Context LLContext() noexcept { return TSContext.getContext(); }
+  DataContext() noexcept : LLModule(getLLContext(), "wasm") {}
 };

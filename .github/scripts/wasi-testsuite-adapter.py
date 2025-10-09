37019ec874c87b2358d85796e1a088005954177b
Shen-Ta Hsieh <beststeve@secondstate.io>
2025-10-09 08:24:17 +0000

    ci(runner): update adapter for wasi-testsuite
    Signed-off-by: Shen-Ta Hsieh <beststeve@secondstate.io>

--- .github/scripts/wasi-testsuite-adapter.py
+++ .github/scripts/wasi-testsuite-adapter.py
@@ -1,30 +1,40 @@
-import argparse
 import subprocess
-import sys
 import os
 import shlex
+from pathlib import Path
+from typing import Dict, List, Tuple
 
 # shlex.split() splits according to shell quoting rules
-WASMEDGE = shlex.split(os.getenv("TEST_RUNTIME_EXE", "wasmedge"))
+WASMEDGE = shlex.split(os.getenv("WASMEDGE", "wasmedge"))
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--version", action="store_true")
-parser.add_argument("--test-file", action="store")
-parser.add_argument("--arg", action="append", default=[])
-parser.add_argument("--env", action="append", default=[])
-parser.add_argument("--dir", action="append", default=[])
 
-args = parser.parse_args()
+def get_name() -> str:
+    return "wasmedge"
 
-if args.version:
+
+def get_version() -> str:
     # ensure no args when version is queried
-    subprocess.run(WASMEDGE[0:1] + ["--version"])
-    sys.exit(0)
+    result = subprocess.run(WASMEDGE[0:1] + ["--version"],
+                            encoding="UTF-8", capture_output=True,
+                            check=True)
+    output = result.stdout.splitlines()[0].split(" ")
+    return output[1]
 
-TEST_FILE = args.test_file
-PROG_ARGS = args.arg
-ENV_ARGS = [j for i in args.env for j in ["--env", i]]
-DIR_ARGS = [j for i in args.dir for j in ["--dir", i]]
 
-r = subprocess.run(WASMEDGE + ENV_ARGS + DIR_ARGS + [TEST_FILE] + PROG_ARGS)
-sys.exit(r.returncode)
+def get_wasi_versions() -> List[str]:
+    return ["wasm32-wasip1"]
+
+
+def compute_argv(test_path: str,
+                 args: List[str],
+                 env: Dict[str, str],
+                 dirs: List[Tuple[Path, str]],
+                 wasi_version: str) -> List[str]:
+    argv = [] + WASMEDGE
+    for k, v in env.items():
+        argv += ["--env", f"{k}={v}"]
+    for host, guest in dirs:
+        argv += ["--dir", f"{guest}:{host}"]
+    argv += [test_path]
+    argv += args
+    return argv

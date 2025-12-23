0e678802782160ac4959c1474cbd370df0fc76f5
Divyansh Khatri <divyanshkhatri200102@gmail.com>
2025-12-23 11:20:41 +0000

    feat: update dependencies

--- cmake/WASINNDeps.cmake
+++ cmake/WASINNDeps.cmake
@@ -426,10 +426,11 @@ function(wasmedge_setup_piper_target target)
     find_program(GIT_CMD git REQUIRED)
     FetchContent_Declare(
       piper
-      GIT_REPOSITORY https://github.com/rhasspy/piper.git
-      GIT_TAG 38917ffd8c0e219c6581d73e07b30ef1d572fce1 # 2023.11.14-2
+      GIT_REPOSITORY https://github.com/OHF-Voice/piper1-gpl.git
+      GIT_TAG 32b95f8c1f0dc0ce27a6acd1143de331f61af777
       UPDATE_DISCONNECTED TRUE
-      PATCH_COMMAND "${GIT_CMD}" "apply" "${CMAKE_SOURCE_DIR}/plugins/wasi_nn/piper.patch"
+      SOURCE_SUBDIR libpiper
+      #PATCH_COMMAND "${GIT_CMD}" "apply" "${CMAKE_SOURCE_DIR}/plugins/wasi_nn/piper.patch"
     )
     set(BUILD_SHARED_LIBS OFF CACHE INTERNAL "Piper not build shared")
     set(BUILD_TESTING OFF CACHE INTERNAL "Piper not build tests")

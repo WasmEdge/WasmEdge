85ab0420dd1489f69733413847bd6fd0f9ea123f
Mohammed ElDegwi <mohammedeldegwi@gmail.com>
2026-02-04 05:22:38 +0000

    feat(wasi-crypto): add secretsManager implementation
    Signed-off-by: Mohammed ElDegwi <mohammedeldegwi@gmail.com>

---
+++ plugins/wasi_crypto/secrets_manager/secrets_manager.h
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2019-2024 Second State INC
+
+//===-- wasmedge/plugins/wasi_crypto/common/secrets_manager.h - SecretsManager --===//
+//
+// Part of the WasmEdge Project.
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file contains the SecretsManager class definitions.
+///
+//===----------------------------------------------------------------------===//
+
+// TODO: Declare secrets manager definitions

0f2023496517261108c5cb54f93f3c4dc1486b1f
Mohammed ElDegwi <mohammedeldegwi@gmail.com>
2026-02-04 22:59:14 +0000

    fix(wasi-crypto): update secretsManager path
    Signed-off-by: Mohammed ElDegwi <mohammedeldegwi@gmail.com>

--- plugins/wasi_crypto/secrets_manager/secrets_manager.cpp
+++ plugins/wasi_crypto/secrets_manager/secrets_manager.cpp
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: 2019-2024 Second State INC
 
-#include "common/secrets_manager.h"
+#include "secrets_manager/secrets_manager.h"
 
 // TODO: Implement secrets manager

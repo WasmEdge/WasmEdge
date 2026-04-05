ca6f4c43b21e2b222cf4ed0494b8af54d184b111
sleepingeight <theallrounder.mst@gmail.com>
2026-04-05 18:13:58 +0000

    fix(llvm): llvm 21 errors
    Signed-off-by: sleepingeight <theallrounder.mst@gmail.com>

--- lib/llvm/data.h
+++ lib/llvm/data.h
@@ -9,27 +9,19 @@ struct WasmEdge::LLVM::Data::DataContext {
 #if LLVM_VERSION_MAJOR >= 21
   LLVM::Context LLContext = LLVM::Context::create();
   LLVM::Context getLLContext() noexcept { return LLContext; }
-  LLVM::OrcThreadSafeContext getTSContext() noexcept {
+  LLVM::OrcThreadSafeContext &getTSContext() noexcept {
     return LLVM::OrcThreadSafeContext(LLContext);
   }
-  void resetModule() noexcept {
-    LLModule = LLVM::Module(getLLContext(), "wasm");
-  }
-  DataContext() noexcept : LLModule(getLLContext(), "wasm") {}
-  DataContext(LLVM::OrcThreadSafeContext &&TSC) noexcept
-      : LLContext(TSC.getContext()), LLModule(getLLContext(), "wasm") {}
 #else
   LLVM::OrcThreadSafeContext TSContext;
   LLVM::Context getLLContext() noexcept { return TSContext.getContext(); }
   LLVM::OrcThreadSafeContext &getTSContext() noexcept { return TSContext; }
-  void resetModule() noexcept {
-    LLModule = LLVM::Module(getLLContext(), "wasm");
-  }
-  DataContext() noexcept : LLModule(getLLContext(), "wasm") {}
-  DataContext(LLVM::OrcThreadSafeContext &&TSC) noexcept
-      : TSContext(std::move(TSC)), LLModule(getLLContext(), "wasm") {}
 #endif
   LLVM::Module LLModule;
   LLVM::TargetMachine TM;
   std::string Prefix;
+  DataContext() noexcept : LLModule(getLLContext(), "wasm") {}
+  void resetModule() noexcept {
+    LLModule = LLVM::Module(getLLContext(), "wasm");
+  }
 };

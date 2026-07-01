8f50a32a46b6111df0111b68758a23578a85ebf4
ShigrafS <shigrafsalik@proton.me>
2026-07-01 11:31:21 +0530

    fix(wasi-crypto): cleanse secret vector and move keypair
        - In SecretVec, cleanse capacity() rather than size() on destruction,
          and cleanse truncated bytes when shrinking the vector. This avoids
          leaving key material in memory.
        - In keypairFromId, move the keypair variant into egisterManager to
          avoid an extra copy of private key material.
    
    Signed-off-by: ShigrafS <shigrafsalik@proton.me>

--- plugins/wasi_crypto/utils/secret_vec.h
+++ plugins/wasi_crypto/utils/secret_vec.h
@@ -43,9 +43,18 @@ public:
 
   SecretVec(size_t Size) noexcept : Data(Size) {}
 
-  ~SecretVec() noexcept { OPENSSL_cleanse(Data.data(), Data.size()); }
+  ~SecretVec() noexcept {
+    if (auto *Ptr = Data.data(); Ptr != nullptr && Data.capacity() != 0) {
+      OPENSSL_cleanse(Ptr, Data.capacity());
+    }
+  }
 
-  void resize(size_t Size) { Data.resize(Size); }
+  void resize(size_t Size) {
+    if (Size < Data.size()) {
+      OPENSSL_cleanse(Data.data() + Size, Data.size() - Size);
+    }
+    Data.resize(Size);
+  }
 
   auto begin() noexcept { return Data.begin(); }
   auto begin() const noexcept { return Data.begin(); }
